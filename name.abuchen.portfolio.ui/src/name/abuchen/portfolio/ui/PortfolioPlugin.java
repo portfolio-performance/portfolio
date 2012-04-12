004d27219e68e57119fa5b030ae0baf7a2d87775
Andreas Buchen <andreas.buchen@gmail.com>
2012-04-12 17:07:27 +0200

    Log throwable method in PortfolioPlugin

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/PortfolioPlugin.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/PortfolioPlugin.java
@@ -2,6 +2,7 @@ package name.abuchen.portfolio.ui;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.FrameworkUtil;
 
@@ -12,6 +13,11 @@ public class PortfolioPlugin extends AbstractUIPlugin
     public static final void log(IStatus status)
     {
         Platform.getLog(FrameworkUtil.getBundle(PortfolioPlugin.class)).log(status);
+    }
+
+    public static void log(Throwable t)
+    {
+        log(new Status(Status.ERROR, PLUGIN_IN, t.getMessage(), t));
     }
 
 }
