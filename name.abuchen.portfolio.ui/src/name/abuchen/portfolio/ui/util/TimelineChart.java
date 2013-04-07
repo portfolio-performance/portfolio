387457561a529dbe29bbfe243137d2a92777dcc0
Andreas Buchen <andreas.buchen@gmail.com>
2013-04-07 09:35:22 +0200

    Stack 'bought/sold shares' information in timeline chart
    Issue: #63

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/TimelineChart.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/TimelineChart.java
@@ -2,6 +2,7 @@ package name.abuchen.portfolio.ui.util;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
@@ -28,7 +30,7 @@ import org.swtchart.Range;
 
 public class TimelineChart extends Chart
 {
-    private static class MarkerLine
+    private static class MarkerLine implements Comparable<MarkerLine>
     {
         private Date date;
         private RGB color;
@@ -39,6 +41,12 @@ public class TimelineChart extends Chart
             this.date = date;
             this.color = color;
             this.label = label;
+        }
+
+        @Override
+        public int compareTo(MarkerLine o)
+        {
+            return date.compareTo(o.date);
         }
     }
 
@@ -57,7 +65,7 @@ public class TimelineChart extends Chart
         getLegend().setVisible(false);
 
         // x axis
-        final IAxis xAxis = getAxisSet().getXAxis(0);
+        IAxis xAxis = getAxisSet().getXAxis(0);
         xAxis.getTitle().setVisible(false);
         xAxis.getTick().setVisible(false);
         xAxis.getGrid().setStyle(LineStyle.NONE);
@@ -70,27 +78,10 @@ public class TimelineChart extends Chart
 
         ((IPlotArea) getPlotArea()).addCustomPaintListener(new ICustomPaintListener()
         {
-
             @Override
             public void paintControl(PaintEvent e)
             {
-                Range range = xAxis.getRange();
-                Date upper = new Date((long) range.upper);
-
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(new Date((long) range.lower));
-                cal.add(Calendar.YEAR, 1);
-                cal.set(Calendar.MONTH, Calendar.JANUARY);
-                cal.set(Calendar.DAY_OF_MONTH, 1);
-
-                while (cal.getTimeInMillis() < upper.getTime())
-                {
-                    int y = xAxis.getPixelCoordinate((double) cal.getTimeInMillis());
-                    e.gc.drawLine(y, 0, y, e.height);
-                    e.gc.drawText(String.valueOf(cal.get(Calendar.YEAR)), y + 5, 5);
-
-                    cal.add(Calendar.YEAR, 1);
-                }
+                paintTimeGrid(e);
             }
 
             @Override
@@ -102,22 +93,10 @@ public class TimelineChart extends Chart
 
         getPlotArea().addPaintListener(new PaintListener()
         {
-
             @Override
             public void paintControl(PaintEvent e)
             {
-                if (markerLines.isEmpty())
-                    return;
-
-                for (MarkerLine marker : markerLines)
-                {
-                    int y = xAxis.getPixelCoordinate((double) marker.date.getTime());
-                    e.gc.setLineStyle(SWT.LINE_SOLID);
-                    e.gc.setForeground(resources.createColor(marker.color));
-                    e.gc.setLineWidth(2);
-                    e.gc.drawLine(y, 0, y, e.height);
-                    e.gc.drawText(marker.label, y + 5, e.height - 20, true);
-                }
+                paintMarkerLines(e);
             }
         });
 
@@ -127,6 +106,7 @@ public class TimelineChart extends Chart
     public void addMarkerLine(Date date, RGB color, String label)
     {
         this.markerLines.add(new MarkerLine(date, color, label));
+        Collections.sort(this.markerLines);
     }
 
     public void clearMarkerLines()
@@ -176,5 +156,55 @@ public class TimelineChart extends Chart
     public TimelineChartToolTip getToolTip()
     {
         return toolTip;
+    }
+
+    private void paintTimeGrid(PaintEvent e)
+    {
+        IAxis xAxis = getAxisSet().getXAxis(0);
+        Range range = xAxis.getRange();
+        Date upper = new Date((long) range.upper);
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date((long) range.lower));
+        cal.add(Calendar.YEAR, 1);
+        cal.set(Calendar.MONTH, Calendar.JANUARY);
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+
+        while (cal.getTimeInMillis() < upper.getTime())
+        {
+            int y = xAxis.getPixelCoordinate((double) cal.getTimeInMillis());
+            e.gc.drawLine(y, 0, y, e.height);
+            e.gc.drawText(String.valueOf(cal.get(Calendar.YEAR)), y + 5, 5);
+
+            cal.add(Calendar.YEAR, 1);
+        }
+    }
+
+    private void paintMarkerLines(PaintEvent e)
+    {
+        if (markerLines.isEmpty())
+            return;
+
+        IAxis xAxis = getAxisSet().getXAxis(0);
+
+        int labelExtentX = 0;
+        int labelStackY = 0;
+
+        for (MarkerLine marker : markerLines)
+        {
+            int x = xAxis.getPixelCoordinate((double) marker.date.getTime());
+
+            Point textExtent = e.gc.textExtent(marker.label);
+            boolean flip = x + 5 + textExtent.x > e.width;
+            int textX = flip ? x - 5 - textExtent.x : x + 5;
+            labelStackY = labelExtentX > textX ? labelStackY += textExtent.y : 0;
+            labelExtentX = x + 5 + textExtent.x;
+
+            e.gc.setLineStyle(SWT.LINE_SOLID);
+            e.gc.setForeground(resources.createColor(marker.color));
+            e.gc.setLineWidth(2);
+            e.gc.drawLine(x, 0, x, e.height);
+            e.gc.drawText(marker.label, textX, e.height - 20 - labelStackY, true);
+        }
     }
 }
