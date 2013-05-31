eee2bca96956e2e23d991d0af31c509ecc983a28
Andreas Buchen <andreas.buchen@gmail.com>
2013-05-31 18:44:55 +0200

    Add model classes Taxonomy, Classification and Assignment

---
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/model/Classification.java
@@ -0,0 +1,133 @@
+package name.abuchen.portfolio.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Classification
+{
+    public static class Assignment
+    {
+        private Object investmentVehicle;
+        private int weight;
+
+        public Assignment(Security security)
+        {
+            this(security, 100 * Values.Weight.factor());
+        }
+
+        public Assignment(Account account)
+        {
+            this(account, 100 * Values.Weight.factor());
+        }
+
+        private Assignment(Object investmentVehicle, int weight)
+        {
+            this.weight = weight;
+            this.investmentVehicle = investmentVehicle;
+        }
+
+        public int getWeight()
+        {
+            return weight;
+        }
+
+        public Object getInvestmentVehicle()
+        {
+            return investmentVehicle;
+        }
+    }
+
+    private String id;
+    private String name;
+    private String description;
+    private String color;
+
+    private Classification parent;
+    private List<Classification> children = new ArrayList<Classification>();
+    private List<Assignment> assignments = new ArrayList<Assignment>();
+
+    private int weight;
+
+    public Classification(String id, String name)
+    {
+        this(null, id, name);
+    }
+
+    public Classification(Classification parent, String id, String name)
+    {
+        this.parent = parent;
+        this.id = id;
+        this.name = name;
+
+        Random r = new Random();
+        this.color = '#' + Integer.toHexString(((r.nextInt(128) + 127) << 16) //
+                        | ((r.nextInt(128) + 127) << 8) //
+                        | (r.nextInt(128) + 127));
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+
+    public Classification getParent()
+    {
+        return parent;
+    }
+
+    public List<Classification> getChildren()
+    {
+        return children;
+    }
+
+    public void addChild(Classification classification)
+    {
+        children.add(classification);
+    }
+
+    public List<Assignment> getAssignments()
+    {
+        return assignments;
+    }
+
+    public void addAssignment(Assignment assignment)
+    {
+        assignments.add(assignment);
+    }
+
+    public int getWeight()
+    {
+        return weight;
+    }
+
+    public void setWeight(int weight)
+    {
+        this.weight = weight;
+    }
+}
---
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/model/Taxonomy.java
@@ -0,0 +1,73 @@
+package name.abuchen.portfolio.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Taxonomy
+{
+    private String id;
+    private String name;
+
+    private List<String> dimensions;
+    private Classification root;
+
+    public Taxonomy(String id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public List<String> getDimensions()
+    {
+        return dimensions;
+    }
+
+    public void setDimensions(List<String> dimensions)
+    {
+        this.dimensions = dimensions;
+    }
+
+    public Classification getRoot()
+    {
+        return root;
+    }
+
+    public void setRootNode(Classification node)
+    {
+        this.root = node;
+    }
+
+    public Classification getClassificationById(String id)
+    {
+        if (id == null)
+            return null;
+
+        LinkedList<Classification> stack = new LinkedList<Classification>();
+        stack.addAll(getRoot().getChildren());
+
+        while (!stack.isEmpty())
+        {
+            Classification c = stack.removeFirst();
+            if (id.equals(c.getId()))
+                return c;
+            stack.addAll(c.getChildren());
+        }
+
+        return null;
+    }
+}
