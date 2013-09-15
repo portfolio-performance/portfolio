03c68449b6822b588392c41bf60f41ae9641591f
gynngr <gynngr@jp-its.de>
2013-09-15 19:22:38 +0200

    new helper class Statistics

--- name.abuchen.portfolio/src/name/abuchen/portfolio/util/Helper.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/util/Helper.java
@@ -23,4 +23,59 @@ public class Helper
     {
         return Days.daysBetween(new DateTime(dateFrom), new DateTime(dateTo)).getDays();
     }
+
+    public static double sqr(double Value)
+    {
+        return Value * Value;
+    }
+
+    public static class Statistics
+    {
+        private int n;
+        private double sum1, sum2, min, max;
+
+        public void clear()
+        {
+            n = 0;
+            sum1 = 0;
+            sum2 = 0;
+            min = 0;
+            max = 0;
+        }
+
+        public void add(double Value)
+        {
+            if (n == 0)
+            {
+                min = Value;
+                max = Value;
+            }
+            else
+            {
+                if (Value < min)
+                    min = Value;
+                if (Value > max)
+                    max = Value;
+            }
+            sum1 += Value;
+            sum2 += sqr(Value);
+            n++;
+        }
+
+        public double mean()
+        {
+            return n == 0 ? 0 : sum1 / n;
+        }
+
+        public double sDev1()
+        {
+            return Math.sqrt(sDev2());
+        }
+
+        public double sDev2()
+        {
+            return n == 0 ? 0 : sum2 / n - sqr(mean());
+        }
+
+    }
 }
