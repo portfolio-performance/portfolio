6b1adbe939fa01d0697c5b500d51edb07ae49d02
Andreas Buchen <andreas.buchen@gmail.com>
2014-10-03 09:20:32 +0200

    Added message if menu item is not applicable (Linux only)
    Menus on Linux are not disabled (most likely due to a Ubuntu Unity bug).
    Added a message box if the user picks such a menu item.
    
    Issue: #178

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/ExportHandler.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/ExportHandler.java
@@ -3,6 +3,7 @@ package name.abuchen.portfolio.ui.handlers;
 import javax.inject.Named;
 
 import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.ui.Messages;
 import name.abuchen.portfolio.ui.PortfolioPart;
 import name.abuchen.portfolio.ui.wizards.datatransfer.ExportWizard;
 
@@ -12,6 +13,7 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
 
@@ -30,7 +32,10 @@ public class ExportHandler
                     @Named(IServiceConstants.ACTIVE_SHELL) Shell shell)
     {
         if (part == null || !(part.getObject() instanceof PortfolioPart))
+        {
+            MessageDialog.openWarning(shell, Messages.MsgNoFileOpen, Messages.MsgNoFileOpenText);
             return;
+        }
 
         Client client = ((PortfolioPart) part.getObject()).getClient();
 
--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/UpdateQuotesHandler.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/UpdateQuotesHandler.java
@@ -3,6 +3,7 @@ package name.abuchen.portfolio.ui.handlers;
 import javax.inject.Named;
 
 import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.ui.Messages;
 import name.abuchen.portfolio.ui.PortfolioPart;
 import name.abuchen.portfolio.ui.UpdateQuotesJob;
 
@@ -12,6 +13,8 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
 public class UpdateQuotesHandler
 {
@@ -24,10 +27,14 @@ public class UpdateQuotesHandler
 
     @Execute
     public void execute(@Named(IServiceConstants.ACTIVE_PART) MPart part,
+                    @Named(IServiceConstants.ACTIVE_SHELL) Shell shell,
                     @Named("name.abuchen.portfolio.ui.param.target") @Optional String target)
     {
         if (part == null || !(part.getObject() instanceof PortfolioPart))
+        {
+            MessageDialog.openWarning(shell, Messages.MsgNoFileOpen, Messages.MsgNoFileOpenText);
             return;
+        }
 
         final PortfolioPart portfolioPart = (PortfolioPart) part.getObject();
         Client client = portfolioPart.getClient();
