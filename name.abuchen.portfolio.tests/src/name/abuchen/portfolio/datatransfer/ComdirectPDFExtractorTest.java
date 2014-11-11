56e4c62aa396ab4ff71db910b582ce33a515a887
simpsus <bastian.kennel@gmail.com>
2014-11-11 18:51:24 +0100

    fixing manifest enhancing tested parameters

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/ComdirectPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/ComdirectPDFExtractorTest.java
@@ -1,16 +1,15 @@
 package name.abuchen.portfolio.datatransfer;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import name.abuchen.portfolio.datatransfer.Extractor.BuySellEntryItem;
 import name.abuchen.portfolio.datatransfer.Extractor.Item;
-import name.abuchen.portfolio.model.Account;
+import name.abuchen.portfolio.datatransfer.Extractor.SecurityItem;
+import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.Client;
-import name.abuchen.portfolio.model.Portfolio;
+import name.abuchen.portfolio.model.Security;
 
 import org.junit.Test;
 
@@ -23,73 +22,73 @@ public class ComdirectPDFExtractorTest
 
     public ComdirectPDFExtractorTest()
     {
-        try
-        {
-            BufferedReader reader = new BufferedReader(new InputStreamReader((new Temp()).getClass()
-                            .getResourceAsStream("/name/abuchen/portfolio/datatransfer/Gutschrift.txt")));
-            StringBuilder out = new StringBuilder();
-            int c;
-            while ((c = reader.read()) != -1)
-            {
-                out.append((char) c);
-            }
-            gutschriftText = out.toString();
-            reader.close();
-            reader = new BufferedReader(new InputStreamReader((new Temp()).getClass().getResourceAsStream(
-                            "/name/abuchen/portfolio/datatransfer/Wertpapierabrechnung_Kauf.txt")));
-            out = new StringBuilder();
-            while ((c = reader.read()) != -1)
-            {
-                out.append((char) c);
-            }
-            kaufText = out.toString();
-            reader.close();
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+        gutschriftText = new Scanner(getClass().getResourceAsStream("Gutschrift.txt"), "UTF-8").useDelimiter("\\A")
+                        .next();
+
+        kaufText = new Scanner(getClass().getResourceAsStream("Wertpapierabrechnung_Kauf.txt"), "UTF-8").useDelimiter(
+                        "\\A").next();
     }
 
     @Test
     public void testGutschrift()
     {
-        Client client = buildClient();
+        Client client = new Client();
         ComdirectPDFExtractor extractor = new ComdirectPDFExtractor(client);
         List<Exception> errors = new ArrayList<Exception>();
         List<Item> results = extractor.extract(gutschriftText, "Gutschrift", errors);
+        SecurityItem secItem = null;
+        AccountTransaction accItem = null;
+        for (Item it : results)
+        {
+            if (it instanceof SecurityItem)
+            {
+                secItem = (SecurityItem) it;
+            }
+            if (it instanceof AccountTransaction)
+            {
+                accItem = (AccountTransaction) it;
+            }
+        }
+        assert (secItem != null);
+        assert (accItem != null);
+        Security security = secItem.getSecurity();
+        assert (security.getName().equals("Name des Wertpapiers"));
+        assert (accItem.getAmount() == 1);
+        assert (accItem.getSecurity().equals(security));
         assert (results.size() == 2);
+        // Should complete without error
         assert (errors.size() == 0);
     }
 
     @Test
     public void testKauf()
     {
-        Client client = buildClient();
+        Client client = new Client();
         ComdirectPDFExtractor extractor = new ComdirectPDFExtractor(client);
         List<Exception> errors = new ArrayList<Exception>();
         List<Item> results = extractor.extract(kaufText, "Wertpapierabrechnung_Kauf", errors);
+        SecurityItem secItem = null;
+        BuySellEntryItem buyItem = null;
+        for (Item it : results)
+        {
+            if (it instanceof SecurityItem)
+            {
+                secItem = (SecurityItem) it;
+            }
+            if (it instanceof BuySellEntryItem)
+            {
+                buyItem = (BuySellEntryItem) it;
+            }
+        }
+        assert (secItem != null);
+        assert (buyItem != null);
+        Security security = secItem.getSecurity();
+        assert (security.getName().equals("Name der Security"));
+        assert (buyItem.getSecurity().equals(security));
+        assert (buyItem.getAmount() == 1);
         assert (results.size() == 2);
+        // Should complete without error
         assert (errors.size() == 0);
-    }
-
-    private Client buildClient()
-    {
-        Client client = new Client();
-
-        Account account = new Account();
-        account.setName("testAccount");
-        client.addAccount(account);
-
-        Portfolio portfolio = new Portfolio();
-        portfolio.setName("testPortfolio");
-        portfolio.setReferenceAccount(account);
-        client.addPortfolio(portfolio);
-        return client;
     }
 
 }
