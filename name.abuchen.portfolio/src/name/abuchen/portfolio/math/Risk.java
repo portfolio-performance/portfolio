70e47af690c10522ffb4caa5d30dba9938db47d5
Bastian Kennel <bastian.kennel@gmail.com>
2015-01-19 18:53:39 +0100

    calculation methods for risk measures maximum drawdown and volatility

---
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/math/Risk.java
@@ -0,0 +1,43 @@
+package name.abuchen.portfolio.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Risk
+{
+
+    public static double calculateMaxDrawdown(List<Double> values) {
+        Double peak = Double.MIN_VALUE;
+        List<Double> dd = new ArrayList<Double>();
+        Double maxDD = 0d;
+        int i = 0;
+        for (Double value: values){
+            peak = Math.max(peak, value);
+            dd.add(i,(peak - values.get(i))/peak);
+            maxDD = Math.max(maxDD, dd.get(i));
+            i++;
+        }
+        return maxDD;
+    }
+    
+    public static double calculateAverageVolatility(List<Double> values) {
+        Double temp;
+        List<Double> vola = new ArrayList<Double>();
+        Double average = 0d;
+        Double variance = 0d;
+        for (int i=1;i<values.size();i++) {
+            temp = (values.get(i)-values.get(i-1))/values.get(i-1);
+            vola.add(temp);
+            average = average + temp;
+        }
+        average = average / vola.size();
+        System.out.println("Average vola " + average);
+        for (int i=0;i<vola.size();i++) {
+            variance = Math.pow(vola.get(i)-average, 2) + variance;
+        }
+        variance = variance / vola.size();
+        variance = Math.sqrt(variance);
+        return variance;
+    }
+        
+}
