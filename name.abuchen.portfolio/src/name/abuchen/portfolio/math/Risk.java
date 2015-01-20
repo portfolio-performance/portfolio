a811937f68b63d9d60a2ce217c5d9f388eba5f7d
Bastian Kennel <bastian.kennel@gmail.com>
2015-01-20 17:51:54 +0100

    using the Formatter removed syso prints

--- name.abuchen.portfolio/src/name/abuchen/portfolio/math/Risk.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/math/Risk.java
@@ -6,38 +6,42 @@ import java.util.List;
 public final class Risk
 {
 
-    public static double calculateMaxDrawdown(List<Double> values) {
+    public static double calculateMaxDrawdown(List<Double> values)
+    {
         Double peak = Double.MIN_VALUE;
         List<Double> dd = new ArrayList<Double>();
         Double maxDD = 0d;
         int i = 0;
-        for (Double value: values){
+        for (Double value : values)
+        {
             peak = Math.max(peak, value);
-            dd.add(i,(peak - values.get(i))/peak);
+            dd.add(i, (peak - values.get(i)) / peak);
             maxDD = Math.max(maxDD, dd.get(i));
             i++;
         }
         return maxDD;
     }
-    
-    public static double calculateAverageVolatility(List<Double> values) {
+
+    public static double calculateAverageVolatility(List<Double> values)
+    {
         Double temp;
         List<Double> vola = new ArrayList<Double>();
         Double average = 0d;
         Double variance = 0d;
-        for (int i=1;i<values.size();i++) {
-            temp = (values.get(i)-values.get(i-1))/values.get(i-1);
+        for (int i = 1; i < values.size(); i++)
+        {
+            temp = (values.get(i) - values.get(i - 1)) / values.get(i - 1);
             vola.add(temp);
             average = average + temp;
         }
         average = average / vola.size();
-        System.out.println("Average vola " + average);
-        for (int i=0;i<vola.size();i++) {
-            variance = Math.pow(vola.get(i)-average, 2) + variance;
+        for (int i = 0; i < vola.size(); i++)
+        {
+            variance = Math.pow(vola.get(i) - average, 2) + variance;
         }
         variance = variance / vola.size();
         variance = Math.sqrt(variance);
         return variance;
     }
-        
+
 }
