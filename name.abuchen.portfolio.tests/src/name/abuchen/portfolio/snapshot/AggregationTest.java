c8c30a3f9fe459ede4aa45b6cc366b7f514a1cde
Andreas Buchen <andreas.buchen@gmail.com>
2015-07-05 08:56:05 +0200

    Fixed locale dependent test checking for first day in week
    Issue: #14

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/snapshot/AggregationTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/snapshot/AggregationTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.time.LocalDate;
+import java.util.Locale;
 
 import org.junit.Test;
 
@@ -12,14 +13,25 @@ public class AggregationTest
     @Test
     public void testWeekly()
     {
-        assertThat(Aggregation.Period.WEEKLY.getStartDateFor(LocalDate.of(2011, 10, 17)), //
-                        is(LocalDate.of(2011, 10, 17)));
-        assertThat(Aggregation.Period.WEEKLY.getStartDateFor(LocalDate.of(2012, 1, 13)), //
-                        is(LocalDate.of(2012, 1, 9)));
-        assertThat(Aggregation.Period.WEEKLY.getStartDateFor(LocalDate.of(2012, 8, 10)), //
-                        is(LocalDate.of(2012, 8, 6)));
-        assertThat(Aggregation.Period.WEEKLY.getStartDateFor(LocalDate.of(2012, 9, 23)), //
-                        is(LocalDate.of(2012, 9, 17)));
+        // first day of week is locale dependent
+        Locale locale = Locale.getDefault();
+        Locale.setDefault(Locale.GERMAN);
+
+        try
+        {
+            assertThat(Aggregation.Period.WEEKLY.getStartDateFor(LocalDate.of(2011, 10, 17)), //
+                            is(LocalDate.of(2011, 10, 17)));
+            assertThat(Aggregation.Period.WEEKLY.getStartDateFor(LocalDate.of(2012, 1, 13)), //
+                            is(LocalDate.of(2012, 1, 9)));
+            assertThat(Aggregation.Period.WEEKLY.getStartDateFor(LocalDate.of(2012, 8, 10)), //
+                            is(LocalDate.of(2012, 8, 6)));
+            assertThat(Aggregation.Period.WEEKLY.getStartDateFor(LocalDate.of(2012, 9, 23)), //
+                            is(LocalDate.of(2012, 9, 17)));
+        }
+        finally
+        {
+            Locale.setDefault(locale);
+        }
     }
 
     @Test
