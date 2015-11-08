c7449473300f2f9cfdd4d058ddbf7f61442ade08
Andreas Buchen <andreas.buchen@gmail.com>
2015-11-08 13:20:35 +0100

    Prevent error popup for annoying NullPointerException on El Capitan
    Issue: #355

--- name.abuchen.portfolio.bootstrap/src/name/abuchen/portfolio/bootstrap/LifeCycleManager.java
+++ name.abuchen.portfolio.bootstrap/src/name/abuchen/portfolio/bootstrap/LifeCycleManager.java
@@ -3,6 +3,10 @@ package name.abuchen.portfolio.bootstrap;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.services.log.Logger;
+import org.eclipse.e4.core.services.statusreporter.StatusReporter;
+import org.eclipse.e4.ui.internal.workbench.swt.IEventLoopAdvisor;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
@@ -18,7 +22,13 @@ import org.eclipse.swt.widgets.Display;
 public class LifeCycleManager
 {
     @PostContextCreate
-    public void beforeCreation()
+    public void doPostContextCreate(IEclipseContext context, Logger logger)
+    {
+        checkForJava8();
+        setupEventLoopAdvisor(context, logger);
+    }
+
+    public void checkForJava8()
     {
         // if the java version is < 8, show a message dialog because otherwise
         // the application would silently not start
@@ -31,6 +41,57 @@ public class LifeCycleManager
                             Messages.MsgMinimumRequiredVersion);
             throw new UnsupportedOperationException("The minimum Java version required is Java 8"); //$NON-NLS-1$
         }
+    }
+
+    public void setupEventLoopAdvisor(final IEclipseContext context, final Logger logger)
+    {
+        // do not show an error popup if is the annoying NPE on El Capitan
+        // https://bugs.eclipse.org/bugs/show_bug.cgi?id=434393
+
+        context.set(IEventLoopAdvisor.class, new IEventLoopAdvisor()
+        {
+            @Override
+            public void eventLoopIdle(final Display display)
+            {
+                display.sleep();
+            }
+
+            @Override
+            public void eventLoopException(final Throwable exception)
+            {
+                boolean isAnnoyingNullPointerOnElCapitan = isAnnoyingNullPointerOnElCapitan(exception);
+
+                StatusReporter statusReporter = (StatusReporter) context.get(StatusReporter.class.getName());
+                if (!isAnnoyingNullPointerOnElCapitan && statusReporter != null)
+                {
+                    statusReporter.show(StatusReporter.ERROR, "Internal Error", exception); //$NON-NLS-1$
+                }
+                else if (logger != null)
+                {
+                    logger.error(exception);
+                }
+                else
+                {
+                    exception.printStackTrace();
+                }
+            }
+
+            private boolean isAnnoyingNullPointerOnElCapitan(Throwable exception)
+            {
+                if (!(exception instanceof NullPointerException))
+                    return false;
+
+                StackTraceElement[] stackTrace = exception.getStackTrace();
+
+                if (!"org.eclipse.swt.widgets.Control".equals(stackTrace[0].getClassName())) //$NON-NLS-1$
+                    return false;
+
+                if (!"internal_new_GC".equals(stackTrace[0].getMethodName())) //$NON-NLS-1$
+                    return false;
+
+                return true;
+            }
+        });
     }
 
     @PreSave
