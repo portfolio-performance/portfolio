c1e0564e108f3b53e09c444d8d48045d0f981146
Andreas Buchen <andreas.buchen@gmail.com>
2016-01-16 17:02:26 +0100

    Removed saving of chart as image file
    The context menu prevented the right click on the chart and therefore
    the display of the series data points. Also, when saving the chart, an
    exception is thrown anyway.

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/htmlchart/HtmlChart.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/htmlchart/HtmlChart.java
@@ -1,18 +1,12 @@
 package name.abuchen.portfolio.ui.util.htmlchart;
 
-import name.abuchen.portfolio.ui.PortfolioPlugin;
-import name.abuchen.portfolio.ui.util.EmbeddedBrowser;
-
-import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.BrowserFunction;
-import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
+
+import name.abuchen.portfolio.ui.PortfolioPlugin;
+import name.abuchen.portfolio.ui.util.EmbeddedBrowser;
 
 public class HtmlChart
 {
@@ -20,7 +14,6 @@ public class HtmlChart
     private EmbeddedBrowser browser;
     private Control browserControl;
     private HtmlChartConfig args;
-    private HtmlChartContextMenu contextMenu;
 
     public HtmlChart(HtmlChartConfig args)
     {
@@ -31,7 +24,6 @@ public class HtmlChart
     {
         browser = new EmbeddedBrowser(args.getHtmlPageUri()); // $NON-NLS-1$
         browserControl = browser.createControl(container, b -> new LoadDataFunction(b, "loadData")); //$NON-NLS-1$
-        this.contextMenu = new HtmlChartContextMenu(this);
         return browserControl;
     }
 
@@ -71,26 +63,4 @@ public class HtmlChart
         }
 
     }
-
-    public void exportMenuAboutToShow(IMenuManager manager, String label)
-    {
-        this.contextMenu.exportMenuAboutToShow(manager, label);
-    }
-
-    public void save(String filename, int format) throws Exception
-    {
-        // TODO: Implement Save Chart as Image
-        // throw new Exception("Not implemented yet!");
-        Display display = new Display();
-        Image image = new Image(display, browserControl.getBounds().width, browserControl.getBounds().height);
-        ImageLoader loader = new ImageLoader();
-
-        GC gc = new GC(image);
-        browserControl.print(gc);
-        gc.dispose();
-
-        loader.data = new ImageData[] { image.getImageData() };
-        loader.save(filename, format);
-    }
-
 }
