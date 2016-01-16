ae492a2b90e505c31e8780947037ea476f78e55a
Andreas Buchen <andreas.buchen@gmail.com>
2016-01-16 07:49:58 +0100

    Fixed more 'flipping' exchange rates when entering buy transactions
    Issue: #401

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/dialogs/transactions/AccountTransactionModel.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/dialogs/transactions/AccountTransactionModel.java
@@ -363,9 +363,9 @@ public class AccountTransactionModel extends AbstractModel
         return BigDecimal.ONE.divide(exchangeRate, 10, BigDecimal.ROUND_HALF_DOWN);
     }
 
-    public void setInverseExchangeRate(BigDecimal exchnageRate)
+    public void setInverseExchangeRate(BigDecimal rate)
     {
-        setExchangeRate(BigDecimal.ONE.divide(exchangeRate, 10, BigDecimal.ROUND_HALF_DOWN));
+        setExchangeRate(BigDecimal.ONE.divide(rate, 10, BigDecimal.ROUND_HALF_DOWN));
     }
 
     public long getAmount()
