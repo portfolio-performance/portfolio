66ec5ccfcd41c2cfb79af754cc16c11b87d54049
fuchsst <fuchsst@gmail.com>
2016-01-16 15:04:07 +0100

    fixed: Chart content of Consumer Price Index was not shown

---
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/dialogs/ChartConfigDialog.java
@@ -0,0 +1,24 @@
+package name.abuchen.portfolio.ui.dialogs;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+
+import name.abuchen.portfolio.ui.util.BindingHelper.Model;
+
+public class ChartConfigDialog  extends AbstractDialog
+{
+
+    public ChartConfigDialog(Shell parentShell, String title, Model model)
+    {
+        super(parentShell, title, model);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void createFormElements(Composite editArea)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
