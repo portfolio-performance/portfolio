b032b1a6f6f200bddd65b354344b481ad1f6d8e8
Andreas Buchen <andreas.buchen@gmail.com>
2016-03-06 18:15:44 +0100

    Fixed error when number has > 11 digits after the decimal separator
    Issue: #461

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/StringToCurrencyConverter.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/StringToCurrencyConverter.java
@@ -7,21 +7,21 @@ import java.text.ParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.eclipse.core.databinding.conversion.IConverter;
+
 import name.abuchen.portfolio.money.Values;
 import name.abuchen.portfolio.ui.Messages;
-
-import org.eclipse.core.databinding.conversion.IConverter;
 
 public class StringToCurrencyConverter implements IConverter
 {
     private final Pattern pattern;
     private final NumberFormat full;
 
-    private final double factor;
+    private final int factor;
 
     public StringToCurrencyConverter(Values<?> type)
     {
-        this.factor = type.divider();
+        this.factor = type.factor();
 
         DecimalFormatSymbols symbols = new DecimalFormatSymbols();
         pattern = Pattern.compile("^([\\d" + symbols.getGroupingSeparator() + "]*)(" //$NON-NLS-1$ //$NON-NLS-2$
@@ -51,7 +51,7 @@ public class StringToCurrencyConverter implements IConverter
         {
             long result = 0;
             for (String part : value.split("\\+")) //$NON-NLS-1$
-                result += convertToLong(part);
+                result += convertToLong(part.trim());
 
             return Long.valueOf(result);
         }
@@ -72,19 +72,21 @@ public class StringToCurrencyConverter implements IConverter
         Number before = strBefore.trim().length() > 0 ? full.parse(strBefore) : Long.valueOf(0);
 
         String strAfter = m.group(3);
-        int after = 0;
+        long after = 0;
         if (strAfter != null && strAfter.length() > 0)
         {
-            after = Integer.parseInt(strAfter);
-
             int length = (int) Math.log10(factor);
-            for (int ii = strAfter.length(); ii > length; ii--)
-                after /= 10;
+
+            if (strAfter.length() > length)
+                strAfter = strAfter.substring(0, length);
+
+            after = Long.parseLong(strAfter);
+
             for (int ii = strAfter.length(); ii < length; ii++)
                 after *= 10;
         }
 
-        long resultValue = before.longValue() * (int) factor + after;
+        long resultValue = before.longValue() * factor + after;
         return resultValue;
     }
 
