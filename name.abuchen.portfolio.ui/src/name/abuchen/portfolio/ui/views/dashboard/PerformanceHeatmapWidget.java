29621cc7880478e23b6251f5d1fedf15dce3aae4
Bastian Kennel <bastian.kennel@gmail.com>
2016-06-11 20:18:29 +0200

    update method of the applet reacts to duration changes performance between months is being calculated and shown label background is graded on a scale from red to green depending on performance

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/PerformanceHeatmapWidget.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/PerformanceHeatmapWidget.java
@@ -5,69 +5,117 @@ import java.time.format.TextStyle;
 import java.util.Locale;
 
 import name.abuchen.portfolio.model.Dashboard.Widget;
+import name.abuchen.portfolio.money.Values;
+import name.abuchen.portfolio.snapshot.PerformanceIndex;
 import name.abuchen.portfolio.snapshot.ReportingPeriod;
 import name.abuchen.portfolio.util.Interval;
 
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.resource.FontDescriptor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 
 public class PerformanceHeatmapWidget extends ReportingPeriodWidget
 {
-    ReportingPeriod period;
+
+    Composite table;
+    Label currLabel;
+    PerformanceIndex performance;
 
     public PerformanceHeatmapWidget(Widget widget, DashboardData data)
     {
         super(widget, data);
-        period = data.getDefaultReportingPeriod();
     }
 
     @Override
     Composite createControl(Composite parent, DashboardResources resources)
     {
-        Composite container = new Composite(parent, SWT.NONE);
-        GridLayoutFactory.fillDefaults().numColumns(13).margins(5, 5).applyTo(container);
-        container.setBackground(parent.getBackground());
+        table = new Composite(parent, SWT.NONE);
+        GridLayoutFactory.fillDefaults().numColumns(13).spacing(1, 1).applyTo(table);
+        table.setBackground(parent.getBackground());
+
+        fillTable();
+        return table;
+    }
+
+    private Color getScaledColorForPerformance(double performance)
+    {
+        double max = 0.05;
+        double min = max * (-1d);
+        performance = Math.min(max, performance);
+        performance = Math.max(min, performance);
+        if (performance > 0d)
+        {
+            return new Color(Display.getCurrent(), new Double(255 * (1 - performance / max)).intValue(), 255,
+                            new Double(255 * (1 - performance / max)).intValue());
+        }
+        else
+        {
+            return new Color(Display.getCurrent(), 255, new Double(255 * (1 - performance / min)).intValue(),
+                            new Double(255 * (1 - performance / min)).intValue());
+        }
+    }
+
+    private void fillTable()
+    {
+        Interval interval = getReportingPeriod().toInterval();
+        ReportingPeriod currPeriod;
         // Top Left is empty
-        Label spacer = new Label(container, SWT.NONE);
-        GridDataFactory.fillDefaults().grab(true, false).applyTo(spacer);
+        Label spacer = new Label(table, SWT.NONE);
+        spacer.setText("%");
+        GridDataFactory.fillDefaults().grab(false, true).applyTo(spacer);
         // then the legend of the months
-        Interval interval = period.toInterval();
-        int year = interval.getStart().getYear();
-        Label currLabel;
-        for (LocalDate legendMonth = LocalDate.of(year, 1, 1); legendMonth.getYear() == year; legendMonth = legendMonth
+        // no harm in hardcoding the year as each year has the same months
+        for (LocalDate legendMonth = LocalDate.of(2016, 1, 1); legendMonth.getYear() == 2016; legendMonth = legendMonth
                         .plusMonths(1))
         {
-            currLabel = new Label(container, SWT.NONE);
+            currLabel = new Label(table, SWT.NONE);
             currLabel.setText(legendMonth.getMonth().getDisplayName(TextStyle.NARROW, Locale.GERMAN));
-            GridDataFactory.fillDefaults().grab(true, false).applyTo(currLabel);
+            currLabel.setAlignment(SWT.CENTER);
+            GridDataFactory.fillDefaults().grab(false, true).applyTo(currLabel);
         }
+        FontDescriptor desc = FontDescriptor.createFrom(currLabel.getFont()).increaseHeight(-2);
+        Font smallFont = desc.createFont(currLabel.getDisplay());
         // now loop the years
         for (LocalDate yearDate : interval.iterYears())
         {
-            currLabel = new Label(container, SWT.NONE);
+            currLabel = new Label(table, SWT.NONE);
             currLabel.setText(((Integer) yearDate.getYear()).toString());
             GridDataFactory.fillDefaults().grab(true, false).applyTo(currLabel);
             for (LocalDate currMonth = LocalDate.of(yearDate.getYear(), 1, 1); currMonth.getYear() == yearDate
                             .getYear(); currMonth = currMonth.plusMonths(1))
             {
-                currLabel = new Label(container, SWT.NONE);
+                currLabel = new Label(table, SWT.NONE);
                 if (interval.contains(currMonth))
                 {
-                    currLabel.setText("!");
+                    currLabel.setFont(smallFont);
+                    currPeriod = new ReportingPeriod.FromXtoY(currMonth, currMonth.plusMonths(1));
+                    performance = getDashboardData().calculate(PerformanceIndex.class, currPeriod);
+                    currLabel.setText(Values.PercentShort.format(performance.getFinalAccumulatedPercentage()));
+                    currLabel.setBackground(getScaledColorForPerformance(performance.getFinalAccumulatedPercentage()));
                 }
                 GridDataFactory.fillDefaults().grab(true, false).applyTo(currLabel);
             }
         }
-        return container;
+        table.getParent().layout(true);
+        table.layout(true);
     }
 
     @Override
     void update()
     {
+        for (Control child : table.getChildren())
+        {
+            child.dispose();
+        }
+        fillTable();
 
     }
 
