1e2e587f5f36bfa9478c275ab35e192ed92380f4
DominikHolzapfel <d.holzapfel@gmail.com>
2017-05-21 17:24:59 +0200

    Alle Datei Speichern Dialog mit einer Rückfrage versehen, falls die Datei schon vorhanden ist

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/QuotesContextMenu.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/QuotesContextMenu.java
@@ -140,6 +140,7 @@ public class QuotesContextMenu
             {
                 FileDialog fileDialog = new FileDialog(Display.getDefault().getActiveShell(), SWT.SAVE);
                 fileDialog.setFileName(security.getName() + ".csv"); //$NON-NLS-1$
+                fileDialog.setOverwrite(true);
                 String fileName = fileDialog.open();
 
                 if (fileName == null)
