cbdbb4f8f7af770a4c7882bb31ed7b56914d7a07
stephanmunich <16959462+stephanmunich@users.noreply.github.com>
2017-08-17 19:03:51 +0200

    added and implemented BaaderBank Tests

---
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/baaderbank/BaaderBankPDFExtractorTest.java
@@ -0,0 +1,357 @@
+package name.abuchen.portfolio.datatransfer.pdf.baaderbank;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+import name.abuchen.portfolio.datatransfer.Extractor.BuySellEntryItem;
+import name.abuchen.portfolio.datatransfer.Extractor.Item;
+import name.abuchen.portfolio.datatransfer.Extractor.SecurityItem;
+import name.abuchen.portfolio.datatransfer.Extractor.TransactionItem;
+import name.abuchen.portfolio.datatransfer.actions.AssertImportActions;
+import name.abuchen.portfolio.datatransfer.pdf.BaaderBankPDFExtractor;
+import name.abuchen.portfolio.model.AccountTransaction;
+import name.abuchen.portfolio.model.BuySellEntry;
+import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.model.PortfolioTransaction;
+import name.abuchen.portfolio.model.Security;
+import name.abuchen.portfolio.model.Transaction.Unit;
+import name.abuchen.portfolio.money.CurrencyUnit;
+import name.abuchen.portfolio.money.Money;
+import name.abuchen.portfolio.money.Values;
+
+import org.junit.Test;
+
+@SuppressWarnings("nls")
+public class BaaderBankPDFExtractorTest
+{
+
+    @Test
+    public void testWertpapierKauf1() throws IOException
+    {
+        BaaderBankPDFExtractor extractor = new BaaderBankPDFExtractor(new Client())
+        {
+            @Override
+            protected String strip(File file) throws IOException
+            {
+                return from("BaaderBankWertpapierKauf1.txt");
+            }
+        };
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(Arrays.asList(new File("t")), errors);
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+        
+        Optional<Item> item;
+        
+        // get security       
+        item = results.stream().filter(i -> i instanceof SecurityItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        Security security = ((SecurityItem) item.get()).getSecurity();
+
+        // assert security
+        assertThat(security.getIsin(), is("IE0032895942"));
+        assertThat(security.getWkn(), is("911950"));
+        assertThat(security.getName(), is("iShs DL Corp Bond UCITS ETF"));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
+        
+        // get transaction
+        item = results.stream().filter(i -> i instanceof BuySellEntryItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        assertThat(item.get().getSubject(), instanceOf(BuySellEntry.class));
+        BuySellEntry entry = (BuySellEntry) item.get().getSubject();
+
+        // assert transaction
+        assertThat(entry.getAccountTransaction()  .getType(),                 is(AccountTransaction.Type.BUY));
+        assertThat(entry.getPortfolioTransaction().getType(),                 is(PortfolioTransaction.Type.BUY));
+        assertThat(entry.getPortfolioTransaction().getAmount(),               is(Values.Amount.factorize(208.95)));
+        assertThat(entry.getPortfolioTransaction().getDate(),                 is(LocalDate.parse("2017-03-22")));
+        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0.21))));
+        assertThat(entry.getPortfolioTransaction().getShares(),               is(Values.Share.factorize(2)));        
+
+    }
+
+    @Test
+    public void testWertpapierKauf2() throws IOException
+    {
+        BaaderBankPDFExtractor extractor = new BaaderBankPDFExtractor(new Client())
+        {
+            @Override
+            protected String strip(File file) throws IOException
+            {
+                return from("BaaderBankWertpapierKauf2.txt");
+            }
+        };
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(Arrays.asList(new File("t")), errors);
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);        
+
+        Optional<Item> item;
+
+        // get security       
+        item = results.stream().filter(i -> i instanceof SecurityItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        Security security = ((SecurityItem) item.get()).getSecurity();
+        
+        // assert security
+        assertThat(security.getIsin(), is("DE000A1C22M3"));
+        assertThat(security.getWkn(), is("A1C22M"));
+        assertThat(security.getName(), is("HSBC S&P 500 UCITS ETF"));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));        
+        
+        // get transaction
+        item = results.stream().filter(i -> i instanceof BuySellEntryItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        assertThat(item.get().getSubject(), instanceOf(BuySellEntry.class));
+        BuySellEntry entry = (BuySellEntry) item.get().getSubject();
+
+        // assert transaction
+        assertThat(entry.getPortfolioTransaction().getType(),                 is(PortfolioTransaction.Type.BUY));
+        assertThat(entry.getAccountTransaction()  .getType(),                 is(AccountTransaction.Type.BUY));
+        assertThat(entry.getPortfolioTransaction().getAmount(),               is(Values.Amount.factorize(1551.00)));
+        assertThat(entry.getPortfolioTransaction().getDate(),                 is(LocalDate.parse("2017-03-22")));
+        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(1.55))));
+        assertThat(entry.getPortfolioTransaction().getShares(),               is(Values.Share.factorize(70)));
+    }
+    
+    @Test
+    public void testWertpapierVerkauf1() throws IOException
+    {
+        BaaderBankPDFExtractor extractor = new BaaderBankPDFExtractor(new Client())
+        {
+            @Override
+            protected String strip(File file) throws IOException
+            {
+                return from("BaaderBankWertpapierVerkauf1.txt");
+            }
+        };
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(Arrays.asList(new File("t")), errors);
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+
+        Optional<Item> item;
+
+        // get security
+        item = results.stream().filter(i -> i instanceof SecurityItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        Security security = ((SecurityItem) item.get()).getSecurity();
+        
+        // assert security
+        assertThat(security.getIsin(), is("LU0446734526"));
+        assertThat(security.getWkn(), is("A0X97T"));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));        
+
+        // get transaction
+        item = results.stream().filter(i -> i instanceof BuySellEntryItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        assertThat(item.get().getSubject(), instanceOf(BuySellEntry.class));
+        BuySellEntry entry = (BuySellEntry) item.get().getSubject();
+
+        // assert transaction
+        assertThat(entry.getPortfolioTransaction().getType(),                 is(PortfolioTransaction.Type.SELL));
+        assertThat(entry.getAccountTransaction()  .getType(),                 is(AccountTransaction.Type.SELL));
+        assertThat(entry.getPortfolioTransaction().getAmount(),               is(Values.Amount.factorize(75.92)));
+        assertThat(entry.getPortfolioTransaction().getDate(),                 is(LocalDate.parse("2017-05-12")));
+        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0.08))));
+        assertThat(entry.getPortfolioTransaction().getShares(),               is(Values.Share.factorize(2)));
+    }
+    
+    @Test
+    public void testSteuerausgleichsrechnung() throws IOException
+    {
+        BaaderBankPDFExtractor extractor = new BaaderBankPDFExtractor(new Client())
+        {
+            @Override
+            protected String strip(File file) throws IOException
+            {
+                return from("BaaderBankSteuerausgleichsrechnung1.txt");
+            }
+        };
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(Arrays.asList(new File("t")), errors);
+        assertThat(errors, empty());
+        assertThat(results.size(), is(1));
+
+        // get transaction
+        Optional<Item> item = results.stream().filter(i -> i instanceof TransactionItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        assertThat(item.get().getSubject(), instanceOf(AccountTransaction.class));
+        AccountTransaction transaction = (AccountTransaction) item.get().getSubject();
+
+        // assert transaction
+        assertThat(transaction.getType(),         is(AccountTransaction.Type.TAX_REFUND));
+        assertThat(transaction.getDate(),         is(LocalDate.parse("2017-06-22")));
+        assertThat(transaction.getAmount(),       is(Values.Amount.factorize(9.01)));
+        assertThat(transaction.getCurrencyCode(), is(CurrencyUnit.EUR));
+    }
+
+    @Test
+    public void testMonatlicherKontoauszug1() throws IOException
+    {
+        BaaderBankPDFExtractor extractor = new BaaderBankPDFExtractor(new Client())
+        {
+            @Override
+            protected String strip(File file) throws IOException
+            {
+                return from("BaaderBankMonatlicherKontoauszug1.txt");
+            }
+        };
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(Arrays.asList(new File("t")), errors);
+        assertThat(errors, empty());
+        assertThat(results.size(), is(1));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        Optional<Item> item = results.stream().filter(i -> i instanceof TransactionItem).findFirst();
+
+        // get transaction
+        assertThat(item.isPresent(), is(true));
+        assertThat(item.get().getSubject(), instanceOf(AccountTransaction.class));
+        AccountTransaction transaction = (AccountTransaction) item.get().getSubject();
+
+        // assert transaction
+        assertThat(transaction.getType(),         is(AccountTransaction.Type.DEPOSIT));
+        assertThat(transaction.getDate(),         is(LocalDate.parse("2017-05-04")));
+        assertThat(transaction.getAmount(),       is(Values.Amount.factorize(100.00)));
+        assertThat(transaction.getCurrencyCode(), is(CurrencyUnit.EUR));
+    }
+    
+    @Test
+    public void testRechnung1() throws IOException
+    {
+        BaaderBankPDFExtractor extractor = new BaaderBankPDFExtractor(new Client())
+        {
+            @Override
+            protected String strip(File file) throws IOException
+            {
+                return from("BaaderBankRechnung1.txt");
+            }
+        };
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(Arrays.asList(new File("t")), errors);
+        assertThat(errors, empty());
+        assertThat(results.size(), is(1));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        Optional<Item> item = results.stream().filter(i -> i instanceof TransactionItem).findFirst();
+
+        // get transaction
+        assertThat(item.isPresent(), is(true));
+        assertThat(item.get().getSubject(), instanceOf(AccountTransaction.class));
+        AccountTransaction transaction = (AccountTransaction) item.get().getSubject();
+
+        // assert transaction
+        assertThat(transaction.getType(),         is(AccountTransaction.Type.FEES));
+        assertThat(transaction.getDate(),         is(LocalDate.parse("2017-08-02")));
+        assertThat(transaction.getAmount(),       is(Values.Amount.factorize(6.48)));
+        assertThat(transaction.getCurrencyCode(), is(CurrencyUnit.EUR));
+    }    
+
+
+    @Test
+    public void testFondsausschuettung1() throws IOException
+    {
+        BaaderBankPDFExtractor extractor = new BaaderBankPDFExtractor(new Client())
+        {
+            @Override
+            protected String strip(File file) throws IOException
+            {
+                return from("BaaderBankFondsausschuettung1.txt");
+            }
+        };
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(Arrays.asList(new File("t")), errors);
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+        
+        Optional<Item> item = results.stream().filter(i -> i instanceof SecurityItem).findFirst();        
+
+        // get security
+        assertThat(item.isPresent(), is(true));
+        Security security = ((SecurityItem) item.get()).getSecurity();
+        
+        // assert security
+        assertThat(security.getIsin(), is("IE00B2NPKV68"));
+        assertThat(security.getWkn(),  is("A0NECU"));
+        
+        // get transaction
+        item = results.stream().filter(i -> i instanceof TransactionItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        assertThat(item.get().getSubject(), instanceOf(AccountTransaction.class));
+        AccountTransaction transaction = (AccountTransaction) item.get().getSubject();
+
+        // assert transaction
+        assertThat(transaction.getType(),                 is(AccountTransaction.Type.DIVIDENDS));
+        assertThat(transaction.getSecurity(),             is(security));
+        assertThat(transaction.getDate(),                 is(LocalDate.parse("2017-06-30")));
+        assertThat(transaction.getMonetaryAmount(),       is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(3.08))));
+        assertThat(transaction.getShares(),               is(Values.Share.factorize(8)));
+        assertThat(transaction.getUnitSum(Unit.Type.TAX), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0.36))));
+    }
+    
+    @Test
+    public void testErtragsthesaurierung1() throws IOException
+    {
+        BaaderBankPDFExtractor extractor = new BaaderBankPDFExtractor(new Client())
+        {
+            @Override
+            protected String strip(File file) throws IOException
+            {
+                return from("BaaderBankErtragsthesaurierung1.txt");
+            }
+        };
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(Arrays.asList(new File("t")), errors);
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+        
+        Optional<Item> item = results.stream().filter(i -> i instanceof SecurityItem).findFirst();        
+
+        // get security
+        assertThat(item.isPresent(), is(true));
+        Security security = ((SecurityItem) item.get()).getSecurity();
+        
+        // assert security
+        assertThat(security.getIsin(), is("DE0005933931"));
+        assertThat(security.getWkn(),  is("593393"));
+        
+        // get transaction
+        item = results.stream().filter(i -> i instanceof TransactionItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        assertThat(item.get().getSubject(), instanceOf(AccountTransaction.class));
+        AccountTransaction transaction = (AccountTransaction) item.get().getSubject();
+
+        // assert transaction
+        assertThat(transaction.getType(),                 is(AccountTransaction.Type.DIVIDENDS));
+        assertThat(transaction.getSecurity(),             is(security));
+        assertThat(transaction.getDate(),                 is(LocalDate.parse("2017-05-12")));
+        assertThat(transaction.getMonetaryAmount(),       is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(1.24))));
+        assertThat(transaction.getShares(),               is(Values.Share.factorize(11)));
+        assertThat(transaction.getUnitSum(Unit.Type.TAX), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(4.63))));
+    }
+    
+    private String from(String resource)
+    {
+        try (Scanner scanner = new Scanner(getClass().getResourceAsStream(resource), StandardCharsets.UTF_8.name()))
+        {
+            return scanner.useDelimiter("\\A").next();
+        }
+    }
+}
