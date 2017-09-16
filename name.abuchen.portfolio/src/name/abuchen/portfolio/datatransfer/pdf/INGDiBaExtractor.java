9b539d68970490211e6c54c79ae7eab8ad0560f2
Ragas13 <marco.forum@t-online.de>
2017-09-16 21:09:33 +0200

    ING DiBa fix date extration at buy transaction
    Refering to https://forum.portfolio-performance.info/t/missing-date-bei-ing-diba-pdf-import/1430
    
    A buy/sell via investment companies the date is defined as "Schlusstag" instead of "Ausführungstag / -zeit"
    
    Regards
    Marco

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/INGDiBaExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/INGDiBaExtractor.java
@@ -89,7 +89,7 @@ public class INGDiBaExtractor extends AbstractPDFExtractor
                         .assign((t, v) -> t.setDate(asDate(v.get("date"))))
 
                         .section("date").optional() //
-                        .match("Schlusstag / -zeit (?<date>\\d+.\\d+.\\d{4}+) .*") //
+                        .match("(Schlusstag . -zeit|Schlusstag) (?<date>\\d+.\\d+.\\d{4}+).*") //
                         .assign((t, v) -> t.setDate(asDate(v.get("date"))))
 
                         .section("amount", "currency") //
@@ -151,6 +151,10 @@ public class INGDiBaExtractor extends AbstractPDFExtractor
 
                         .section("date") //
                         .match("(Ausf.hrungstag . -zeit|Ausf.hrungstag) (?<date>\\d+.\\d+.\\d{4}+).*") //
+                        .assign((t, v) -> t.setDate(asDate(v.get("date"))))
+
+                        .section("date").optional() //
+                        .match("(Schlusstag . -zeit|Schlusstag) (?<date>\\d+.\\d+.\\d{4}+).*") //
                         .assign((t, v) -> t.setDate(asDate(v.get("date"))))
 
                         .section("amount", "currency") //
