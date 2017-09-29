c9db4565fa288e6346e144f7469fb3e510a2d8d9
Sebastian Baumhekel <sebastian.baumhekel@gmail.com>
2017-09-29 21:45:31 +0200

    Add regions according to market classification by MSCI. This makes it easier to classify securities as it does include meta classification like "Emerging Markets". Furthermore the user is assisted by allowing him to choose a country, which then gets mapped to such a region.
    Source: https://www.msci.com/market-classification respective https://www.justetf.com/uk/news/etf/msci-index-classification-and-how-they-divide-up-the-world.html
    
    Note: I just included the English version as I do not see too much sense in translating specific terms like "Frontier Markets".

--- name.abuchen.portfolio/src/name/abuchen/portfolio/model/TaxonomyTemplate.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/model/TaxonomyTemplate.java
@@ -19,7 +19,8 @@ public final class TaxonomyTemplate
                     new TaxonomyTemplate(INDUSTRY_GICS), //
                     new TaxonomyTemplate(INDUSTRY_SIMPLE2LEVEL), //
                     new TaxonomyTemplate("kommer"), //$NON-NLS-1$
-                    new TaxonomyTemplate("regions")); //$NON-NLS-1$
+                    new TaxonomyTemplate("regions"), //$NON-NLS-1$
+                    new TaxonomyTemplate("regions-msci")); //$NON-NLS-1$
 
     private String id;
     private String name;
