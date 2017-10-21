a7e70e61eff567ff0152db4a0b85a1dae3efd562
Andreas Buchen <andreas.buchen@gmail.com>
2017-10-21 21:23:09 +0200

    Improved formatting of heat map
    * keep fixed column width to allow for better comparison between
      multiple heat maps on the same page
    * changed color range from red -> yellow -> green

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/PerformanceHeatmapWidget.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/PerformanceHeatmapWidget.java
@@ -1,14 +1,21 @@
 package name.abuchen.portfolio.ui.views.dashboard;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
@@ -18,12 +25,82 @@ import name.abuchen.portfolio.money.Values;
 import name.abuchen.portfolio.snapshot.PerformanceIndex;
 import name.abuchen.portfolio.snapshot.ReportingPeriod;
 import name.abuchen.portfolio.ui.Messages;
+import name.abuchen.portfolio.ui.util.Colors;
 import name.abuchen.portfolio.ui.util.InfoToolTip;
 import name.abuchen.portfolio.ui.views.dataseries.DataSeries;
 import name.abuchen.portfolio.util.Interval;
 
 public class PerformanceHeatmapWidget extends WidgetDelegate
 {
+    private interface CellDataProvider
+    {
+        default Color getBackground()
+        {
+            return Colors.WHITE;
+        }
+
+        default Font getFont()
+        {
+            return null;
+        }
+
+        String getText();
+    }
+
+    private class Cell extends Canvas // NOSONAR
+    {
+        private static final int MARGIN = 2;
+
+        private CellDataProvider dataProvider;
+
+        public Cell(Composite parent, CellDataProvider dataProvider)
+        {
+            super(parent, SWT.NO_BACKGROUND | SWT.NO_FOCUS);
+            this.dataProvider = dataProvider;
+
+            addPaintListener(this::paintControl);
+        }
+
+        @Override
+        public Point computeSize(int wHint, int hHint, boolean changed)
+        {
+            GC gc = new GC(this);
+            Point extent = gc.stringExtent(dataProvider.getText());
+            gc.dispose();
+
+            return new Point(10, extent.y + 2 * MARGIN);
+        }
+
+        private void paintControl(PaintEvent e)
+        {
+            GC gc = e.gc;
+
+            Color oldBackground = gc.getBackground();
+            Color oldForeground = gc.getForeground();
+            Font oldFont = gc.getFont();
+
+            Rectangle bounds = getClientArea();
+
+            gc.setBackground(dataProvider.getBackground());
+            gc.setForeground(Colors.BLACK);
+            gc.fillRectangle(bounds.x, bounds.y, bounds.width, bounds.height);
+
+            Font newFont = dataProvider.getFont();
+            if (newFont != null)
+                gc.setFont(newFont);
+
+            String text = dataProvider.getText();
+            Point extend = gc.stringExtent(text);
+
+            gc.drawText(text, bounds.x + (bounds.width - extend.x) / 2, bounds.y + (bounds.height - extend.y) / 2);
+
+            gc.setBackground(oldBackground);
+            gc.setForeground(oldForeground);
+            gc.setFont(oldFont);
+        }
+
+    }
+
     private Composite table;
     private Label title;
     private DashboardResources resources;
@@ -52,8 +129,9 @@ public class PerformanceHeatmapWidget extends WidgetDelegate
         table = new Composite(container, SWT.NONE);
         // 13 columns, one for the legend and 12 for the months
         GridDataFactory.fillDefaults().grab(true, false).applyTo(table);
-        GridLayoutFactory.fillDefaults().numColumns(13).spacing(1, 1).applyTo(table);
+        GridLayoutFactory.fillDefaults().numColumns(13).equalWidth(true).spacing(1, 1).applyTo(table);
         table.setBackground(container.getBackground());
+
         fillTable();
 
         return container;
@@ -61,75 +139,123 @@ public class PerformanceHeatmapWidget extends WidgetDelegate
 
     private Color getScaledColorForPerformance(double performance)
     {
-        double max = 0.07;
-        double p = Math.min(max, Math.abs(performance));
-        int colorValue = (int) (255 * (1 - p / max));
-        RGB color = performance > 0d ? new RGB(colorValue, 255, colorValue) : new RGB(255, colorValue, colorValue);
-        return resources.getResourceManager().createColor(color);
+        // convert to 0 = -0.07 -> 1 = +0.07
+
+        final float max = 0.07f;
+
+        float p = (float) performance;
+        p = Math.max(-max, p);
+        p = Math.min(max, p);
+        p = (p + max) * (1 / (2 * max));
+
+        float hue = p * 120f; // 0 = red, 60 = yellow, 120 = red
+        return resources.getResourceManager().createColor(new RGB(hue, 0.9f, 1f));
     }
 
     private void fillTable()
     {
         // fill the table lines according to the supplied period
         // calculate the performance with a temporary reporting period
-        // calculate the color interpolated between red and green with white as
+        // calculate the color interpolated between red and green with yellow as
         // the median
         Interval interval = get(ReportingPeriodConfig.class).getReportingPeriod().toInterval();
-        ReportingPeriod currPeriod;
 
-        // Top Left is empty
-        Label topLeft = new Label(table, SWT.NONE);
-        topLeft.setText(""); //$NON-NLS-1$
+        addHeaderRow();
 
-        // then the legend of the months
-        // no harm in hardcoding the year as each year has the same months
-        for (LocalDate legendMonth = LocalDate.of(2016, 1, 1); legendMonth.getYear() == 2016; legendMonth = legendMonth
-                        .plusMonths(1))
-        {
-            Label currLabel = new Label(table, SWT.NONE);
-            currLabel.setText(legendMonth.getMonth().getDisplayName(TextStyle.NARROW, Locale.GERMAN));
-            currLabel.setAlignment(SWT.CENTER);
-            GridDataFactory.fillDefaults().grab(true, false).align(SWT.FILL, SWT.FILL).applyTo(currLabel);
-        }
-
-        // now loop the years
         DataSeries dataSeries = get(DataSeriesConfig.class).getDataSeries();
 
         for (Integer year : interval.iterYears())
         {
-            Label currLabel = new Label(table, SWT.NONE);
-            currLabel.setText(year.toString());
-            GridDataFactory.fillDefaults().grab(true, false).applyTo(currLabel);
+            // year
+            Cell cell = new Cell(table, () -> {
+                int numColumns = getDashboardData().getDashboard().getColumns().size();
+                return numColumns > 2 ? String.valueOf(year % 100) : String.valueOf(year);
+            });
+            GridDataFactory.fillDefaults().grab(true, false).applyTo(cell);
 
-            for (LocalDate currMonth = LocalDate.of(year, 1, 1); currMonth.getYear() == year; currMonth = currMonth
-                            .plusMonths(1))
+            // monthly data
+            for (LocalDate month = LocalDate.of(year, 1, 1); month.getYear() == year; month = month.plusMonths(1))
             {
-                currLabel = new Label(table, SWT.RIGHT);
-                if (interval.contains(currMonth))
+                if (interval.contains(month))
                 {
-                    currPeriod = new ReportingPeriod.FromXtoY(currMonth.minusDays(1),
-                                    currMonth.withDayOfMonth(currMonth.lengthOfMonth()));
-                    PerformanceIndex performance = getDashboardData().calculate(dataSeries, currPeriod);
-
-                    currLabel.setFont(resources.getSmallFont());
-                    currLabel.setAlignment(SWT.CENTER);
-                    currLabel.setText(Values.PercentShort.format(performance.getFinalAccumulatedPercentage()));
-                    currLabel.setBackground(getScaledColorForPerformance(performance.getFinalAccumulatedPercentage()));
-                    InfoToolTip.attach(currLabel, Messages.PerformanceHeatmapToolTip);
+                    cell = createCell(dataSeries, month);
+                    InfoToolTip.attach(cell, Messages.PerformanceHeatmapToolTip);
                 }
-                GridDataFactory.fillDefaults().grab(true, false).align(SWT.FILL, SWT.FILL).applyTo(currLabel);
+                else
+                {
+                    cell = new Cell(table, () -> ""); //$NON-NLS-1$
+                }
+                GridDataFactory.fillDefaults().grab(true, false).align(SWT.FILL, SWT.FILL).applyTo(cell);
             }
         }
         table.layout(true);
+    }
+
+    private Cell createCell(DataSeries dataSeries, LocalDate month)
+    {
+        ReportingPeriod period = new ReportingPeriod.FromXtoY(month.minusDays(1),
+                        month.withDayOfMonth(month.lengthOfMonth()));
+        PerformanceIndex performance = getDashboardData().calculate(dataSeries, period);
+
+        return new Cell(table, new CellDataProvider()
+        {
+            @Override
+            public Color getBackground()
+            {
+                return getScaledColorForPerformance(performance.getFinalAccumulatedPercentage());
+            }
+
+            @Override
+            public Font getFont()
+            {
+                return resources.getSmallFont();
+            }
+
+            @Override
+            public String getText()
+            {
+                return Values.PercentShort.format(performance.getFinalAccumulatedPercentage());
+            }
+        });
+    }
+
+    private void addHeaderRow()
+    {
+        // Top Left is empty
+        new Cell(table, () -> ""); //$NON-NLS-1$
+
+        // then the legend of the months
+        // no harm in hardcoding the year as each year has the same months
+        for (LocalDate m = LocalDate.of(2016, 1, 1); m.getYear() == 2016; m = m.plusMonths(1))
+        {
+            Month month = m.getMonth();
+            Cell cell = new Cell(table, () -> {
+                int numColumns = getDashboardData().getDashboard().getColumns().size();
+
+                TextStyle textStyle;
+                if (numColumns == 1)
+                    textStyle = TextStyle.FULL;
+                else if (numColumns == 2)
+                    textStyle = TextStyle.SHORT;
+                else
+                    textStyle = TextStyle.NARROW;
+
+                return month.getDisplayName(textStyle, Locale.getDefault());
+            });
+            GridDataFactory.fillDefaults().grab(true, false).align(SWT.FILL, SWT.FILL).applyTo(cell);
+        }
     }
 
     @Override
     void update()
     {
         title.setText(getWidget().getLabel() != null ? getWidget().getLabel() : ""); //$NON-NLS-1$
+
         for (Control child : table.getChildren())
             child.dispose();
+
         fillTable();
+
         table.getParent().layout(true);
         table.getParent().getParent().layout(true);
     }
