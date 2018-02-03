e25b80fd63a07b39a01ba177547ca0b99bfa22e7
Colinas Maoling <colinas.maoling@t-online.de>
2018-02-03 22:35:09 +0100

    introduce context menu item to toggle security retirement

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/SecuritiesTable.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/SecuritiesTable.java
@@ -665,6 +665,9 @@ public final class SecuritiesTable implements ModificationListener
         }
 
         manager.add(new Separator());
+        System.err.println("SecuritiesTable.fillContextMenu selection: " + selection.toString() + "  \n ========== END ===========");
+
+        manager.add(new HideSecurityAction(selection));
         if (watchlist == null)
         {
             manager.add(new DeleteSecurityAction(selection));
@@ -803,6 +806,39 @@ public final class SecuritiesTable implements ModificationListener
 
                 MessageDialog.openError(getShell(), Messages.MsgDeletionNotPossible,
                                 MessageFormat.format(Messages.MsgDeletionNotPossibleDetail, label));
+            }
+
+            if (isDirty)
+            {
+                markDirty();
+                securities.setInput(getClient().getSecurities());
+            }
+
+        }
+    }
+
+    private final class HideSecurityAction extends Action
+    {
+        private IStructuredSelection selection;
+
+        private HideSecurityAction(IStructuredSelection selection)
+        {
+            super(Messages.SecurityMenuHideSecurity);
+
+            this.selection = selection;
+        }
+
+        @Override
+        public void run()
+        {
+            boolean isDirty = false;
+
+            for (Object obj : selection.toArray())
+            {
+                Security security = (Security) obj;
+
+                security.setRetired(!security.isRetired());
+                isDirty = true;
             }
 
             if (isDirty)
