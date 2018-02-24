fa4eef14c6ce5cda680f2cc89a34baca65fc3a2f
Andreas Buchen <andreas.buchen@gmail.com>
2018-02-24 07:53:53 +0100

    Added more date formats to CSV import

--- name.abuchen.portfolio/src/name/abuchen/portfolio/Messages.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/Messages.java
@@ -51,9 +51,12 @@ public class Messages extends NLS
     public static String CSVDefSecurities;
     public static String CSVFormatApostrophe;
     public static String CSVFormatDDMMYY;
+    public static String CSVFormatDDMMYY1;
     public static String CSVFormatDDMMYYYY;
     public static String CSVFormatDDMMYYYY1;
     public static String CSVFormatISO;
+    public static String CSVFormatMMDDYY;
+    public static String CSVFormatMMDDYYYY;
     public static String CSVFormatNumberGermany;
     public static String CSVFormatNumberUS;
     public static String CSVFormatYYYYMMDD;
