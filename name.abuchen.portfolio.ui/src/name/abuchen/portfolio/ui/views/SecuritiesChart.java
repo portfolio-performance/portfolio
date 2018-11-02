03defdbaee743db9dd727ec358f4c77095816930
Ragas13 <marco.forum@t-online.de>
2018-11-03 00:03:52 +0100

    Undo last fix / @SuppressWarnings("nls")

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/SecuritiesChart.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/SecuritiesChart.java
@@ -707,9 +707,8 @@ public class SecuritiesChart
         if (smaLines == null || smaLines.getValues() == null || smaLines.getDates() == null)
             return;
 
-        String lineID = smaSeries + " (";
-        lineID = lineID + smaDaysWording; //$NON-NLS-1$
-        lineID = lineID + ")"; 
+        @SuppressWarnings("nls")
+        String lineID = smaSeries + " (" + smaDaysWording + ")"; 
 
         ILineSeries lineSeriesSMA = (ILineSeries) chart.getSeriesSet().createSeries(SeriesType.LINE, lineID);
         lineSeriesSMA.setXDateSeries(smaLines.getDates());
