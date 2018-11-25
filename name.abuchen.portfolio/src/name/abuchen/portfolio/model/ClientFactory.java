b532b0d67410095b1869987a5d1b6e590fa5fd84
Ragas13 <marco.forum@t-online.de>
2018-11-25 11:36:17 +0100

    Update client file version

--- name.abuchen.portfolio/src/name/abuchen/portfolio/model/ClientFactory.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/model/ClientFactory.java
@@ -563,6 +563,8 @@ public class ClientFactory
                 // converted from LocalDate to LocalDateTime
             case 37:
                 // added boolean attribute type
+            case 38:
+                // added security exchange calendar
 
                 client.setVersion(Client.CURRENT_VERSION);
                 break;
