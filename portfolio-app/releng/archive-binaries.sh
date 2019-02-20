3f8bb895826f7fe1387e08d15784d5104d8c1100
Andreas Buchen <andreas.buchen@gmail.com>
2019-02-20 19:53:03 +0100

    Updated releng scripts

--- portfolio-app/releng/archive-binaries.sh
+++ portfolio-app/releng/archive-binaries.sh
@@ -7,12 +7,24 @@ if [ -z $REPO_PATH ]; then
 fi
 
 BASE=$(pwd)/../../portfolio-product/target/products/
+BASE_GPG=$(pwd)/../../portfolio-product/target/gpg/target/products/
 
 FILES=${BASE}/PortfolioPerformance-*
+FILES_GPG=${BASE_GPG}/PortfolioPerformance-*
 
 mkdir $(pwd)/../../../Archive/$REPO_PATH
 
 for f in $FILES;
+do
+  if [ ! -d $f ]; then
+    filename=$(basename -- "$f")
+    echo "Processing $filename file..."
+    mv $f $(pwd)/../../../Archive/$REPO_PATH
+    echo ""
+  fi
+done
+
+for f in $FILES_GPG;
 do
   if [ ! -d $f ]; then
     filename=$(basename -- "$f")
---
+++ portfolio-app/releng/publish-to-github.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PCK_VERSION=$1
+if [ -z $PCK_VERSION ]; then
+  echo "There is no valid version available (e.g.: 0.30.1)."
+  exit 1
+fi
+
+github-release release \
+    --user buchen \
+    --repo portfolio \
+    --tag ${PCK_VERSION} \
+    --name ${PCK_VERSION} \
+    --pre-release
+
+BASE=$(pwd)/../../portfolio-product/target/products/
+BASE_GPG=$(pwd)/../../portfolio-product/target/gpg/target/products/
+
+FILES=$(cat <<EOF
+PortfolioPerformance-${PCK_VERSION}-linux.gtk.x86_64.tar.gz
+PortfolioPerformance-${PCK_VERSION}-linux.gtk.x86.tar.gz
+PortfolioPerformance-distro-${PCK_VERSION}-macosx.cocoa.x86_64.tar.gz
+PortfolioPerformance-distro-${PCK_VERSION}-win32.win32.x86_64.zip
+PortfolioPerformance-distro-${PCK_VERSION}-win32.win32.x86.zip
+EOF
+)
+
+for f in $FILES;
+do
+  if [ ! -d $f ]; then
+    echo "Processing $f file..."
+    github-release upload --user buchen --repo portfolio --tag ${PCK_VERSION} --name "$f" --file $BASE/$f
+    github-release upload --user buchen --repo portfolio --tag ${PCK_VERSION} --name "$f.asc" --file $BASE_GPG/$f.asc
+    echo ""
+  fi
+done
