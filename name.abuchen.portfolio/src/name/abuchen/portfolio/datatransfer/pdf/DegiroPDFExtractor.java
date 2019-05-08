b6168922376060201e011acb0c5e96be63d0988f
ZfT2 <boener@freenet.de>
2019-05-08 19:00:02 +0200

    Degiro Erweiterungen Kontoauszüge, Import aus Transaktionsübersichten - Anpassungen, Erweiterungen Buchungen

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/DegiroPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/DegiroPDFExtractor.java
@@ -14,6 +14,7 @@ import name.abuchen.portfolio.model.BuySellEntry;
 import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.PortfolioTransaction;
 import name.abuchen.portfolio.model.Transaction.Unit;
+import name.abuchen.portfolio.money.CurrencyUnit;
 import name.abuchen.portfolio.money.Money;
 
 public class DegiroPDFExtractor extends AbstractPDFExtractor
@@ -37,7 +38,7 @@ public class DegiroPDFExtractor extends AbstractPDFExtractor
 
         // 02-08-2017 00:00 Einzahlung EUR 350,00 EUR 350,00
         // 01-02-2019 11:44 01-02-2019 Einzahlung EUR 0,01 EUR 0,01
-        Block blockDeposit = new Block(".* Einzahlung .*");
+        Block blockDeposit = new Block("^.* Einzahlung .*$");
         type.addBlock(blockDeposit);
         blockDeposit.set(new Transaction<AccountTransaction>().subject(() -> {
             AccountTransaction t = new AccountTransaction();
@@ -52,6 +53,83 @@ public class DegiroPDFExtractor extends AbstractPDFExtractor
                                 t.setDateTime(asDate(v.get("date")));
                                 t.setAmount(asAmount(v.get("amount")));
                         })
+                        .wrap(t -> new TransactionItem(t)));
+        
+        // 17-07-2017 00:00 IS.DJ U.S.SELEC.DIV.U.ETF DE000A0D8Q49 Dividende USD 0,86 USD 0,86
+        // 17-07-2017 00:00 ISH.S.EU.SEL.DIV.30 U.ETF DE0002635299 Dividende EUR 2,07 EUR 521,41
+        // 17-07-2017 00:00 ISH.S.EU.SEL.DIV.30 U.ETF DE0002635299 Dividendensteuer EUR -0,55 EUR 519,34
+        // 17-07-2017 00:00 ISH.S.EU.SEL.DIV.30 U.ETF DE0002635299 Dividende EUR 22,64 EUR 519,89
+        Block blockDividends = new Block("^\\d+-\\d+-\\d{4} \\d+:\\d+ (\\d+-\\d+-\\d{4} )?.*Dividende .*");
+        type.addBlock(blockDividends);
+        blockDividends.set(new Transaction<AccountTransaction>().subject(() -> {
+            AccountTransaction t = new AccountTransaction();
+            t.setType(AccountTransaction.Type.DIVIDENDS);
+            return t;
+        })
+
+                        .section("date", "name", "isin", "currency", "amount", "balance")
+                        .match("^(?<date>\\d+-\\d+-\\d{4} \\d+:\\d+) (\\d+-\\d+-\\d{4} )?(?<name>.*) (?<isin>\\w{12}+) .*Dividende (?<currency>\\w{3}) -?(?<amount>[\\d.]+,\\d{2}) (\\w{3}) -?(?<balance>[\\d.]+,\\d{2}).*$")
+                        .assign((t, v) -> {
+                            t.setSecurity(getOrCreateSecurity(v));
+                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setDateTime(asDate(v.get("date")));
+                            t.setAmount(asAmount(v.get("amount")));
+                            type.getCurrentContext().put("balance", v.get("balance"));
+                        })
+                        
+                        .section("currencyTax", "tax").optional()
+                        .match("^(\\d+-\\d+-\\d{4} \\d+:\\d+) (\\d+-\\d+-\\d{4} )?(.*) (\\w{12}+) .*Dividendensteuer\\s(?<currencyTax>\\w{3}) -?(?<tax>[\\d.]+,\\d{2}) .*$")
+                        .assign((t, v) -> {
+                            t.addUnit(new Unit(Unit.Type.TAX, Money.of(asCurrencyCode(v.get("currencyTax")), asAmount(v.get("tax")))));
+                        })
+                        
+                        .wrap(t -> {
+                            // (Dididendenzahlungen in Fremdwährung, die den Saldo nicht ändern, sind wohl eher nicht relevant...?)
+                            // 17-07-2017 00:00 IS.DJ U.S.SELEC.DIV.U.ETF DE000A0D8Q49 Dividende USD 0,86 USD 0,86
+                            if (!CurrencyUnit.EUR.equalsIgnoreCase(t.getCurrencyCode()) && t.getAmount() == asAmount(type.getCurrentContext().get("balance")))
+                                return new NonImportableItem("Währung der Dividende passt nicht zur Währung des Wertpapiers");
+                            else
+                                return new TransactionItem(t);
+                        }));
+        
+        //31-07-2017 00:00 Zinsen EUR -0,07 EUR -84,16
+        Block blockInterest = new Block("^\\d+-\\d+-\\d{4} \\d+:\\d+ (\\d+-\\d+-\\d{4} )?.*Zinsen .*");
+        type.addBlock(blockInterest);
+        blockInterest.set(new Transaction<AccountTransaction>().subject(() -> {
+            AccountTransaction t = new AccountTransaction();
+            t.setType(AccountTransaction.Type.INTEREST_CHARGE);
+            return t;
+        })
+
+                        .section("date", "currency", "amount")
+                        .match("^(?<date>\\d+-\\d+-\\d{4} \\d+:\\d+) (\\d+-\\d+-\\d{4} )?.*Zinsen (?<currency>\\w{3}) -?(?<amount>[\\d.]+,\\d{2}).*$")
+                        .assign((t, v) -> {
+                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setDateTime(asDate(v.get("date")));
+                            t.setAmount(asAmount(v.get("amount")));
+                        })
+
+                        .wrap(t -> new TransactionItem(t)));
+        
+        // 30-06-2017 00:00 Einrichtung von EUR -2,50 EUR 1.116,79
+        // Handelsmodalitäten
+        // 2017
+        Block blockTrademodalities = new Block("^\\d+-\\d+-\\d{4} \\d+:\\d+ (\\d+-\\d+-\\d{4} )?.*Einrichtung von .*$");
+        type.addBlock(blockTrademodalities);
+        blockTrademodalities.set(new Transaction<AccountTransaction>().subject(() -> {
+            AccountTransaction t = new AccountTransaction();
+            t.setType(AccountTransaction.Type.FEES);
+            return t;
+        })
+
+                        .section("date", "currency", "amount")
+                        .match("^(?<date>\\d+-\\d+-\\d{4} \\d+:\\d+) (\\d+-\\d+-\\d{4} )?.*Einrichtung von (?<currency>\\w{3}) -?(?<amount>[\\d.]+,\\d{2}).*$")
+                        .assign((t, v) -> {
+                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setDateTime(asDate(v.get("date")));
+                            t.setAmount(asAmount(v.get("amount")));
+                        })
+
                         .wrap(t -> new TransactionItem(t)));
         
         //22-03-2019 14:10 22-03-2019 ODX4 P11550.00 22MAR19 DE000C2ZNL25 Gebühr für Ausübung/Zuteilung EUR -1,00 EUR 2.204,26
