44ab7a2abbca6a0602b63c0f547caba5f4bc96ad
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2019-06-27 22:18:42 +0200

    Add PDF extractor for PostFinance (swiss bank)

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
@@ -45,6 +45,7 @@ public class PDFImportAssistant
         extractors.add(new HelloBankPDFExtractor(client));
         extractors.add(new ViacPDFExtractor(client));
         extractors.add(new TradeRepublicPDFExtractor(client));
+        extractors.add(new PostfinancePDFExtractor(client));
     }
 
     public Map<Extractor, List<Item>> run(IProgressMonitor monitor, Map<File, List<Exception>> errors)
