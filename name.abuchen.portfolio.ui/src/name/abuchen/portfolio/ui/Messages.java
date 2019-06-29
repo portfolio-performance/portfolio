417eb671850d44545e49bcb566a0918fe27023e7
Andreas Buchen <andreas.buchen@gmail.com>
2019-06-29 07:49:04 +0200

    Added menu item to hide/show sidebar

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/Messages.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/Messages.java
@@ -667,6 +667,7 @@ public class Messages extends NLS
     public static String MenuExportChartData;
     public static String MenuExportData;
     public static String MenuExportDiagram;
+    public static String MenuHideSidebar;
     public static String MenuLabelSetAsStartPage;
     public static String MenuNewDashboard;
     public static String MenuNewDashboardColumn;
@@ -689,6 +690,7 @@ public class Messages extends NLS
     public static String MenuSettingsNew;
     public static String MenuShow;
     public static String MenuShowHideColumns;
+    public static String MenuShowSidebar;
     public static String MenuTaxonomyAssignmentRemove;
     public static String MenuTaxonomyClassificationCreate;
     public static String MenuTaxonomyClassificationDelete;
