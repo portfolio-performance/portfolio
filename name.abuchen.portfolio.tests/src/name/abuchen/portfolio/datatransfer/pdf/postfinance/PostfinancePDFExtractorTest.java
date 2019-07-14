2f5d85293ced381835a0648e0cb83c22508ab594
Andreas Buchen <andreas.buchen@gmail.com>
2019-07-14 09:52:01 +0200

    Suppressed warnings non-externalized strings in tests

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/postfinance/PostfinancePDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/postfinance/PostfinancePDFExtractorTest.java
@@ -28,6 +28,7 @@ import name.abuchen.portfolio.money.CurrencyUnit;
 import name.abuchen.portfolio.money.Money;
 import name.abuchen.portfolio.money.Values;
 
+@SuppressWarnings("nls")
 public class PostfinancePDFExtractorTest
 {
     @Test
