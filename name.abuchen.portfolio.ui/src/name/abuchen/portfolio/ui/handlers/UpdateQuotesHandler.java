f56825ce7624f538a506a0948bfe4f6041fbc132
Andreas Buchen <andreas.buchen@gmail.com>
2019-11-18 22:12:45 +0100

    Added Ctrl-U T key binding to update quotes of selected security
    Issue: https://forum.portfolio-performance.info/t/tastaturkuerzel-fuer-kurs-aktualisierung-eines-einzelnen-titels/6324

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/UpdateQuotesHandler.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/UpdateQuotesHandler.java
@@ -6,11 +6,13 @@ import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.widgets.Shell;
 
 import name.abuchen.portfolio.ui.jobs.UpdateQuotesJob;
+import name.abuchen.portfolio.ui.selection.SelectionService;
 
 public class UpdateQuotesHandler
 {
@@ -22,9 +24,19 @@ public class UpdateQuotesHandler
 
     @Execute
     public void execute(@Named(IServiceConstants.ACTIVE_PART) MPart part,
-                    @Named(IServiceConstants.ACTIVE_SHELL) Shell shell)
+                    @Named(IServiceConstants.ACTIVE_SHELL) Shell shell, SelectionService selectionService,
+                    @Named("name.abuchen.portfolio.ui.param.only-current-security") @Optional String onlyCurrentSecurity)
     {
-        MenuHelper.getActiveClient(part).ifPresent(
-                        client -> new UpdateQuotesJob(client, EnumSet.allOf(UpdateQuotesJob.Target.class)).schedule());
+        MenuHelper.getActiveClient(part).ifPresent(client -> {
+            if (Boolean.parseBoolean(onlyCurrentSecurity))
+            {
+                selectionService.getSelection(client)
+                                .ifPresent(s -> new UpdateQuotesJob(client, s.getSecurity()).schedule());
+            }
+            else
+            {
+                new UpdateQuotesJob(client, EnumSet.allOf(UpdateQuotesJob.Target.class)).schedule();
+            }
+        });
     }
 }
