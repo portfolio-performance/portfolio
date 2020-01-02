ba8b86647f858ff16216cc504ef48dd0b6990c6e
Andreas Buchen <andreas.buchen@gmail.com>
2020-01-02 21:50:36 +0100

    Fixed saving chart files (removes artifacts in PNG files)

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/chart/ChartUtil.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/chart/ChartUtil.java
@@ -3,8 +3,10 @@ package name.abuchen.portfolio.ui.util.chart;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageDataProvider;
 import org.eclipse.swt.graphics.ImageLoader;
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.graphics.PaletteData;
+import org.eclipse.swt.graphics.Point;
 import org.swtchart.Chart;
 import org.swtchart.IAxis;
 import org.swtchart.Range;
@@ -38,14 +40,41 @@ public final class ChartUtil
 
     public static void save(Chart chart, String filename, int format)
     {
-        Image image = new Image(Display.getDefault(), chart.getBounds());
-        GC gc = new GC(chart);
-        gc.copyArea(image, 0, 0);
-        gc.dispose();
-        ImageData imageData = image.getImageData();
-        ImageLoader loader = new ImageLoader();
-        loader.data = new ImageData[] { imageData };
-        loader.save(filename, format);
-        image.dispose();
+        // based on https://github.com/eclipse/swtchart/issues/86
+        ImageLoader imageLoader = new ImageLoader();
+        imageLoader.data = new ImageData[] { getImageData(chart) };
+        imageLoader.save(filename, format);
+    }
+
+    private static ImageData getImageData(Chart chart)
+    {
+        chart.redraw();
+        chart.update();
+
+        Point chartSize = chart.getSize();
+
+        ImageDataProvider chartImageDataProvider = zoom -> {
+            if (zoom != 100)
+                return null;
+            PaletteData palette = new PaletteData(0xFF, 0xFF00, 0xFF0000);
+            return new ImageData(chartSize.x, chartSize.y, 32, palette);
+        };
+
+        Image image = null;
+        GC gc = null;
+        try
+        {
+            image = new Image(chart.getDisplay(), chartImageDataProvider);
+            gc = new GC(chart);
+            gc.copyArea(image, 0, 0);
+            return image.getImageData();
+        }
+        finally
+        {
+            if (gc != null && !gc.isDisposed())
+                gc.dispose();
+            if (image != null && !image.isDisposed())
+                image.dispose();
+        }
     }
 }
