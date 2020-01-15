c8b9e88f983f45c0e7f0bbaf22c9ad9701eec4af
Andreas Buchen <andreas.buchen@gmail.com>
2020-01-15 21:48:13 +0100

    Fixed earnings chart to draw at data point for January of current year
    If January is the only data point, then no line is drawn (there is no
    February value to draw to). Therefore we make sure that at least a
    symbol is drawn for the January data point
    
    Issue: https://forum.portfolio-performance.info/t/diagrammanzeige-ertraege-kumuliert-fehlerhaft/7087

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/earnings/EarningsAccumulatedChartTab.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/earnings/EarningsAccumulatedChartTab.java
@@ -1,5 +1,8 @@
 package name.abuchen.portfolio.ui.views.earnings;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 import org.eclipse.swt.SWT;
 import org.swtchart.ILineSeries;
 import org.swtchart.ILineSeries.PlotSymbolType;
@@ -19,6 +22,9 @@ public class EarningsAccumulatedChartTab extends AbstractChartTab
     @Override
     protected void createSeries()
     {
+        LocalDate now = LocalDate.now();
+        boolean isJanuary = now.getMonth() == Month.JANUARY;
+
         for (int index = 0; index < model.getNoOfMonths(); index += 12)
         {
             int year = model.getStartYear() + (index / 12);
@@ -40,6 +46,16 @@ public class EarningsAccumulatedChartTab extends AbstractChartTab
             lineSeries.setLineWidth(2);
             lineSeries.setSymbolType(PlotSymbolType.NONE);
             lineSeries.setAntialias(SWT.ON);
+
+            // if January is the only data point, then no line is drawn (there
+            // is no February value to draw to). Therefore we make sure that at
+            // least a symbol is drawn for the January data point
+            if (isJanuary && year == now.getYear())
+            {
+                lineSeries.setSymbolSize(1);
+                lineSeries.setSymbolType(PlotSymbolType.SQUARE);
+            }
+
         }
     }
 }
