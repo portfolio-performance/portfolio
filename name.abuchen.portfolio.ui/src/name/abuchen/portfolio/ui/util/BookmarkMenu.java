1f057c7cf052eec94f0e9dd54622e825fed0f305
Andreas Buchen <andreas.buchen@gmail.com>
2020-02-08 07:25:43 +0100

    Added all bookmark templates to a submenu for easy selection

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/BookmarkMenu.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/BookmarkMenu.java
@@ -1,10 +1,14 @@
 package name.abuchen.portfolio.ui.util;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 
 import name.abuchen.portfolio.model.Bookmark;
 import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.model.ClientSettings;
 import name.abuchen.portfolio.model.Security;
 import name.abuchen.portfolio.ui.Messages;
 import name.abuchen.portfolio.ui.editor.PortfolioPart;
@@ -48,8 +52,18 @@ public class BookmarkMenu extends MenuManager
                         .forEach(bm -> add(new SimpleAction(bm.getLabel(), a -> DesktopAPI.browse(bm.getPattern()))));
 
         add(new Separator());
+
+        MenuManager templatesMenu = new MenuManager(Messages.LabelTaxonomyTemplates);
+        add(templatesMenu);
+
+        List<Bookmark> templates = ClientSettings.getDefaultBookmarks();
+        Collections.sort(templates, (r, l) -> r.getLabel().compareTo(l.getLabel()));
+        templates.forEach(bookmark -> templatesMenu.add(new SimpleAction(bookmark.getLabel(),
+                        a -> DesktopAPI.browse(bookmark.constructURL(client, security)))));
+
+        add(new Separator());
         add(new SimpleAction(Messages.BookmarkMenu_EditBookmarks,
                         a -> editor.activateView(SettingsView.class, Integer.valueOf(0))));
     }
-    
+
 }
