ad3d14fbce634b6c0e1e14ce68191e2bd8d4a501
Andreas Buchen <andreas.buchen@gmail.com>
2020-02-17 18:53:07 +0100

    Generify PageCache

--- name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/HTMLTableQuoteFeed.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/HTMLTableQuoteFeed.java
@@ -261,7 +261,7 @@ public class HTMLTableQuoteFeed implements QuoteFeed
     private static final Column[] COLUMNS = new Column[] { new DateColumn(), new CloseColumn(), new HighColumn(),
                     new LowColumn() };
 
-    private final PageCache cache = new PageCache();
+    private final PageCache<List<LatestSecurityPrice>> cache = new PageCache<>();
 
     @Override
     public String getId()
--- name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/PageCache.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/PageCache.java
@@ -2,19 +2,16 @@ package name.abuchen.portfolio.online.impl;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
-import name.abuchen.portfolio.model.LatestSecurityPrice;
-
-/* package */ class PageCache
+/* package */ class PageCache<T>
 {
-    private static class PageEntry
+    private static class PageEntry<T>
     {
         long ts;
-        List<LatestSecurityPrice> answer;
+        T answer;
 
-        public PageEntry(List<LatestSecurityPrice> prices)
+        public PageEntry(T prices)
         {
             this.ts = System.currentTimeMillis();
             this.answer = prices;
@@ -24,12 +21,12 @@ import name.abuchen.portfolio.model.LatestSecurityPrice;
     private static final int CACHE_SIZE = 50;
     private static final long EXPIRATION_TIME = 5L * 60L * 1000L; // 5 minutes
 
-    private HashMap<String, PageEntry> map = new LinkedHashMap<String, PageEntry>()
+    private HashMap<String, PageEntry<T>> map = new LinkedHashMap<String, PageEntry<T>>()
     {
         private static final long serialVersionUID = 1L;
 
         @Override
-        protected boolean removeEldestEntry(Map.Entry<String, PageEntry> eldest)
+        protected boolean removeEldestEntry(Map.Entry<String, PageEntry<T>> eldest)
         {
             return size() >= CACHE_SIZE;
         }
@@ -40,9 +37,9 @@ import name.abuchen.portfolio.model.LatestSecurityPrice;
      * 
      * @return list of prices; null if no cache entry exists
      */
-    public synchronized List<LatestSecurityPrice> lookup(String url)
+    public synchronized T lookup(String url)
     {
-        PageEntry entry = map.get(url);
+        PageEntry<T> entry = map.get(url);
 
         if (entry == null)
             return null; // NOSONAR
@@ -58,8 +55,8 @@ import name.abuchen.portfolio.model.LatestSecurityPrice;
         }
     }
 
-    public synchronized void put(String url, List<LatestSecurityPrice> prices)
+    public synchronized void put(String url, T prices)
     {
-        map.put(url, new PageEntry(prices));
+        map.put(url, new PageEntry<T>(prices));
     }
 }
