a21ce2ce8a0906d4c14381f6755a1a6ea4bdc64d
Timothy Zemp <timothy.zemp@gmail.com>
2020-03-24 10:02:45 +0100

    Add test for Security.getPricesIncludingLatest

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/model/SecurityTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/model/SecurityTest.java
@@ -12,6 +12,7 @@ import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import java.util.UUID;
@@ -90,6 +91,40 @@ public class SecurityTest
 
         LatestSecurityPrice same = new LatestSecurityPrice(LocalDate.now(), 2);
         assertThat(security.setLatest(same), is(false));
+    }
+    
+    @Test
+    public void testGetPricesIncludingLatest()
+    {
+        Security security = new Security();
+        
+        //create historical price
+        LatestSecurityPrice historical = new LatestSecurityPrice(LocalDate.parse("2019-02-28"), 100);
+        security.addPrice(historical);
+        List<SecurityPrice> prices = security.getPricesIncludingLatest();
+        
+        assertThat(prices.size(), is(1)); 
+        assertThat(prices.contains(historical), is(true));
+        
+        //create latest price same as historical
+        LatestSecurityPrice latest = new LatestSecurityPrice(LocalDate.parse("2019-02-28"), 150);
+        security.setLatest(latest);
+        List<SecurityPrice> prices2 = security.getPricesIncludingLatest();
+        
+        assertThat(prices2.size(), is(1)); 
+        assertThat(prices2.contains(historical), is(true));
+        assertThat(prices2.contains(latest), is(false));
+        
+        //create latest with different date
+        LatestSecurityPrice latest2 = new LatestSecurityPrice(LocalDate.parse("2020-02-28"), 150);
+        security.setLatest(latest2);
+        
+        List<SecurityPrice> prices3 = security.getPricesIncludingLatest();
+        
+        assertThat(prices3.size(), is(2)); 
+        assertThat(prices2.contains(historical), is(true));
+        assertThat(prices3.contains(latest2), is(true));
+
     }
 
     @Test(expected = NullPointerException.class)
