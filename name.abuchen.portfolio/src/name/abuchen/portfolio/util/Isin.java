116d89e4d63271d45c2abebeaec912f6f7d5384f
Andreas Buchen <andreas.buchen@gmail.com>
2020-10-04 18:57:40 +0200

    Do not calculate check digit for ISIN numbers with invalid country codes
    Apparently, some pension funds are using invalid ISIN numbers. For the
    time being, we accept those codes as correct even though the check digit
    is invalid. Later, we could
    
    Issue: #1761

--- name.abuchen.portfolio/src/name/abuchen/portfolio/util/Isin.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/util/Isin.java
@@ -1,8 +1,16 @@
 package name.abuchen.portfolio.util;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Isin
 {
     private static final String CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //$NON-NLS-1$
+    @SuppressWarnings("nls")
+    private static final Set<String> INVALID_CODES = new HashSet<>(
+                    Arrays.asList("DU", "EV", "HF", "HS", "QS", "QT", "QU", "QY", "TE", "XF", "XX", "ZZ"));
+
     public static final String PATTERN = "[A-Z]{2}[A-Z0-9]{9}\\d"; //$NON-NLS-1$
 
     private Isin()
@@ -13,6 +21,15 @@ public class Isin
     {
         if (isin == null || isin.length() != 12)
             return false;
+
+        // do not calculate checksum for ISIN numbers where the first two digits
+        // are considered invalid codes because some institutions use them
+        // internally
+
+        // see https://github.com/buchen/portfolio/issues/1761
+
+        if (INVALID_CODES.contains(isin.substring(0, 2)))
+            return true;
 
         int sum = 0;
         boolean even = false;
