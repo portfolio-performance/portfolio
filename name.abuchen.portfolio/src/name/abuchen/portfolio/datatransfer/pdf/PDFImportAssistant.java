27b8e2b01320955ae902a123a1dbb02f5e984edd
inv-trad <59824809+inv-trad@users.noreply.github.com>
2020-11-09 00:30:43 +0100

    Enhancement FFB PDF import to support types of purchase and dividend
    Issue: https://forum.portfolio-performance.info/t/frankfurter-fondsbank-ffb-pdf-import/4751/54

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
@@ -61,7 +61,7 @@ public class PDFImportAssistant
 
         extractors.add(new JSONPDFExtractor(client, "deutsche-bank-purchase.json")); //$NON-NLS-1$
         extractors.add(new JSONPDFExtractor(client, "deutsche-bank-sale.json")); //$NON-NLS-1$
-        extractors.add(new JSONPDFExtractor(client, "ffb-purchase.json")); //$NON-NLS-1$
+        extractors.add(new JSONPDFExtractor(client, "ffb.json")); //$NON-NLS-1$
         extractors.add(new JSONPDFExtractor(client, "trade-republic-dividends.json")); //$NON-NLS-1$
         extractors.add(new JSONPDFExtractor(client, "trade-republic-investmentplan.json")); //$NON-NLS-1$
         extractors.add(new JSONPDFExtractor(client, "ebase.json")); //$NON-NLS-1$
