bdfa7c98f44878c82b5ab6e889c0ce9d67066e48
Andreas Buchen <andreas.buchen@gmail.com>
2020-12-28 19:58:39 +0100

    Updated hashCode and equals methods

--- name.abuchen.portfolio/src/name/abuchen/portfolio/model/LatestSecurityPrice.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/model/LatestSecurityPrice.java
@@ -1,6 +1,7 @@
 package name.abuchen.portfolio.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class LatestSecurityPrice extends SecurityPrice
 {
@@ -74,13 +75,7 @@ public class LatestSecurityPrice extends SecurityPrice
     @Override
     public int hashCode()
     {
-        final int prime = 31;
-        int result = super.hashCode();
-        result = prime * result + (int) (high ^ (high >>> 32));
-        result = prime * result + (int) (low ^ (low >>> 32));
-        result = prime * result + (int) (previousClose ^ (previousClose >>> 32));
-        result = prime * result + (int) (volume ^ (volume >>> 32));
-        return result;
+        return 31 * super.hashCode() + Objects.hash(high, low, previousClose, volume);
     }
 
     @Override
@@ -93,14 +88,6 @@ public class LatestSecurityPrice extends SecurityPrice
         if (getClass() != obj.getClass())
             return false;
         LatestSecurityPrice other = (LatestSecurityPrice) obj;
-        if (high != other.high)
-            return false;
-        if (low != other.low)
-            return false;
-        if (previousClose != other.previousClose)
-            return false;
-        if (volume != other.volume)
-            return false;
-        return true;
+        return high == other.high && low == other.low && previousClose == other.previousClose && volume == other.volume;
     }
 }
