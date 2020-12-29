7389253dfc5dee540afe86fcd9bc1a9bf6263b16
Marvin Frick <marvin.frick@shopify.com>
2020-12-29 15:33:41 +0100

    Fix: ETF-Data.com only searches for ISIN

--- name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/ETFDataComSearchProvider.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/ETFDataComSearchProvider.java
@@ -1,10 +1,12 @@
 package name.abuchen.portfolio.online.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import name.abuchen.portfolio.model.Security;
 import name.abuchen.portfolio.online.SecuritySearchProvider;
+import name.abuchen.portfolio.util.Isin;
 
 public class ETFDataComSearchProvider implements SecuritySearchProvider
 {
@@ -18,6 +20,9 @@ public class ETFDataComSearchProvider implements SecuritySearchProvider
     @Override
     public List<ResultItem> search(String query, Type type) throws IOException
     {
+        if (!Isin.isValid(query))
+            return new ArrayList<>();
+
         return new ETFDataCom().search(query);
     }
 
