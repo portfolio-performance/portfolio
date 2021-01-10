226fcc647e41470c5ad635105b400073a70fbca9
Andreas Buchen <andreas.buchen@gmail.com>
2021-01-10 12:40:02 +0100

    Fixed duplicate key values in some taxonomy translations

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/model/TaxonomyLoadingTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/model/TaxonomyLoadingTest.java
@@ -1,8 +1,14 @@
 package name.abuchen.portfolio.model;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,6 +45,14 @@ public class TaxonomyLoadingTest
             for (TaxonomyTemplate template : TaxonomyTemplate.list())
             {
                 template.build();
+
+                Taxonomy original = template.buildOriginal();
+
+                // throws exception if template contains duplicate keys
+                Map<String, Classification> id2classification = original.getAllClassifications() //
+                                .stream().collect(Collectors.toMap(Classification::getId, c -> c));
+
+                assertThat(id2classification, is(notNullValue()));
             }
         }
         finally
