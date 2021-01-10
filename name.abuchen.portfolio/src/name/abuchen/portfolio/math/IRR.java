8b983038bffb322bd1ef3cf4546a8fceb7d7d3d7
Pascal Hein <pascal.hein@rwth-aachen.de>
2021-01-10 11:07:51 +0100

    Perform short pre-optimisation for IRR
    In some cases where the NPV curve is very degenerate (#1904),
    Newton fails to converge to an IRR. This commit attempts
    to remedy the problem by inserting an additional
    pre-optimisation phase, where an initial guess in the interval
    (-1,0) is derived via interval halving. If no such value is
    found, we just guess an IRR of 1.05 for now (maybe this
    needs to be improved to cover more cases).
    A more accurate IRR value is then obtained by applying the
    usual Newton iterations.

--- name.abuchen.portfolio/src/name/abuchen/portfolio/math/IRR.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/math/IRR.java
@@ -2,6 +2,7 @@ package name.abuchen.portfolio.math;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import name.abuchen.portfolio.math.NewtonGoalSeek.Function;
 
@@ -11,10 +12,39 @@ public final class IRR
     {
     }
 
+    @SuppressWarnings("nls")
+    private static double halving(Function f, double left, double right, double fLeft, double fRight)
+    {
+        if (fLeft * fRight >= 0)
+            throw new UnsupportedOperationException("Endpoints of interval must have different sign in f");
+
+        double center = (left + right) / 2;
+
+        if (right - left < 0.001d)
+            return center;
+
+        double fCenter = f.compute(center);
+        if (fCenter == 0)
+            return center;
+        else if (fCenter * fLeft < 0)
+            return halving(f, left, center, fLeft, fCenter);
+        else
+            return halving(f, center, right, fCenter, fRight);
+    }
+
     public static double calculate(List<LocalDate> dates, List<Double> values)
     {
         Function npv = new NPVFunction(dates, values);
         Function derivative = new PseudoDerivativeFunction(npv);
-        return NewtonGoalSeek.seek(npv, derivative, 0.05d) - 1;
+
+        // find a crude initial guess in interval (0,1)
+        // npv(0) is undefined, but diverges with sign given by last term
+        double fLeft = Double.POSITIVE_INFINITY * values.get(values.size() - 1);
+        // npv(1) is the sum of undiscounted flows
+        double fRight = values.stream().collect(Collectors.summingDouble(f -> f));
+        // if they have the same sign, we hopefully don't have a very extreme case, so just guess 5%
+        double guess = fLeft * fRight < 0 ? halving(npv, 0, 1, fLeft, fRight) : 1.05;
+
+        return NewtonGoalSeek.seek(npv, derivative, guess) - 1;
     }
 }
