2b7330b5216513ad7f2610d98b0a787e87f1f90f
Andreas Buchen <andreas.buchen@gmail.com>
2021-01-12 23:18:56 +0100

    Removed download of macOS zip version - Only DMG is enough

--- portfolio-app/releng/publish-to-github.sh
+++ portfolio-app/releng/publish-to-github.sh
@@ -20,7 +20,6 @@ FILES=$(cat <<EOF
 PortfolioPerformance-${PCK_VERSION}-linux.gtk.x86_64.tar.gz
 PortfolioPerformance-${PCK_VERSION}-win32.win32.x86_64.zip
 PortfolioPerformance-distro-${PCK_VERSION}-win32.win32.x86_64.zip
-PortfolioPerformance-distro-${PCK_VERSION}-macosx.cocoa.x86_64.zip
 PortfolioPerformance-${PCK_VERSION}.dmg
 EOF
 )
