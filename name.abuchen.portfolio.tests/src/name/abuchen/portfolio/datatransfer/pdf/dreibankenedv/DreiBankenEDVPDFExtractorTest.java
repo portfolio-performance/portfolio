faac6dd21a5f06a849012cbadb7bb6d5bbf24fb1
Nirus2000 <45203494+Nirus2000@users.noreply.github.com>
2021-01-22 11:05:47 +0100

    Adding 3BankenEDV importer

---
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/dreibankenedv/DreiBankenEDVPDFExtractorTest.java
@@ -0,0 +1,99 @@
+package name.abuchen.portfolio.datatransfer.pdf.dreibankenedv;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+
+import name.abuchen.portfolio.datatransfer.Extractor.BuySellEntryItem;
+import name.abuchen.portfolio.datatransfer.Extractor.Item;
+import name.abuchen.portfolio.datatransfer.Extractor.SecurityItem;
+import name.abuchen.portfolio.datatransfer.pdf.DreiBankenEDVPDFExtractor;
+import name.abuchen.portfolio.datatransfer.pdf.PDFInputFile;
+import name.abuchen.portfolio.model.AccountTransaction;
+import name.abuchen.portfolio.model.BuySellEntry;
+import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.model.PortfolioTransaction;
+import name.abuchen.portfolio.model.Security;
+import name.abuchen.portfolio.model.Transaction.Unit;
+import name.abuchen.portfolio.money.Money;
+import name.abuchen.portfolio.money.Values;
+
+
+@SuppressWarnings("nls")
+public class DreiBankenEDVPDFExtractorTest
+{
+
+    @Test
+    public void testWertpapierKauf01() throws IOException
+    {
+        DreiBankenEDVPDFExtractor extractor = new DreiBankenEDVPDFExtractor(new Client());
+
+        List<Exception> errors = new ArrayList<Exception>();
+
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "3BankenEDVKauf01.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+
+        // check security
+        Security security = results.stream().filter(i -> i instanceof SecurityItem).findFirst().get().getSecurity();
+        assertThat(security.getIsin(), is("LU0675401409"));
+        assertThat(security.getName(), is("Lyxor Emerg Market 2x Lev ETF Inhaber-Anteile I o.N."));
+
+        // check buy sell transaction
+        Optional<Item> item = results.stream().filter(i -> i instanceof BuySellEntryItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        assertThat(item.get().getSubject(), instanceOf(BuySellEntry.class));
+        BuySellEntry entry = (BuySellEntry) item.get().getSubject();
+
+        assertThat(entry.getPortfolioTransaction().getType(), is(PortfolioTransaction.Type.BUY));
+        assertThat(entry.getAccountTransaction().getType(), is(AccountTransaction.Type.BUY));
+
+        assertThat(entry.getPortfolioTransaction().getAmount(), is(Values.Amount.factorize(205.30)));
+        assertThat(entry.getPortfolioTransaction().getDateTime(), is(LocalDateTime.parse("2021-01-04T12:05:55")));
+        assertThat(entry.getPortfolioTransaction().getShares(), is(Values.Share.factorize(2)));
+        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE), is(Money.of("EUR", Values.Amount.factorize(0.02))));
+    }
+
+    @Test
+    public void testWertpapierVerkauf01() throws IOException
+    {
+        DreiBankenEDVPDFExtractor extractor = new DreiBankenEDVPDFExtractor(new Client());
+
+        List<Exception> errors = new ArrayList<Exception>();
+
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "3BankenEDVVerkauf01.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+
+        // check security
+        Security security = results.stream().filter(i -> i instanceof SecurityItem).findFirst().get().getSecurity();
+        assertThat(security.getIsin(), is("DE0007664039"));
+        assertThat(security.getName(), is("VOLKSWAGEN AG VORZUGSAKTIEN O.ST. O.N."));
+
+        // check buy sell transaction
+        Optional<Item> item = results.stream().filter(i -> i instanceof BuySellEntryItem).findFirst();
+        assertThat(item.isPresent(), is(true));
+        assertThat(item.get().getSubject(), instanceOf(BuySellEntry.class));
+        BuySellEntry entry = (BuySellEntry) item.get().getSubject();
+
+        assertThat(entry.getPortfolioTransaction().getType(), is(PortfolioTransaction.Type.SELL));
+        assertThat(entry.getAccountTransaction().getType(), is(AccountTransaction.Type.SELL));
+
+        assertThat(entry.getPortfolioTransaction().getAmount(), is(Values.Amount.factorize(680.30)));
+        assertThat(entry.getPortfolioTransaction().getDateTime(), is(LocalDateTime.parse("2017-01-02T13:54:46")));
+        assertThat(entry.getPortfolioTransaction().getShares(), is(Values.Share.factorize(5)));
+        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE), is(Money.of("EUR", Values.Amount.factorize(0.08))));
+        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.TAX).getAmount(), is(Values.Amount.factorize(3.37)));
+    }
+}
---
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/DreiBankenEDVPDFExtractor.java
@@ -0,0 +1,111 @@
+package name.abuchen.portfolio.datatransfer.pdf;
+
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
+import name.abuchen.portfolio.model.BuySellEntry;
+import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.model.PortfolioTransaction;
+import name.abuchen.portfolio.model.Transaction.Unit;
+import name.abuchen.portfolio.money.Money;
+
+
+public class DreiBankenEDVPDFExtractor extends AbstractPDFExtractor
+{   
+    public DreiBankenEDVPDFExtractor(Client client)
+    {
+        super(client);
+
+        addBankIdentifier("91810s/Klagenfurt"); //$NON-NLS-1$
+ 
+        addBuySellTransaction();
+    }
+
+    @Override
+    public String getPDFAuthor()
+    {
+        return "3BankenEDV"; //$NON-NLS-1$
+    }
+    
+    @Override
+    public String getLabel()
+    {
+        return "3BankenEDV"; //$NON-NLS-1$
+    }
+    
+    @SuppressWarnings("nls")
+    private void addBuySellTransaction()
+    {
+        DocumentType newType = new DocumentType(".*(Kauf|Verkauf).*");
+        this.addDocumentTyp(newType);
+
+        Transaction<BuySellEntry> pdfTransaction = new Transaction<>();
+        pdfTransaction.subject(() -> {
+            BuySellEntry entry = new BuySellEntry();
+            entry.setType(PortfolioTransaction.Type.BUY);
+            return entry;
+        });
+
+        Block firstRelevantLine = new Block(".*(Kauf|Verkauf).*");
+        newType.addBlock(firstRelevantLine);
+        firstRelevantLine.set(pdfTransaction);
+
+        pdfTransaction
+    
+            // Is type --> "Verkauf" change from BUY to SELL
+            .section("type").optional()
+            .match("Wertpapier-Abrechnu.*(?<type>Verkauf?).*") //
+            .assign((t, v) -> {
+                if (v.get("type").equals("Verkauf")) 
+                {
+                    t.setType(PortfolioTransaction.Type.SELL);
+                }
+            })
+    
+            // LU0675401409 Lyxor Emerg Market 2x Lev ETF Zugang Stk .               2,00
+            // Inhaber-Anteile I o.N.
+            .section("isin", "name", "shares", "nameContinued")
+            .match("(?<isin>[\\w]{12}.*?) (?<name>.*?) (Zugang|Abgang).*(?<shares>[\\d.]+(,\\d+)).*")
+            .match("(?<nameContinued>.*)")
+            .assign((t, v) -> {
+                v.put("isin", v.get("isin"));
+                v.put("name", v.get("name"));                            
+                t.setSecurity(getOrCreateSecurity(v));
+                t.setShares(asShares(v.get("shares")));
+            })
+                            
+            // Handelszeitpunkt: 04.01.2021 12:05:55
+            .section("date", "time")
+            .match("^(Handelszeitpunkt:).*(?<date>\\d+.\\d+.\\d{4}+) (?<time>\\d+:\\d+:\\d+).*")
+            .assign((t, v) -> {
+                if (v.get("time") != null)
+                    t.setDate(asDate(v.get("date"), v.get("time")));
+                else
+                    t.setDate(asDate(v.get("date")));
+            })
+            
+            // Wertpapierrechnung Wert 06.01.2021 EUR 205,30
+            .section("currency", "amount")
+            .match("^(Wertpapierrechn.* Wert) (\\d+.\\d+.\\d{4}+) (?<currency>\\w{3}+) *(?<amount>[\\d.-]+,\\d+).*")
+            .assign((t, v) -> {
+                t.setAmount(asAmount(v.get("amount")));
+                t.setCurrencyCode(v.get("currency"));
+            })
+    
+            // Kursgewinn-KESt EUR                -3,37
+            .section("tax", "currency").optional()
+            .match("^(Kursgewinn-KESt) (?<currency>\\w{3}).*(?<tax>-[\\d.]+,\\d{2})")
+            .assign((t, v) -> t.getPortfolioTransaction().addUnit(new Unit(Unit.Type.TAX,
+                            Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("tax"))))))
+            
+            // Dritt- und Börsengebühr EUR                0,02
+            .section("fee", "currency").optional() //
+            .match("^(Dritt.*B.*sengeb.*) (?<currency>\\w{3}+).*(?<fee>[\\d.-]+,\\d+).*")
+            .assign((t, v) -> t.getPortfolioTransaction()
+                            .addUnit(new Unit(Unit.Type.FEE,
+                                            Money.of(asCurrencyCode(v.get("currency")),
+                                                            asAmount(v.get("fee"))))))
+    
+            .wrap(BuySellEntryItem::new);
+    }
+}
