e3bc76f57777035ec9237df078ccf3f466617ecc
Nirus2000 <45203494+Nirus2000@users.noreply.github.com>
2021-01-23 16:29:07 +0100

    Adding Dividende to Importer

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/dreibankenedv/DreiBankenEDVPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/dreibankenedv/DreiBankenEDVPDFExtractorTest.java
@@ -16,14 +16,20 @@ import org.junit.Test;
 import name.abuchen.portfolio.datatransfer.Extractor.BuySellEntryItem;
 import name.abuchen.portfolio.datatransfer.Extractor.Item;
 import name.abuchen.portfolio.datatransfer.Extractor.SecurityItem;
+import name.abuchen.portfolio.datatransfer.Extractor.TransactionItem;
+import name.abuchen.portfolio.datatransfer.ImportAction.Status;
+import name.abuchen.portfolio.datatransfer.actions.AssertImportActions;
+import name.abuchen.portfolio.datatransfer.actions.CheckCurrenciesAction;
 import name.abuchen.portfolio.datatransfer.pdf.DreiBankenEDVPDFExtractor;
 import name.abuchen.portfolio.datatransfer.pdf.PDFInputFile;
+import name.abuchen.portfolio.model.Account;
 import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.BuySellEntry;
 import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.PortfolioTransaction;
 import name.abuchen.portfolio.model.Security;
 import name.abuchen.portfolio.model.Transaction.Unit;
+import name.abuchen.portfolio.money.CurrencyUnit;
 import name.abuchen.portfolio.money.Money;
 import name.abuchen.portfolio.money.Values;
 
@@ -96,4 +102,136 @@ public class DreiBankenEDVPDFExtractorTest
         assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE), is(Money.of("EUR", Values.Amount.factorize(0.08))));
         assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.TAX).getAmount(), is(Values.Amount.factorize(3.37)));
     }
+    
+    @Test
+    public void testAusschuettung01() throws IOException
+    {
+        DreiBankenEDVPDFExtractor extractor = new DreiBankenEDVPDFExtractor(new Client());
+
+        List<Exception> errors = new ArrayList<Exception>();
+
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "3BankenEDVDividende01.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+        new AssertImportActions().check(results, "EUR");
+
+        Security security = results.stream().filter(i -> i instanceof SecurityItem).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+        assertThat(security.getIsin(), is("IE00B0M63284"));
+        assertThat(security.getName(), is("iShs Euro.Property Yield U.ETF Registered Shares EUR (Dist)oN"));
+
+        AccountTransaction transaction = (AccountTransaction) results.stream().filter(i -> i instanceof TransactionItem)
+                        .findFirst().orElseThrow(IllegalArgumentException::new).getSubject();
+
+        assertThat(transaction.getType(), is(AccountTransaction.Type.DIVIDENDS));
+        assertThat(transaction.getMonetaryAmount(), is(Money.of("EUR", Values.Amount.factorize(0.30))));
+        assertThat(transaction.getShares(), is(Values.Share.factorize(4)));
+        assertThat(transaction.getDateTime(), is(LocalDateTime.parse("2020-12-10T00:00")));
+
+        assertThat(transaction.getUnitSum(Unit.Type.TAX), is(Money.of("EUR", Values.Amount.factorize(0.02))));
+    }
+    
+    @Test
+    public void testAusschuettung02() throws IOException
+    {
+        DreiBankenEDVPDFExtractor extractor = new DreiBankenEDVPDFExtractor(new Client());
+        List<Exception> errors = new ArrayList<Exception>();
+
+        List<Item> results = extractor
+                        .extract(PDFInputFile.loadTestCase(getClass(), "3BankenEDVDividende02.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+
+        AccountTransaction t = results.stream()
+                        .filter(i -> i instanceof TransactionItem)
+                        .map(i -> (AccountTransaction) ((TransactionItem) i).getSubject())
+                        .findAny().get();
+
+        assertThat(t.getSecurity().getName(), is("GILEAD SCIENCES INC. Registered Shares DL -,001"));
+        assertThat(t.getSecurity().getIsin(), is("US3755581036"));
+        assertThat(t.getSecurity().getCurrencyCode(), is(CurrencyUnit.EUR));
+
+        assertThat(t.getDateTime(), is(LocalDateTime.parse("2020-12-14T00:00")));
+        assertThat(t.getShares(), is(Values.Share.factorize(3)));
+        assertThat(t.getMonetaryAmount(), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(1.21))));
+        assertThat(t.getUnitSum(Unit.Type.TAX),
+                        is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0.25 + 0.21))));
+
+        CheckCurrenciesAction c = new CheckCurrenciesAction();
+        Account account = new Account();
+        account.setCurrencyCode(CurrencyUnit.EUR);
+        Status s = c.process(t, account);
+        assertThat(s, is(Status.OK_STATUS));
+    }
+    
+    @Test
+    public void testAusschuettung03() throws IOException
+    {
+        DreiBankenEDVPDFExtractor extractor = new DreiBankenEDVPDFExtractor(new Client());
+        List<Exception> errors = new ArrayList<Exception>();
+
+        List<Item> results = extractor
+                        .extract(PDFInputFile.loadTestCase(getClass(), "3BankenEDVDividende03.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+
+        AccountTransaction t = results.stream()
+                        .filter(i -> i instanceof TransactionItem)
+                        .map(i -> (AccountTransaction) ((TransactionItem) i).getSubject())
+                        .findAny().get();
+
+        assertThat(t.getSecurity().getName(), is("OLD REPUBLIC INTL CORP. Registered Shares DL 1"));
+        assertThat(t.getSecurity().getIsin(), is("US6802231042"));
+        assertThat(t.getSecurity().getCurrencyCode(), is(CurrencyUnit.EUR));
+
+        assertThat(t.getDateTime(), is(LocalDateTime.parse("2021-01-04T00:00")));
+        assertThat(t.getShares(), is(Values.Share.factorize(7)));
+        assertThat(t.getMonetaryAmount(), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(4.16))));
+        assertThat(t.getUnitSum(Unit.Type.TAX),
+                        is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(1.58))));
+
+        CheckCurrenciesAction c = new CheckCurrenciesAction();
+        Account account = new Account();
+        account.setCurrencyCode(CurrencyUnit.EUR);
+        Status s = c.process(t, account);
+        assertThat(s, is(Status.OK_STATUS));
+    }
+    
+    @Test
+    public void testAusschuettung04() throws IOException
+    {
+        DreiBankenEDVPDFExtractor extractor = new DreiBankenEDVPDFExtractor(new Client());
+        List<Exception> errors = new ArrayList<Exception>();
+
+        List<Item> results = extractor
+                        .extract(PDFInputFile.loadTestCase(getClass(), "3BankenEDVDividende04.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+
+        AccountTransaction t = results.stream()
+                        .filter(i -> i instanceof TransactionItem)
+                        .map(i -> (AccountTransaction) ((TransactionItem) i).getSubject())
+                        .findAny().get();
+
+        assertThat(t.getSecurity().getName(), is("Main Street Capital Corp. Registered Shares DL -,01"));
+        assertThat(t.getSecurity().getIsin(), is("US56035L1044"));
+        assertThat(t.getSecurity().getCurrencyCode(), is(CurrencyUnit.EUR));
+
+        assertThat(t.getDateTime(), is(LocalDateTime.parse("2021-01-04T00:00")));
+        assertThat(t.getShares(), is(Values.Share.factorize(6)));
+        assertThat(t.getMonetaryAmount(), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0.73))));
+        assertThat(t.getUnitSum(Unit.Type.TAX),
+                        is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0.28))));
+
+        CheckCurrenciesAction c = new CheckCurrenciesAction();
+        Account account = new Account();
+        account.setCurrencyCode(CurrencyUnit.EUR);
+        Status s = c.process(t, account);
+        assertThat(s, is(Status.OK_STATUS));
+    }
+
 }
--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/DreiBankenEDVPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/DreiBankenEDVPDFExtractor.java
@@ -1,8 +1,14 @@
 package name.abuchen.portfolio.datatransfer.pdf;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+
+import name.abuchen.portfolio.datatransfer.Extractor.TransactionItem;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
+import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.BuySellEntry;
 import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.PortfolioTransaction;
@@ -19,6 +25,7 @@ public class DreiBankenEDVPDFExtractor extends AbstractPDFExtractor
         addBankIdentifier("91810s/Klagenfurt"); //$NON-NLS-1$
  
         addBuySellTransaction();
+        addDividendeTransaction();
     }
 
     @Override
@@ -107,5 +114,154 @@ public class DreiBankenEDVPDFExtractor extends AbstractPDFExtractor
                                                             asAmount(v.get("fee"))))))
     
             .wrap(BuySellEntryItem::new);
+    }
+    
+    @SuppressWarnings("nls")
+    private void addDividendeTransaction()
+    {
+        DocumentType newType = new DocumentType(".*(Ausschüttung|Dividende).*");
+        this.addDocumentTyp(newType);
+
+        Block block = new Block(".*(Ausschüttung|Dividende).*");
+        newType.addBlock(block);
+        Transaction<AccountTransaction> pdfTransaction = new Transaction<AccountTransaction>()
+
+        .subject(() -> {
+            AccountTransaction entry = new AccountTransaction();
+            entry.setType(AccountTransaction.Type.DIVIDENDS);
+            return entry;
+        });
+
+        Block firstRelevantLine = new Block(".*(Kauf|Verkauf).*");
+        newType.addBlock(firstRelevantLine);
+        firstRelevantLine.set(pdfTransaction);
+
+        pdfTransaction
+        
+            // IE00B0M63284 iShs Euro.Property Yield U.ETF Stk . 4,00
+            // Registered Shares EUR (Dist)oN
+            .section("isin", "name", "shares", "nameContinued")
+            .match("(?<isin>[\\w]{12}.*?) (?<name>.*?) (Stk .).*(?<shares>[\\d.]+(,\\d+)).*")
+            .match("(?<nameContinued>.*)")
+            .assign((t, v) -> {
+                v.put("isin", v.get("isin"));
+                v.put("name", v.get("name"));                            
+                t.setSecurity(getOrCreateSecurity(v));
+                t.setShares(asShares(v.get("shares")));
+            })
+    
+            // Wertpapierrechnung Wert 23.12.2020 EUR 0,30
+            .section("currency", "amount")
+            .match("^(Wertpapierrechn.* Wert) (\\d+.\\d+.\\d{4}+) (?<currency>\\w{3}+) *(?<amount>[\\d.-]+,\\d+).*")
+            .assign((t, v) -> {
+                t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                t.setAmount(asAmount(v.get("amount")));
+            })
+
+            // Extag 10.12.2020
+            .section("date")
+            .match("^Extag (?<date>\\d+.\\d+.\\d{4}+).*")
+            .assign((t, v) -> t.setDateTime(asDate(v.get("date"))))
+           
+            // Ertrag 0,68 USD Kurswert USD                2,04Quellensteuer USD               -0,31
+            // Auslands-KESt USD               -0,26 <--- Sometimes 
+            // Zwischensumme USD                1,47
+            // 15 % QUSt a 1,224 v. 28.12.2020 EUR                1,21
+            .section("exchangeRate", "fxAmount", "fxCurrency", "amount", "currency").optional()
+            .match(".*Kurswert.*(?<fxCurrency>\\w{3}).*(?<fxAmount>[\\d.]+,\\d+).*\\w+.*(\\w{3}).*([\\d.]+,\\d+)")
+            .match("(?<line>\\w.*)")
+            .match("(?<line>\\w.*)")
+            .match(".*(?<exchangeRate>[\\d.]+,\\d+) v. (\\d+.\\d+.\\d{4}).*(?<currency>\\w{3}).*(?<amount>[\\d.]+,\\d+).*")
+            .assign((t, v) -> {
+                
+                BigDecimal exchangeRate = asExchangeRate(v.get("exchangeRate"));
+                if (t.getCurrencyCode().contentEquals(asCurrencyCode(v.get("fxCurrency"))))
+                {
+                    exchangeRate = BigDecimal.ONE.divide(exchangeRate, 10, RoundingMode.HALF_DOWN);
+                }
+                newType.getCurrentContext().put("exchangeRate", exchangeRate.toPlainString());
+
+                if (!t.getCurrencyCode().equals(t.getSecurity().getCurrencyCode()))
+                {
+                    BigDecimal inverseRate = BigDecimal.ONE.divide(exchangeRate, 10,
+                                    RoundingMode.HALF_DOWN);
+
+                    // check, if forex currency is transaction
+                    // currency or not and swap amount, if necessary
+                    Unit grossValue;
+                    if (!asCurrencyCode(v.get("fxCurrency")).equals(t.getCurrencyCode()))
+                    {
+                        Money fxAmount = Money.of(asCurrencyCode(v.get("fxCurrency")),
+                                        asAmount(v.get("fxAmount")));
+                        Money amount = Money.of(asCurrencyCode(v.get("currency")),
+                                        asAmount(v.get("amount")));
+                        grossValue = new Unit(Unit.Type.GROSS_VALUE, amount, fxAmount, inverseRate);
+                    }
+                    else
+                    {
+                        Money amount = Money.of(asCurrencyCode(v.get("fxCurrency")),
+                                        asAmount(v.get("fxAmount")));
+                        Money fxAmount = Money.of(asCurrencyCode(v.get("currency")),
+                                        asAmount(v.get("amount")));
+                        grossValue = new Unit(Unit.Type.GROSS_VALUE, amount, fxAmount, inverseRate);
+                    }
+                    t.addUnit(grossValue);
+                }
+            })
+            
+            .wrap(TransactionItem::new);
+
+        addTaxesSectionsTransaction(pdfTransaction, newType);
+
+        block.set(pdfTransaction);
+    }
+    
+    @SuppressWarnings("nls")
+    private <T extends Transaction<?>> void addTaxesSectionsTransaction(T Transaction, DocumentType type)
+    {
+        Transaction       
+                 // Auslands-KESt USD               -0,26
+                .section("tax", "currency").optional()
+                .match("Auslands-KESt.*(?<currency>\\w{3}).*-(?<tax>[\\d.]+,\\d+).*")
+                .assign((t, v) -> {
+                    processTaxEntries(t, v, type);
+                })
+                
+                // Zwischensumme USD                0,39
+                .section("tax", "currency").optional()
+                .match("Zwischensumme.*(?<currency>\\w{3}).*-(?<tax>[\\d.]+,\\d+).*")
+                .assign((t, v) -> {
+                    processTaxEntries(t, v, type);
+                })
+
+                // Ertrag 0,0806 EUR Kurswert EUR                0,32KESt-Neu EUR               -0,02
+                .section("tax", "currency").optional()
+                .match(".*KESt-Neu.*(?<currency>\\w{3}).*-(?<tax>[\\d.]+,\\d+).*")
+                .assign((t, v) -> {
+                    processTaxEntries(t, v, type);
+                })
+               
+                // Ertrag 0,68 USD Kurswert USD                2,04Quellensteuer USD               -0,31
+                .section("tax", "currency").optional()
+                .match(".*Quellensteuer.*(?<currency>\\w{3}).*-(?<tax>[\\d.]+,\\d+).*")
+                .assign((t, v) -> {
+                    processTaxEntries(t, v, type);
+                });                
+    }
+    
+    @SuppressWarnings("nls")
+    private void processTaxEntries(Object t, Map<String, String> v, DocumentType type)
+    {
+        if (t instanceof name.abuchen.portfolio.model.Transaction)
+        {
+            Money tax = Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("tax")));
+            PDFExtractorUtils.checkAndSetTax(tax, (name.abuchen.portfolio.model.Transaction) t, type);
+        }
+        else
+        {
+            Money tax = Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("tax")));
+            PDFExtractorUtils.checkAndSetTax(tax,
+                            ((name.abuchen.portfolio.model.BuySellEntry) t).getPortfolioTransaction(), type);
+        }
     }
 }
