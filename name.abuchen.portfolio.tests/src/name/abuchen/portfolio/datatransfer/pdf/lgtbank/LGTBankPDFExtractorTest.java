5556f8571f1eafac66a34889fe3148b05531fad5
Nirus2000 <45203494+Nirus2000@users.noreply.github.com>
2021-02-23 06:41:39 +0100

    Modify LGT PDF-Import to support dividends Issue https://forum.portfolio-performance.info/t/pdf-import-von-lgt-bank/14487/2 To be closed: #1491

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/lgtbank/LGTBankPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/lgtbank/LGTBankPDFExtractorTest.java
@@ -15,6 +15,8 @@ import org.junit.Test;
 import name.abuchen.portfolio.datatransfer.Extractor.BuySellEntryItem;
 import name.abuchen.portfolio.datatransfer.Extractor.Item;
 import name.abuchen.portfolio.datatransfer.Extractor.SecurityItem;
+import name.abuchen.portfolio.datatransfer.Extractor.TransactionItem;
+import name.abuchen.portfolio.datatransfer.actions.AssertImportActions;
 import name.abuchen.portfolio.datatransfer.pdf.LGTBankPDFExtractor;
 import name.abuchen.portfolio.datatransfer.pdf.PDFInputFile;
 import name.abuchen.portfolio.model.AccountTransaction;
@@ -29,7 +31,6 @@ import name.abuchen.portfolio.money.Values;
 @SuppressWarnings("nls")
 public class LGTBankPDFExtractorTest
 {
-
     @Test
     public void testWertpapierKauf01()
     {
@@ -46,7 +47,7 @@ public class LGTBankPDFExtractorTest
         Security security = results.stream().filter(i -> i instanceof SecurityItem).findFirst()
                         .orElseThrow(IllegalArgumentException::new).getSecurity();
         assertThat(security.getIsin(), is("DK0010244508"));
-        assertThat(security.getName(), is("A.P. Moeller - Maersk A/S"));
+        assertThat(security.getName(), is("A.P. Moeller - Maersk A/S Namen- und Inhaber-Aktien -B-"));
 
         // check buy sell transaction
         Optional<Item> item = results.stream().filter(i -> i instanceof BuySellEntryItem).findFirst();
@@ -63,5 +64,69 @@ public class LGTBankPDFExtractorTest
                         is(Money.of("DKK", Values.Amount.factorize(1534.90 + 12.12))));
         assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.TAX), 
                         is(Money.of("DKK", Values.Amount.factorize(121.19))));
+    }
+
+    @Test
+    public void testWertpapierKauf02()
+    {
+        LGTBankPDFExtractor extractor = new LGTBankPDFExtractor(new Client());
+        
+        List<Exception> errors = new ArrayList<>();
+
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "LGTBankKauf02.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+
+        // check security
+        Security security = results.stream().filter(i -> i instanceof SecurityItem).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+        assertThat(security.getIsin(), is("DK0060534915"));
+        assertThat(security.getName(), is("Novo Nordisk A/S Namen-Aktien -B-"));
+
+        // check buy sell transaction
+        Optional<Item> item = results.stream().filter(i -> i instanceof BuySellEntryItem).findFirst();
+        assertThat(item.orElseThrow(IllegalArgumentException::new).getSubject(), instanceOf(BuySellEntry.class));
+        BuySellEntry entry = (BuySellEntry) item.orElseThrow(IllegalArgumentException::new).getSubject();
+
+        assertThat(entry.getPortfolioTransaction().getType(), is(PortfolioTransaction.Type.BUY));
+        assertThat(entry.getAccountTransaction().getType(), is(AccountTransaction.Type.BUY));
+
+        assertThat(entry.getPortfolioTransaction().getAmount(), is(Values.Amount.factorize(72811.75)));
+        assertThat(entry.getPortfolioTransaction().getDateTime(), is(LocalDateTime.parse("2020-04-14T09:00")));
+        assertThat(entry.getPortfolioTransaction().getShares(), is(Values.Share.factorize(180)));
+        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE), 
+                        is(Money.of("DKK", Values.Amount.factorize(1414.16 + 10.69))));
+        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.TAX), 
+                        is(Money.of("DKK", Values.Amount.factorize(106.90))));
+    }
+
+    @Test
+    public void testAusschuettung01()
+    {
+        LGTBankPDFExtractor extractor = new LGTBankPDFExtractor(new Client());
+
+        List<Exception> errors = new ArrayList<>();
+
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "LGTBankAusschuettung01.txt"),
+                        errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+        new AssertImportActions().check(results, "EUR");
+
+        Security security = results.stream().filter(i -> i instanceof SecurityItem).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+        assertThat(security.getIsin(), is("FR0000124141"));
+        assertThat(security.getName(), is("Veolia Environnement SA Namen- und Inhaber-Aktien"));
+
+        AccountTransaction transaction = (AccountTransaction) results.stream().filter(i -> i instanceof TransactionItem)
+                        .findFirst().orElseThrow(IllegalArgumentException::new).getSubject();
+
+        assertThat(transaction.getType(), is(AccountTransaction.Type.DIVIDENDS));
+        assertThat(transaction.getMonetaryAmount(), is(Money.of("EUR", Values.Amount.factorize(198.36))));
+        assertThat(transaction.getShares(), is(Values.Share.factorize(551)));
+        assertThat(transaction.getDateTime(), is(LocalDateTime.parse("2020-05-12T00:00")));
+        assertThat(transaction.getUnitSum(Unit.Type.TAX), is(Money.of("EUR", Values.Amount.factorize(77.14))));
     }
 }
