98c5ebc66db6086084dd6cd01915d1040c856f7d
Nirus2000 <45203494+Nirus2000@users.noreply.github.com>
2021-02-25 14:06:31 +0100

    Modify HTMLTableQuoteFeed to support DateTimeFormat Issue https://forum.portfolio-performance.info/t/quelle-tabelle-auf-einer-webseite/7967/2

--- name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/HTMLTableQuoteFeed.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/HTMLTableQuoteFeed.java
@@ -185,6 +185,7 @@ public class HTMLTableQuoteFeed implements QuoteFeed
                             DateTimeFormatter.ofPattern("d. MMM y"), //$NON-NLS-1$
                             DateTimeFormatter.ofPattern("d. MMMM y"), //$NON-NLS-1$
                             DateTimeFormatter.ofPattern("d. MMM. y"), //$NON-NLS-1$
+                            DateTimeFormatter.ofPattern("M/d/y", Locale.ENGLISH), //$NON-NLS-1$
                             DateTimeFormatter.ofPattern("MMM d, y", Locale.ENGLISH), //$NON-NLS-1$
                             DateTimeFormatter.ofPattern("MMM dd, y", Locale.ENGLISH), //$NON-NLS-1$
                             DateTimeFormatter.ofPattern("MMM dd y", Locale.ENGLISH), //$NON-NLS-1$
