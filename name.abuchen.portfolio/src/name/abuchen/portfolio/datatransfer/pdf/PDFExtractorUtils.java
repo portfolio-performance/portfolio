c680aee1aae664d72a52563dec142e370d384806
Nirus2000 <45203494+Nirus2000@users.noreply.github.com>
2021-03-11 15:45:19 +0100

    Rebuild Commerzbank PDF Importer
    Issue: https://forum.portfolio-performance.info/t/pdf-import-von-commerzbank/9865/6
    Issue: https://forum.portfolio-performance.info/t/pdf-import-von-commerzbank/9865/7
    Signed-off-by: Nirus2000 <45203494+Nirus2000@users.noreply.github.com>
    [squashed commits; rebased to master]
    Signed-off-by: Andreas Buchen <andreas.buchen@gmail.com>

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFExtractorUtils.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFExtractorUtils.java
@@ -38,4 +38,32 @@ class PDFExtractorUtils
         }
     }
 
+    @SuppressWarnings("nls")
+    public static void checkAndSetFee(Money fee, name.abuchen.portfolio.model.Transaction t, DocumentType type) 
+    {
+        if (fee.getCurrencyCode().equals(t.getCurrencyCode()))
+        {
+            t.addUnit(new Unit(Unit.Type.FEE, fee));
+        }
+        else if (type.getCurrentContext().containsKey("exchangeRate"))
+        {
+            BigDecimal exchangeRate = new BigDecimal(type.getCurrentContext().get("exchangeRate"));
+            BigDecimal inverseRate = BigDecimal.ONE.divide(exchangeRate, 10, RoundingMode.HALF_DOWN);
+
+            Money fxFee = Money.of(t.getCurrencyCode(),
+                            BigDecimal.valueOf(fee.getAmount()).multiply(inverseRate)
+                                            .setScale(0, RoundingMode.HALF_UP).longValue());
+
+            // store fee value in both currencies, if security's currency
+            // is different to transaction currency
+            if (t.getCurrencyCode().equals(t.getSecurity().getCurrencyCode()))
+            {
+                t.addUnit(new Unit(Unit.Type.FEE, fxFee));
+            }
+            else
+            {
+                t.addUnit(new Unit(Unit.Type.FEE, fxFee, fee, inverseRate));
+            }
+        }
+    }
 }
