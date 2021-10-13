95eb87f2c714759af14ac83e2c346730f993125e
OnkelDok <90478568+OnkelDok@users.noreply.github.com>
2021-10-13 20:51:55 +0200

    re-arrange view

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/AttributeSettingsPane.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/AttributeSettingsPane.java
@@ -17,6 +17,7 @@ import org.eclipse.swt.widgets.ColorDialog;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 
 import name.abuchen.portfolio.model.AttributeType;
@@ -32,7 +33,7 @@ public class AttributeSettingsPane implements InformationPanePage
     @Override
     public String getLabel()
     {
-        return "Attribute Settings";
+        return "Attribute Settings"; //$NON-NLS-1$
     }
     
     private Composite viewContainer;
@@ -70,133 +71,17 @@ public class AttributeSettingsPane implements InformationPanePage
         Class<?> type = attribute.getType();
         if(type == LimitPrice.class)
         {
-            createLimitPriceSettingsView(attribute, parent);            
+            LimitPriceSettingsViewFactory.createView(attribute, parent);            
         }
         else
         {          
             Label lb = new Label(parent, SWT.BORDER);
-            lb.setText("no setting for " + attribute.getName());
+            lb.setText("no setting for " + attribute.getName()); //$NON-NLS-1$
         }
         
     }
     
-    private void createLimitPriceSettingsView(AttributeType attribute, Composite parent)
-    {       
-        LimitPriceSettings settings = new LimitPriceSettings(attribute.getProperties());
-        Composite container = new Composite(parent, SWT.NONE);
-        RowLayout layout = new RowLayout(SWT.VERTICAL);
-        container.setLayout(layout);
-
-        Button btn = new Button(container, SWT.CHECK);
-        btn.setText("ShowRelativeDiff"); //$NON-NLS-1$
-        btn.setSelection(settings.getShowRelativeDiff());
-        btn.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> {
-            settings.setShowRelativeDiff(((Button)event.getSource()).getSelection());
-        }));
-        container.setBackground(Colors.RED);
-
-
-        btn = new Button(container, SWT.CHECK);
-        btn.setText("ShowAbsoluteDiff"); //$NON-NLS-1$
-        btn.setSelection(settings.getShowAbsoluteDiff());
-        btn.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> {
-            settings.setShowAbsoluteDiff(((Button)event.getSource()).getSelection());
-        }));
-
-        ColoredLabel exceedLabel = createColorButtons(container, settings::getLimitExceededColor, settings::setLimitExceededColor, "LimitExceededColor: ", () -> Colors.theme().greenBackground()); //$NON-NLS-1$
-        ColoredLabel undercutLabel = createColorButtons(container, settings::getLimitUndercutColor, settings::setLimitUndercutColor, "LimitUndercutColor: ", () -> Colors.theme().redBackground()); //$NON-NLS-1$                      
-
-        // swap colors button
-        Button button = new Button(container, SWT.PUSH);
-        button.setText("swap colors"); //$NON-NLS-1$
-        //button.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-        button.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> 
-        {
-            // swap settings
-            Color exceed = settings.getLimitExceededColor();
-            Color undercut = settings.getLimitUndercutColor();
-            settings.setLimitUndercutColor(exceed);
-            settings.setLimitExceededColor(undercut);
-            
-            // swap label colors
-            exceedLabel.setBackdropColor(undercut);
-            exceedLabel.redraw();
-            undercutLabel.setBackdropColor(exceed);
-            undercutLabel.redraw();
-        })) ;
-        
-        // reset to default colors button
-        Button resetButton = new Button(container, SWT.PUSH);
-        resetButton.setText("reset to default"); //$NON-NLS-1$
-        //button.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-        resetButton.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> 
-        {
-            // swap settings
-            Color exceed = Colors.theme().greenBackground();
-            Color undercut = Colors.theme().redBackground();
-            settings.setLimitExceededColor(exceed);
-            settings.setLimitUndercutColor(undercut);
-            
-            // swap label colors
-            exceedLabel.setBackdropColor(exceed);
-            exceedLabel.redraw();
-            undercutLabel.setBackdropColor(undercut);
-            undercutLabel.redraw();
-        })) ;
-        
-    }
     
-    private ColoredLabel createColorButtons(Composite parent, Supplier<Color> getColor, Consumer<Color> setColor, String labelText, Supplier<Color> getThemeColor)
-    {
-        Composite colorContainer = new Composite(parent, SWT.NONE);
-        colorContainer.setLayout(new GridLayout(3, false));
-        
-        CLabel textLabel = new CLabel(colorContainer, SWT.CENTER);
-        textLabel.setText(labelText); 
-        
-        ColoredLabel colorLabel = new ColoredLabel(colorContainer, SWT.CENTER| SWT.BORDER); 
-        //colorLabel.setBackgroundMode(SWT.INHERIT_NONE);
-        Color color = getColor.get();
-        if(color == null)
-            color = getThemeColor.get();
-        colorLabel.setBackground(color); 
-        colorLabel.setBackdropColor(color);
-        colorLabel.setSize(300, 100); 
-        colorLabel.setLayoutData( new GridData( SWT.CENTER, SWT.CENTER, false, false ) );
-        
-        colorLabel.setText("            "); //$NON-NLS-1$
-         
-        Button button = new Button(colorContainer, SWT.PUSH);
-        button.setText("..."); //$NON-NLS-1$
-        button.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-        button.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> 
-        {
-         // Create the color-change dialog
-            ColorDialog dlg = new ColorDialog(Display.getDefault().getActiveShell());
-
-            // Set the selected color in the dialog from
-            // user's selected color
-            
-            Color color1 = getColor.get();
-            if(color1 == null)
-                color1 = getThemeColor.get();
-            dlg.setRGB(color1.getRGB());
-
-            // Change the title bar text
-            dlg.setText("Choose a Color"); //$NON-NLS-1$
-
-            // Open the dialog and retrieve the selected color
-            RGB rgb = dlg.open();
-            if (rgb != null) 
-            {
-                setColor.accept(new Color(rgb));
-                colorLabel.setBackground(getColor.get()); 
-                colorLabel.setBackdropColor(getColor.get());
-            }
-        })) ;
-        
-        return colorLabel;
-    }
 
     @Override
     public void onRecalculationNeeded()
@@ -205,4 +90,149 @@ public class AttributeSettingsPane implements InformationPanePage
         
     }
 
+    
+    private static class LimitPriceSettingsViewFactory
+    {
+        
+        private static void createView(AttributeType attribute, Composite parent)
+        {       
+            LimitPriceSettings settings = new LimitPriceSettings(attribute.getProperties());
+            Composite container = new Composite(parent, SWT.NONE);
+            RowLayout layout = new RowLayout(SWT.HORIZONTAL);
+            container.setLayout(layout);
+
+            createColorView(container, settings);
+            createDistanceView(container, settings);          
+            
+        }
+        
+        
+        private static void createDistanceView(Composite parent, LimitPriceSettings settings)
+        {
+          //---------------------------------------------------
+            //-- Distance settings
+            Group gpDistance = new Group(parent, SWT.NONE);
+            gpDistance.setText("Distance between last price and Limit"); //$NON-NLS-1$
+            gpDistance.setLayout(new RowLayout(SWT.VERTICAL));
+            
+            Button btn = new Button(gpDistance, SWT.CHECK);
+            btn.setText("ShowRelativeDiff"); //$NON-NLS-1$
+            btn.setSelection(settings.getShowRelativeDiff());
+            btn.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> settings.setShowRelativeDiff(((Button)event.getSource()).getSelection())));
+
+            btn = new Button(gpDistance, SWT.CHECK);
+            btn.setText("ShowAbsoluteDiff"); //$NON-NLS-1$
+            btn.setSelection(settings.getShowAbsoluteDiff());
+            btn.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> settings.setShowAbsoluteDiff(((Button)event.getSource()).getSelection())));
+        }
+        
+        private static void createColorView(Composite parent, LimitPriceSettings settings)
+        {
+          //---------------------------------------------------
+            //-- Color settings
+            Group gpColors = new Group(parent, SWT.NONE);
+            gpColors.setText("Limit Colors"); //$NON-NLS-1$
+            gpColors.setLayout(new RowLayout(SWT.VERTICAL));
+            
+            
+            Composite colorContainer = new Composite(gpColors, SWT.NONE);
+            colorContainer.setLayout(new GridLayout(3, false));
+            ColoredLabel exceedLabel = createColorButtons(colorContainer, settings::getLimitExceededColor, settings::setLimitExceededColor, "LimitExceededColor: ", () -> Colors.theme().greenBackground()); //$NON-NLS-1$
+            ColoredLabel undercutLabel = createColorButtons(colorContainer, settings::getLimitUndercutColor, settings::setLimitUndercutColor, "LimitUndercutColor: ", () -> Colors.theme().redBackground()); //$NON-NLS-1$                      
+
+            Composite buttonsContainer = new Composite(gpColors, SWT.NONE);
+            RowLayout layout = new RowLayout(SWT.HORIZONTAL);
+            layout.pack = false;
+            buttonsContainer.setLayout(layout);
+            
+            // swap colors button
+            Button button = new Button(buttonsContainer, SWT.PUSH);
+            button.setText("swap colors"); //$NON-NLS-1$
+            button.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> 
+            {
+                // swap settings
+                Color exceed = settings.getLimitExceededColor();
+                Color undercut = settings.getLimitUndercutColor();
+                settings.setLimitUndercutColor(exceed);
+                settings.setLimitExceededColor(undercut);
+                
+                // swap label colors
+                exceedLabel.setBackdropColor(undercut);
+                exceedLabel.redraw();
+                undercutLabel.setBackdropColor(exceed);
+                undercutLabel.redraw();
+            })) ;
+            
+            // reset to default colors button
+            Button resetButton = new Button(buttonsContainer, SWT.PUSH);
+            resetButton.setText("reset to default"); //$NON-NLS-1$
+            resetButton.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> 
+            {
+                // swap settings
+                Color exceed = Colors.theme().greenBackground();
+                Color undercut = Colors.theme().redBackground();
+                settings.setLimitExceededColor(exceed);
+                settings.setLimitUndercutColor(undercut);
+                
+                // swap label colors
+                exceedLabel.setBackdropColor(exceed);
+                exceedLabel.redraw();
+                undercutLabel.setBackdropColor(undercut);
+                undercutLabel.redraw();
+            })) ;
+            
+        }
+        
+       
+        
+        private static ColoredLabel createColorButtons(Composite parent, Supplier<Color> getColor, Consumer<Color> setColor, String labelText, Supplier<Color> getThemeColor)
+        {
+            
+            
+            CLabel textLabel = new CLabel(parent, SWT.CENTER);
+            textLabel.setText(labelText); 
+            
+            ColoredLabel colorLabel = new ColoredLabel(parent, SWT.CENTER| SWT.BORDER); 
+            Color color = getColor.get();
+            if(color == null)
+                color = getThemeColor.get();
+            colorLabel.setBackground(color); 
+            colorLabel.setBackdropColor(color);
+            colorLabel.setSize(300, 100); 
+            colorLabel.setLayoutData( new GridData( SWT.CENTER, SWT.CENTER, false, false ) );
+            
+            colorLabel.setText("            "); //$NON-NLS-1$
+             
+            Button button = new Button(parent, SWT.PUSH);
+            button.setText("..."); //$NON-NLS-1$
+            button.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+            button.addSelectionListener(SelectionListener.widgetSelectedAdapter(event -> 
+            {
+             // Create the color-change dialog
+                ColorDialog dlg = new ColorDialog(Display.getDefault().getActiveShell());
+
+                // Set the selected color in the dialog from
+                // user's selected color
+                
+                Color color1 = getColor.get();
+                if(color1 == null)
+                    color1 = getThemeColor.get();
+                dlg.setRGB(color1.getRGB());
+
+                // Change the title bar text
+                dlg.setText("Choose a Color"); //$NON-NLS-1$
+
+                // Open the dialog and retrieve the selected color
+                RGB rgb = dlg.open();
+                if (rgb != null) 
+                {
+                    setColor.accept(new Color(rgb));
+                    colorLabel.setBackground(getColor.get()); 
+                    colorLabel.setBackdropColor(getColor.get());
+                }
+            })) ;
+            
+            return colorLabel;
+        }
+    }
 }
