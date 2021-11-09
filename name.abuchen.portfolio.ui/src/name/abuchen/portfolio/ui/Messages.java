a151f1d437642321665b4384e8bd3554d71ae7c4
Karsten <79692288+kasch-code@users.noreply.github.com>
2021-11-09 17:01:45 +0100

    Add CoinGecko CoinID as parameter to provider

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/Messages.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/Messages.java
@@ -546,6 +546,7 @@ public class Messages extends NLS
     public static String LabelClientFilterNoCustomFilterExisting;
     public static String LabelClose;
     public static String FilterOnlyClosedTrades;
+    public static String LabelCoinGeckoCoinId;
     public static String LabelCollapseAll;
     public static String LabelColorSchema;
     public static String LabelColumns;
