3df75d24bfd5ab9fd5c24d37715f7887e28cb6f5
Nirus2000 <webmaster@nirus-online.de>
2021-11-14 08:08:20 +0100

    Add new Score Priority Corp. PDF-Importer
    Fixes #2512
    https://forum.portfolio-performance.info/t/pdf-import-from-scorepriority/18532/1

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
@@ -68,6 +68,7 @@ public class PDFImportAssistant
         extractors.add(new RaiffeisenBankgruppePDFExtractor(client));
         extractors.add(new RenaultBankDirektPDFExtractor(client));
         extractors.add(new SBrokerPDFExtractor(client));
+        extractors.add(new ScorePriorityIncPDFExtractor(client));
         extractors.add(new SelfWealthPDFExtractor(client));
         extractors.add(new SwissquotePDFExtractor(client));
         extractors.add(new TargobankPDFExtractor(client));
