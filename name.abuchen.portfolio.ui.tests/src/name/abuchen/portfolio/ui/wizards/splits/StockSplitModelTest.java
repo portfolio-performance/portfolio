a49d4b84dd8bc7b37a61b9fde8e6b2e4fec9fafa
Andreas Buchen <andreas.buchen@gmail.com>
2021-11-20 07:56:57 +0100

    Removed unnecessary code

--- name.abuchen.portfolio.ui.tests/src/name/abuchen/portfolio/ui/wizards/splits/StockSplitModelTest.java
+++ name.abuchen.portfolio.ui.tests/src/name/abuchen/portfolio/ui/wizards/splits/StockSplitModelTest.java
@@ -1,14 +1,12 @@
 package name.abuchen.portfolio.ui.wizards.splits;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import java.math.BigDecimal;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import org.junit.Test;
 
-@SuppressWarnings("nls")
 public class StockSplitModelTest
 {
 
@@ -18,10 +16,10 @@ public class StockSplitModelTest
         StockSplitModel model = new StockSplitModel(null, null);
         // 2 shares for 1
         model.setNewShares(new BigDecimal(2));
-        model.setOldShares(new BigDecimal(1));        
-        
+        model.setOldShares(new BigDecimal(1));
+
         assertThat(model.calculateNewQuote(4), is(2L)); // quote is halved
         assertThat(model.calculateNewStock(4), is(8L)); // stock is doubled
     }
-    
+
 }
