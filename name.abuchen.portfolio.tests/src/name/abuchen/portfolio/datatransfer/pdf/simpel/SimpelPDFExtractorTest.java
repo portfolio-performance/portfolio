d18de3cfda7b512498d5f7b2c588173804194b06
Christoph Auer <auer.chrisi.priv@gmail.com>
2022-01-23 10:04:24 +0100

    Added dividend PDF importer for Simpel S.A.

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/simpel/SimpelPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/simpel/SimpelPDFExtractorTest.java
@@ -3,6 +3,7 @@ package name.abuchen.portfolio.datatransfer.pdf.simpel;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.Assert.assertNull;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ import org.junit.Test;
 import name.abuchen.portfolio.datatransfer.Extractor.BuySellEntryItem;
 import name.abuchen.portfolio.datatransfer.Extractor.Item;
 import name.abuchen.portfolio.datatransfer.Extractor.SecurityItem;
+import name.abuchen.portfolio.datatransfer.Extractor.TransactionItem;
 import name.abuchen.portfolio.datatransfer.actions.AssertImportActions;
 import name.abuchen.portfolio.datatransfer.pdf.PDFInputFile;
 import name.abuchen.portfolio.datatransfer.pdf.SimpelPDFExtractor;
@@ -112,5 +114,44 @@ public class SimpelPDFExtractorTest
                         is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(31.61))));
         assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE),
                         is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0))));
+    }
+
+    @Test
+    public void testDividende01()
+    {
+        Client client = new Client();
+
+        SimpelPDFExtractor extractor = new SimpelPDFExtractor(client);
+
+        List<Exception> errors = new ArrayList<>();
+
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "SimpelDividend01.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        // check security
+        Security security = results.stream().filter(SecurityItem.class::isInstance).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+        assertThat(security.getIsin(), is("AT0000A1Z882"));
+        assertThat(security.getName(), is("Standortfonds Deutschland"));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
+
+        // check dividend transaction
+        AccountTransaction t = (AccountTransaction) results.stream().filter(i -> i instanceof TransactionItem)
+                        .findFirst().orElseThrow(IllegalArgumentException::new).getSubject();
+
+        assertThat(t.getType(), is(AccountTransaction.Type.DIVIDENDS));
+
+        assertThat(t.getDateTime(), is(LocalDateTime.parse("2021-12-21T00:00")));
+        assertThat(t.getShares(), is(Values.Share.factorize(6.48708487)));
+        assertThat(t.getSource(), is("SimpelDividend01.txt"));
+        assertNull(t.getNote());
+
+        assertThat(t.getMonetaryAmount(), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(13.3))));
+        assertThat(t.getGrossValue(), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(17.58))));
+        assertThat(t.getUnitSum(Unit.Type.TAX), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(4.28))));
+        assertThat(t.getUnitSum(Unit.Type.FEE), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0.00))));
     }
 }
