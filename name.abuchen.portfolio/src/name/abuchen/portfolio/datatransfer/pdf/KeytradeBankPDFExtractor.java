e61095c83d5926a275ce09788a96d0a1b519395a
Nirus2000 <45203494+Nirus2000@users.noreply.github.com>
2022-04-02 20:24:43 +0200

    Improve KeyTrade PDF-Importer (#2771)
    Add missing testparts
    Add missing testCases
    Optimize regEx
    Optimize testcases (Standardization)

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/KeytradeBankPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/KeytradeBankPDFExtractor.java
@@ -1,7 +1,6 @@
 package name.abuchen.portfolio.datatransfer.pdf;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,6 +14,7 @@ import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.PortfolioTransaction;
 import name.abuchen.portfolio.model.Transaction.Unit;
 import name.abuchen.portfolio.money.Money;
+import name.abuchen.portfolio.money.Values;
 
 @SuppressWarnings("nls")
 public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
@@ -38,7 +38,7 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
     private void addBuySellTransaction()
     {
         final DocumentType type = new DocumentType("(Kauf|Achat|Vente|Verkauf)", (context, lines) -> {
-            Pattern pTransaction = Pattern.compile("^Num.ro.*(Achat|Vente) (?<shares>[\\.,\\d]+) (?<name>.*) \\((?<isin>[\\w]{12})\\) . [\\.,\\d]+ (?<currency>[\\w]{3})(.*)?$");
+            Pattern pTransaction = Pattern.compile("^Num.ro.*(Achat|Vente) (?<shares>[\\.,\\d]+) (?<name>.*) \\((?<isin>[\\w]{12})\\) .* [\\.,\\d]+ (?<currency>[\\w]{3})(.*)?$");
             // read the current context here
 
             /***
@@ -99,7 +99,7 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
                                 // Numéro de réf. : 7763C9846/01AAchat 15 KINEPOLIS GROUP (BE0974274061) à 46,3 EURType d' instrument: Actions
                                 section -> section
                                         .attributes("securityIsOutsideTheBlock")
-                                        .match("^(?<securityIsOutsideTheBlock>Num.ro.*)(\\s)?: .*$")
+                                        .match("^(?<securityIsOutsideTheBlock>Num.ro.*)([\\s]+)?: .*$")
                                         .assign((t, v) -> {
                                             Map<String, String> context = type.getCurrentContext();
 
@@ -119,19 +119,19 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
                                 // Date et heure d'exécution : 19/10/2020 11:53:03 CET 
                                 section -> section
                                         .attributes("date", "time")
-                                        .match("^(Ausf.hrungsdatum und \\-zeit|Date et heure d'ex.cution)(\\s)?: (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2})(.*)?$")
+                                        .match("^(Ausf.hrungsdatum und \\-zeit|Date et heure d.ex.cution)([\\s]+)?: (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2})(.*)?$")
                                         .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
                                 ,
                                 // Ordre créé à: 26/P11/2021L10:05:13 CETDate et heure d'exécution: 26/11/2021 13:25:24 CETDate de comptabilisation: 26/11/2021Date valeur: 30/11/2021Lieu d'exécution: EURONEXT - EURONEXT BRUSS
                                 section -> section
                                         .attributes("date", "time")
-                                        .match("^Ordre cr.. .(\\s)?: .*Date et heure d'ex.cution(\\s)?: (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2})(.*)?$")
+                                        .match("^Ordre cr.. .([\\s]+)?: .*Date et heure d.ex.cution([\\s]+)?: (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2})(.*)?$")
                                         .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
                                 ,
                                 // Ordre créé à: 05/11/2020 11:25:43 CET
                                 section -> section
                                         .attributes("date", "time")
-                                        .match("^Ordre cr.. .(\\s)?: (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2})(.*)?$")
+                                        .match("^Ordre cr.. .([\\s]+)?: (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2})(.*)?$")
                                         .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
                         )
 
@@ -148,26 +148,39 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
                     t.setCurrencyCode(v.get("currency"));
                 })
 
+                /***
+                 * Only add gross value with forex if the security is actually denoted in the foreign currency
+                 * (often users actually have the quotes in their home country currency)
+                 */
                 // Wechselkurs EUR/USD 1.123000 849,47 EUR
                 // Lastschrift 953,95 USD Valutadatum 21/06/2016
-                .section("amount", "currency", "exchangeRate", "forexCurrency", "forexAmount").optional()
-                .match("^Wechselkurs [\\w]{3}\\/[\\w]{3} (?<exchangeRate>[\\.,\\d]+) (?<forexAmount>[\\.,\\d]+) (?<forexCurrency>[\\w]{3})$")
+                .section("exchangeRate", "fxAmount", "fxCurrency", "amount", "currency").optional()
+                .match("^Wechselkurs [\\w]{3}\\/[\\w]{3} (?<exchangeRate>[\\.,\\d]+) (?<fxAmount>[\\.,\\d]+) (?<fxCurrency>[\\w]{3})$")
                 .match("^(Gutschrift|Cr.dit|Lastschrift|D.bit) (?<amount>[\\.,\\d]+) (?<currency>[\\w]{3})(.*)?$")
                 .assign((t, v) -> {
-                     Money forex = Money.of(asCurrencyCode(v.get("forexCurrency")), asAmount(v.get("forexAmount")));
-                     BigDecimal exchangeRate = asExchangeRate(v.get("exchangeRate"));
-                     Money gross = Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("amount")));
+                    // read the forex currency, exchange rate and gross
+                    // amount in forex currency
+                    String forex = asCurrencyCode(v.get("fxCurrency"));
+                    if (t.getPortfolioTransaction().getSecurity().getCurrencyCode().equals(forex))
+                    {
+                        BigDecimal exchangeRate = asExchangeRate(v.get("exchangeRate"));
 
-                     // only add gross value with forex if the security
-                     // is actually denoted in the foreign currency
-                     // (often users actually have the quotes in their
-                     // home country currency)
-                     if (forex.getCurrencyCode()
-                                     .equals(t.getPortfolioTransaction().getSecurity().getCurrencyCode()))
-                     {
-                         t.getPortfolioTransaction()
-                                         .addUnit(new Unit(Unit.Type.GROSS_VALUE, gross, forex, exchangeRate));
-                     }
+                        Money fxAmount = Money.of(asCurrencyCode(v.get("fxCurrency")),
+                                        asAmount(v.get("fxAmount")));
+                        Money amount = Money.of(asCurrencyCode(v.get("currency")),
+                                        asAmount(v.get("amount")));
+
+                        Unit grossValue = new Unit(Unit.Type.GROSS_VALUE, amount, fxAmount, exchangeRate);
+
+                        t.getPortfolioTransaction().addUnit(grossValue);
+                    }
+                })
+
+                .section("exchangeRate").optional()
+                .match("^Wechselkurs [\\w]{3}\\/[\\w]{3} (?<exchangeRate>[\\.,\\d]+) [\\.,\\d]+ [\\w]{3}$")
+                .assign((t, v) -> {
+                    BigDecimal exchangeRate = asExchangeRate(v.get("exchangeRate"));
+                    type.getCurrentContext().put("exchangeRate", exchangeRate.toPlainString());
                 })
 
                 // Auftragstyp : Limit (16 EUR)
@@ -175,7 +188,7 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
                 // Type d'ordre: Market
                 // Type d'ordre: Market I
                 .section("note").optional()
-                .match("^(Auftragstyp|Type d'(\\s)?ordre)(\\s)?: (?<note>(Limit \\([\\.,\\d]+ [\\w]{3}\\)|Market))(.*)?$")
+                .match("^(Auftragstyp|Type d'([\\s]+)?ordre)([\\s]+)?: (?<note>(Limit \\([\\.,\\d]+ [\\w]{3}\\)|Market))(.*)?$")
                 .assign((t, v) -> t.setNote(v.get("note")))
 
                 .wrap(BuySellEntryItem::new);
@@ -187,7 +200,7 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
     private void addDividendeTransaction()
     {
         final DocumentType type = new DocumentType("(Einkommensart|Nature des revenus)", (context, lines) -> {
-            Pattern pTransaction = Pattern.compile("^CREDIT COUPON NR .*: (?<shares>[\\.,\\d]+) (?<name>.*) \\((?<isin>[\\w]{12})\\) . [\\.,\\d]+ (?<currency>[\\w]{3})(.*)?$");
+            Pattern pTransaction = Pattern.compile("^CREDIT COUPON NR .*: (?<shares>[\\.,\\d]+) (?<name>.*) \\((?<isin>[\\w]{12})\\) .* [\\.,\\d]+ (?<currency>[\\w]{3})(.*)?$");
             // read the current context here
 
             /***
@@ -210,7 +223,7 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
         });
         this.addDocumentTyp(type);
 
-        Block block = new Block("^(Einkommensart|Nature des revenus)(\\s)?: .*$");
+        Block block = new Block("^(Einkommensart|Nature des revenus)([\\s]+)?: .*$");
         type.addBlock(block);
         Transaction<AccountTransaction> pdfTransaction = new Transaction<AccountTransaction>()
             .subject(() -> {
@@ -227,7 +240,7 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
                                 section -> section
                                         .attributes("shares", "name", "isin", "currency")
                                         .match("^(?<shares>[\\.,\\d]+) (?<name>.*) NR .* [\\.,\\d]+ (?<currency>[\\w]{3})$")
-                                        .match("^(Wertschriftenkonto|Compte-titres):.* (Wertpapier|Titre):[\\d]+\\/(?<isin>[\\w]{12}) .*$")
+                                        .match("^(Wertschriftenkonto|Compte\\-titres):.* (Wertpapier|Titre):[\\d]+\\/(?<isin>[\\w]{12}) .*$")
                                         .assign((t, v) -> {
                                             t.setShares(asShares(v.get("shares")));
                                             t.setSecurity(getOrCreateSecurity(v));
@@ -236,7 +249,7 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
                                 // CREDIT COUPON NR 210104 : 35 TOTALENERGIES (FR0000120271) à 0,66 EUR
                                 section -> section
                                         .attributes("securityIsOutsideTheBlock")
-                                        .match("^(?<securityIsOutsideTheBlock>Nature des revenus)(\\s)?: .*$")
+                                        .match("^(?<securityIsOutsideTheBlock>Nature des revenus)([\\s]+)?: .*$")
                                         .assign((t, v) -> {
                                             Map<String, String> context = type.getCurrentContext();
 
@@ -256,14 +269,14 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
                                 // Net CREDIT 45,65 EUR Date  03/02/2021
                                 section -> section
                                         .attributes("date")
-                                        .match("^(Nettoguthaben|Net CREDIT) [\\.,\\d]+ [\\w]{3} (Datum|Date) (\\s)?(?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4})$")
+                                        .match("^(Nettoguthaben|Net CREDIT) [\\.,\\d]+ [\\w]{3} (Datum|Date) ([\\s]+)?(?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4})$")
                                         .assign((t, v) -> t.setDateTime(asDate(v.get("date"))))
                                 ,
                                 // Date valeur: 11/01/2021
                                 // Date valeur: 17/11/2021 C
                                 section -> section
                                         .attributes("date")
-                                        .match("^Date valeur(\\s)?: (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4})(.*)?$")
+                                        .match("^Date valeur([\\s]+)?: (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4})(.*)?$")
                                         .assign((t, v) -> t.setDateTime(asDate(v.get("date"))))
                         )
 
@@ -296,35 +309,39 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
                 .match("^Wechselkurs (?<exchangeRate>[\\.,\\d]+) (?<fxAmount>[\\.,\\d]+) (?<fxCurrency>[\\w]{3})$")
                 .match("^(Nettoguthaben|Net CREDIT) (?<amount>[\\.,\\d]+) (?<currency>[\\w]{3})(.*)?$")
                 .assign((t, v) -> {
-                    BigDecimal exchangeRate = asExchangeRate(v.get("exchangeRate"));
-                    if (t.getCurrencyCode().contentEquals(asCurrencyCode(v.get("fxCurrency"))))
-                    {
-                        exchangeRate = BigDecimal.ONE.divide(exchangeRate, 10, RoundingMode.HALF_DOWN);
-                    }
-                    type.getCurrentContext().put("exchangeRate", exchangeRate.toPlainString());
-
                     if (!t.getCurrencyCode().equals(t.getSecurity().getCurrencyCode()))
                     {
-                        BigDecimal inverseRate = BigDecimal.ONE.divide(exchangeRate, 10,
-                                        RoundingMode.HALF_DOWN);
+                        BigDecimal exchangeRate = asExchangeRate(v.get("exchangeRate"));
 
                         // check, if forex currency is transaction
                         // currency or not and swap amount, if necessary
                         Unit grossValue;
                         if (!asCurrencyCode(v.get("fxCurrency")).equals(t.getCurrencyCode()))
                         {
-                            Money fxAmount = Money.of(asCurrencyCode(v.get("fxCurrency")),asAmount(v.get("fxAmount")));
-                            Money amount = Money.of(asCurrencyCode(v.get("currency")),asAmount(v.get("amount")));
-                            grossValue = new Unit(Unit.Type.GROSS_VALUE, amount, fxAmount, inverseRate);
+                            Money fxAmount = Money.of(asCurrencyCode(v.get("fxCurrency")),
+                                            asAmount(v.get("fxAmount")));
+                            Money amount = Money.of(asCurrencyCode(v.get("currency")),
+                                            asAmount(v.get("amount")));
+                            grossValue = new Unit(Unit.Type.GROSS_VALUE, amount, fxAmount, exchangeRate);
                         }
                         else
                         {
-                            Money amount = Money.of(asCurrencyCode(v.get("fxCurrency")), asAmount(v.get("fxAmount")));
-                            Money fxAmount = Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("amount")));
-                            grossValue = new Unit(Unit.Type.GROSS_VALUE, amount, fxAmount, inverseRate);
+                            Money amount = Money.of(asCurrencyCode(v.get("fxCurrency")),
+                                            asAmount(v.get("fxAmount")));
+                            Money fxAmount = Money.of(asCurrencyCode(v.get("currency")),
+                                            asAmount(v.get("amount")));
+                            grossValue = new Unit(Unit.Type.GROSS_VALUE, amount, fxAmount, exchangeRate);
                         }
                         t.addUnit(grossValue);
                     }
+                })
+
+                // Wechselkurs 1,05 1,01 EUR
+                .section("exchangeRate").optional()
+                .match("^Wechselkurs [\\w]{3}\\/[\\w]{3} (?<exchangeRate>[\\.,\\d]+) [\\.,\\d]+ [\\w]{3}$")
+                .assign((t, v) -> {
+                    BigDecimal exchangeRate = asExchangeRate(v.get("exchangeRate"));
+                    type.getCurrentContext().put("exchangeRate", exchangeRate.toPlainString());
                 })
 
                 .wrap(TransactionItem::new);
@@ -392,5 +409,62 @@ public class KeytradeBankPDFExtractor extends AbstractPDFExtractor
                 .section("fee", "currency").optional()
                 .match("(^.* .*|^)Courtage \\- (?<fee>[\\.,\\d]+) (?<currency>[\\w]{3})(.*)?$")
                 .assign((t, v) -> processFeeEntries(t, v, type));
+    }
+
+    @Override
+    protected long asAmount(String value)
+    {
+        String language = "de"; //$NON-NLS-1$
+        String country = "DE"; //$NON-NLS-1$
+
+        int lastDot = value.lastIndexOf("."); //$NON-NLS-1$
+        int lastComma = value.lastIndexOf(","); //$NON-NLS-1$
+
+        // returns the greater of two int values
+        if (Math.max(lastDot, lastComma) == lastDot)
+        {
+            language = "en"; //$NON-NLS-1$
+            country = "US"; //$NON-NLS-1$
+        }
+
+        return PDFExtractorUtils.convertToNumberLong(value, Values.Amount, language, country);
+    }
+
+    @Override
+    protected long asShares(String value)
+    {
+        String language = "de"; //$NON-NLS-1$
+        String country = "DE"; //$NON-NLS-1$
+
+        int lastDot = value.lastIndexOf("."); //$NON-NLS-1$
+        int lastComma = value.lastIndexOf(","); //$NON-NLS-1$
+
+        // returns the greater of two int values
+        if (Math.max(lastDot, lastComma) == lastDot)
+        {
+            language = "en"; //$NON-NLS-1$
+            country = "US"; //$NON-NLS-1$
+        }
+
+        return PDFExtractorUtils.convertToNumberLong(value, Values.Share, language, country);
+    }
+
+    @Override
+    protected BigDecimal asExchangeRate(String value)
+    {
+        String language = "de"; //$NON-NLS-1$
+        String country = "DE"; //$NON-NLS-1$
+
+        int lastDot = value.lastIndexOf("."); //$NON-NLS-1$
+        int lastComma = value.lastIndexOf(","); //$NON-NLS-1$
+
+        // returns the greater of two int values
+        if (Math.max(lastDot, lastComma) == lastDot)
+        {
+            language = "en"; //$NON-NLS-1$
+            country = "US"; //$NON-NLS-1$
+        }
+
+        return PDFExtractorUtils.convertToNumberBigDecimal(value, Values.Share, language, country);
     }
 }
