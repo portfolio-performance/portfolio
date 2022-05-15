672d91cf21e8370066d64b471fa6548d20d26ba6
Ulrich Klauer <ulrich@chirlu.de>
2022-05-15 18:10:17 +0200

    Add missing days to Vienna trade calendar
    Add Labour Day, Christmas Day and Saint Stephen’s Day to the trade
    calendar for Vienna Stock Exchange; they were probably missing because they
    were weekends when the calendar was first created.

--- name.abuchen.portfolio/src/name/abuchen/portfolio/util/TradeCalendarManager.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/util/TradeCalendarManager.java
@@ -203,9 +203,12 @@ public class TradeCalendarManager
         tc.add(fixed(NEW_YEAR, Month.JANUARY, 1));
         tc.add(easter(GOOD_FRIDAY, -2));
         tc.add(easter(EASTER_MONDAY, 1));
+        tc.add(fixed(LABOUR_DAY, Month.MAY, 1));
         tc.add(easter(WHIT_MONDAY, 50));
         tc.add(fixed(NATION_DAY, Month.OCTOBER, 26));
         tc.add(fixed(CHRISTMAS_EVE, Month.DECEMBER, 24));
+        tc.add(fixed(CHRISTMAS, Month.DECEMBER, 25));
+        tc.add(fixed(SAINT_STEPHEN, Month.DECEMBER, 26));
         tc.add(fixed(NEW_YEARS_EVE, Month.DECEMBER, 31));
         CACHE.put(tc.getCode(), tc);
 
