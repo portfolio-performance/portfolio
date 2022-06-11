0784560ec255ef2a6a82720b8bbbe073519d8bcb
Andreas Buchen <andreas.buchen@gmail.com>
2022-06-11 17:45:55 +0200

    Added option to copy error message to clipboard

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/ExceptionView.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/ExceptionView.java
@@ -8,9 +8,15 @@ import javax.inject.Named;
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.widgets.WidgetFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Text;
 
 import name.abuchen.portfolio.ui.Messages;
@@ -24,7 +30,10 @@ public class ExceptionView extends AbstractFinanceView
     @Override
     protected String getDefaultTitle()
     {
-        return Messages.LabelError;
+        if (exception != null)
+            return exception.getMessage();
+        else
+            return Messages.LabelError;
     }
 
     @Inject
@@ -36,12 +45,24 @@ public class ExceptionView extends AbstractFinanceView
     @Override
     protected Control createBody(Composite parent)
     {
+        String text = createText();
+
         Composite container = new Composite(parent, SWT.None);
         GridDataFactory.fillDefaults().grab(true, true).applyTo(container);
         GridLayoutFactory.fillDefaults().numColumns(1).applyTo(container);
 
+        WidgetFactory.button(SWT.PUSH) //
+                        .text(Messages.LabelCopyToClipboard) //
+                        .font(JFaceResources.getDialogFont()) //
+                        .onSelect(event -> {
+                            Clipboard cb = new Clipboard(Display.getCurrent());
+                            TextTransfer textTransfer = TextTransfer.getInstance();
+                            cb.setContents(new Object[] { "```\n" + text + "\n```" }, // //$NON-NLS-1$ //$NON-NLS-2$
+                                            new Transfer[] { textTransfer });
+                        }).create(container);
+
         Text entryText = new Text(container, SWT.MULTI | SWT.WRAP | SWT.READ_ONLY | SWT.BORDER | SWT.V_SCROLL);
-        entryText.setText(createText());
+        entryText.setText(text);
         GridDataFactory.fillDefaults().grab(true, true).applyTo(entryText);
         return container;
     }
