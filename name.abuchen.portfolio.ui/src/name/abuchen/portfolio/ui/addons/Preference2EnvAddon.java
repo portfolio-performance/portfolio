b962b971db5da6af0dc3e3274cb1de15af27a70c
Egal <31034254+ah4c@users.noreply.github.com>
2022-07-04 20:54:33 +0200

    Added DivvyDiarySearchProvider to use the free DivvyDiary.com Symbols Lookup API to search for a security by ISIN when adding a new investment vehicle. This works in the same way as the existing FinnhubSearchProvider and provides additional results.
    If the user has not configured a DivvyDiary.com API key then the search
    API is not called. This avoids authentication errors for those users.
    The API only accepts valid ISINs for the search API so queries that are
    not exactly 12 characters after trimming spaces are ignored. This avoids
    validation errors.
    
    The DivvyDiary.com Symbols API returns:
    - symbol
    - name
    - WKN
    - ISIN
    - exchange (4-character MIC)
    - currency (3-characters)
    - dividendCurrency (3-characters)
    - dividendFrequency
    
    The symbol does not include an exchange suffix. The
    getExchangeSuffixFromIsoMic() method uses the 4-character MIC exchange
    to get a suitable suffix for the symbol. 60 exchanges are currently
    mapped using https://stockmarketmba.com/globalstockexchanges.php.
    
    The DivvyDiary.com API key is provided to DivvyDiarySearchProvider by
    Preference2EnvAddon in the same setDivvyDiaryApiKey() method where it is
    provided to the existing DivvyDiaryDividendFeed.

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/addons/Preference2EnvAddon.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/addons/Preference2EnvAddon.java
@@ -8,6 +8,7 @@ import org.eclipse.e4.core.di.extensions.Preference;
 import name.abuchen.portfolio.online.Factory;
 import name.abuchen.portfolio.online.impl.AlphavantageQuoteFeed;
 import name.abuchen.portfolio.online.impl.DivvyDiaryDividendFeed;
+import name.abuchen.portfolio.online.impl.DivvyDiarySearchProvider;
 import name.abuchen.portfolio.online.impl.EODHistoricalDataQuoteFeed;
 import name.abuchen.portfolio.online.impl.FinnhubQuoteFeed;
 import name.abuchen.portfolio.online.impl.FinnhubSearchProvider;
@@ -57,6 +58,7 @@ public class Preference2EnvAddon
                     @Preference(value = UIConstants.Preferences.DIVVYDIARY_API_KEY) String divvyDiaryApiKey)
     {
         Factory.getDividendFeed(DivvyDiaryDividendFeed.class).setApiKey(divvyDiaryApiKey);
+        Factory.getSearchProvider(DivvyDiarySearchProvider.class).setApiKey(divvyDiaryApiKey);
     }
 
     @Inject
