b7cb4c119ad898f260394801ac0f857485e263e1
Andreas Buchen <andreas.buchen@gmail.com>
2022-07-17 12:03:22 +0200

    Allow multi-line edits of description widget + limit length of menu entry
    Issue: #2908

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/DescriptionWidget.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/DescriptionWidget.java
@@ -19,6 +19,8 @@ public class DescriptionWidget extends WidgetDelegate<Object>
     public DescriptionWidget(Widget widget, DashboardData data)
     {
         super(widget, data);
+        
+        get(LabelConfig.class).setMultiLine(true);
     }
 
     @Override
