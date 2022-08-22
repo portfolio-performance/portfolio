45a6a70d0bfe9a2f6c3e565369fd234ff8073b93
Andreas Buchen <andreas.buchen@gmail.com>
2022-08-22 16:35:31 +0200

    Upon clicking into a dashboard indicator, display security in info pane

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/AbstractIndicatorWidget.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/AbstractIndicatorWidget.java
@@ -2,15 +2,20 @@ package name.abuchen.portfolio.ui.views.dashboard;
 
 import java.util.function.Predicate;
 
+import javax.inject.Inject;
+
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 
 import name.abuchen.portfolio.model.Dashboard.Widget;
+import name.abuchen.portfolio.model.Security;
 import name.abuchen.portfolio.ui.UIConstants;
+import name.abuchen.portfolio.ui.editor.AbstractFinanceView;
 import name.abuchen.portfolio.ui.util.Colors;
 import name.abuchen.portfolio.ui.util.swt.ColoredLabel;
 import name.abuchen.portfolio.ui.views.dataseries.DataSeries;
@@ -18,6 +23,9 @@ import name.abuchen.portfolio.util.TextUtil;
 
 public abstract class AbstractIndicatorWidget<D> extends WidgetDelegate<D>
 {
+    @Inject
+    protected AbstractFinanceView view;
+
     protected Label title;
     protected ColoredLabel indicator;
 
@@ -48,6 +56,11 @@ public abstract class AbstractIndicatorWidget<D> extends WidgetDelegate<D>
         indicator.setData(UIConstants.CSS.CLASS_NAME, UIConstants.CSS.KPI);
         indicator.setBackground(Colors.theme().defaultBackground());
         indicator.setText(""); //$NON-NLS-1$
+        indicator.addMouseListener(MouseListener.mouseUpAdapter(e -> {
+            DataSeries ds = get(DataSeriesConfig.class).getDataSeries();
+            if (ds.getInstance() instanceof Security)
+                view.setInformationPaneInput(ds.getInstance());
+        }));
         GridDataFactory.fillDefaults().grab(true, false).applyTo(indicator);
 
         return container;
