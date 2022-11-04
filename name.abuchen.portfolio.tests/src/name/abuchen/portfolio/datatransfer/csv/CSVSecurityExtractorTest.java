cc37be09962c2eff7e6602e43689a47522f6c375
Nirus2000 <webmaster@nirus-online.de>
2022-11-04 18:37:45 +0100

    Revision of the test cases CVS security creation
    Revision of the test cases CVS security creation

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/csv/CSVSecurityExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/csv/CSVSecurityExtractorTest.java
@@ -2,16 +2,20 @@ package name.abuchen.portfolio.datatransfer.csv;
 
 import static name.abuchen.portfolio.datatransfer.csv.CSVExtractorTestUtil.buildField2Column;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.Assert.assertNull;
 
+import java.text.MessageFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 
 import org.junit.Test;
 
+import name.abuchen.portfolio.Messages;
 import name.abuchen.portfolio.datatransfer.Extractor.Item;
 import name.abuchen.portfolio.datatransfer.Extractor.SecurityItem;
 import name.abuchen.portfolio.datatransfer.actions.AssertImportActions;
@@ -23,81 +27,274 @@ import name.abuchen.portfolio.money.CurrencyUnit;
 public class CSVSecurityExtractorTest
 {
     @Test
-    public void testSecurityCreation() throws ParseException
+    public void testSecurityCreationWithAllSecurityData() throws ParseException
     {
         Client client = new Client();
 
         CSVExtractor extractor = new CSVSecurityExtractor(client);
 
         List<Exception> errors = new ArrayList<Exception>();
-        List<Item> results = extractor.extract(0,
-                        Arrays.<String[]>asList(
-                                        new String[] { "DE0007164600", "716460", "SAP.DE", "SAP SE", "EUR", "Notiz" }),
-                        buildField2Column(extractor), errors);
+        List<Item> results = extractor.extract(0, Arrays.<String[]>asList( //
+                        new String[] { //
+                                        "DE0007164600", // ISIN
+                                        "716460", // WKN
+                                        "SAP.DE", // TickerSymbol
+                                        "SAP SE", // Security name
+                                        "EUR", // Currency
+                                        "Notiz" // Note
+                        }), buildField2Column(extractor), errors);
 
         assertThat(errors, empty());
         assertThat(results.size(), is(1));
         new AssertImportActions().check(results, CurrencyUnit.EUR);
 
-        Security security = results.stream().filter(i -> i instanceof SecurityItem).findAny().get().getSecurity();
-        assertThat(security.getName(), is("SAP SE"));
+        Security security = results.stream().filter(SecurityItem.class::isInstance).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+
         assertThat(security.getIsin(), is("DE0007164600"));
         assertThat(security.getWkn(), is("716460"));
         assertThat(security.getTickerSymbol(), is("SAP.DE"));
-        assertThat(security.getCurrencyCode(), is("EUR"));
+        assertThat(security.getName(), is("SAP SE"));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
         assertThat(security.getNote(), is("Notiz"));
     }
 
     @Test
-    public void testSecurityIsNotCreatedIfItAlreadyExists() throws ParseException
+    public void testSecurityIsNotCreatedIfItAlreadyExists()
     {
-        Client client = new Client();
-        Security security = new Security();
+        Security security = new Security("SAP SE", CurrencyUnit.EUR);
         security.setIsin("DE0007164600");
+        security.setWkn("716460");
+        security.setTickerSymbol("SAP.DE");
+        security.setNote("Notiz");
+
+        Client client = new Client();
         client.addSecurity(security);
 
         CSVExtractor extractor = new CSVSecurityExtractor(client);
 
         List<Exception> errors = new ArrayList<Exception>();
-        List<Item> results = extractor.extract(0,
-                        Arrays.<String[]>asList(
-                                        new String[] { "DE0007164600", "716460", "SAP.DE", "SAP SE", "EUR", "Notiz" }),
-                        buildField2Column(extractor), errors);
+        List<Item> results = extractor.extract(0, Arrays.<String[]>asList( //
+                        new String[] { //
+                                        "DE0007164600", // ISIN
+                                        "716460", // WKN
+                                        "SAP.DE", // TickerSymbol
+                                        "SAP SE", // Security name
+                                        "EUR", // Currency
+                                        "Notiz" // Note
+                        }), buildField2Column(extractor), errors);
 
         assertThat(errors, empty());
         assertThat(results, empty());
     }
 
     @Test
-    public void testSecurityIsCreatedOnlyOnce() throws ParseException
+    public void testSecurityCreationOnlyWithISIN()
     {
         Client client = new Client();
 
         CSVExtractor extractor = new CSVSecurityExtractor(client);
 
         List<Exception> errors = new ArrayList<Exception>();
-        List<Item> results = extractor.extract(0,
-                        Arrays.<String[]>asList(new String[] { "DE0007164600", "716460", "", "SAP SE", "EUR", "Notiz" },
-                                        new String[] { "DE0007164600", "716460", "SAP.DE", "SAP SE", "EUR", "Notiz" }),
-                        buildField2Column(extractor), errors);
+        List<Item> results = extractor.extract(0, Arrays.<String[]>asList( //
+                        new String[] { //
+                                        "DE0007164600", // ISIN
+                                        "", // WKN
+                                        "", // TickerSymbol
+                                        "", // Security name
+                                        "", // Currency
+                                        "" // Note
+                        }), buildField2Column(extractor), errors);
 
+        assertThat(errors, empty());
         assertThat(results.size(), is(1));
-        assertThat(errors.size(), is(0)); // no warning a/b duplicate imports
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        Security security = results.stream().filter(SecurityItem.class::isInstance).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+
+        assertThat(security.getIsin(), is("DE0007164600"));
+        assertNull(security.getWkn());
+        assertNull(security.getTickerSymbol());
+        assertThat(security.getName(), is(MessageFormat.format(Messages.CSVImportedSecurityLabel, "DE0007164600")));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
+        assertNull(security.getNote());
     }
 
     @Test
-    public void testErrorMessage() throws ParseException
+    public void testSecurityCreationOnlyWithWKN()
     {
         Client client = new Client();
 
         CSVExtractor extractor = new CSVSecurityExtractor(client);
 
         List<Exception> errors = new ArrayList<Exception>();
-        List<Item> results = extractor.extract(0,
-                        Arrays.<String[]>asList(new String[] { "", "", "", "", "EUR", "Notiz" }),
-                        buildField2Column(extractor), errors);
+        List<Item> results = extractor.extract(0, Arrays.<String[]>asList( //
+                        new String[] { //
+                                        "", // ISIN
+                                        "716460", // WKN
+                                        "", // TickerSymbol
+                                        "", // Security name
+                                        "", // Currency
+                                        "" // Note
+                        }), buildField2Column(extractor), errors);
 
-        assertThat(errors.size(), is(1));
+        assertThat(errors, empty());
+        assertThat(results.size(), is(1));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        Security security = results.stream().filter(SecurityItem.class::isInstance).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+
+        assertNull(security.getIsin());
+        assertThat(security.getWkn(), is("716460"));
+        assertNull(security.getTickerSymbol());
+        assertThat(security.getName(), is(MessageFormat.format(Messages.CSVImportedSecurityLabel, "716460")));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
+        assertNull(security.getNote());
+    }
+
+    @Test
+    public void testSecurityCreationOnlyWithTickerSymbol()
+    {
+        Client client = new Client();
+
+        CSVExtractor extractor = new CSVSecurityExtractor(client);
+
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(0, Arrays.<String[]>asList( //
+                        new String[] { //
+                                        "", // ISIN
+                                        "", // WKN
+                                        "SAP.DE", // TickerSymbol
+                                        "", // Security name
+                                        "", // Currency
+                                        "" // Note
+                        }), buildField2Column(extractor), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(1));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        Security security = results.stream().filter(SecurityItem.class::isInstance).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+
+        assertNull(security.getIsin());
+        assertNull(security.getWkn());
+        assertThat(security.getTickerSymbol(), is("SAP.DE"));
+        assertThat(security.getName(), is(MessageFormat.format(Messages.CSVImportedSecurityLabel, "SAP.DE")));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
+        assertNull(security.getNote());
+    }
+
+    @Test
+    public void testSecurityCreationOnlyWithSecurityName()
+    {
+        Client client = new Client();
+
+        CSVExtractor extractor = new CSVSecurityExtractor(client);
+
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(0, Arrays.<String[]>asList( //
+                        new String[] { //
+                                        "", // ISIN
+                                        "", // WKN
+                                        "", // TickerSymbol
+                                        "SAP SE", // Security name
+                                        "", // Currency
+                                        "" // Note
+                        }), buildField2Column(extractor), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(1));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        Security security = results.stream().filter(SecurityItem.class::isInstance).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+
+        assertNull(security.getIsin());
+        assertNull(security.getWkn());
+        assertNull(security.getTickerSymbol());
+        assertThat(security.getName(), is("SAP SE"));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
+        assertNull(security.getNote());
+    }
+
+    @Test
+    public void testSecurityIsCreatedOnlyOnce()
+    {
+        Client client = new Client();
+
+        CSVExtractor extractor = new CSVSecurityExtractor(client);
+
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(0, Arrays.<String[]>asList( //
+                        new String[] { //
+                                        "DE0007164600", // ISIN
+                                        "716460", // WKN
+                                        "SAP.DE", // TickerSymbol
+                                        "SAP SE", // Security name
+                                        "EUR", // Currency
+                                        "Notiz" // Note
+                        }, //
+                        new String[] { //
+                                        "DE0007164600", // ISIN
+                                        "716460", // WKN
+                                        "SAP.DE", // TickerSymbol
+                                        "SAP SE", // Security name
+                                        "EUR", // Currency
+                                        "Notiz" // Note
+                        }), buildField2Column(extractor), errors);
+
+        assertThat(results.size(), is(1));
+        assertThat(errors.size(), is(0)); // no warning a/b duplicate imports
+
+        Security security = results.stream().filter(SecurityItem.class::isInstance).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+
+        assertThat(security.getIsin(), is("DE0007164600"));
+        assertThat(security.getWkn(), is("716460"));
+        assertThat(security.getTickerSymbol(), is("SAP.DE"));
+        assertThat(security.getName(), is("SAP SE"));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
+        assertThat(security.getNote(), is("Notiz"));
+    }
+
+    @Test
+    public void testSecurityCreationWithoutEnoughSecurityData()
+    {
+        Client client = new Client();
+
+        CSVExtractor extractor = new CSVSecurityExtractor(client);
+
+        List<Exception> errors = new ArrayList<Exception>();
+        List<Item> results = extractor.extract(0, Arrays.<String[]>asList( //
+                        new String[] { //
+                                        "", // ISIN
+                                        "", // WKN
+                                        "", // TickerSymbol
+                                        "", // Security name
+                                        "USD", // Currency
+                                        "Notiz" // Note
+                        }), buildField2Column(extractor), errors);
+
         assertThat(results, empty());
+        assertThat(errors.size(), is(1));
+
+        assertThat(errors.get(0).getMessage(), is(MessageFormat.format(Messages.CSVLineXwithMsgY, "1", //
+                        MessageFormat.format(Messages.CSVImportMissingSecurity, //
+                                        new StringJoiner(", ") //
+                                                        .add(Messages.CSVColumn_ISIN) // ISIN
+                                                        .add(Messages.CSVColumn_TickerSymbol) // TickerSymbol
+                                                        .add(Messages.CSVColumn_WKN) // WKN
+                                                        .toString()), //
+                        new ArrayList<>(Arrays.asList( //
+                                        "", // ISIN
+                                        "", // WKN
+                                        "", // TickerSymbol
+                                        "", // Security name
+                                        "USD", // Currency
+                                        "Notiz" // Note
+                        )))));
     }
 }
