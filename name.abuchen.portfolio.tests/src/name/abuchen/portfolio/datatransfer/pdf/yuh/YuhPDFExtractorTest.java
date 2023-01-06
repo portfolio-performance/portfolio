b78b5458042ba7176b79f2735f399d3c0e7b6d5f
Nirus2000 <webmaster@nirus-online.de>
2023-01-06 15:12:08 +0100

    Modify Yuh PDF-Importer to support new transaction
    https://forum.portfolio-performance.info/t/pdf-import-von-yuh/22109/11
    https://forum.portfolio-performance.info/t/pdf-import-von-yuh/22109/13
    https://forum.portfolio-performance.info/t/pdf-import-von-yuh/22109/14

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/yuh/YuhPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/yuh/YuhPDFExtractorTest.java
@@ -77,13 +77,13 @@ public class YuhPDFExtractorTest
     }
 
     @Test
-    public void testDepoteinzahlung01()
+    public void testZahlungsverkehr01()
     {
         YuhPDFExtractor extractor = new YuhPDFExtractor(new Client());
 
         List<Exception> errors = new ArrayList<>();
 
-        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "Depoteinzahlung01.txt"), errors);
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "Zahlungsverkehr01.txt"), errors);
 
         assertThat(errors, empty());
         assertThat(results.size(), is(1));
@@ -100,7 +100,63 @@ public class YuhPDFExtractorTest
         assertThat(transaction.getType(), is(AccountTransaction.Type.DEPOSIT));
         assertThat(transaction.getDateTime(), is(LocalDateTime.parse("2022-10-27T00:00")));
         assertThat(transaction.getMonetaryAmount(), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(1000.00))));
-        assertThat(transaction.getSource(), is("Depoteinzahlung01.txt"));
+        assertThat(transaction.getSource(), is("Zahlungsverkehr01.txt"));
         assertThat(transaction.getNote(), is("Referenz: 312345678"));
+    }
+
+    @Test
+    public void testZahlungsverkehr02()
+    {
+        YuhPDFExtractor extractor = new YuhPDFExtractor(new Client());
+
+        List<Exception> errors = new ArrayList<>();
+
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "Zahlungsverkehr02.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(1));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        // check transaction
+        Iterator<Extractor.Item> iter = results.stream().filter(TransactionItem.class::isInstance).iterator();
+        assertThat(results.stream().filter(TransactionItem.class::isInstance).count(), is(1L));
+
+        Item item = iter.next();
+
+        // assert transaction
+        AccountTransaction transaction = (AccountTransaction) item.getSubject();
+        assertThat(transaction.getType(), is(AccountTransaction.Type.DEPOSIT));
+        assertThat(transaction.getDateTime(), is(LocalDateTime.parse("2022-12-27T00:00")));
+        assertThat(transaction.getMonetaryAmount(), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(250.00))));
+        assertThat(transaction.getSource(), is("Zahlungsverkehr02.txt"));
+        assertThat(transaction.getNote(), is("Referenz: 123456789"));
+    }
+
+    @Test
+    public void testZahlungsverkehr03()
+    {
+        YuhPDFExtractor extractor = new YuhPDFExtractor(new Client());
+
+        List<Exception> errors = new ArrayList<>();
+
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "Zahlungsverkehr03.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(1));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        // check transaction
+        Iterator<Extractor.Item> iter = results.stream().filter(TransactionItem.class::isInstance).iterator();
+        assertThat(results.stream().filter(TransactionItem.class::isInstance).count(), is(1L));
+
+        Item item = iter.next();
+
+        // assert transaction
+        AccountTransaction transaction = (AccountTransaction) item.getSubject();
+        assertThat(transaction.getType(), is(AccountTransaction.Type.REMOVAL));
+        assertThat(transaction.getDateTime(), is(LocalDateTime.parse("2022-12-04T00:00")));
+        assertThat(transaction.getMonetaryAmount(), is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(11.00))));
+        assertThat(transaction.getSource(), is("Zahlungsverkehr03.txt"));
+        assertThat(transaction.getNote(), is("Referenz: 123456789"));
     }
 }
