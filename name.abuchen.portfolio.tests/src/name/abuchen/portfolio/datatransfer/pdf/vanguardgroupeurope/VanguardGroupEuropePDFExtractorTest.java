c2d61ddf9b5c3893b7a7ad67904aefe35a710911
Nirus2000 <webmaster@nirus-online.de>
2023-02-05 13:45:43 +0100

    Add new Vanguard PDF-Importer
    https://forum.portfolio-performance.info/t/pdf-import-von-vanguard/23084

---
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/vanguardgroupeurope/VanguardGroupEuropePDFExtractorTest.java
@@ -0,0 +1,75 @@
+package name.abuchen.portfolio.datatransfer.pdf.vanguardgroupeurope;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import name.abuchen.portfolio.datatransfer.Extractor.BuySellEntryItem;
+import name.abuchen.portfolio.datatransfer.Extractor.Item;
+import name.abuchen.portfolio.datatransfer.Extractor.SecurityItem;
+import name.abuchen.portfolio.datatransfer.actions.AssertImportActions;
+import name.abuchen.portfolio.datatransfer.pdf.PDFInputFile;
+import name.abuchen.portfolio.datatransfer.pdf.VanguardGroupEuropePDFExtractor;
+import name.abuchen.portfolio.model.AccountTransaction;
+import name.abuchen.portfolio.model.BuySellEntry;
+import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.model.PortfolioTransaction;
+import name.abuchen.portfolio.model.Security;
+import name.abuchen.portfolio.model.Transaction.Unit;
+import name.abuchen.portfolio.money.CurrencyUnit;
+import name.abuchen.portfolio.money.Money;
+import name.abuchen.portfolio.money.Values;
+
+@SuppressWarnings("nls")
+public class VanguardGroupEuropePDFExtractorTest
+{
+    @Test
+    public void testWertpapierKauf01()
+    {
+        Client client = new Client();
+
+        VanguardGroupEuropePDFExtractor extractor = new VanguardGroupEuropePDFExtractor(client);
+
+        List<Exception> errors = new ArrayList<>();
+
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "Kauf01.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(results.size(), is(2));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        // check security
+        Security security = results.stream().filter(SecurityItem.class::isInstance).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSecurity();
+        assertThat(security.getIsin(), is("IE00BMVB5Q68"));
+        assertThat(security.getName(), is("FDBL Vanguard LifeStrategy 60% Equity UCITS ETF (EUR) Distributing"));
+        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
+
+        // check buy sell transaction
+        BuySellEntry entry = (BuySellEntry) results.stream().filter(BuySellEntryItem.class::isInstance).findFirst()
+                        .orElseThrow(IllegalArgumentException::new).getSubject();
+
+        assertThat(entry.getPortfolioTransaction().getType(), is(PortfolioTransaction.Type.BUY));
+        assertThat(entry.getAccountTransaction().getType(), is(AccountTransaction.Type.BUY));
+
+        assertThat(entry.getPortfolioTransaction().getDateTime(), is(LocalDateTime.parse("2023-02-03T11:11:17")));
+        assertThat(entry.getPortfolioTransaction().getShares(), is(Values.Share.factorize(11.956954)));
+        assertThat(entry.getSource(), is("Kauf01.txt"));
+        assertThat(entry.getNote(), is("Referenznummer 12345678"));
+
+        assertThat(entry.getPortfolioTransaction().getMonetaryAmount(),
+                        is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(300.00))));
+        assertThat(entry.getPortfolioTransaction().getGrossValue(),
+                        is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(300.00))));
+        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.TAX),
+                        is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0.00))));
+        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE),
+                        is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0.00))));
+    }
+}
---
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/VanguardGroupEuropePDFExtractor.java
@@ -0,0 +1,92 @@
+package name.abuchen.portfolio.datatransfer.pdf;
+
+import static name.abuchen.portfolio.util.TextUtil.trim;
+
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
+import name.abuchen.portfolio.model.BuySellEntry;
+import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.model.PortfolioTransaction;
+
+@SuppressWarnings("nls")
+public class VanguardGroupEuropePDFExtractor extends AbstractPDFExtractor
+{
+    public VanguardGroupEuropePDFExtractor(Client client)
+    {
+        super(client);
+
+        addBankIdentifier("Vanguard Group Europe GmbH"); //$NON-NLS-1$^
+
+        addBuySellTransaction();
+    }
+
+    @Override
+    public String getLabel()
+    {
+        return "Vanguard Group Europe GmbH"; //$NON-NLS-1$
+    }
+
+    private void addBuySellTransaction()
+    {
+        DocumentType type = new DocumentType("Wertpapierabrechnung: Kauf");
+        this.addDocumentTyp(type);
+
+        Transaction<BuySellEntry> pdfTransaction = new Transaction<>();
+        pdfTransaction.subject(() -> {
+            BuySellEntry entry = new BuySellEntry();
+            entry.setType(PortfolioTransaction.Type.BUY);
+            return entry;
+        });
+
+        Block firstRelevantLine = new Block("^Wertpapierabrechnung: Kauf$");
+        type.addBlock(firstRelevantLine);
+        firstRelevantLine.set(pdfTransaction);
+
+        pdfTransaction
+                // Wertpapierbezeichnung FDBL Vanguard LifeStrategy 60% Equity UCITS ETF
+                // (EUR) Distributing
+                // ISIN IE00BMVB5Q68
+                // Kurs (Stückpreis) EUR 25,0900
+                .section("name", "name1", "isin", "currency")
+                .match("^Wertpapierbezeichnung (?<name>.*)")
+                .match("^(?<name1>.*)$")
+                .match("^ISIN (?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9])$")
+                .match("^Kurs \\(.*\\) (?<currency>[\\w]{3}) [\\.,\\d]+$")
+                .assign((t, v) -> {
+                    if (!v.get("name1").startsWith("ISIN"))
+                        v.put("name", v.get("name") + " " + v.get("name1"));
+
+                    t.setSecurity(getOrCreateSecurity(v));
+                })
+
+                // Nominal / Stück 11,956954
+                .section("shares")
+                .match("^Nominal \\/ St.ck (?<shares>[\\.,\\d]+)$")
+                .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
+
+                // Ausführungstag / -zeit 03.02.2023 um 11:11:17 Uhr
+                .section("date", "time")
+                .match("^Ausf.hrungstag \\/ \\-zeit (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) .* (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2}).*$")
+                .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
+
+                // Kurswert EUR 300,00
+                // EUR 300,00
+                // Abrechnungskonto DE01234567891234567890
+                .section("currency", "amount")
+                .find("Kurswert [\\w]{3} [\\.,\\d]+$")
+                .match("^(?<currency>[\\w]{3}) (?<amount>[\\.,\\d]+)$")
+                .match("^Abrechnungskonto .*$")
+                .assign((t, v) -> {
+                    t.setAmount(asAmount(v.get("amount")));
+                    t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                })
+
+                // Referenznummer 12345678
+                .section("note").optional()
+                .match("^(?<note>Referenznummer .*)$")
+                .assign((t, v) -> t.setNote(trim(v.get("note"))))
+
+                .wrap(BuySellEntryItem::new);
+    }
+}
