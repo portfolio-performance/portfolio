6965156c59872b2839c3c6536587a46261d1c3c5
OnkelDok <90478568+OnkelDok@users.noreply.github.com>
2023-02-20 21:31:21 +0100

    fixes wrong dates when exporting transactions as CSV
    https://forum.portfolio-performance.info/t/wrong-dates-when-exporting-security-transactions-as-csv/21226/10
    not visible table items get wrong datetime when exporting the table
    after re-order

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/TableViewerCSVExporter.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/TableViewerCSVExporter.java
@@ -94,8 +94,9 @@ public class TableViewerCSVExporter extends AbstractCSVExporter
             {
                 // if the table is tagged SWT.VIRTUAL, then the item is only
                 // resolved if the text (or other properties are requested)
-                Object subject = item.getData();
-                if (subject == null)
+                // Also for invisible items the getText sometimes returns "" so
+                // in case of blank string, additional call of getText required
+                if (item.getData() == null || item.getText().isEmpty())
                     item.getText();
 
                 for (int ii = 0; ii < columnCount; ii++)
