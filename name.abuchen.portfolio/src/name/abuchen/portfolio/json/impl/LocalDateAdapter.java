5820f2a268cb9dab07254e44f5e20b04a2b753fe
Andreas Buchen <andreas.buchen@gmail.com>
2023-02-25 09:56:51 +0100

    Fixed market rates not being parsed if JSON was written in old format

--- name.abuchen.portfolio/src/name/abuchen/portfolio/json/impl/LocalDateAdapter.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/json/impl/LocalDateAdapter.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 public class LocalDateAdapter extends TypeAdapter<LocalDate>
@@ -18,6 +19,33 @@ public class LocalDateAdapter extends TypeAdapter<LocalDate>
     @Override
     public LocalDate read(final JsonReader jsonReader) throws IOException
     {
-        return LocalDate.parse(jsonReader.nextString());
+        // previous versions have written the JSON as
+        // <pre>{"year":2021,"month":8,"day":12}</pre>
+
+        if (jsonReader.peek() == JsonToken.BEGIN_OBJECT)
+        {
+            int year = 0;
+            int month = 0;
+            int day = 0;
+            jsonReader.beginObject();
+            while (jsonReader.hasNext())
+            {
+                String name = jsonReader.nextName();
+                if ("year".equals(name)) //$NON-NLS-1$
+                    year = jsonReader.nextInt();
+                else if ("month".equals(name)) //$NON-NLS-1$
+                    month = jsonReader.nextInt();
+                else if ("day".equals(name)) //$NON-NLS-1$
+                    day = jsonReader.nextInt();
+                else
+                    jsonReader.skipValue();
+            }
+            jsonReader.endObject();
+            return LocalDate.of(year, month, day);
+        }
+        else
+        {
+            return LocalDate.parse(jsonReader.nextString());
+        }
     }
 }
