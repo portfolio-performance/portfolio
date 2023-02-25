4cb0be62bb11fe08bca859a323e8244b79f4c2c8
Andreas Buchen <andreas.buchen@gmail.com>
2023-02-25 10:03:45 +0100

    Added toMutableList collector to mark the need for a mutable list
    It's useful in cases where we need to modify the resulting list, which
    is not possible with the immutable list returned by the Stream#toList
    method. We use this method to differentiate between cases where a
    mutable list is necessary and to prevent the IDE from suggesting the use
    of Stream#toList.
    
    Issue: #3197

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/DashboardView.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/DashboardView.java
@@ -1,5 +1,7 @@
 package name.abuchen.portfolio.ui.views.dashboard;
 
+import static name.abuchen.portfolio.util.CollectorsUtil.toMutableList;
+
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,7 +13,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -879,7 +880,7 @@ public class DashboardView extends AbstractHistoricView
         Dashboard.Column newColumn = new Dashboard.Column();
         dashboard.getColumns().add(index, newColumn);
 
-        newColumn.setWidgets(column.getWidgets().stream().map(Widget::copy).collect(Collectors.toList()));
+        newColumn.setWidgets(column.getWidgets().stream().map(Widget::copy).collect(toMutableList()));
 
         getClient().touch();
 
