5a6fdb4eddd5b48bdb946d469e5fe654455ad3b4
Hasakirii <96695600+Hasakirii@users.noreply.github.com>
2023-04-27 15:15:28 +0200

    Added MyDividends24Uploader
    - Fixed some issues with the Upload
    - Added the new file form Nirus2000

--- name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/MyDividends24Uploader.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/online/impl/MyDividends24Uploader.java
@@ -2,10 +2,11 @@ package name.abuchen.portfolio.online.impl;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -13,96 +14,73 @@ import org.json.simple.JSONValue;
 import org.osgi.framework.FrameworkUtil;
 
 import name.abuchen.portfolio.model.Client;
-import name.abuchen.portfolio.model.PortfolioTransaction;
 import name.abuchen.portfolio.money.CurrencyConverter;
 import name.abuchen.portfolio.snapshot.ClientSnapshot;
 import name.abuchen.portfolio.snapshot.PortfolioSnapshot;
 import name.abuchen.portfolio.util.Pair;
 import name.abuchen.portfolio.util.WebAccess;
 
-public class MyDividends24Uploader
-{
-    private String apiKey;
-
-    public MyDividends24Uploader(String apiKey)
+    public class MyDividends24Uploader 
     {
-        this.apiKey = Objects.requireNonNull(apiKey);
-    }
+     
+        private final String apiKey;
 
-    public List<Pair<Integer, String>> getPortfolios() throws IOException
-    {
-        List<Pair<Integer, String>> answer = new ArrayList<>();
-
-        String response = new WebAccess("dividend-jsa-pp-bnp8.vercel.app", "/api/import/retrieve-depots") //$NON-NLS-1$ //$NON-NLS-2$
-                        .addHeader("Authorization", "Bearer: " + apiKey)//$NON-NLS-1$ //$NON-NLS-2$
-                        .addUserAgent("PortfolioPerformance/" //$NON-NLS-1$
-                                        + FrameworkUtil.getBundle(PortfolioReportNet.class).getVersion().toString())
-                        .get();
-
-        JSONObject session = (JSONObject) JSONValue.parse(response);
-        JSONArray portfolios = (JSONArray) session.get("depots"); //$NON-NLS-1$
-
-        if (portfolios != null)
-        {
-            int i = 0;
-            for (Object p : portfolios)
-            {
-                String portfolio = (String) p;
-                answer.add(new Pair<>(i, portfolio));
-            }
+        public MyDividends24Uploader(String apiKey) {
+            this.apiKey = Objects.requireNonNull(apiKey, "MyDividende24.de ApiKey must not be null"); //$NON-NLS-1$
         }
 
-        return answer;
-    }
+        public List<Pair<Integer, String>> getPortfolios() throws IOException {
+            String response = new WebAccess("dividend-jsa-pp-bnp8.vercel.app", "/api/import/retrieve-depots")  //$NON-NLS-1$//$NON-NLS-2$
+                    .addHeader("Authorization", "Bearer: " + apiKey).addUserAgent("PortfolioPerformance/"  //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
+                            + FrameworkUtil.getBundle(PortfolioReportNet.class).getVersion().toString())
+                    .get();
 
-    @SuppressWarnings("unchecked")
-    public void upload(Client client, CurrencyConverter converter, String portfolioId) throws IOException
-    {
-        ClientSnapshot snapshot = ClientSnapshot.create(client, converter, LocalDate.now());
-        PortfolioSnapshot portfolio = snapshot.getJointPortfolio();
+            // Parses the response from the API endpoint into a JSONObject
+            JSONObject session = (JSONObject) JSONValue.parse(response);
 
-        List<JSONObject> resultTransactions = new ArrayList<>();
+            // Retrieves the list of portfolios from the JSONObject
+            @SuppressWarnings("unchecked")
+            JSONArray portfolios = (JSONArray) session.getOrDefault("depots", Collections.emptyList()); //$NON-NLS-1$
 
-        List<PortfolioTransaction> transactions = portfolio.getPortfolio().getTransactions().stream()
-                        .collect(Collectors.toList());
+            // Maps each portfolio to a pair of its index and its name and returns the list
+            // of pairs.
+            return IntStream.range(0, portfolios.size()).mapToObj(i -> new Pair<>(i, (String) portfolios.get(i)))
+                    .collect(Collectors.toList());
+        }
 
-        transactions.forEach(item -> {
-            double quantity = item.getShares() / 100000000;
-            double buyingprice = item.getGrossValueAmount() / 100 / quantity;
-            String purchasedate = item.getDateTime().toString();
-            String isin = item.getSecurity().getIsin();
-            String type = "buy"; //$NON-NLS-1$
-            if (!item.getType().isPurchase())
-            {
-                type = "sell"; //$NON-NLS-1$
+        @SuppressWarnings("unchecked")
+        public void upload(Client client, CurrencyConverter converter, String portfolioId) throws IOException {
+            ClientSnapshot snapshot = ClientSnapshot.create(client, converter, LocalDate.now());
+            PortfolioSnapshot portfolio = snapshot.getJointPortfolio();
+
+            // Filters out any transactions that do not have an ISIN.
+            List<JSONObject> resultTransactions = portfolio.getPortfolio().getTransactions().stream()
+                    .filter(item -> item.getSecurity().getIsin() != null).map(item -> {
+                        double quantity = item.getShares() / 100000000;
+                        double buyingprice = item.getGrossValueAmount() / 100 / quantity;
+                        String purchasedate = item.getDateTime().toString();
+                        String isin = item.getSecurity().getIsin();
+                        String type = item.getType().isPurchase() ? "buy" : "sell"; //$NON-NLS-1$ //$NON-NLS-2$
+
+                        // Creates a JSONObject for each transaction with the calculated data.
+                        JSONObject jsonObject = new JSONObject();
+                        jsonObject.put("type", type); //$NON-NLS-1$
+                        jsonObject.put("quantity", quantity); //$NON-NLS-1$
+                        jsonObject.put("stockprice", buyingprice); //$NON-NLS-1$
+                        jsonObject.put("date", purchasedate); //$NON-NLS-1$
+                        jsonObject.put("isin", isin); //$NON-NLS-1$
+                        return jsonObject;
+                    }).collect(Collectors.toList());
+
+            if (resultTransactions.isEmpty()) {
+                return;
             }
 
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("type", type); //$NON-NLS-1$
-            jsonObject.put("quantity", quantity); //$NON-NLS-1$
-            jsonObject.put("stockprice", buyingprice); //$NON-NLS-1$
-            jsonObject.put("date", purchasedate); //$NON-NLS-1$
-            jsonObject.put("isin", isin); //$NON-NLS-1$
-
-            if (isin != null)
-            {
-                resultTransactions.add(jsonObject);
-            }
-        });
-
-        if (resultTransactions.isEmpty())
-            return;
-
-        WebAccess upload = new WebAccess("dividend-jsa-pp-bnp8.vercel.app", "/api/import/import"); //$NON-NLS-1$ //$NON-NLS-2$
-        upload.addHeader("Authorization", "Bearer: " + apiKey); //$NON-NLS-1$ //$NON-NLS-2$
-        upload.addHeader("Content-Type", "application/json"); //$NON-NLS-1$ //$NON-NLS-2$
-
-        JSONObject uploadData = new JSONObject();
-        uploadData.put("transactions", resultTransactions); //$NON-NLS-1$
-        uploadData.put("depot", portfolioId); //$NON-NLS-1$
-
-        upload.post(JSONValue.toJSONString(uploadData));
-
+            JSONObject uploadData = new JSONObject();
+            uploadData.put("transactions", resultTransactions); //$NON-NLS-1$
+            uploadData.put("depot", portfolioId); //$NON-NLS-1$
+            new WebAccess("dividend-jsa-pp-bnp8.vercel.app", "/api/import/import") //$NON-NLS-1$ //$NON-NLS-2$
+                    .addHeader("Authorization", "Bearer: " + apiKey).addHeader("Content-Type", "application/json")  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+                    .post(JSONValue.toJSONString(uploadData));
+        }
     }
-
-}
