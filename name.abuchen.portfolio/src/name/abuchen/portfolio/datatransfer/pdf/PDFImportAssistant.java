287f59b827bb22af796a28afb66826ffd971d237
Nirus2000 <webmaster@nirus-online.de>
2023-04-30 09:29:28 +0200

    Add new StarMoney PDF-Importer
    Add new StarMoney PDF-Importer

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
@@ -81,6 +81,7 @@ public class PDFImportAssistant
         extractors.add(new SelfWealthPDFExtractor(client));
         extractors.add(new SimpelPDFExtractor(client));
         extractors.add(new SolarisbankAGPDFExtractor(client));
+        extractors.add(new StarMoneyPDFExtractor(client));
         extractors.add(new SuresseDirektBankPDFExtractor(client));
         extractors.add(new SwissquotePDFExtractor(client));
         extractors.add(new TargobankPDFExtractor(client));
