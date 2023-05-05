84a1a6d5cf400f4604e8982210ef5503997b4b4c
Oliver Fueckert <oliver@cubinet.de>
2023-05-05 20:36:23 +0200

    Refactor to project requirements

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/merkurprivatbank/MerkurPrivatBankPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/merkurprivatbank/MerkurPrivatBankPDFExtractorTest.java
@@ -14,14 +14,9 @@ import org.junit.Test;
 import name.abuchen.portfolio.datatransfer.Extractor.BuySellEntryItem;
 import name.abuchen.portfolio.datatransfer.Extractor.Item;
 import name.abuchen.portfolio.datatransfer.Extractor.SecurityItem;
-import name.abuchen.portfolio.datatransfer.Extractor.TransactionItem;
-import name.abuchen.portfolio.datatransfer.ImportAction.Status;
 import name.abuchen.portfolio.datatransfer.actions.AssertImportActions;
-import name.abuchen.portfolio.datatransfer.actions.CheckCurrenciesAction;
-import name.abuchen.portfolio.datatransfer.pdf.DZBankGruppePDFExtractor;
+import name.abuchen.portfolio.datatransfer.pdf.MerkurPrivatBankPDFExtractor;
 import name.abuchen.portfolio.datatransfer.pdf.PDFInputFile;
-import name.abuchen.portfolio.model.Account;
-import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.BuySellEntry;
 import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.PortfolioTransaction;
@@ -40,39 +35,55 @@ public class MerkurPrivatBankPDFExtractorTest
         MerkurPrivatBankPDFExtractor extractor = new MerkurPrivatBankPDFExtractor(new Client());
 
         List<Exception> errors = new ArrayList<>();
-
         List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "Kauf01.txt"), errors);
 
         assertThat(errors, empty());
+        assertThat(extractor.getLabel(), is("MERKUR PRIVATBANK KGaA"));
         assertThat(results.size(), is(2));
+        
         new AssertImportActions().check(results, CurrencyUnit.EUR);
 
-     // check security
+        // Security identification:
+        // Stück 125,3258 XTR.(IE) - MSCI WORLD IE00BJ0KDQ92
+        // (A1XB5U)
+        // REGISTERED SHARES 1C O.N.
         Security security = results.stream().filter(SecurityItem.class::isInstance).findFirst()
                         .orElseThrow(IllegalArgumentException::new).getSecurity();
         assertThat(security.getIsin(), is("IE00BJ0KDQ92"));
         assertThat(security.getWkn(), is("A1XB5U"));
         assertNull(security.getTickerSymbol());
         assertThat(security.getName(), is("XTR.(IE) - MSCI WORLD REGISTERED SHARES 1C O.N."));
-        assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
+        // assertThat(security.getCurrencyCode(), is(CurrencyUnit.EUR));
 
         // check buy transaction
         BuySellEntry entry = (BuySellEntry) results.stream().filter(BuySellEntryItem.class::isInstance).findFirst()
                         .orElseThrow(IllegalArgumentException::new).getSubject();
-
-        assertThat(entry.getPortfolioTransaction().getType(), is(PortfolioTransaction.Type.BUY));
-        assertThat(entry.getAccountTransaction().getType(), is(AccountTransaction.Type.BUY));
-
-        assertThat(entry.getPortfolioTransaction().getDateTime(), is(LocalDateTime.parse("2023-05-02T09:34:40")));
-        assertThat(entry.getPortfolioTransaction().getShares(), is(Values.Share.factorize(125.3258)));
+        
+        // Test document name
         assertThat(entry.getSource(), is("Kauf01.txt"));
-
+        
+        assertThat(entry.getPortfolioTransaction().getType(), is(PortfolioTransaction.Type.BUY));
+        
+        // Date and time
+        // Schlusstag/-Zeit 02.05.2023 09:34:40
+        assertThat(entry.getPortfolioTransaction().getDateTime(), is(LocalDateTime.parse("2023-05-02T09:34:40")));
+        
+        // Shares of the transaction
+        // Stück 125,3258
+        assertThat(entry.getPortfolioTransaction().getShares(), is(Values.Share.factorize(125.3258)));
+        
+        // Total amount (With fees and taxes)
+        // Kurswert 10.000,00- EUR
+        // Provision 2,50- EUR
+        // Ausmachender Betrag 10.002,50- EUR
         assertThat(entry.getPortfolioTransaction().getMonetaryAmount(),
                         is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(10002.50))));
         assertThat(entry.getPortfolioTransaction().getGrossValue(),
                         is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(10000.00))));
+        // No tax on doc
         assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.TAX),
                         is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(0.00))));
+        // Provision 2,50- EUR
         assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE),
                         is(Money.of(CurrencyUnit.EUR, Values.Amount.factorize(2.50))));
     }
--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/MerkurPrivatBankPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/MerkurPrivatBankPDFExtractor.java
@@ -1,59 +1,25 @@
 package name.abuchen.portfolio.datatransfer.pdf;
 
-import static name.abuchen.portfolio.datatransfer.ExtractorUtils.checkAndSetFee;
-import static name.abuchen.portfolio.datatransfer.ExtractorUtils.checkAndSetGrossUnit;
-import static name.abuchen.portfolio.util.TextUtil.stripBlanks;
 import static name.abuchen.portfolio.util.TextUtil.trim;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import name.abuchen.portfolio.datatransfer.DocumentContext;
-import name.abuchen.portfolio.datatransfer.ExtrExchangeRate;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
-import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.BuySellEntry;
 import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.PortfolioTransaction;
-import name.abuchen.portfolio.money.CurrencyUnit;
-import name.abuchen.portfolio.money.Money;
-import name.abuchen.portfolio.money.Values;
 
 @SuppressWarnings("nls")
 public class MerkurPrivatBankPDFExtractor extends AbstractPDFExtractor
 {
-    private static final String isJointAccount = "isJointAccount"; //$NON-NLS-1$
-
-    BiConsumer<DocumentContext, String[]> jointAccount = (context, lines) -> {
-        Pattern pJointAccount = Pattern.compile("Anteilige Berechnungsgrundlage .* \\(50,00 %\\).*"); //$NON-NLS-1$
-        Boolean bJointAccount = false;
-
-        for (String line : lines)
-        {
-            Matcher m = pJointAccount.matcher(line);
-            if (m.matches())
-            {
-                context.put(isJointAccount, Boolean.TRUE.toString());
-                bJointAccount = true;
-                break;
-            }
-        }
-
-        if (!bJointAccount)
-            context.put(isJointAccount, Boolean.FALSE.toString());
-    };
 
     public MerkurPrivatBankPDFExtractor(Client client)
     {
         super(client);
-
+        
+        // @formatter:off
+        // Am Marktplatz 10 · 97762 Hammelburg
+        // @formatter:on
         addBankIdentifier("Am Marktplatz 10 · 97762 Hammelburg"); //$NON-NLS-1$
 
         addBuySellTransaction();
@@ -67,7 +33,7 @@ public class MerkurPrivatBankPDFExtractor extends AbstractPDFExtractor
 
     private void addBuySellTransaction()
     {
-        DocumentType type = new DocumentType("Wertpapier Abrechnung (Kauf|Verkauf)", jointAccount);
+        DocumentType type = new DocumentType("Kauf");
         this.addDocumentTyp(type);
 
         Transaction<BuySellEntry> pdfTransaction = new Transaction<>();
@@ -76,181 +42,74 @@ public class MerkurPrivatBankPDFExtractor extends AbstractPDFExtractor
             entry.setType(PortfolioTransaction.Type.BUY);
             return entry;
         });
-
-        // Handshake for tax refund transaction
-        Map<String, String> context = type.getCurrentContext();
-
-        Block firstRelevantLine = new Block("^Wertpapier Abrechnung (Kauf|Verkauf).*$");
+        
+        // Start from: Wertpapier Abrechnung Kauf
+        Block firstRelevantLine = new Block("^Wertpapier Abrechnung Kauf.*$");
         type.addBlock(firstRelevantLine);
         firstRelevantLine.set(pdfTransaction);
-
+        
         pdfTransaction
-                .section("type").optional()
-                .match("^Wertpapier Abrechnung (?<type>(Kauf|Verkauf)).*$")
-                .assign((t, v) -> {
-                    if (v.get("type").equals("Verkauf"))
-                        t.setType(PortfolioTransaction.Type.SELL);
-                })
+                        // @formatter:off
+                        // Type (Optional)
+                        // Wertpapier Abrechnung Kauf
+                        // @formatter:on
+                        .section("type").optional().match("^Wertpapier Abrechnung (?<type>Kauf).*$").assign((t, v) -> {
+                            if (v.get("type").equals("Kauf"))
+                                t.setType(PortfolioTransaction.Type.BUY);
+                        })
+                        // @formatter:off
+                        // Security identification:
+                        // Stück 125,3258 XTR.(IE) - MSCI WORLD              IE00BJ0KDQ92 (A1XB5U)
+                        // REGISTERED SHARES 1C O.N.
+                        // @formatter:on
+                        .section("name", "ln2", "isin", "wkn")
+                        .match("^St.ck [\\,\\d]+ (?<name>.*) (?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9]) \\((?<wkn>[A-Z0-9]{6})\\)$")
+                        .match("^(?<ln2>.*)$").assign((t, v) -> {
+                            // Assemble name from both lines
+                            if (!v.get("ln2").startsWith("Handels-/Ausführungsplatz"))
+                                v.put("name", trim(v.get("name")) + " " + trim(v.get("ln2")));
+                            t.setSecurity(getOrCreateSecurity(v));
+                        })
+                        // @formatter:off
+                        // Shares of the transaction
+                        // Stück 125,3258
+                        // @formatter:on
+                        .section("shares").match("^St.ck (?<shares>[\\,\\d]+) .*$").assign((t, v) -> 
+                            t.setShares(asShares(v.get("shares")))
+                        )
+                        // @formatter:off
+                        // Date and time
+                        // Schlusstag/-Zeit 02.05.2023 09:34:40
+                        // @formatter:on
+                        .section("date", "time")
+                        .match("^Schlusstag\\/\\-Zeit (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2}) .*$")
+                        .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
 
-                .section("name", "isin", "wkn", "name1", "currency")
-                .match("^St.ck [\\.,\\d]+ (?<name>.*) (?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9]) \\((?<wkn>[A-Z0-9]{6})\\)$")
-                .match("^(?<name1>.*)$")
-                .match("^Kurswert [\\.,\\d]+(\\-)? (?<currency>[\\w]{3})$")
-                .assign((t, v) -> {
-                    if (!v.get("name1").startsWith("Handels-/Ausführungsplatz"))
-                        v.put("name", trim(v.get("name")) + " " + trim(v.get("name1")));
-
-                    t.setSecurity(getOrCreateSecurity(v));
-
-                    context.put("name", v.get("name"));
-                    context.put("isin", v.get("isin"));
-                    context.put("wkn", v.get("wkn"));
-                })
-
-                .section("shares")
-                .match("^St.ck (?<shares>[\\.,\\d]+) .*$")
-                .assign((t, v) -> {
-                    t.setShares(asShares(v.get("shares")));
-
-                    // Handshake, if there is a tax refund
-                    context.put("shares", v.get("shares"));
-                })
-
-                .section("date", "time")
-                .match("^Schlusstag\\/\\-Zeit (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2}) .*$")
-                .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
-
-                .section("amount", "currency")
-                .match("^Ausmachender Betrag (?<amount>[\\.,\\d]+)([\\-|\\+])? (?<currency>[\\w]{3})$")
-                .assign((t, v) -> {
-                    t.setAmount(asAmount(v.get("amount")));
-                    t.setCurrencyCode(asCurrencyCode(v.get("currency")));
-                })
-
-                .section("note").optional()
-                .match("^(?<note>(Limit|Stoplimit) .*)$")
-                .assign((t, v) -> t.setNote(trim(v.get("note"))))
-
-                .wrap(BuySellEntryItem::new);
+                        // @formatter:off
+                        // Total amount (With fees and taxes)
+                        // Ausmachender Betrag 10.002,50- EUR
+                        // @formatter:on
+                        .section("amount", "totalcurrency")
+                        .match("^Ausmachender Betrag (?<amount>[\\.,\\d]+)([\\-|\\+])? (?<totalcurrency>[\\w]{3})$")
+                        .assign((t, v) -> {
+                            t.setAmount(asAmount(v.get("amount")));
+                            t.setCurrencyCode(asCurrencyCode(v.get("totalcurrency")));
+                        }).wrap(BuySellEntryItem::new);
 
         addFeesSectionsTransaction(pdfTransaction, type);
+
     }
 
     private <T extends Transaction<?>> void addFeesSectionsTransaction(T transaction, DocumentType type)
     {
         transaction
-                .section("fee", "currency").optional()
-                .match("^Provision [\\.,\\d]+ % .* (?<fee>[\\.,\\d]+)\\- (?<currency>[\\w]{3})$")
-                .assign((t, v) -> processFeeEntries(t, v, type))
+                        // @formatter:off
+                        // Fee section
+                        // Provision 2,50- EUR
+                        // @formatter:on
+                        .section("fee", "currency").optional()
+                        .match("^Provision (?<fee>[\\.,\\d]+)\\- (?<currency>[\\w]{3})$")
+                        .assign((t, v) -> processFeeEntries(t, v, type));
 
-                .section("fee", "currency").optional()
-                .match("^Provision (?<fee>[\\.,\\d]+)\\- (?<currency>[\\w]{3})$")
-                .assign((t, v) -> processFeeEntries(t, v, type))
-
-                .section("fee", "currency").optional()
-                .match("^Transaktionsentgelt B.rse (?<fee>[\\.,\\d]+)\\- (?<currency>[\\w]{3})$")
-                .assign((t, v) -> processFeeEntries(t, v, type))
-
-                .section("fee", "currency").optional()
-                .match("^.bertragungs-\\/Liefergeb.hr (?<fee>[\\.,\\d]+)\\- (?<currency>[\\w]{3})$")
-                .assign((t, v) -> processFeeEntries(t, v, type))
-
-                .section("fee", "currency").optional()
-                .match("^Eigene Spesen (?<fee>[\\.,\\d]+)\\- (?<currency>[\\w]{3})$")
-                .assign((t, v) -> processFeeEntries(t, v, type))
-
-                .section("amount", "percentageFee").optional()
-                .match("^Anlage (?<amount>[\\.,\\d]+) (?<percentageFee>[\\.,\\d]+) [\\.,\\d]+ [\\.,\\d]+")
-                .assign((t, v) -> {
-                    Map<String, String> context = type.getCurrentContext();
-                    v.put("currency", asCurrencyCode(context.get("baseCurrency")));
-
-                    BigDecimal percentageFee = asBigDecimal(v.get("percentageFee"));
-                    BigDecimal amount = asBigDecimal(v.get("amount"));
-
-                    if (percentageFee.compareTo(BigDecimal.ZERO) != 0)
-                    {
-                        // fxFee = (amount / (1 + percentageFee / 100)) * (percentageFee / 100)
-                        BigDecimal fxFee = amount
-                                        .divide(percentageFee.divide(BigDecimal.valueOf(100))
-                                                        .add(BigDecimal.ONE), Values.MC)
-                                        .multiply(percentageFee, Values.MC);
-
-                        Money fee = Money.of(asCurrencyCode(v.get("currency")),
-                                        fxFee.setScale(0, Values.MC.getRoundingMode()).longValue());
-
-                        checkAndSetFee(fee, t, type.getCurrentContext());
-                    }
-                })
-
-                .section("amount", "percentageFee").optional()
-                .match("^[\\s\\d]{2,3}\\.[\\d]{2}\\.[\\d]{4} Wiederanlage (?<amount>[\\.,\\d]+) (?<percentageFee>[\\.,\\d]+) [\\.,\\d]+ [\\.,\\d]+$")
-                .assign((t, v) -> {
-                    Map<String, String> context = type.getCurrentContext();
-                    v.put("currency", asCurrencyCode(context.get("baseCurrency")));
-
-                    BigDecimal percentageFee = asBigDecimal(v.get("percentageFee"));
-                    BigDecimal amount = asBigDecimal(v.get("amount"));
-
-                    if (percentageFee.compareTo(BigDecimal.ZERO) != 0)
-                    {
-                        // fxFee = (amount / (1 + percentageFee / 100)) * (percentageFee / 100)
-                        BigDecimal fxFee = amount
-                                        .divide(percentageFee.divide(BigDecimal.valueOf(100))
-                                                        .add(BigDecimal.ONE), Values.MC)
-                                        .multiply(percentageFee, Values.MC);
-
-                        Money fee = Money.of(asCurrencyCode(v.get("currency")),
-                                        fxFee.setScale(0, Values.MC.getRoundingMode()).longValue());
-
-                        checkAndSetFee(fee, t, type.getCurrentContext());
-                    }
-                });
-    }
-
-    private Security getSecurity(Map<String, String> context, Integer startTransactionLine)
-    {
-        for (String key : context.keySet())
-        {
-            String[] parts = key.split("_"); //$NON-NLS-1$
-            if (parts[0].equalsIgnoreCase("security")) //$NON-NLS-1$
-            {
-                if (startTransactionLine >= Integer.parseInt(parts[3]) && startTransactionLine <= Integer.parseInt(parts[4]))
-                {
-                    // returns security name, isin, security currency
-                    return new Security(parts[1], context.get(key), parts[2]);
-                }
-            }
-        }
-        return null;
-    }
-
-    private static class Security
-    {
-        public Security(String name, String isin, String currency)
-        {
-            this.name = name;
-            this.isin = isin;
-            this.currency = currency;
-        }
-
-        private String name;
-        private String isin;
-        private String currency;
-
-        public String getName()
-        {
-            return name;
-        }
-
-        public String getIsin()
-        {
-            return isin;
-        }
-
-        public String getCurrency()
-        {
-            return currency;
-        }
     }
 }
