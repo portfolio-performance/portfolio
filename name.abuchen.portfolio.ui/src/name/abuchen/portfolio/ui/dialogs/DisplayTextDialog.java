d5708aaeb47c928cf5de6eff0d0fd4f1d34f1bb3
Andreas Buchen <andreas.buchen@gmail.com>
2023-05-07 20:49:20 +0200

    Refactored DisplayTextDialog - remove PDF specifics, use StyledLabel
    Issue: #3293

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/dialogs/DisplayTextDialog.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/dialogs/DisplayTextDialog.java
@@ -6,40 +6,27 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StyleRange;
-import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.dnd.Transfer;
-import org.eclipse.swt.events.MouseAdapter;
-import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
-import name.abuchen.portfolio.money.CurrencyUnit;
 import name.abuchen.portfolio.ui.Messages;
 import name.abuchen.portfolio.ui.util.Colors;
-import name.abuchen.portfolio.ui.util.DesktopAPI;
+import name.abuchen.portfolio.ui.util.swt.StyledLabel;
 
 public class DisplayTextDialog extends Dialog
 {
@@ -48,7 +35,6 @@ public class DisplayTextDialog extends Dialog
     private File source;
     private String text;
     private Text widget;
-    private boolean createPDFDebug;
 
     public DisplayTextDialog(Shell parentShell, String text)
     {
@@ -119,9 +105,10 @@ public class DisplayTextDialog extends Dialog
     }
 
     @Override
-    protected Control createDialogArea(Composite parent)
+    protected final Control createDialogArea(Composite parent)
     {
         Composite container = new Composite(parent, SWT.None);
+        container.setBackground(Colors.WHITE);
         GridDataFactory.fillDefaults().grab(true, true).hint(800, 500).applyTo(container);
         GridLayoutFactory.fillDefaults().numColumns(1).applyTo(container);
 
@@ -130,31 +117,24 @@ public class DisplayTextDialog extends Dialog
 
         if (additionalText != null)
         {
-            List<StyleRange> styles = new ArrayList<>();
-            addBoldFirstLine(additionalText, styles);
-            additionalText = addMarkdownLikeHyperlinks(additionalText, styles);
-
-            StyledText additionalTextBox = new StyledText(container, SWT.MULTI | SWT.NONE | SWT.WRAP | SWT.READ_ONLY);
+            StyledLabel additionalTextBox = new StyledLabel(container, SWT.MULTI | SWT.WRAP | SWT.READ_ONLY);
+            additionalTextBox.setBackground(Colors.WHITE);
             additionalTextBox.setText(additionalText);
-            additionalTextBox.setStyleRanges(styles.toArray(new StyleRange[0]));
-
-            additionalTextBox.addListener(SWT.MouseDown, e -> openBrowser(e, additionalTextBox));
-
-            container.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
-            GridDataFactory.fillDefaults().grab(true, false).hint(SWT.DEFAULT, SWT.DEFAULT).align(SWT.FILL, SWT.TOP)
+            GridDataFactory.fillDefaults().grab(true, false).align(SWT.FILL, SWT.TOP).indent(5, 5)
                             .applyTo(additionalTextBox);
-            additionalTextBox.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1));
-            additionalTextBox.setMargins(5, 5, 5, 5);
         }
 
-        widget = new Text(container, SWT.MULTI | SWT.WRAP | SWT.READ_ONLY | SWT.BORDER | SWT.V_SCROLL);
-        widget.setText(text);
-        GridDataFactory.fillDefaults().grab(true, true).applyTo(widget);
-
-        if (createPDFDebug)
-            widget.addMouseListener(new PDFDebugMouseListener(widget));
+        widget = createTextArea(container);
 
         return container;
+    }
+
+    protected Text createTextArea(Composite container)
+    {
+        Text textArea = new Text(container, SWT.MULTI | SWT.WRAP | SWT.READ_ONLY | SWT.BORDER | SWT.V_SCROLL);
+        textArea.setText(text);
+        GridDataFactory.fillDefaults().grab(true, true).applyTo(textArea);
+        return textArea;
     }
 
     public void setDialogTitle(String dialogTitle)
@@ -165,139 +145,5 @@ public class DisplayTextDialog extends Dialog
     public void setAdditionalText(String additionalText)
     {
         this.additionalText = additionalText;
-    }
-
-    public void createPDFDebug(boolean createPDFDebug)
-    {
-        this.createPDFDebug = createPDFDebug;
-    }
-
-    private static class PDFDebugMouseListener extends MouseAdapter
-    {
-        private final Text widget;
-
-        public PDFDebugMouseListener(Text widget)
-        {
-            this.widget = widget;
-        }
-
-        @Override
-        public void mouseDoubleClick(MouseEvent e)
-        {
-            String selectedText = widget.getSelectionText();
-
-            // Check if selectedText is not empty and does not contain forbidden
-            // characters or currency codes
-            if (!selectedText.isEmpty() && !containsForbiddenCharacters(selectedText)
-                            && !CurrencyUnit.containsCurrencyCode(selectedText.trim()))
-            {
-                // Generate a new string of random characters to replace the
-                // selected text
-                StringBuilder replacementTextBuilder = new StringBuilder();
-                for (int i = 0; i < selectedText.length(); i++)
-                {
-                    char c = selectedText.charAt(i);
-                    if (Character.isLetter(c))
-                    {
-                        replacementTextBuilder.append(generateRandomLetter());
-                    }
-                    else if (Character.isDigit(c))
-                    {
-                        replacementTextBuilder.append(generateRandomNumber());
-                    }
-                    else
-                    {
-                        replacementTextBuilder.append(c);
-                    }
-                }
-                String replacementText = replacementTextBuilder.toString();
-
-                // Replace selectedText with replacementText
-                int startIndex = widget.getSelection().x;
-                widget.insert(replacementText);
-                widget.setSelection(startIndex, startIndex + replacementText.length());
-            }
-        }
-
-        // Check if the selected string contains any forbidden characters
-        private static boolean containsForbiddenCharacters(String string)
-        {
-            return string.matches(".*[\\-\\.,':\\/].*") || string.matches(".*[A-Z]{2}[A-Z0-9]{9}[0-9].*"); //$NON-NLS-1$ //$NON-NLS-2$
-        }
-
-        // Generates a random letter
-        private static char generateRandomLetter()
-        {
-            Random random = new Random();
-            boolean isUpperCase = random.nextBoolean();
-            int offset = isUpperCase ? 'A' : 'a';
-            return (char) (random.nextInt(26) + offset);
-        }
-
-        // Generates a random digit between 0 and 9
-        private static char generateRandomNumber()
-        {
-            Random random = new Random();
-            return (char) (random.nextInt(10) + '0');
-        }
-    }
-
-    private String addMarkdownLikeHyperlinks(String additionalText, List<StyleRange> styles)
-    {
-        Pattern pattern = Pattern.compile("\\[(?<text>[^\\]]*)\\]\\((?<link>[^\\)]*)\\)"); //$NON-NLS-1$
-        Matcher matcher = pattern.matcher(additionalText);
-
-        StringBuilder answer = new StringBuilder(additionalText.length());
-        int pointer = 0;
-
-        while (matcher.find())
-        {
-            int start = matcher.start();
-            int end = matcher.end();
-
-            answer.append(additionalText.substring(pointer, start));
-
-            String text = matcher.group("text"); //$NON-NLS-1$
-            String link = matcher.group("link"); //$NON-NLS-1$
-
-            StyleRange styleRange = new StyleRange();
-            styleRange.underline = true;
-            styleRange.underlineStyle = SWT.UNDERLINE_LINK;
-            styleRange.underlineColor = Colors.theme().hyperlink();
-            styleRange.foreground = Colors.theme().hyperlink();
-            styleRange.data = link;
-            styleRange.start = answer.length();
-            styleRange.length = text.length();
-            styles.add(styleRange);
-
-            answer.append(text);
-
-            pointer = end;
-        }
-
-        if (pointer < additionalText.length())
-            answer.append(additionalText.substring(pointer));
-
-        return answer.toString();
-    }
-
-    private void openBrowser(Event e, StyledText textBox)
-    {
-        int offset = textBox.getOffsetAtPoint(new Point(e.x, e.y));
-        if (offset == -1)
-            return;
-
-        StyleRange style = textBox.getStyleRangeAtOffset(offset);
-        if (style != null && style.data != null)
-            DesktopAPI.browse(String.valueOf(style.data));
-    }
-
-    private void addBoldFirstLine(String additionalText, List<StyleRange> ranges)
-    {
-        StyleRange styleRange = new StyleRange();
-        styleRange.fontStyle = SWT.BOLD;
-        styleRange.start = 0;
-        styleRange.length = additionalText.indexOf('\n');
-        ranges.add(styleRange);
     }
 }
--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/CreateTextFromPDFHandler.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/CreateTextFromPDFHandler.java
@@ -2,6 +2,7 @@ package name.abuchen.portfolio.ui.handlers;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.inject.Named;
 
@@ -10,10 +11,15 @@ import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 import name.abuchen.portfolio.datatransfer.pdf.PDFInputFile;
+import name.abuchen.portfolio.money.CurrencyUnit;
 import name.abuchen.portfolio.ui.Messages;
 import name.abuchen.portfolio.ui.PortfolioPlugin;
 import name.abuchen.portfolio.ui.dialogs.DisplayTextDialog;
@@ -56,10 +62,9 @@ public class CreateTextFromPDFHandler
 
             String text = textBuilder.toString();
 
-            DisplayTextDialog dialog = new DisplayTextDialog(shell, file, text);
+            DisplayTextDialog dialog = new DisplayPDFTextDialog(shell, file, text);
             dialog.setDialogTitle(Messages.PDFImportDebugTextExtraction);
             dialog.setAdditionalText(Messages.PDFImportDebugInformation);
-            dialog.createPDFDebug(true);
             dialog.open();
         }
         catch (IOException e)
@@ -68,4 +73,93 @@ public class CreateTextFromPDFHandler
             MessageDialog.openError(shell, Messages.LabelError, e.getMessage());
         }
     }
+
+    private static class DisplayPDFTextDialog extends DisplayTextDialog
+    {
+
+        public DisplayPDFTextDialog(Shell parentShell, File source, String text)
+        {
+            super(parentShell, source, text);
+        }
+
+        @Override
+        protected Text createTextArea(Composite container)
+        {
+            Text textArea = super.createTextArea(container);
+
+            textArea.addMouseListener(new PDFDebugMouseListener(textArea));
+
+            return textArea;
+        }
+    }
+
+    private static class PDFDebugMouseListener extends MouseAdapter
+    {
+        private final Random random = new Random();
+        private final Text widget;
+
+        public PDFDebugMouseListener(Text widget)
+        {
+            this.widget = widget;
+        }
+
+        @Override
+        public void mouseDoubleClick(MouseEvent e)
+        {
+            String selectedText = widget.getSelectionText();
+
+            // Check if selectedText is not empty and does not contain forbidden
+            // characters or currency codes
+            if (!selectedText.isEmpty() && !containsForbiddenCharacters(selectedText)
+                            && !CurrencyUnit.containsCurrencyCode(selectedText.trim()))
+            {
+                // Generate a new string of random characters to replace the
+                // selected text
+                StringBuilder replacementTextBuilder = new StringBuilder();
+                for (int i = 0; i < selectedText.length(); i++)
+                {
+                    char c = selectedText.charAt(i);
+                    if (Character.isLetter(c))
+                    {
+                        replacementTextBuilder.append(generateRandomLetter());
+                    }
+                    else if (Character.isDigit(c))
+                    {
+                        replacementTextBuilder.append(generateRandomNumber());
+                    }
+                    else
+                    {
+                        replacementTextBuilder.append(c);
+                    }
+                }
+                String replacementText = replacementTextBuilder.toString();
+
+                // Replace selectedText with replacementText
+                int startIndex = widget.getSelection().x;
+                widget.insert(replacementText);
+                widget.setSelection(startIndex, startIndex + replacementText.length());
+            }
+        }
+
+        /** Check if the selected string contains any forbidden characters */
+        private boolean containsForbiddenCharacters(String string)
+        {
+            return string.matches(".*[\\-\\.,':\\/].*") || string.matches(".*[A-Z]{2}[A-Z0-9]{9}\\d.*"); //$NON-NLS-1$ //$NON-NLS-2$
+        }
+
+        /** Generates a random letter */
+        private char generateRandomLetter()
+        {
+            boolean isUpperCase = random.nextBoolean();
+            int offset = isUpperCase ? 'A' : 'a';
+            return (char) (random.nextInt(26) + offset);
+        }
+
+        /** Generates a random digit between 0 and 9 */
+        private char generateRandomNumber()
+        {
+            return (char) (random.nextInt(10) + '0');
+        }
+    }
+
 }
