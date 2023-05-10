c120e45dade8ed83885cc028538a29d305d936e2
Oliver Fueckert <oliver@cubinet.de>
2023-05-10 18:19:34 +0200

    Add two new document types to MerkurPrivatBankPDFExtractor

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/MerkurPrivatBankPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/MerkurPrivatBankPDFExtractor.java
@@ -1,6 +1,5 @@
 package name.abuchen.portfolio.datatransfer.pdf;
 
-import static name.abuchen.portfolio.util.TextUtil.replaceMultipleBlanks;
 import static name.abuchen.portfolio.util.TextUtil.trim;
 
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
@@ -18,7 +17,7 @@ public class MerkurPrivatBankPDFExtractor extends AbstractPDFExtractor
     {
         super(client);
 
-        addBankIdentifier("Am Marktplatz 10 · 97762 Hammelburg"); //$NON-NLS-1$
+        addBankIdentifier("Umsatzsteuer-ID DE198159260"); //$NON-NLS-1$
 
         addBuySellTransaction();
     }
@@ -31,7 +30,7 @@ public class MerkurPrivatBankPDFExtractor extends AbstractPDFExtractor
 
     private void addBuySellTransaction()
     {
-        DocumentType type = new DocumentType("Kauf");
+        DocumentType type = new DocumentType("Wertpapier Abrechnung (Kauf|Verkauf)");
         this.addDocumentTyp(type);
 
         Transaction<BuySellEntry> pdfTransaction = new Transaction<>();
@@ -44,15 +43,25 @@ public class MerkurPrivatBankPDFExtractor extends AbstractPDFExtractor
         // @formatter:off
         // Wertpapier Abrechnung Kauf 
         // @formatter:on
-        Block firstRelevantLine = new Block("^Depotnummer [\\d]+.*$");
+        Block firstRelevantLine = new Block("^Am Marktplatz.*$");
         type.addBlock(firstRelevantLine);
         firstRelevantLine.set(pdfTransaction);
 
         pdfTransaction // @formatter:off
-                        // Stück 125,3258 XTR.(IE) - MSCI WORLD              IE00BJ0KDQ92 (A1XB5U)
-                        // REGISTERED SHARES 1C O.N.          
+                        // Wertpapier Abrechnung Verkauf 
+                        // or
+                        // Wertpapier Abrechnung Kauf 
                         // @formatter:on
-                        .section("name", "name1", "isin", "wkn")
+                        .section("type").match("^Wertpapier Abrechnung (?<type>(Kauf|Verkauf)).*").assign((t, v) -> {
+                            if (v.get("type").equals("Verkauf"))
+                                t.setType(PortfolioTransaction.Type.SELL);
+                        })
+
+                        // @formatter:off
+                        // Stück 600 XTR.(IE) - MSCI WORLD              IE00BJ0KDQ92 (A1XB5U)
+                        // REGISTERED SHARES 1C O.N.     
+                        // @formatter:on
+                        .section("name", "name1", "isin", "wkn", "currency")
                         .match("^St.ck [\\.,\\d]+ (?<name>.*) (?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9]) \\((?<wkn>[A-Z0-9]{6})\\)$")
                         .match("^(?<name1>.*)$") //
                         .match("^Ausf.hrungskurs [\\.,\\d]+ (?<currency>[\\w]{3}) .*$").assign((t, v) -> {
@@ -62,24 +71,24 @@ public class MerkurPrivatBankPDFExtractor extends AbstractPDFExtractor
                         })
 
                         // @formatter:off
-                        // Stück 125,3258 XTR.(IE) - MSCI WORLD              IE00BJ0KDQ92 (A1XB5U)
+                        // Stück 600 XTR.(IE) - MSCI WORLD              IE00BJ0KDQ92 (A1XB5U)
                         // @formatter:on
                         .section("shares") //
                         .match("^St.ck (?<shares>[\\.,\\d]+) .*$")
                         .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
 
                         // @formatter:off
-                        // Schlusstag/-Zeit 02.05.2023 09:34:40 Auftraggeber Max Mustermann
+                        // Schlusstag/-Zeit 09.05.2023 09:26:37 Auftraggeber jvDDiy QfmZL LloZHJy
                         // @formatter:on
                         .section("date", "time")
                         .match("^Schlusstag\\/\\-Zeit (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2}) .*$")
                         .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
 
                         // @formatter:off
-                        // Ausmachender Betrag 10.002,50- EUR
+                        // Ausmachender Betrag 47.811,46- EUR
                         // @formatter:on
                         .section("amount", "currency")
-                        .match("^Ausmachender Betrag (?<amount>[\\.,\\d]+)\\- (?<currency>[\\w]{3})$")
+                        .match("^Ausmachender Betrag (?<amount>[\\.,\\d]+)(\\-)? (?<currency>[\\w]{3})$")
                         .assign((t, v) -> {
                             t.setAmount(asAmount(v.get("amount")));
                             t.setCurrencyCode(asCurrencyCode(v.get("currency")));
@@ -87,17 +96,10 @@ public class MerkurPrivatBankPDFExtractor extends AbstractPDFExtractor
 
                         // @formatter:off
                         // Auftragsnummer 284722/61.00
-                        // Ausf. erfolgte über Quotrix
-                        // Ihr ETF-Sparplan Nr.      1
-                        // Für das Geschäft wurde keine Anlageberatung erbracht.
                         // @formatter:on
-                        .section("note", "note1").optional() //
+                        .section("note").optional() //
                         .match("^.*(?<note>Auftragsnummer .*)$") //
-                        .find("Ausf\\. erfolgte .ber .*") //
-                        .match("^(?<note1>.*)$") //
-                        .match("^F.r das Gesch.ft .*$")
-                        .assign((t, v) -> t.setNote(
-                                        trim(v.get("note")) + "\n" + trim(replaceMultipleBlanks(v.get("note1")))))
+                        .assign((t, v) -> t.setNote(trim(v.get("note"))))
 
                         .wrap(BuySellEntryItem::new);
 
@@ -108,10 +110,17 @@ public class MerkurPrivatBankPDFExtractor extends AbstractPDFExtractor
     private <T extends Transaction<?>> void addFeesSectionsTransaction(T transaction, DocumentType type)
     {
         transaction // @formatter:off
-                        // Provision 2,50- EUR
+                        // Provision 25,00- EUR
                         // @formatter:on
                         .section("fee", "currency").optional()
                         .match("^Provision (?<fee>[\\.,\\d]+)\\- (?<currency>[\\w]{3})$")
+                        .assign((t, v) -> processFeeEntries(t, v, type))
+
+                        // @formatter:off
+                        // Übertragungs-/Liefergebühr 0,06- EUR
+                        // @formatter:on
+                        .section("fee", "currency").optional()
+                        .match("^.bertragungs-\\/Liefergeb.hr (?<fee>[\\.,\\d]+)\\- (?<currency>[\\w]{3})$")
                         .assign((t, v) -> processFeeEntries(t, v, type));
 
     }
