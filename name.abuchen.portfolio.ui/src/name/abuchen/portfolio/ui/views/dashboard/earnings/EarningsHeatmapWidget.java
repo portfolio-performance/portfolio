0c77eab67ed64786dff815d6b6ba9f70f1ab2307
Andreas Buchen <andreas.buchen@gmail.com>
2023-05-30 13:27:10 +0200

    Refactored Earnings Heatmap: move to separate package, re-use config

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/earnings/EarningsHeatmapWidget.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/earnings/EarningsHeatmapWidget.java
@@ -1,138 +1,33 @@
-package name.abuchen.portfolio.ui.views.dashboard.heatmap;
+package name.abuchen.portfolio.ui.views.dashboard.earnings;
 
 import java.time.LocalDate;
 import java.time.Year;
 import java.time.YearMonth;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Objects;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
-import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.Client;
-import name.abuchen.portfolio.model.Dashboard;
 import name.abuchen.portfolio.model.Dashboard.Widget;
 import name.abuchen.portfolio.money.CurrencyConverter;
-import name.abuchen.portfolio.money.Money;
 import name.abuchen.portfolio.money.Values;
-import name.abuchen.portfolio.ui.Messages;
 import name.abuchen.portfolio.ui.views.dashboard.ClientFilterConfig;
 import name.abuchen.portfolio.ui.views.dashboard.DashboardData;
-import name.abuchen.portfolio.ui.views.dashboard.EnumBasedConfig;
 import name.abuchen.portfolio.ui.views.dashboard.ReportingPeriodConfig;
-import name.abuchen.portfolio.ui.views.dashboard.WidgetDelegate;
+import name.abuchen.portfolio.ui.views.dashboard.heatmap.AbstractHeatmapWidget;
+import name.abuchen.portfolio.ui.views.dashboard.heatmap.Average;
+import name.abuchen.portfolio.ui.views.dashboard.heatmap.AverageConfig;
+import name.abuchen.portfolio.ui.views.dashboard.heatmap.HeatmapModel;
 import name.abuchen.portfolio.util.Interval;
 
 public class EarningsHeatmapWidget extends AbstractHeatmapWidget<Long>
 {
-    enum EarningType
-    {
-        EARNINGS(Messages.LabelDividends + " + " + Messages.LabelInterest, //$NON-NLS-1$
-                        t -> t.getType() == AccountTransaction.Type.DIVIDENDS
-                                        || t.getType() == AccountTransaction.Type.INTEREST
-                                        || t.getType() == AccountTransaction.Type.INTEREST_CHARGE), //
-        DIVIDENDS(Messages.LabelDividends, t -> t.getType() == AccountTransaction.Type.DIVIDENDS), //
-        INTEREST(Messages.LabelInterest, t -> t.getType() == AccountTransaction.Type.INTEREST
-                        || t.getType() == AccountTransaction.Type.INTEREST_CHARGE);
-
-        private String label;
-        private Predicate<AccountTransaction> predicate;
-
-        private EarningType(String label, Predicate<AccountTransaction> predicate)
-        {
-            this.label = label;
-            this.predicate = predicate;
-        }
-
-        @Override
-        public String toString()
-        {
-            return label;
-        }
-
-        public boolean isIncluded(AccountTransaction t)
-        {
-            return predicate.test(t);
-        }
-    }
-
-    static class EarningsConfig extends EnumBasedConfig<EarningType>
-    {
-        public EarningsConfig(WidgetDelegate<?> delegate)
-        {
-            super(delegate, Messages.LabelEarnings, EarningType.class, Dashboard.Config.EARNING_TYPE,
-                            Policy.EXACTLY_ONE);
-        }
-    }
-
-    enum GrossNetType
-    {
-        NET(Messages.LabelNet, t -> t.getMonetaryAmount()), //
-        GROSS(Messages.LabelGross, t -> t.getGrossValue());
-
-        private String label;
-        private Function<AccountTransaction, Money> valueExtractor;
-
-        private GrossNetType(String label, Function<AccountTransaction, Money> valueExtractor)
-        {
-            this.label = label;
-            this.valueExtractor = valueExtractor;
-        }
-
-        @Override
-        public String toString()
-        {
-            return label;
-        }
-
-        public Money getValue(AccountTransaction t)
-        {
-            return valueExtractor.apply(t);
-        }
-    }
-
-    static class GrossNetConfig extends EnumBasedConfig<GrossNetType>
-    {
-        public GrossNetConfig(WidgetDelegate<?> delegate)
-        {
-            super(delegate, Messages.LabelGrossNetCalculation, GrossNetType.class, Dashboard.Config.NET_GROSS,
-                            Policy.EXACTLY_ONE);
-        }
-    }
-
-    public enum Average
-    {
-        AVERAGE(Messages.HeatmapOrnamentAverage);
-
-        private String label;
-
-        private Average(String label)
-        {
-            this.label = label;
-        }
-
-        @Override
-        public String toString()
-        {
-            return label;
-        }
-    }
-
-    static class AverageConfig extends EnumBasedConfig<Average>
-    {
-        public AverageConfig(WidgetDelegate<?> delegate)
-        {
-            super(delegate, Messages.HeatmapOrnamentAverage, Average.class, Dashboard.Config.LAYOUT, Policy.MULTIPLE);
-        }
-    }
-
     public EarningsHeatmapWidget(Widget widget, DashboardData data)
     {
         super(widget, data);
 
         addConfig(new ClientFilterConfig(this));
-        addConfig(new EarningsConfig(this));
-        addConfig(new GrossNetConfig(this));
+        addConfig(new EarningTypeConfig(this));
+        addConfig(new GrossNetTypeConfig(this));
         addConfig(new AverageConfig(this));
     }
 
@@ -176,8 +71,8 @@ public class EarningsHeatmapWidget extends AbstractHeatmapWidget<Long>
         }
 
         CurrencyConverter converter = getDashboardData().getCurrencyConverter();
-        EarningType type = get(EarningsConfig.class).getValue();
-        GrossNetType grossNet = get(GrossNetConfig.class).getValue();
+        EarningType type = get(EarningTypeConfig.class).getValue();
+        GrossNetType grossNet = get(GrossNetTypeConfig.class).getValue();
 
         // iterate over transactions and add to model
 
