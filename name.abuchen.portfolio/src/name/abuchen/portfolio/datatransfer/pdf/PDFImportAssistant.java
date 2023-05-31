b38154fcce053020c1fa727ba67cf52e2da1dc96
flywire <flywire0@gmail.com>
2023-05-31 16:56:38 +1000

    Added Stake PDF extractor
    Issue: #3378
    Co-authored-by: flywire <flywire0@gmail.com>
    Co-authored-by: Andreas Buchen <andreas.buchen@gmail.com>

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
@@ -82,6 +82,7 @@ public class PDFImportAssistant
         extractors.add(new SelfWealthPDFExtractor(client));
         extractors.add(new SimpelPDFExtractor(client));
         extractors.add(new SolarisbankAGPDFExtractor(client));
+        extractors.add(new StakePDFExtractor(client));
         extractors.add(new SuresseDirektBankPDFExtractor(client));
         extractors.add(new SwissquotePDFExtractor(client));
         extractors.add(new TargobankPDFExtractor(client));
