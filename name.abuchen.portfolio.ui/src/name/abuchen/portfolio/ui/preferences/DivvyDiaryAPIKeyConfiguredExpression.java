a055b9cf80b86d1fbbd9e6bffb2841a908498af6
Andreas Buchen <andreas.buchen@gmail.com>
2023-06-17 07:44:00 +0200

    Use constant instead of string

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/preferences/DivvyDiaryAPIKeyConfiguredExpression.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/preferences/DivvyDiaryAPIKeyConfiguredExpression.java
@@ -4,12 +4,14 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.e4.core.di.annotations.Evaluate;
 import org.eclipse.e4.core.di.extensions.Preference;
 
+import name.abuchen.portfolio.ui.UIConstants;
+
 public class DivvyDiaryAPIKeyConfiguredExpression
 {
     @Evaluate
     public boolean evaluate(@Preference IEclipsePreferences preferences)
     {
-        final String key = preferences.get("DIVVYDIARY_API_KEY", null); //$NON-NLS-1$
+        final String key = preferences.get(UIConstants.Preferences.DIVVYDIARY_API_KEY, null);
         return key != null && !key.isBlank();
     }
 }
