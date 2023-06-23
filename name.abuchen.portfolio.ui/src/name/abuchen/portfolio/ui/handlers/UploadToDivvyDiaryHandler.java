c3bf8537cbc6959def6e73dcfc46babdb06ef8e6
Andreas Buchen <andreas.buchen@gmail.com>
2023-06-23 10:46:28 +0200

    Allow selection of which portfolio to upload to DivvyDiary

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/UploadToDivvyDiaryHandler.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/UploadToDivvyDiaryHandler.java
@@ -1,6 +1,7 @@
 package name.abuchen.portfolio.ui.handlers;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,26 +15,48 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
 
+import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.model.Portfolio;
 import name.abuchen.portfolio.money.CurrencyConverter;
 import name.abuchen.portfolio.money.CurrencyConverterImpl;
 import name.abuchen.portfolio.online.impl.DivvyDiaryUploader;
+import name.abuchen.portfolio.online.impl.DivvyDiaryUploader.DDPortfolio;
+import name.abuchen.portfolio.snapshot.filter.PortfolioClientFilter;
 import name.abuchen.portfolio.ui.Messages;
 import name.abuchen.portfolio.ui.PortfolioPlugin;
 import name.abuchen.portfolio.ui.UIConstants;
-import name.abuchen.portfolio.ui.dialogs.ListSelectionDialog;
 import name.abuchen.portfolio.ui.editor.ClientInput;
 import name.abuchen.portfolio.ui.jobs.AbstractClientJob;
 import name.abuchen.portfolio.ui.util.swt.ActiveShell;
-import name.abuchen.portfolio.util.Pair;
+import name.abuchen.portfolio.ui.util.viewers.CopyPasteSupport;
 
 public class UploadToDivvyDiaryHandler
 {
+    record Selection(long divvyDiaryPortfolioId, Portfolio portfolio)
+    {
+    }
+
     @CanExecute
     boolean isVisible(@Named(IServiceConstants.ACTIVE_PART) MPart part,
                     @org.eclipse.e4.core.di.annotations.Optional @Preference(value = UIConstants.Preferences.DIVVYDIARY_API_KEY) String divvyDiaryApiKey)
@@ -56,16 +79,16 @@ public class UploadToDivvyDiaryHandler
         MenuHelper.getActiveClientInput(part).ifPresent(clientInput -> {
             DivvyDiaryUploader uploader = new DivvyDiaryUploader(divvyDiaryApiKey);
 
-            retrieveAndPickPortfolio(shell, uploader)
-                            .ifPresent(portfolioId -> uploadPortfolio(clientInput, uploader, portfolioId, enableExperimentalFeatures));
+            retrieveAndPickPortfolio(shell, clientInput.getClient(), uploader).ifPresent(
+                            selection -> uploadPortfolio(clientInput, uploader, selection.divvyDiaryPortfolioId(),
+                                            selection.portfolio, enableExperimentalFeatures));
         });
 
     }
 
-    @SuppressWarnings("unchecked")
-    private Optional<Long> retrieveAndPickPortfolio(Shell shell, DivvyDiaryUploader uploader)
+    private Optional<Selection> retrieveAndPickPortfolio(Shell shell, Client client, DivvyDiaryUploader uploader)
     {
-        List<Pair<Long, String>> portfolios;
+        List<DDPortfolio> portfolios;
 
         try
         {
@@ -82,36 +105,30 @@ public class UploadToDivvyDiaryHandler
         if (portfolios.isEmpty())
             return Optional.empty();
 
-        if (portfolios.size() == 1)
+        PortfolioSelectionDialog dialog = new PortfolioSelectionDialog(shell);
+        dialog.setDivvyDiaryPortfolio(portfolios);
+
+        List<Object> ppPortfolios = new ArrayList<>();
+        ppPortfolios.add(name.abuchen.portfolio.Messages.LabelJointPortfolio);
+        ppPortfolios.addAll(client.getPortfolios());
+        dialog.setPortfolio(ppPortfolios);
+
+        if (dialog.open() == Window.OK)
         {
-            if (!MessageDialog.openConfirm(shell, Messages.LabelInfo, Messages.DivvyDiaryConfirmUpload))
-                return Optional.empty();
+            DDPortfolio selected = dialog.getSelectedDDPortfolio();
 
-            return Optional.of(portfolios.get(0).getLeft());
-        }
-        else
-        {
-            LabelProvider labelProvider = LabelProvider.createTextProvider(e -> ((Pair<Long, String>) e).getRight());
-            ListSelectionDialog dialog = new ListSelectionDialog(shell, labelProvider);
+            Portfolio portfolio = null;
+            if (dialog.getSelectedPPPortfolio() instanceof Portfolio p)
+                portfolio = p;
 
-            dialog.setTitle(Messages.LabelInfo);
-            dialog.setMessage(Messages.DivvyDiaryConfirmUpload);
-            dialog.setMultiSelection(false);
-
-            dialog.setElements(portfolios);
-
-            if (dialog.open() == Window.OK)
-            {
-                Object[] selected = dialog.getResult();
-                if (selected.length > 0)
-                    return Optional.of(((Pair<Long, String>) selected[0]).getLeft());
-            }
+            return Optional.of(new Selection(selected.id(), portfolio));
         }
 
         return Optional.empty();
     }
 
-    private void uploadPortfolio(ClientInput clientInput, DivvyDiaryUploader uploader, Long portfolioId, boolean enableExperimentalFeatures)
+    private void uploadPortfolio(ClientInput clientInput, DivvyDiaryUploader uploader, Long portfolioId,
+                    Portfolio portfolio, boolean enableExperimentalFeatures)
     {
         new AbstractClientJob(clientInput.getClient(), Messages.DivvyDiaryMsgUploading)
         {
@@ -123,7 +140,11 @@ public class UploadToDivvyDiaryHandler
                     CurrencyConverter converter = new CurrencyConverterImpl(clientInput.getExchangeRateProviderFacory(),
                                     clientInput.getClient().getBaseCurrency());
 
-                    uploader.upload(getClient(), converter, portfolioId, enableExperimentalFeatures);
+                    Client client = getClient();
+                    if (portfolio != null)
+                        client = new PortfolioClientFilter(portfolio).filter(client);
+
+                    uploader.upload(client, converter, portfolioId, enableExperimentalFeatures);
 
                     Display.getDefault().asyncExec(() -> MessageDialog.openInformation(ActiveShell.get(),
                                     Messages.LabelInfo, Messages.DivvyDiaryUploadSuccessfulMsg));
@@ -146,5 +167,143 @@ public class UploadToDivvyDiaryHandler
                 }
             }
         }.schedule();
+    }
+}
+
+class PortfolioSelectionDialog extends Dialog
+{
+    private List<Object> portfolios;
+    private List<DDPortfolio> ddPortfolios;
+
+    private Object selectedPortfolio;
+    private DDPortfolio selectedDDPortfolio;
+
+    private TableViewer portfoliosTableViewer;
+    private TableViewer ddTableViewer;
+
+    public PortfolioSelectionDialog(Shell parentShell)
+    {
+        super(parentShell);
+    }
+
+    public void setDivvyDiaryPortfolio(List<DDPortfolio> ddPortfolios)
+    {
+        this.ddPortfolios = ddPortfolios;
+    }
+
+    public void setPortfolio(List<Object> portfolios)
+    {
+        this.portfolios = portfolios;
+    }
+
+    public Object getSelectedPPPortfolio()
+    {
+        return selectedPortfolio;
+    }
+
+    public DDPortfolio getSelectedDDPortfolio()
+    {
+        return selectedDDPortfolio;
+    }
+
+    @Override
+    protected void setShellStyle(int newShellStyle)
+    {
+        super.setShellStyle(newShellStyle | SWT.RESIZE);
+    }
+
+    @Override
+    protected Control createContents(Composite parent)
+    {
+        Control contents = super.createContents(parent);
+        getShell().setText(Messages.LabelInfo);
+        return contents;
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent)
+    {
+        Composite composite = (Composite) super.createDialogArea(parent);
+
+        Composite container = new Composite(composite, SWT.None);
+        GridDataFactory.fillDefaults().grab(true, true).hint(400, 300).applyTo(container);
+        GridLayoutFactory.fillDefaults().numColumns(2).equalWidth(true).applyTo(container);
+
+        Label label = new Label(container, SWT.NONE | SWT.WRAP);
+        label.setText(Messages.DivvyDiaryConfirmUpload);
+        GridDataFactory.fillDefaults().span(2, 1).grab(true, false).applyTo(label);
+
+        label = new Label(container, SWT.NONE);
+        label.setText(Messages.ColumnPortfolio);
+
+        label = new Label(container, SWT.NONE);
+        label.setText(Messages.PrefTitleDivvyDiary);
+
+        buildPortfolioTable(container);
+        buildDivvyDiaryTable(container);
+
+        return composite;
+    }
+
+    private void buildPortfolioTable(Composite container)
+    {
+        Composite tableArea = new Composite(container, SWT.NONE);
+        GridDataFactory.fillDefaults().grab(false, true).applyTo(tableArea);
+        tableArea.setLayout(new FillLayout());
+
+        TableColumnLayout layout = new TableColumnLayout();
+        tableArea.setLayout(layout);
+
+        portfoliosTableViewer = new TableViewer(tableArea, SWT.BORDER | SWT.FULL_SELECTION | SWT.VIRTUAL);
+        portfoliosTableViewer.setUseHashlookup(true);
+        CopyPasteSupport.enableFor(portfoliosTableViewer);
+
+        Table table = portfoliosTableViewer.getTable();
+        table.setHeaderVisible(false);
+        table.setLinesVisible(false);
+
+        TableViewerColumn column = new TableViewerColumn(portfoliosTableViewer, SWT.None);
+        layout.setColumnData(column.getColumn(), new ColumnWeightData(100));
+
+        portfoliosTableViewer.setLabelProvider(LabelProvider.createTextProvider(String::valueOf));
+        portfoliosTableViewer.setContentProvider(ArrayContentProvider.getInstance());
+        portfoliosTableViewer.setInput(portfolios);
+
+        portfoliosTableViewer.setSelection(new StructuredSelection(portfolios.get(0)));
+    }
+
+    private void buildDivvyDiaryTable(Composite container)
+    {
+        Composite tableArea = new Composite(container, SWT.NONE);
+        GridDataFactory.fillDefaults().grab(false, true).applyTo(tableArea);
+        tableArea.setLayout(new FillLayout());
+
+        TableColumnLayout layout = new TableColumnLayout();
+        tableArea.setLayout(layout);
+
+        ddTableViewer = new TableViewer(tableArea, SWT.BORDER | SWT.FULL_SELECTION | SWT.VIRTUAL);
+        ddTableViewer.setUseHashlookup(true);
+        CopyPasteSupport.enableFor(ddTableViewer);
+
+        Table table = ddTableViewer.getTable();
+        table.setHeaderVisible(false);
+        table.setLinesVisible(false);
+
+        TableViewerColumn column = new TableViewerColumn(ddTableViewer, SWT.None);
+        layout.setColumnData(column.getColumn(), new ColumnWeightData(100));
+
+        ddTableViewer.setLabelProvider(LabelProvider.createTextProvider(e -> ((DDPortfolio) e).name()));
+        ddTableViewer.setContentProvider(ArrayContentProvider.getInstance());
+        ddTableViewer.setInput(ddPortfolios);
+
+        ddTableViewer.setSelection(new StructuredSelection(ddPortfolios.get(0)));
+    }
+
+    @Override
+    protected void okPressed()
+    {
+        this.selectedPortfolio = portfoliosTableViewer.getStructuredSelection().getFirstElement();
+        this.selectedDDPortfolio = (DDPortfolio) ddTableViewer.getStructuredSelection().getFirstElement();
+        super.okPressed();
     }
 }
