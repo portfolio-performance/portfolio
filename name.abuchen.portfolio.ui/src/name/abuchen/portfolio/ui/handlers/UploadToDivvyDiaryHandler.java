77f169e87fb415f53157cb280a1178dafcf558a6
Andreas Buchen <andreas.buchen@gmail.com>
2023-06-24 08:16:19 +0200

    Control upload of activities to DivvyDiary by explicit user flag
    Use user controlled flag instead of experimental feature flag.

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/UploadToDivvyDiaryHandler.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/handlers/UploadToDivvyDiaryHandler.java
@@ -28,7 +28,9 @@ import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -53,7 +55,7 @@ import name.abuchen.portfolio.ui.util.viewers.CopyPasteSupport;
 
 public class UploadToDivvyDiaryHandler
 {
-    record Selection(long divvyDiaryPortfolioId, Portfolio portfolio)
+    record Selection(long divvyDiaryPortfolioId, Portfolio portfolio, boolean includeTransations)
     {
     }
 
@@ -67,8 +69,7 @@ public class UploadToDivvyDiaryHandler
     @Execute
     public void execute(@Named(IServiceConstants.ACTIVE_PART) MPart part,
                     @Named(IServiceConstants.ACTIVE_SHELL) Shell shell,
-                    @org.eclipse.e4.core.di.annotations.Optional @Preference(UIConstants.Preferences.DIVVYDIARY_API_KEY) String divvyDiaryApiKey,
-                    @org.eclipse.e4.core.di.annotations.Optional @Preference(UIConstants.Preferences.ENABLE_EXPERIMENTAL_FEATURES) boolean enableExperimentalFeatures)
+                    @org.eclipse.e4.core.di.annotations.Optional @Preference(UIConstants.Preferences.DIVVYDIARY_API_KEY) String divvyDiaryApiKey)
     {
         if (divvyDiaryApiKey == null)
         {
@@ -79,14 +80,14 @@ public class UploadToDivvyDiaryHandler
         MenuHelper.getActiveClientInput(part).ifPresent(clientInput -> {
             DivvyDiaryUploader uploader = new DivvyDiaryUploader(divvyDiaryApiKey);
 
-            retrieveAndPickPortfolio(shell, clientInput.getClient(), uploader).ifPresent(
-                            selection -> uploadPortfolio(clientInput, uploader, selection.divvyDiaryPortfolioId(),
-                                            selection.portfolio, enableExperimentalFeatures));
+            retrieveAndPickPortfolio(shell, clientInput, uploader)
+                            .ifPresent(selection -> uploadPortfolio(clientInput, uploader, selection));
         });
 
     }
 
-    private Optional<Selection> retrieveAndPickPortfolio(Shell shell, Client client, DivvyDiaryUploader uploader)
+    private Optional<Selection> retrieveAndPickPortfolio(Shell shell, ClientInput clientInput,
+                    DivvyDiaryUploader uploader)
     {
         List<DDPortfolio> portfolios;
 
@@ -110,8 +111,11 @@ public class UploadToDivvyDiaryHandler
 
         List<Object> ppPortfolios = new ArrayList<>();
         ppPortfolios.add(name.abuchen.portfolio.Messages.LabelJointPortfolio);
-        ppPortfolios.addAll(client.getPortfolios());
+        ppPortfolios.addAll(clientInput.getClient().getPortfolios());
         dialog.setPortfolio(ppPortfolios);
+
+        dialog.setIncludeTransactions(
+                        clientInput.getPreferenceStore().getBoolean(UploadToDivvyDiaryHandler.class.getSimpleName()));
 
         if (dialog.open() == Window.OK)
         {
@@ -121,14 +125,16 @@ public class UploadToDivvyDiaryHandler
             if (dialog.getSelectedPPPortfolio() instanceof Portfolio p)
                 portfolio = p;
 
-            return Optional.of(new Selection(selected.id(), portfolio));
+            clientInput.getPreferenceStore().setValue(UploadToDivvyDiaryHandler.class.getSimpleName(),
+                            dialog.includeTransactions());
+
+            return Optional.of(new Selection(selected.id(), portfolio, dialog.includeTransactions()));
         }
 
         return Optional.empty();
     }
 
-    private void uploadPortfolio(ClientInput clientInput, DivvyDiaryUploader uploader, Long portfolioId,
-                    Portfolio portfolio, boolean enableExperimentalFeatures)
+    private void uploadPortfolio(ClientInput clientInput, DivvyDiaryUploader uploader, Selection selection)
     {
         new AbstractClientJob(clientInput.getClient(), Messages.DivvyDiaryMsgUploading)
         {
@@ -141,10 +147,10 @@ public class UploadToDivvyDiaryHandler
                                     clientInput.getClient().getBaseCurrency());
 
                     Client client = getClient();
-                    if (portfolio != null)
-                        client = new PortfolioClientFilter(portfolio).filter(client);
+                    if (selection.portfolio != null)
+                        client = new PortfolioClientFilter(selection.portfolio).filter(client);
 
-                    uploader.upload(client, converter, portfolioId, enableExperimentalFeatures);
+                    uploader.upload(client, converter, selection.divvyDiaryPortfolioId, selection.includeTransations);
 
                     Display.getDefault().asyncExec(() -> MessageDialog.openInformation(ActiveShell.get(),
                                     Messages.LabelInfo, Messages.DivvyDiaryUploadSuccessfulMsg));
@@ -181,6 +187,8 @@ class PortfolioSelectionDialog extends Dialog
     private TableViewer portfoliosTableViewer;
     private TableViewer ddTableViewer;
 
+    private boolean includeTransactions = false;
+
     public PortfolioSelectionDialog(Shell parentShell)
     {
         super(parentShell);
@@ -204,6 +212,16 @@ class PortfolioSelectionDialog extends Dialog
     public DDPortfolio getSelectedDDPortfolio()
     {
         return selectedDDPortfolio;
+    }
+
+    public boolean includeTransactions()
+    {
+        return includeTransactions;
+    }
+
+    public void setIncludeTransactions(boolean includeTransactions)
+    {
+        this.includeTransactions = includeTransactions;
     }
 
     @Override
@@ -241,6 +259,13 @@ class PortfolioSelectionDialog extends Dialog
 
         buildPortfolioTable(container);
         buildDivvyDiaryTable(container);
+
+        Button btnCheckbox = new Button(container, SWT.CHECK);
+        btnCheckbox.setText("Including historic transactions in upload");
+        btnCheckbox.setSelection(includeTransactions);
+        btnCheckbox.addSelectionListener(
+                        SelectionListener.widgetSelectedAdapter(e -> includeTransactions = btnCheckbox.getSelection()));
+        GridDataFactory.fillDefaults().span(2, 1).grab(true, false).applyTo(btnCheckbox);
 
         return composite;
     }
