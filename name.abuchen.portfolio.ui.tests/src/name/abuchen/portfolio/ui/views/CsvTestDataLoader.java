152db19d028e237fef34a73d136864266df0c75b
daveln <107140966+daveln@users.noreply.github.com>
2023-06-24 20:33:11 +0200

    Added MACD as an indicator
    Also requested in
    https://forum.portfolio-performance.info/t/chartanalyse-charttools-und-indikatoren/744/7
    and https://forum.portfolio-performance.info/t/chart-analyses-in-stock-graph-like-rsi-or-macd-or-others/21484
    
    * Added formatting to the MACD/MACDSignal values in the
      SecuritiesChart/TimelineChartToolTip.
    * Added an excel file with matching csv that contains the suitable
      calculations for MACD, RSI, EMA, SMA for a security.
    * It is used in the test to validate calculation correctness for MACD.
    
    Issue: #3423

---
+++ name.abuchen.portfolio.ui.tests/src/name/abuchen/portfolio/ui/views/CsvTestDataLoader.java
@@ -0,0 +1,55 @@
+package name.abuchen.portfolio.ui.views;
+
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.csv.CSVRecord;
+
+import com.google.common.primitives.Doubles;
+
+import name.abuchen.portfolio.model.Security;
+import name.abuchen.portfolio.model.SecurityPrice;
+import name.abuchen.portfolio.money.Values;
+import name.abuchen.portfolio.ui.util.chart.TimelineChart;
+
+class CsvTestDataLoader
+{
+    static Security loadSecurity(List<CSVRecord> records, DateTimeFormatter dateFormatter, String dateHeader,
+                    NumberFormat numberFormat, String valueHeader) throws Exception
+    {
+        Security result = new Security();
+        for (CSVRecord csvRecord : records)
+        {
+            String value = csvRecord.get(valueHeader);
+            if (!value.isBlank())
+            {
+                result.addPrice(new SecurityPrice(LocalDate.parse(csvRecord.get(dateHeader), dateFormatter),
+                                Values.Quote.factorize(numberFormat.parse(value).doubleValue())));
+            }
+        }
+        return result;
+    }
+
+    static ChartLineSeriesAxes loadChartLineSeriesAxes(List<CSVRecord> records, DateTimeFormatter dateFormatter,
+                    String dateHeader, NumberFormat numberFormat, String valueHeader) throws Exception
+    {
+        List<LocalDate> dates = new ArrayList<>();
+        List<Double> values = new ArrayList<>();
+        for (CSVRecord csvRecord : records)
+        {
+            String value = csvRecord.get(valueHeader);
+            if (!value.isBlank())
+            {
+                dates.add(LocalDate.parse(csvRecord.get(dateHeader), dateFormatter));
+                values.add(numberFormat.parse(value).doubleValue());
+            }
+        }
+        ChartLineSeriesAxes result = new ChartLineSeriesAxes();
+        result.setDates(TimelineChart.toJavaUtilDate(dates.toArray(new LocalDate[0])));
+        result.setValues(Doubles.toArray(values));
+        return result;
+    }
+}
