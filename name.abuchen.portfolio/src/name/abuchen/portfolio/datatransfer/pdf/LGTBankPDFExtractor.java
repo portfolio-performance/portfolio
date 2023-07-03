941629c24d314fd3ef55a17f542bab7903adb644
Nirus2000 <webmaster@nirus-online.de>
2023-07-03 20:36:05 +0200

    Modify LGT Bank PDF-Importer to support new transactions
    https://forum.portfolio-performance.info/t/pdf-import-von-lgt-bank/14487/5
    https://forum.portfolio-performance.info/t/pdf-import-von-lgt-bank/14487/7

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/LGTBankPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/LGTBankPDFExtractor.java
@@ -21,7 +21,7 @@ public class LGTBankPDFExtractor extends AbstractPDFExtractor
     {
         super(client);
 
-        addBankIdentifier("LGT Bank AG");
+        addBankIdentifier("LGT Bank");
 
         addBuySellTransaction();
         addDividendeTransaction();
@@ -35,7 +35,7 @@ public class LGTBankPDFExtractor extends AbstractPDFExtractor
 
     private void addBuySellTransaction()
     {
-        DocumentType type = new DocumentType("Abrechnung Kauf");
+        DocumentType type = new DocumentType("Abrechnung (Kauf|Verkauf|Zeichnung)");
         this.addDocumentTyp(type);
 
         Transaction<BuySellEntry> pdfTransaction = new Transaction<>();
@@ -45,47 +45,109 @@ public class LGTBankPDFExtractor extends AbstractPDFExtractor
             return entry;
         });
 
-        Block firstRelevantLine = new Block("^Abrechnung Kauf .*$");
+        Block firstRelevantLine = new Block("^Abrechnung (Kauf|Verkauf|Zeichnung) .*$");
         type.addBlock(firstRelevantLine);
         firstRelevantLine.set(pdfTransaction);
 
         pdfTransaction
-                // Titel A.P. Moeller - Maersk A/S
-                // Namen- und Inhaber-Aktien -B-
-                // ISIN DK0010244508
-                // Valorennummer 906020
-                // Wertpapierkennnummer 861837
-                // Kurswert DKK 80'784.00
-                .section("name", "nameContinued", "isin", "wkn", "currency")
-                .match("^Titel (?<name>.*)$")
-                .match("(?<nameContinued>.*)")
-                .match("^ISIN (?<isin>[\\w]{12})$")
-                .match("^Wertpapierkennnummer (?<wkn>.*)$")
-                .match("^Kurswert (?<currency>[\\w]{3}) [\\.,'\\d]+$")
-                .assign((t, v) -> t.setSecurity(getOrCreateSecurity(v)))
+                // Is type --> "Verkauf" change from BUY to SELL
+                .section("type").optional()
+                .match("^Abrechnung (?<type>(Kauf|Verkauf|Zeichnung)) .*$")
+                .assign((t, v) -> {
+                    if ("Verkauf".equals(v.get("type")))
+                        t.setType(PortfolioTransaction.Type.SELL);
+                })
 
+                .oneOf(
+                                // @formatter:off
+                                // Titel A.P. Moeller - Maersk A/S
+                                // Namen- und Inhaber-Aktien -B-
+                                // ISIN DK0010244508
+                                // Valorennummer 906020
+                                // Wertpapierkennnummer 861837
+                                // Kurswert DKK 80'784.00
+                                // @formatter:on
+                                section -> section
+                                        .attributes("name", "nameContinued", "isin", "wkn", "currency")
+                                        .match("^Titel (?<name>.*)$")
+                                        .match("^(?<nameContinued>.*)$")
+                                        .match("^ISIN (?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9])$")
+                                        .match("^Wertpapierkennnummer (?<wkn>.*)$")
+                                        .match("^Kurswert (?<currency>[\\w]{3}) [\\.'\\d]+$")
+                                        .assign((t, v) -> t.setSecurity(getOrCreateSecurity(v)))
+                                ,
+                                // @formatter:off
+                                // Titel JPMorgan Funds SICAV - Emerging Markets Sustainable Equity
+                                // Fund
+                                // Namen-Anteile -C- / Class USD
+                                // ISIN LU2051469208
+                                // Valorennummer 50139326
+                                // Kurswert USD 50'520.00
+                                // @formatter:on
+                                section -> section
+                                        .attributes("name", "nameContinued", "isin", "currency")
+                                        .match("^Titel (?<name>.*)$")
+                                        .match("^(?<nameContinued>.*)$")
+                                        .match("^ISIN (?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9])$")
+                                        .match("^Kurswert (?<currency>[\\w]{3}) [\\.'\\d]+$")
+                                        .assign((t, v) -> t.setSecurity(getOrCreateSecurity(v)))
+                        )
+
+                // @formatter:off
                 // Anzahl 12 Stück
+                // @formatter:on
                 .section("shares")
-                .match("^Anzahl (?<shares>[\\d.,]+) St.ck$")
+                .match("^Anzahl (?<shares>[\\.,\\d]+) St.ck$")
                 .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
 
-                // Abschlussdatum 14.04.2020 09:00:02
-                .section("date", "time")
-                .match("^Abschlussdatum (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2})$")
-                .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
+                .oneOf(
+                                // @formatter:off
+                                // Abschlussdatum 14.04.2020 09:00:02
+                                // @formatter:on
+                                section -> section
+                                        .attributes("date", "time")
+                                        .match("^Abschlussdatum (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2})$")
+                                        .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
+                                ,
+                                // @formatter:off
+                                // Zeichnungstag (NAV) 08.06.2023
+                                // @formatter:on
+                                section -> section
+                                        .attributes("date")
+                                        .match("^Zeichnungstag .* (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4})$")
+                                        .assign((t, v) -> t.setDate(asDate(v.get("date"))))
+                        )
 
+                // @formatter:off
                 // Belastung DKK Konto 0037156.021 DKK 82'452.21
+                // Belastung CHF Freizügigkeitskonto 2026457.031 CHF 48'502.01
+                // Gutschrift USD Konto 2026457.055 USD 8'332.19
+                // @formatter:on
                 .section("currency", "amount")
-                .match("^Belastung [\\w]{3} Konto .* (?<currency>[\\w]{3}) (?<amount>[\\.',\\d]+)$")
+                .match("^(Belastung|Gutschrift) [\\w]{3} .* (?<currency>[\\w]{3}) (?<amount>[\\.'\\d]+)$")
                 .assign((t, v) -> {
                     t.setCurrencyCode(asCurrencyCode(v.get("currency")));
                     t.setAmount(asAmount(v.get("amount")));
                 })
 
+                // @formatter:off
+                // Auftragsnummer: 210796978 Kundenportfolio
+                // @formatter:on
+                .section("note").optional()
+                .match("^(?<note>Auftragsnummer: .*) .*$")
+                .assign((t, v) -> t.setNote(trim(v.get("note"))))
+
+                // @formatter:off
                 // Valorennummer 906020
+                // @formatter:on
                 .section("note").optional()
                 .match("^(?<note>Valorennummer .*)$")
-                .assign((t, v) -> t.setNote(trim(v.get("note"))))
+                .assign((t, v) -> {
+                    if (t.getNote() != null)
+                        t.setNote(t.getNote() + " | " + trim(v.get("note")));
+                    else
+                        t.setNote(trim(v.get("note")));
+                })
 
                 .wrap(BuySellEntryItem::new);
 
@@ -100,45 +162,57 @@ public class LGTBankPDFExtractor extends AbstractPDFExtractor
 
         Block block = new Block("^Baraussch.ttung .*$");
         type.addBlock(block);
-        Transaction<AccountTransaction> pdfTransaction = new Transaction<AccountTransaction>().subject(() -> {
+        Transaction<AccountTransaction> pdfTransaction = new Transaction<>();
+
+        pdfTransaction.subject(() -> {
             AccountTransaction entry = new AccountTransaction();
             entry.setType(AccountTransaction.Type.DIVIDENDS);
             return entry;
         });
 
         pdfTransaction
+                // @formatter:off
                 // 551 Veolia Environnement SA
                 // Namen- und Inhaber-Aktien
                 // ISIN: FR0000124141, Valoren-Nr.: 1098758
                 // Ausschüttung EUR 0.50
+                // @formatter:on
                 .section("name", "nameContinued", "isin", "wkn", "currency")
-                .find("Stand Ihres Depots am [\\d]{2}\\. .* [\\d]{4}:")
+                .find("Stand Ihres Depots am [\\d]{1,2}\\. .* [\\d]{4}:")
                 .match("^[\\.,\\d]+ (?<name>.*)$")
                 .match("^(?<nameContinued>.*)$")
-                .match("^ISIN: (?<isin>[\\w]{12}), Valoren\\-Nr\\.: (?<wkn>.*)$")
+                .match("^.*ISIN: (?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9]), Valoren\\-Nr\\.: (?<wkn>.*)$")
                 .match("^Aussch.ttung (?<currency>[\\w]{3}) [\\.,'\\d]+$")
                 .assign((t, v) -> t.setSecurity(getOrCreateSecurity(v)))
 
+                // @formatter:off
                 // 551 Veolia Environnement SA
+                // @formatter:on
                 .section("shares")
-                .find("Stand Ihres Depots am [\\d]{2}\\. .* [\\d]{4}:")
+                .find("Stand Ihres Depots am [\\d]{1,2}\\. .* [\\d]{4}:")
                 .match("^(?<shares>[\\.,\\d]+) .*$")
                 .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
 
+                // @formatter:off
                 // Valuta 14. Mai 2020
+                // @formatter:on
                 .section("date")
                 .match("^Valuta (?<date>[\\d]{2}\\. .* [\\d]{4})$")
                 .assign((t, v) -> t.setDateTime(asDate(v.get("date"))))
 
+                // @formatter:off
                 // Netto EUR 198.36
+                // @formatter:on
                 .section("currency", "amount")
-                .match("^Netto (?<currency>[\\w]{3}) (?<amount>[\\.,'\\d]+)$")
+                .match("^Netto (?<currency>[\\w]{3}) (?<amount>[\\.'\\d]+)$")
                 .assign((t, v) -> {
                     t.setCurrencyCode(asCurrencyCode(v.get("currency")));
                     t.setAmount(asAmount(v.get("amount")));
                 })
 
+                // @formatter:off
                 // Ausschüttungsart Ordentliche Dividende
+                // @formatter:on
                 .section("note").optional()
                 .match("^Aussch.ttungsart (?<note>.*)$")
                 .assign((t, v) -> t.setNote(trim(v.get("note"))))
@@ -154,28 +228,51 @@ public class LGTBankPDFExtractor extends AbstractPDFExtractor
     private <T extends Transaction<?>> void addTaxesSectionsTransaction(T transaction, DocumentType type)
     {
         transaction
+                // @formatter:off
                 // Eidg. Umsatzabgabe  DKK 121.19
+                // Eidg. Umsatzabgabe  USD -12.51
+                // @formatter:on
                 .section("tax", "currency").optional()
-                .match("^Eidg\\. Umsatzabgabe ([\\s]+)?(?<currency>[\\w]{3}) (?<tax>[\\.,'\\d]+)$")
+                .match("^Eidg\\. Umsatzabgabe ([\\s]+)?(?<currency>[\\w]{3}) (\\-)?(?<tax>[\\.'\\d]+)$")
                 .assign((t, v) -> processTaxEntries(t, v, type))
 
+                // @formatter:off
+                // Verrechnungssteuer 35 % CHF -851.20
+                // @formatter:on
+                .section("tax", "currency").optional()
+                .match("^Verrechnungssteuer [\\d]+ % (?<currency>[\\w]{3}) (\\-)?(?<tax>[\\.'\\d]+)$")
+                .assign((t, v) -> processTaxEntries(t, v, type))
+
+                // @formatter:off
                 // Quellensteuer 28 % EUR -77.14
+                // @formatter:on
                 .section("withHoldingTax", "currency").optional()
-                .match("^Quellensteuer [\\d]+ % (?<currency>[\\w]{3}) \\-(?<withHoldingTax>[\\.,'\\d]+)$")
+                .match("^Quellensteuer [\\d]+ % (?<currency>[\\w]{3}) (\\-)?(?<withHoldingTax>[\\.'\\d]+)$")
                 .assign((t, v) -> processWithHoldingTaxEntries(t, v, "withHoldingTax", type));
     }
 
     private <T extends Transaction<?>> void addFeesSectionsTransaction(T transaction, DocumentType type)
     {
         transaction
+                // @formatter:off
                 // Courtage  DKK 1'534.90
+                // @formatter:on
                 .section("fee", "currency").optional()
-                .match("^Courtage ([\\s]+)?(?<currency>[\\w]{3}) (?<fee>[\\.,'\\d]+)$")
+                .match("^Courtage ([\\s]+)?(?<currency>[\\w]{3}) (?<fee>[\\.'\\d]+)$")
                 .assign((t, v) -> processFeeEntries(t, v, type))
 
-                // Broker Kommission  DKK 12.12
+                // @formatter:off
+                // SIX Meldegebühr  CHF 0.20
+                // @formatter:on
                 .section("fee", "currency").optional()
-                .match("^Broker Kommission ([\\s]+)?(?<currency>[\\w]{3}) (?<fee>[\\.,'\\d]+)$")
+                .match("^.* Meldegebühr ([\\s]+)?(?<currency>[\\w]{3}) (?<fee>[\\.'\\d]+)$")
+                .assign((t, v) -> processFeeEntries(t, v, type))
+
+                // @formatter:off
+                // Broker Kommission  DKK 12.12
+                // @formatter:on
+                .section("fee", "currency").optional()
+                .match("^Broker Kommission ([\\s]+)?(?<currency>[\\w]{3}) (?<fee>[\\.'\\d]+)$")
                 .assign((t, v) -> processFeeEntries(t, v, type));
     }
 
