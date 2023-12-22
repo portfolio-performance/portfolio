8abdcf0d1246bdbb81d2b62c10ce38d92745a073
ggrossbe <guenter.grossberger@gmx.at>
2023-12-22 08:43:26 +0100

    use locale en_US directly and currency from text where possible

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/FidelityPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/FidelityPDFExtractor.java
@@ -1,9 +1,7 @@
 package name.abuchen.portfolio.datatransfer.pdf;
 
-import java.time.LocalDateTime;
 import java.util.Locale;
 
-import name.abuchen.portfolio.datatransfer.ExtractorUtils;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
@@ -12,7 +10,6 @@ import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.PortfolioTransaction;
 import name.abuchen.portfolio.model.Transaction.Unit;
 import name.abuchen.portfolio.money.Money;
-import name.abuchen.portfolio.money.Values;
 
 @SuppressWarnings("nls")
 public class FidelityPDFExtractor extends AbstractPDFExtractor
@@ -57,24 +54,25 @@ public class FidelityPDFExtractor extends AbstractPDFExtractor
 
                         .section("date") //
                         .match("^.* \\w{2}# (?<date>[\\d]{2}\\-[\\d]{2}\\-[\\d]{2}) .*$")
-                        .assign((t, v) -> t.setDate(asDate(v.get("date"))))
+                        .assign((t, v) -> t.setDate(asDate(v.get("date"), Locale.US)))
 
                         .section("type", "gross", "tickerSymbol", "name") //
                         .match("^You (?<type>Bought|Sold) (?<name>.*) Principal Amount\\s+(?<gross>[\\.,\\d]+)$") //
                         .match("^(?!REFERENCE)(?!DESCRIPTION)(?<tickerSymbol>[A-Z]{2,5}).*$") //
                         .assign((t, v) -> {
-                            t.setCurrencyCode(asCurrencyCode("USD"));
+                            // there is no currency in the text
+                            t.setCurrencyCode("USD");
                             v.put("currency", "USD");
                             t.setSecurity(getOrCreateSecurity(v));
                             if ("Sold".equals(v.get("type")))
                             {
-                                    t.setType(PortfolioTransaction.Type.SELL);
+                                t.setType(PortfolioTransaction.Type.SELL);
                             }
                         })
 
                         .section("amount") //
                         .match(".*Settlement Amount\\s+(?<amount>[\\.,\\d]+)$")
-                        .assign((t, v) -> t.setAmount(asAmount(v.get("amount"))))
+                        .assign((t, v) -> t.setAmount(asAmount(v.get("amount"), "en", "US")))
 
                         .section("fee") //
                         .optional() //
@@ -82,13 +80,13 @@ public class FidelityPDFExtractor extends AbstractPDFExtractor
                         .assign((t, v) -> {
                             t.getPortfolioTransaction()
                                         .addUnit(new Unit(Unit.Type.FEE,
-                                                        Money.of(asCurrencyCode(asCurrencyCode("USD")),
-                                                                            asAmount(v.get("fee")))));
+                                                            Money.of(asCurrencyCode(("USD")),
+                                                                            asAmount(v.get("fee"), "en", "US"))));
                         })
 
                         .section("shares") //
                         .match("^\\s+(?<shares>[\\.,\\d]+)\\s.*$") //
-                        .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
+                        .assign((t, v) -> t.setShares(asShares(v.get("shares"), "en", "US")))
 
                         .wrap(BuySellEntryItem::new));
     }
@@ -133,31 +131,30 @@ public class FidelityPDFExtractor extends AbstractPDFExtractor
                             {
                                 date = date.charAt(0) + date.substring(1, 3).toLowerCase() + date.substring(3);
                             }
-                            t.setDate(asDate(date));
+                            t.setDate(asDate(date, Locale.US));
                         })
 
                         .section("shares") //
                         .match("^YOU SOLD (?<shares>[\\.,\\d]+) AT.*$") //
-                        .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
+                        .assign((t, v) -> t.setShares(asShares(v.get("shares"), "en", "US")))
 
-                        .section("gross", "tickerSymbol") //
-                        .match("SECURITY DESCRIPTION SYMBOL: (?<tickerSymbol>[A-Z]{2,5}) Sale Proceeds\\s+\\$(?<gross>[\\.,\\d]+)$") //
+                        .section("gross", "tickerSymbol", "currency") //
+                        .match("SECURITY DESCRIPTION SYMBOL: (?<tickerSymbol>[A-Z]{2,5}) Sale Proceeds\\s+(?<currency>\\p{Sc})(?<gross>[\\.,\\d]+)$") //
                         .assign((t, v) -> {
-                            t.setCurrencyCode(asCurrencyCode("USD"));
-                            v.put("currency", "USD");
+                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
                             t.setSecurity(getOrCreateSecurity(v));
                         })
 
                         .section("amount") //
                         .match(".*Net Cash Proceeds.*\\$(?<amount>[\\.,\\d]+)$")
-                        .assign((t, v) -> t.setAmount(asAmount((v.get("amount")))))
+                        .assign((t, v) -> t.setAmount(asAmount(v.get("amount"), "en", "US")))
 
-                        .section("fee") //
+                        .section("fee", "currency") //
                         .optional() //
-                        .match(".*Total Fees\\s+\\$(?<fee>[\\.,\\d]+)").assign((t, v) -> {
+                        .match(".*Total Fees\\s+(?<currency>\\p{Sc})(?<fee>[\\.,\\d]+)").assign((t, v) -> {
                             t.getPortfolioTransaction().addUnit(
-                                            new Unit(Unit.Type.FEE, Money.of(asCurrencyCode(asCurrencyCode("USD")),
-                                                            asAmount((v.get("fee"))))));
+                                            new Unit(Unit.Type.FEE, Money.of(asCurrencyCode(v.get("currency")),
+                                                            asAmount(v.get("fee"), "en", "US"))));
                         })
 
                         .wrap(BuySellEntryItem::new));
@@ -168,49 +165,4 @@ public class FidelityPDFExtractor extends AbstractPDFExtractor
     {
         return "Fidelity"; //$NON-NLS-1$
     }
-
-    @Override
-    protected long asAmount(String value)
-    {
-        String language = "de";
-        String country = "DE";
-
-        int lastDot = value.lastIndexOf(".");
-        int lastComma = value.lastIndexOf(",");
-
-        // returns the greater of two int values
-        if (Math.max(lastDot, lastComma) == lastDot)
-        {
-            language = "en";
-            country = "US";
-        }
-
-        return ExtractorUtils.convertToNumberLong(value, Values.Amount, language, country);
-    }
-
-    @Override
-    protected long asShares(String value)
-    {
-        String language = "de";
-        String country = "DE";
-
-        int lastDot = value.lastIndexOf(".");
-        int lastComma = value.lastIndexOf(",");
-
-        // returns the greater of two int values
-        if (Math.max(lastDot, lastComma) == lastDot)
-        {
-            language = "en";
-            country = "US";
-        }
-
-        return ExtractorUtils.convertToNumberLong(value, Values.Share, language, country);
-    }
-
-    @Override
-    protected LocalDateTime asDate(String value, Locale... hints)
-    {
-        return ExtractorUtils.asDate(value, Locale.US);
-    }
-
 }
