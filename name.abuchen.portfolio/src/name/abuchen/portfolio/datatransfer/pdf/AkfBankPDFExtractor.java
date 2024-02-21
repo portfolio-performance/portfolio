ccfb9a3f2d11bad43da6edf214e63d552d042020
Nirus2000 <webmaster@nirus-online.de>
2024-02-21 10:54:59 +0100

    Improve AKF Bank PDF-Importer
    Rename test files (missing "s" in name)
    Rename test cases (missing "s" in name)
    Remove BiConsumer
    Add fees transaction
    Add note
    Improve source code

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/AkfBankPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/AkfBankPDFExtractor.java
@@ -1,46 +1,44 @@
 package name.abuchen.portfolio.datatransfer.pdf;
 
-import java.util.Map;
-import java.util.function.BiConsumer;
-
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.ParsedData;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
 import name.abuchen.portfolio.model.AccountTransaction;
-import name.abuchen.portfolio.model.AccountTransaction.Type;
 import name.abuchen.portfolio.model.Client;
 
 @SuppressWarnings("nls")
 public class AkfBankPDFExtractor extends AbstractPDFExtractor
 {
+    private static final String DEPOSIT = "^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} (?<note>(Gutschrift|SEPA Gutschrift Bank)) (?<amount>[\\.,\\d]+)$";
+    private static final String REMOVAL = "^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} (?<note>(?!Einzel)(.*berweisung.*|Festgeld Anlage|Sparkonto K.ndigung)) \\-(?<amount>[\\.,\\d]+)$";
+    private static final String INTEREST = "^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Kontoabschluß (?<amount>[\\.,\\d]+)$";
 
-    private static final String DEPOSIT = "^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ ([\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) (.*Gutschrift.*) (?<amount>[\\.\\d]+,[\\d]{2})";
-    private static final String REMOVAL = "^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ ([\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) (.*berweisung.*|Festgeld Anlage|Sparkonto K[ü|ue]ndigung) [-](?<amount>[\\.\\d]+,[\\d]{2})";
-    private static final String INTEREST = "^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ ([\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) (Kontoabschlu[ß|ss]) (?<amount>[\\.\\d]+,[\\d]{2})";
-    private static final String FEES = "^(?<fee>.*)$";
-
-    private static final String CONTEXT_KEY_CURRENCY = "currency";
+    private static final String FEES = "^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Einzel.berweisung \\-(?<amount>[\\.,\\d]+)$";
+    private static final String FEES_LINE_2 = "^Geb.hren .*$";
 
     public AkfBankPDFExtractor(Client client)
     {
         super(client);
 
-        addBankIdentifier("akf bank");
+        addBankIdentifier("akf bank GmbH & Co KG");
 
-        addTransactions();
+        addAccountStatementTransaction();
     }
 
     @Override
     public String getLabel()
     {
-        return "akf bank";
+        return "akf bank GmbH & Co KG";
     }
 
-    private void addTransactions()
+    private void addAccountStatementTransaction()
     {
         final DocumentType type = new DocumentType("akf bank", //
                         documentContext -> documentContext //
+                                        // @formatter:off
+                                        // Nr. Valuta / Buchungstag Buchungstext EUR
+                                        // @formatter:on
                                         .section("currency") //
                                         .match("^Nr\\. Valuta \\/ Buchungstag Buchungstext (?<currency>[\\w]{3}).*$") //
                                         .assign((ctx, v) -> ctx.put("currency", asCurrencyCode(v.get("currency")))));
@@ -52,54 +50,104 @@ public class AkfBankPDFExtractor extends AbstractPDFExtractor
         type.addBlock(depositBlock);
 
         Block removalBlock = new Block(REMOVAL);
-        removalBlock.set(removalTransaction(type, REMOVAL, FEES));
+        removalBlock.set(removalTransaction(type, REMOVAL));
         type.addBlock(removalBlock);
 
         Block interestBlock = new Block(INTEREST);
         interestBlock.set(interestTransaction(type, INTEREST));
         type.addBlock(interestBlock);
+
+        Block feeBlock = new Block(FEES);
+        feeBlock.set(feesTransaction(type, FEES, FEES_LINE_2));
+        type.addBlock(feeBlock);
     }
 
     private Transaction<AccountTransaction> depositTransaction(DocumentType type, String regex)
     {
-        return new Transaction<AccountTransaction>().subject(() -> {
-            AccountTransaction entry = new AccountTransaction();
-            entry.setType(AccountTransaction.Type.DEPOSIT);
-            return entry;
-        }).section("date", "amount").match(regex).assign(assignmentsProvider(type)).wrap(TransactionItem::new);
+        return new Transaction<AccountTransaction>() //
+
+                        .subject(() -> {
+                            AccountTransaction accountTransaction = new AccountTransaction();
+                            accountTransaction.setType(AccountTransaction.Type.DEPOSIT);
+                            return accountTransaction;
+                        })
+
+                        .section("date", "amount", "note") //
+                        .documentContext("currency") //
+                        .match(regex) //
+                        .assign((t, v) -> {
+                            assignmentsProvider(t, v);
+                        })
+
+                        .wrap(TransactionItem::new);
+    }
+
+    private Transaction<AccountTransaction> removalTransaction(DocumentType type, String regex)
+    {
+        return new Transaction<AccountTransaction>() //
+
+                        .subject(() -> {
+                            AccountTransaction accountTransaction = new AccountTransaction();
+                            accountTransaction.setType(AccountTransaction.Type.REMOVAL);
+                            return accountTransaction;
+                        })
+
+                        .section("date", "amount", "note") //
+                        .documentContext("currency") //
+                        .match(regex) //
+                        .assign((t, v) -> {
+                            assignmentsProvider(t, v);
+                        })
+
+                        .wrap(TransactionItem::new);
     }
 
     private Transaction<AccountTransaction> interestTransaction(DocumentType type, String regex)
     {
-        return new Transaction<AccountTransaction>().subject(() -> {
-            AccountTransaction entry = new AccountTransaction();
-            entry.setType(AccountTransaction.Type.INTEREST);
-            return entry;
-        }).section("date", "amount").match(regex).assign(assignmentsProvider(type)).wrap(TransactionItem::new);
-    }
+        return new Transaction<AccountTransaction>() //
 
-    private Transaction<AccountTransaction> removalTransaction(DocumentType type, String regex, String regexFee)
-    {
-        return new Transaction<AccountTransaction>().subject(() -> {
-            AccountTransaction entry = new AccountTransaction();
-            entry.setType(AccountTransaction.Type.REMOVAL);
-            return entry;
-        }).section("date", "amount", "fee").match(regex).match(regexFee).assign(assignmentsProvider(type))
+                        .subject(() -> {
+                            AccountTransaction accountTransaction = new AccountTransaction();
+                            accountTransaction.setType(AccountTransaction.Type.INTEREST);
+                            return accountTransaction;
+                        })
+
+                        .section("date", "amount") //
+                        .documentContext("currency") //
+                        .match(regex) //
+                        .assign((t, v) -> {
+                            assignmentsProvider(t, v);
+                        })
+
                         .wrap(TransactionItem::new);
     }
 
-    private BiConsumer<AccountTransaction, ParsedData> assignmentsProvider(DocumentType type)
+    private Transaction<AccountTransaction> feesTransaction(DocumentType type, String regexFeeLine1, String regexFeeLine2)
     {
-        return (transaction, matcherMap) -> {
-            Map<String, String> context = type.getCurrentContext();
+        return new Transaction<AccountTransaction>() //
 
-            transaction.setDateTime(asDate(matcherMap.get("date")));
-            transaction.setAmount(asAmount(matcherMap.get("amount")));
-            if ((matcherMap.get("fee") != null && matcherMap.get("fee").contains("Gebühren")))
-            {
-                transaction.setType(Type.FEES);
-            }
-            transaction.setCurrencyCode(context.get(AkfBankPDFExtractor.CONTEXT_KEY_CURRENCY));
-        };
+                        .subject(() -> {
+                            AccountTransaction accountTransaction = new AccountTransaction();
+                            accountTransaction.setType(AccountTransaction.Type.FEES);
+                            return accountTransaction;
+                        })
+
+                        .section("date", "amount") //
+                        .documentContext("currency") //
+                        .match(regexFeeLine1) //
+                        .match(regexFeeLine2) //
+                        .assign((t, v) -> {
+                            assignmentsProvider(t, v);
+                        })
+
+                        .wrap(TransactionItem::new);
+    }
+
+    private void assignmentsProvider(AccountTransaction t, ParsedData v)
+    {
+        t.setDateTime(asDate(v.get("date")));
+        t.setAmount(asAmount(v.get("amount")));
+        t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+        t.setNote(v.get("note"));
     }
 }
