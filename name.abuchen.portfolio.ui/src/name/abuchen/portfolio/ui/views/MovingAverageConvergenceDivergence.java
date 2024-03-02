4d321669657f2f56cb7191d831e383cf1a9af049
Morpheus1w3 <103072607+Morpheus1w3@users.noreply.github.com>
2024-03-02 03:45:53 +0100

    Bug MACD

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/MovingAverageConvergenceDivergence.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/MovingAverageConvergenceDivergence.java
@@ -95,12 +95,12 @@ public class MovingAverageConvergenceDivergence
             }
         }
 
-        if (valuesMacd.length >= MAX_MACD_PERIOD)
+        if (valuesMacd.size() >= MAX_MACD_PERIOD)
         {
             macd.setDates(TimelineChart.toJavaUtilDate(datesMacd.toArray(new LocalDate[0])));
             macd.setValues(Doubles.toArray(valuesMacd));
         }
-        if (valuesSignal.length >= SIGNAL_LINE_START)
+        if (valuesSignal.size() >= SIGNAL_LINE_START)
         {
             signalLine.setDates(TimelineChart.toJavaUtilDate(datesSignal.toArray(new LocalDate[0])));
             signalLine.setValues(Doubles.toArray(valuesSignal));
