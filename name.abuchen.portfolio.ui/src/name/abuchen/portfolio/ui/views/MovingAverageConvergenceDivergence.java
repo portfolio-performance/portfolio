5234e6b46e3d46a44843af7dec9bc60ffdaeb578
Morpheus1w3 <103072607+Morpheus1w3@users.noreply.github.com>
2024-03-02 03:07:58 +0100

    Bug fix MACD

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/MovingAverageConvergenceDivergence.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/MovingAverageConvergenceDivergence.java
@@ -95,10 +95,16 @@ public class MovingAverageConvergenceDivergence
             }
         }
 
-        macd.setDates(TimelineChart.toJavaUtilDate(datesMacd.toArray(new LocalDate[0])));
-        signalLine.setDates(TimelineChart.toJavaUtilDate(datesSignal.toArray(new LocalDate[0])));
-        macd.setValues(Doubles.toArray(valuesMacd));
-        signalLine.setValues(Doubles.toArray(valuesSignal));
+        if (valuesMacd.length > 0)
+            {
+            macd.setDates(TimelineChart.toJavaUtilDate(datesMacd.toArray(new LocalDate[0])));
+            macd.setValues(Doubles.toArray(valuesMacd));
+        }
+        if (valuesSignal.length > 0)
+        {
+            signalLine.setDates(TimelineChart.toJavaUtilDate(datesSignal.toArray(new LocalDate[0])));
+            signalLine.setValues(Doubles.toArray(valuesSignal));
+        }
     }
 
     private static final class ExponentialMovingAverageCalculator
