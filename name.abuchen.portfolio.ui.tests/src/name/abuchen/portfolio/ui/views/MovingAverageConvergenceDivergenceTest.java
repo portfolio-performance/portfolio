c11eac2169adc6213d5cf22376f85ac771714211
Morpheus1w3 <103072607+Morpheus1w3@users.noreply.github.com>
2024-03-02 14:27:35 +0100

    Bug MACD

--- name.abuchen.portfolio.ui.tests/src/name/abuchen/portfolio/ui/views/MovingAverageConvergenceDivergenceTest.java
+++ name.abuchen.portfolio.ui.tests/src/name/abuchen/portfolio/ui/views/MovingAverageConvergenceDivergenceTest.java
@@ -55,8 +55,8 @@ public class MovingAverageConvergenceDivergenceTest
         MovingAverageConvergenceDivergence macd = new MovingAverageConvergenceDivergence(security, interval);
         ChartLineSeriesAxes macdLine = macd.getMacdLine();
         ChartLineSeriesAxes signalLine = macd.getSignalLine();
-        assertThat(macdLine.getDates(), is(null));
-        assertThat(signalLine.getDates(), is(null));
+        assertThat(macdLine.getDates(), is(IsNull.nullValue()));
+        assertThat(signalLine.getDates(), is(IsNull.nullValue()));
     }
 
     @SuppressWarnings("unchecked")
@@ -96,10 +96,10 @@ public class MovingAverageConvergenceDivergenceTest
         MovingAverageConvergenceDivergence macd = new MovingAverageConvergenceDivergence(security, interval);
         ChartLineSeriesAxes macdLine = macd.getMacdLine();
         ChartLineSeriesAxes signalLine = macd.getSignalLine();
-        assertThat(macdLine.getDates(), is(null));
-        assertThat(signalLine.getDates(), is(null));
-        assertThat(macdLine.getValues().length, 0));
-        assertThat(signalLine.getValues().length, 0));
+        assertThat(macdLine.getDates(), is(IsNull.nullValue()));
+        assertThat(signalLine.getDates(), is(IsNull.nullValue()));
+        assertThat(macdLine.getValues().length, 0);
+        assertThat(signalLine.getValues().length, 0);
     }
 
     @Test
