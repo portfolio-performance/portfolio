28477a3e3e3e5a84a32a463b50618347163802c3
Nirus2000 <webmaster@nirus-online.de>
2024-03-20 07:22:20 +0100

    Improve Stake PDF-Importer
    Update source
    Format source to standard eclipse format
    Rename importer

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/StakeshopPtyLtdPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/StakeshopPtyLtdPDFExtractor.java
@@ -2,8 +2,6 @@ package name.abuchen.portfolio.datatransfer.pdf;
 
 import static name.abuchen.portfolio.util.TextUtil.trim;
 
-import java.math.BigDecimal;
-
 import name.abuchen.portfolio.datatransfer.ExtractorUtils;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
@@ -13,14 +11,24 @@ import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.PortfolioTransaction;
 import name.abuchen.portfolio.money.Values;
 
+/**
+ * @formatter:off
+ * @implNote Stakeshop Pty Ltd, trading as Stake, is a Australian dollar-based financial services company.
+ *           The currency is AUD --> A$.
+ *
+ * @implSpec The PDF does not include the name of the security.
+ *           If it is created, identify it at least by the ticker.
+ * @formatter:on
+ */
+
 @SuppressWarnings("nls")
-public class StakePDFExtractor extends AbstractPDFExtractor
+public class StakeshopPtyLtdPDFExtractor extends AbstractPDFExtractor
 {
-    public StakePDFExtractor(Client client)
+    public StakeshopPtyLtdPDFExtractor(Client client)
     {
         super(client);
 
-        addBankIdentifier("Stake");
+        addBankIdentifier("Stakeshop Pty Ltd");
 
         addBuySellTransaction();
     }
@@ -28,7 +36,7 @@ public class StakePDFExtractor extends AbstractPDFExtractor
     @Override
     public String getLabel()
     {
-        return "Stake";
+        return "Stakeshop Pty Ltd (Stake)";
     }
 
     private void addBuySellTransaction()
@@ -36,20 +44,21 @@ public class StakePDFExtractor extends AbstractPDFExtractor
         DocumentType type = new DocumentType("(BUY|SELL) CONFIRMATION");
         this.addDocumentTyp(type);
 
+        Transaction<BuySellEntry> pdfTransaction = new Transaction<>();
+
         Block firstRelevantLine = new Block("^(BUY|SELL) CONFIRMATION$");
         type.addBlock(firstRelevantLine);
-
-        Transaction<BuySellEntry> pdfTransaction = new Transaction<>();
-        pdfTransaction.subject(() -> {
-            BuySellEntry entry = new BuySellEntry();
-            entry.setType(PortfolioTransaction.Type.BUY);
-            return entry;
-        });
-
         firstRelevantLine.set(pdfTransaction);
 
-        pdfTransaction
-                        // Is type --> "Sell" change from BUY to SELL
+        pdfTransaction //
+
+                        .subject(() -> {
+                            BuySellEntry portfolioTransaction = new BuySellEntry();
+                            portfolioTransaction.setType(PortfolioTransaction.Type.BUY);
+                            return portfolioTransaction;
+                        })
+
+                        // Is type --> "sell" change from BUY to SELL
                         .section("type").optional() //
                         .match("^(?<type>(BUY|SELL)) CONFIRMATION$") //
                         .assign((t, v) -> {
@@ -57,40 +66,46 @@ public class StakePDFExtractor extends AbstractPDFExtractor
                                 t.setType(PortfolioTransaction.Type.SELL);
                         })
 
-                        // QUANTITY 512
-                        .section("shares") //
-                        .match("^QUANTITY (?<shares>[\\.,\\d]+)$") //
-                        .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
-
+                        // @formatter:off
                         // EFFECTIVE PRICE $20.43 TICKER FLT.ASX
-                        .section("tickerSymbol", "currency")
-                        .match("^EFFECTIVE PRICE \\p{Sc}[\\.,\\d]+ TICKER (?<tickerSymbol>[\\w]{3,4})\\..*$")
+                        // @formatter:on
+                        .section("tickerSymbol", "currency") //
+                        .match("^EFFECTIVE PRICE \\p{Sc}[\\.,\\d]+ TICKER (?<tickerSymbol>[\\w]{3,4})\\..*$") //
                         .match("^VALUE (?<currency>[\\w]{1}\\p{Sc}).*") //
                         .assign((t, v) -> {
-                            // the PDF does not include the name of the
-                            // security; if it is created, identify it at least
-                            // by the ticker
                             v.put("name", v.get("tickerSymbol"));
                             t.setSecurity(getOrCreateSecurity(v));
                         })
 
+                        // @formatter:off
+                        // QUANTITY 512
+                        // @formatter:on
+                        .section("shares") //
+                        .match("^QUANTITY (?<shares>[\\.,\\d]+)$") //
+                        .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
+
+                        // @formatter:off
                         // VALUE A$10,455.04 EXECUTION DATE 27-05-2022
+                        // @formatter:on
                         .section("date") //
-                        .match("^VALUE A\\p{Sc}[\\.,\\d]+ EXECUTION DATE (?<date>[\\d]+-.*-[\\d]{4})$")
+                        .match("^VALUE A\\p{Sc}[\\.,\\d]+ EXECUTION DATE (?<date>[\\d]{1,2}\\-[\\d]{1,2}\\-[\\d]{4})$") //
                         .assign((t, v) -> t.setDate(asDate(v.get("date"))))
 
+                        // @formatter:off
                         // AMOUNT DUE & PAYABLE A$10,458.04 Funds have already
-                        // been deducted from your buying power. No action
-                        .section("currency", "amount")
-                        .match("^AMOUNT DUE & PAYABLE (?<currency>[\\w]{1}\\p{Sc})(?<amount>[\\.,\\d]+) .*")
+                        // @formatter:on
+                        .section("currency", "amount") //
+                        .match("^AMOUNT DUE & PAYABLE (?<currency>A\\p{Sc})(?<amount>[\\.,\\d]+).*") //
                         .assign((t, v) -> {
                             t.setCurrencyCode(asCurrencyCode(v.get("currency")));
                             t.setAmount(asAmount(v.get("amount")));
                         })
 
+                        // @formatter:off
                         // CONFIRMATION NUMBER 0000001 PID 3556
+                        // @formatter:on
                         .section("note").optional()//
-                        .match("^CONFIRMATION NUMBER (?<note>.*) PID 3556$")
+                        .match("^CONFIRMATION NUMBER (?<note>.*) PID [\\d]+$")
                         .assign((t, v) -> t.setNote(trim(v.get("note"))))
 
                         .wrap(BuySellEntryItem::new);
@@ -100,10 +115,13 @@ public class StakePDFExtractor extends AbstractPDFExtractor
 
     private <T extends Transaction<?>> void addFeesSectionsTransaction(T transaction, DocumentType type)
     {
-        transaction
+        transaction //
+
+                        // @formatter:off
                         // BROKERAGE & GST A$3.00 SIDE BUY
+                        // @formatter:on
                         .section("fee", "currency").optional() //
-                        .match("^BROKERAGE & GST (?<currency>[\\w]{1}\\p{Sc})(?<fee>[\\.,\\d]+) SIDE (BUY|SELL)$")
+                        .match("^BROKERAGE & GST (?<currency>A\\p{Sc})(?<fee>[\\.,\\d]+) SIDE (BUY|SELL)$")
                         .assign((t, v) -> processFeeEntries(t, v, type));
     }
 
@@ -117,11 +135,5 @@ public class StakePDFExtractor extends AbstractPDFExtractor
     protected long asShares(String value)
     {
         return ExtractorUtils.convertToNumberLong(value, Values.Share, "en", "AU");
-    }
-
-    @Override
-    protected BigDecimal asExchangeRate(String value)
-    {
-        return ExtractorUtils.convertToNumberBigDecimal(value, Values.Share, "en", "AU");
     }
 }
