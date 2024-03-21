4bd467458a3b50c9a0065efd091f1159919362e2
Nirus2000 <webmaster@nirus-online.de>
2024-03-21 07:23:35 +0100

    Improve KBC Group NV PDF-Importer
    Format source to standard eclipse format
    Improve regulare expressions
    Remove obsolet source

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/KBCGroupNVPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/KBCGroupNVPDFExtractor.java
@@ -33,48 +33,60 @@ public class KBCGroupNVPDFExtractor extends AbstractPDFExtractor
         this.addDocumentTyp(type);
 
         Transaction<BuySellEntry> pdfTransaction = new Transaction<>();
-        pdfTransaction.subject(() -> {
-            BuySellEntry entry = new BuySellEntry();
-            entry.setType(PortfolioTransaction.Type.BUY);
-            return entry;
-        });
 
         Block firstRelevantLine = new Block("^Borderel [\\d]+.*$");
         type.addBlock(firstRelevantLine);
         firstRelevantLine.set(pdfTransaction);
 
-        pdfTransaction
-                // Uw Aankoop Online van 15 PROSUS N.V. (AS) aan 57,2 EUR 858,00 EUR
-                // Waardecode NL0013654783
-                .section("name", "currency", "isin")
-                .match("^.* van [\\.,\\d]+ (?<name>.*) aan [\\.,\\d]+ (?<currency>[\\w]{3}) [\\.,\\d]+ [\\w]{3}$")
-                .match("^Waardecode (?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9])$")
-                .assign((t, v) -> t.setSecurity(getOrCreateSecurity(v)))
+        pdfTransaction //
 
-                // Uw Aankoop Online van 15 PROSUS N.V. (AS) aan 57,2 EUR 858,00 EUR
-                .section("shares").optional()
-                .match("^.* van (?<shares>[\\.,\\d]+) .* aan [\\.,\\d]+ [\\w]{3} [\\.,\\d]+ [\\w]{3}$")
-                .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
+                        .subject(() -> {
+                            BuySellEntry portfolioTransaction = new BuySellEntry();
+                            portfolioTransaction.setType(PortfolioTransaction.Type.BUY);
+                            return portfolioTransaction;
+                        })
 
-                // 02/02/2022 14:50:02 Valuta 04/02/2022 Euronext A'dam
-                .section("date", "time")
-                .match("^(?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2}) Valuta [\\d]{2}\\/[\\d]{2}\\/[\\d]{4} .*$")
-                .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
+                        // @formatter:off
+                        // Uw Aankoop Online van 15 PROSUS N.V. (AS) aan 57,2 EUR 858,00 EUR
+                        // Waardecode NL0013654783
+                        // @formatter:on
+                        .section("name", "currency", "isin") //
+                        .match("^.* van [\\.,\\d]+ (?<name>.*) aan [\\.,\\d]+ (?<currency>[\\w]{3}) [\\.,\\d]+ [\\w]{3}$") //
+                        .match("^Waardecode (?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9])$") //
+                        .assign((t, v) -> t.setSecurity(getOrCreateSecurity(v)))
 
-                // Netto debit -868,50 EUR
-                .section("amount", "currency")
-                .match("^Netto debit \\-(?<amount>[\\.,\\d]+) (?<currency>[\\w]{3})$")
-                .assign((t, v) -> {
-                    t.setCurrencyCode(asCurrencyCode(v.get("currency")));
-                    t.setAmount(asAmount(v.get("amount")));
-                })
+                        // @formatter:off
+                        // Uw Aankoop Online van 15 PROSUS N.V. (AS) aan 57,2 EUR 858,00 EUR
+                        // @formatter:on
+                        .section("shares") //
+                        .match("^.* van (?<shares>[\\.,\\d]+) .* aan [\\.,\\d]+ [\\w]{3} [\\.,\\d]+ [\\w]{3}$") //
+                        .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
 
-                // Borderel 275825809 Limit order
-                .section("note").optional()
-                .match("^(?<note>Borderel [\\d]+).*$")
-                .assign((t, v) -> t.setNote(trim(v.get("note"))))
+                        // @formatter:off
+                        // 02/02/2022 14:50:02 Valuta 04/02/2022 Euronext A'dam
+                        // @formatter:on
+                        .section("date", "time") //
+                        .match("^(?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2}) Valuta [\\d]{2}\\/[\\d]{2}\\/[\\d]{4} .*$") //
+                        .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
 
-                .wrap(BuySellEntryItem::new);
+                        // @formatter:off
+                        // Netto debit -868,50 EUR
+                        // @formatter:on
+                        .section("amount", "currency") //
+                        .match("^Netto debit \\-(?<amount>[\\.,\\d]+) (?<currency>[\\w]{3})$") //
+                        .assign((t, v) -> {
+                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setAmount(asAmount(v.get("amount")));
+                        })
+
+                        // @formatter:off
+                        // Borderel 275825809 Limit order
+                        // @formatter:on
+                        .section("note").optional() //
+                        .match("^(?<note>Borderel [\\d]+).*$") //
+                        .assign((t, v) -> t.setNote(trim(v.get("note"))))
+
+                        .wrap(BuySellEntryItem::new);
 
         addTaxesSectionsTransaction(pdfTransaction, type);
         addFeesSectionsTransaction(pdfTransaction, type);
@@ -82,19 +94,25 @@ public class KBCGroupNVPDFExtractor extends AbstractPDFExtractor
 
     private <T extends Transaction<?>> void addTaxesSectionsTransaction(T transaction, DocumentType type)
     {
-        transaction
-                // Beurstaks 3,00 EUR
-                .section("tax", "currency").optional()
-                .match("^Beurstaks (?<tax>[\\.,\\d]+) (?<currency>[\\w]{3})$")
-                .assign((t, v) -> processTaxEntries(t, v, type));
+        transaction //
+
+                        // @formatter:off
+                        // Beurstaks 3,00 EUR
+                        // @formatter:on
+                        .section("tax", "currency").optional() //
+                        .match("^Beurstaks (?<tax>[\\.,\\d]+) (?<currency>[\\w]{3})$") //
+                        .assign((t, v) -> processTaxEntries(t, v, type));
     }
 
     private <T extends Transaction<?>> void addFeesSectionsTransaction(T transaction, DocumentType type)
     {
-        transaction
-                // Makelaarsloon 7,50 EUR
-                .section("fee", "currency").optional()
-                .match("^Makelaarsloon (?<fee>[\\.,\\d]+) (?<currency>[\\w]{3})$")
-                .assign((t, v) -> processFeeEntries(t, v, type));
+        transaction //
+        
+                        // @formatter:off
+                        // Makelaarsloon 7,50 EUR
+                        // @formatter:on
+                        .section("fee", "currency").optional() //
+                        .match("^Makelaarsloon (?<fee>[\\.,\\d]+) (?<currency>[\\w]{3})$") //
+                        .assign((t, v) -> processFeeEntries(t, v, type));
     }
 }
