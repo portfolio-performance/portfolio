23a4dda1939fb7437cd099f1749418683b26648b
Andreas Buchen <andreas.buchen@gmail.com>
2024-03-25 13:43:00 +0100

    Limit the width of notes in preference pages to enforce word wrap

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/preferences/GeneralPreferencePage.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/preferences/GeneralPreferencePage.java
@@ -24,7 +24,7 @@ public class GeneralPreferencePage extends FieldEditorPreferencePage
         addField(new BooleanFieldEditor(UIConstants.Preferences.STORE_SETTINGS_NEXT_TO_FILE, //
                         Messages.PrefStoreSettingsNextToFile, getFieldEditorParent()));
 
-        createNoteComposite(getFieldEditorParent().getFont(), getFieldEditorParent(), //
+        PreferenceDialogUtil.createNoteComposite(getFieldEditorParent().getFont(), getFieldEditorParent(),
                         Messages.PrefLabelNote, Messages.PrefNoteStoreSettingsNextToFile);
 
         addField(new BooleanFieldEditor(UIConstants.Preferences.ENABLE_EXPERIMENTAL_FEATURES, //
