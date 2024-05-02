f1d23843cb22a0410058812d56902ba917e46676
Mikerion <m1k3w0rks@gmail.com>
2024-05-02 16:48:33 +0200

    Add Transaction filters
    For Transactions Pane and Account Transactions Pane

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/panes/TransactionsPane.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/panes/TransactionsPane.java
@@ -6,10 +6,19 @@ import java.util.stream.Collectors;
 
 import jakarta.inject.Inject;
 
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+
+import com.google.common.base.Strings;
 
 import name.abuchen.portfolio.model.Account;
 import name.abuchen.portfolio.model.Adaptor;
@@ -21,13 +30,82 @@ import name.abuchen.portfolio.model.TransactionPair;
 import name.abuchen.portfolio.ui.Images;
 import name.abuchen.portfolio.ui.Messages;
 import name.abuchen.portfolio.ui.editor.AbstractFinanceView;
+import name.abuchen.portfolio.ui.editor.PortfolioPart;
 import name.abuchen.portfolio.ui.util.DropDown;
+import name.abuchen.portfolio.ui.util.LabelOnly;
 import name.abuchen.portfolio.ui.util.SimpleAction;
 import name.abuchen.portfolio.ui.util.TableViewerCSVExporter;
+import name.abuchen.portfolio.ui.views.AllTransactionsView.TransactionFilter;
 import name.abuchen.portfolio.ui.views.TransactionsViewer;
 
 public class TransactionsPane implements InformationPanePage
 {
+    private static final String TRANSACTION_FILTER_PREFERENCE_NAME = TransactionsPane.class.getSimpleName()
+                    + "-transaction-type-filter"; //$NON-NLS-1$
+    private static final TransactionFilter DEFAULT_TYPE_FILTER = TransactionFilter.NONE;
+
+    private class FilterDropDown extends DropDown implements IMenuListener
+    {
+        public FilterDropDown(IPreferenceStore preferenceStore)
+        {
+            super(Messages.SecurityFilter, Images.FILTER_OFF, SWT.NONE);
+
+            preferenceStore.setDefault(TRANSACTION_FILTER_PREFERENCE_NAME, DEFAULT_TYPE_FILTER.name());
+            TransactionFilter transactionFilter = TransactionFilter
+                            .valueOf(preferenceStore.getString(TRANSACTION_FILTER_PREFERENCE_NAME));
+            typeFilter = transactionFilter;
+
+            setMenuListener(this);
+
+            updateIcon();
+
+            addDisposeListener(e -> preferenceStore.setValue(TRANSACTION_FILTER_PREFERENCE_NAME, typeFilter.name()));
+        }
+
+        private void updateIcon()
+        {
+            boolean hasActiveFilter = typeFilter != DEFAULT_TYPE_FILTER;
+            setImage(hasActiveFilter ? Images.FILTER_ON : Images.FILTER_OFF);
+        }
+
+        @Override
+        public void menuAboutToShow(IMenuManager manager)
+        {
+            manager.add(new LabelOnly(Messages.TransactionFilter));
+            for (TransactionFilter f : TransactionFilter.values())
+                manager.add(addTypeFilter(f));
+        }
+
+        private Action addTypeFilter(TransactionFilter filter)
+        {
+            Action action = new Action(Strings.repeat(" ", filter.getLevel() * 2) + filter.getName(), //$NON-NLS-1$
+                            IAction.AS_CHECK_BOX)
+            {
+                @Override
+                public void run()
+                {
+                    boolean isChecked = typeFilter == filter;
+
+                    // only one TransactionFilter can be selected at a time
+                    if (isChecked)
+                        typeFilter = DEFAULT_TYPE_FILTER;
+                    else
+                        typeFilter = filter;
+
+                    updateIcon();
+                    notifyModelUpdated();
+                }
+            };
+            action.setChecked(typeFilter == filter);
+            return action;
+        }
+    }
+
+    private TransactionFilter typeFilter;
+
+    @Inject
+    private PortfolioPart part;
+
     @Inject
     private Client client;
 
@@ -49,12 +127,26 @@ public class TransactionsPane implements InformationPanePage
     {
         transactions = new TransactionsViewer(TransactionsPane.class.getName(), parent, view);
         view.inject(transactions);
+
+        transactions.addFilter(new ViewerFilter()
+        {
+
+            @Override
+            public boolean select(Viewer viewer, Object parentElement, Object element)
+            {
+                TransactionPair<?> tx = (TransactionPair<?>) element;
+                return typeFilter.matches(tx.getTransaction());
+            }
+        });
+
         return transactions.getControl();
     }
 
     @Override
     public void addButtons(ToolBarManager toolBar)
     {
+        toolBar.add(new FilterDropDown(part.getPreferenceStore()));
+
         toolBar.add(new SimpleAction(Messages.MenuExportData, Images.EXPORT,
                         a -> new TableViewerCSVExporter(transactions.getTableViewer()).export(getLabel(), source)));
 
@@ -112,6 +204,11 @@ public class TransactionsPane implements InformationPanePage
     {
         if (source != null)
             setInput(source);
+    }
+
+    public void notifyModelUpdated()
+    {
+        onRecalculationNeeded();
     }
 
     public void markTransactions(List<TransactionPair<?>> list)
