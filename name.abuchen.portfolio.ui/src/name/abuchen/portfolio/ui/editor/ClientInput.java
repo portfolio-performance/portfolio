68d20f70075ce60f413dabc619e4c51f2c3ee7f3
Andreas Buchen <andreas.buchen@gmail.com>
2024-05-19 21:18:57 +0200

    Added dispose event to ClientInput

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/editor/ClientInput.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/editor/ClientInput.java
@@ -100,6 +100,9 @@ public class ClientInput
      */
     public void dispose()
     {
+        // list of listeners might be modified during onDisposed callback
+        new ArrayList<>(this.listeners).forEach(ClientInputListener::onDisposed);
+
         for (Job job : regularJobs)
             job.cancel();
 
