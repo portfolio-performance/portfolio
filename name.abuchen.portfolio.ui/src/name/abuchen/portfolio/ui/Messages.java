3fe7f9af33202f5e7328e0dfdc96447c59572d6b
Alexander Ott <webmaster@nirus-online.de>
2024-07-01 10:28:46 +0200

    Separation of the numerical values in the security chart
    There is the problem that too many numerical values are separated when displaying buy, sell, deposit... overlap as soon as the chart has a certain format.
    This is how we separate the display of values from investment markers and dividend values.

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/Messages.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/Messages.java
@@ -578,6 +578,7 @@ public class Messages extends NLS
     public static String LabelChartDetailSettingsShowLimits;
     public static String LabelChartDetailSettingsShowMarkerLines;
     public static String LabelChartDetailSettingsShowDataLabel;
+    public static String LabelChartDetailSettingsShowDividendDataLabel;
     public static String LabelChartDetailSettingsShowMissingTradingDays;
     public static String LabelChartDetailSettingsShowPercentageAxis;
     public static String LabelChartShowLabels;
