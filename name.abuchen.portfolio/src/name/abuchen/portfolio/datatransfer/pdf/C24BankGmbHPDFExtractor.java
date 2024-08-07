78d1e7c2a7839c36981c8cde742311350a256dcd
Alexander Ott <webmaster@nirus-online.de>
2024-08-07 07:05:39 +0200

    Modify C24 Bank GmbH PDF-Importer to support new transaction
    https://forum.portfolio-performance.info/t/pdf-import-von-c24-bank-gmbh/28636/3

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/C24BankGmbHPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/C24BankGmbHPDFExtractor.java
@@ -31,9 +31,10 @@ public class C24BankGmbHPDFExtractor extends AbstractPDFExtractor
                         documentContext -> documentContext //
                                         // @formatter:off
                                         // Kontoauszug 05/2024 Kontostand 0,00 €
+                                        // Vorläufiger Kontoauszug 08/2024 Kontostand 0,00 €
                                         // @formatter:on
                                         .section("year") //
-                                        .match("^Kontoauszug [\\d]{2}\\/(?<year>[\\d]{4}) .*$") //
+                                        .match("^(Vorl.ufiger )?Kontoauszug [\\d]{2}\\/(?<year>[\\d]{4}) .*$") //
                                         .assign((ctx, v) -> ctx.put("year", v.get("year"))));
 
         this.addDocumentTyp(type);
@@ -41,8 +42,12 @@ public class C24BankGmbHPDFExtractor extends AbstractPDFExtractor
         // @formatter:off
         // 17.05. 17.05. Überweisung - 1.508,42 €
         // 17.05. 17.05. Überweisung + 1.115,22 €
+        // 05.08. 05.08. Echtzeitüberweisung - 2.800,00 €
         // @formatter:on
-        Block depositRemovalBlock = new Block("^[\\d]{2}\\.[\\d]{2}\\. [\\d]{2}\\.[\\d]{2}\\. .berweisung [\\-|\\+] [\\.,\\d]+ \\p{Sc}.*$");
+        Block depositRemovalBlock = new Block("^[\\d]{2}\\.[\\d]{2}\\. [\\d]{2}\\.[\\d]{2}\\. "
+                        + "(.berweisung"
+                        + "|Echtzeit.berweisung) "
+                        + "[\\-|\\+] [\\.,\\d]+ \\p{Sc}.*$");
         type.addBlock(depositRemovalBlock);
         depositRemovalBlock.set(new Transaction<AccountTransaction>()
 
@@ -55,7 +60,8 @@ public class C24BankGmbHPDFExtractor extends AbstractPDFExtractor
                         .section("date", "note", "type", "amount", "currency") //
                         .documentContext("year") //
                         .match("^(?<date>[\\d]{2}\\.[\\d]{2}\\.) [\\d]{2}\\.[\\d]{2}\\. " //
-                                        + "(?<note>.berweisung) " //
+                                        + "(?<note>.berweisung"
+                                        + "|Echtzeit.berweisung) " //
                                         + "(?<type>[\\-|\\+]) " //
                                         + "(?<amount>[\\.,\\d]+) (?<currency>\\p{Sc}).*$") //
                         .assign((t, v) -> {
@@ -94,6 +100,38 @@ public class C24BankGmbHPDFExtractor extends AbstractPDFExtractor
                             // Is sign --> "-" change from INTEREST to INTEREST_CHARGE
                             if ("-".equals(v.get("type")))
                                 t.setType(AccountTransaction.Type.INTEREST_CHARGE);
+
+                            t.setDateTime(asDate(v.get("date") + v.get("year")));
+                            t.setAmount(asAmount(v.get("amount")));
+                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setNote(v.get("note"));
+                        })
+
+                        .wrap(TransactionItem::new));
+
+        // @formatter:off
+        // 30.06. 30.06. Steuern - 4,04 €
+        // @formatter:on
+        Block taxesBlock = new Block("^[\\d]{2}\\.[\\d]{2}\\. [\\d]{2}\\.[\\d]{2}\\. Steuern [\\-|\\+] [\\.,\\d]+ \\p{Sc}.*$");
+        type.addBlock(taxesBlock);
+        taxesBlock.set(new Transaction<AccountTransaction>()
+
+                        .subject(() -> {
+                            AccountTransaction accountTransaction = new AccountTransaction();
+                            accountTransaction.setType(AccountTransaction.Type.TAX_REFUND);
+                            return accountTransaction;
+                        })
+
+                        .section("date", "note", "type", "amount", "currency") //
+                        .documentContext("year") //
+                        .match("^(?<date>[\\d]{2}\\.[\\d]{2}\\.) [\\d]{2}\\.[\\d]{2}\\. " //
+                                        + "(?<note>Steuern) " //
+                                        + "(?<type>[\\-|\\+]) " //
+                                        + "(?<amount>[\\.,\\d]+) (?<currency>\\p{Sc}).*$") //
+                        .assign((t, v) -> {
+                            // Is sign --> "-" change from TAXES to TAX_REFUND
+                            if ("-".equals(v.get("type")))
+                                t.setType(AccountTransaction.Type.TAXES);
 
                             t.setDateTime(asDate(v.get("date") + v.get("year")));
                             t.setAmount(asAmount(v.get("amount")));
