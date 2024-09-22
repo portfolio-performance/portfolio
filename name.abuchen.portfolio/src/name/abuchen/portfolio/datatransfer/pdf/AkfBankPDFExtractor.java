27b936fc4722f240a0ad6b62db451acd8d7e048f
Alexander Ott <45203494+Nirus2000@users.noreply.github.com>
2024-09-22 09:19:00 +0200

    Modify AKF Bank PDF-Importer to support new transaction (#4242)
    https://forum.portfolio-performance.info/t/pdf-import-von-akf-bank/27325/3

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/AkfBankPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/AkfBankPDFExtractor.java
@@ -2,20 +2,16 @@ package name.abuchen.portfolio.datatransfer.pdf;
 
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
-import name.abuchen.portfolio.datatransfer.pdf.PDFParser.ParsedData;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
 import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.AccountTransaction.Type;
 import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.model.Transaction.Unit;
+import name.abuchen.portfolio.money.Money;
 
 @SuppressWarnings("nls")
 public class AkfBankPDFExtractor extends AbstractPDFExtractor
 {
-    private static final String DEPOSIT = "^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} (?<note>(Gutschrift|SEPA Gutschrift( Bank)?)) (?<amount>[\\.,\\d]+)$";
-    private static final String REMOVAL = "^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} (?<note>(Einzel)?(.*berweisung.*|Festgeld Anlage|Sparkonto K.ndigung)) \\-(?<amount>[\\.,\\d]+)$";
-    private static final String REMOVAL_LINE_2 = "^(?<type>[^\\s]+).*$";
-    private static final String INTEREST = "^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Kontoabschluß (?<amount>[\\.,\\d]+)$";
-
     public AkfBankPDFExtractor(Client client)
     {
         super(client);
@@ -44,22 +40,13 @@ public class AkfBankPDFExtractor extends AbstractPDFExtractor
 
         this.addDocumentTyp(type);
 
-        Block depositBlock = new Block(DEPOSIT);
-        depositBlock.set(depositTransaction(type, DEPOSIT));
+        // @formatter:off
+        // 01 08.04.2011 / 08.04.2011 Gutschrift 29.000,12
+        // 01 16.08.2022 / 16.08.2022 SEPA Gutschrift Bank 755,00
+        // @formatter:on
+        Block depositBlock = new Block("^[\\d]{2} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} (Gutschrift|SEPA Gutschrift( Bank)?) .*$");
         type.addBlock(depositBlock);
-
-        Block removalBlock = new Block(REMOVAL);
-        removalBlock.set(removalTransaction(type, REMOVAL, REMOVAL_LINE_2));
-        type.addBlock(removalBlock);
-
-        Block interestBlock = new Block(INTEREST);
-        interestBlock.set(interestTransaction(type, INTEREST));
-        type.addBlock(interestBlock);
-    }
-
-    private Transaction<AccountTransaction> depositTransaction(DocumentType type, String regex)
-    {
-        return new Transaction<AccountTransaction>() //
+        depositBlock.set(new Transaction<AccountTransaction>()
 
                         .subject(() -> {
                             AccountTransaction accountTransaction = new AccountTransaction();
@@ -67,19 +54,35 @@ public class AkfBankPDFExtractor extends AbstractPDFExtractor
                             return accountTransaction;
                         })
 
-                        .section("date", "amount", "note") //
+                        .section("date", "note", "amount") //
                         .documentContext("currency") //
-                        .match(regex) //
+                        .match("^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} " //
+                                        + "(?<note>(Gutschrift" //
+                                        + "|SEPA Gutschrift( Bank)?)) " //
+                                        + "(?<amount>[\\.,\\d]+)$") //
                         .assign((t, v) -> {
-                            assignmentsProvider(t, v);
+                            t.setDateTime(asDate(v.get("date")));
+                            t.setAmount(asAmount(v.get("amount")));
+                            t.setCurrencyCode(v.get("currency"));
+                            t.setNote(v.get("note"));
                         })
 
-                        .wrap(TransactionItem::new);
-    }
+                        .wrap(TransactionItem::new));
 
-    private Transaction<AccountTransaction> removalTransaction(DocumentType type, String regex, String regexLine2)
-    {
-        return new Transaction<AccountTransaction>() //
+        // @formatter:off
+        // 02 05.12.2012 / 04.12.2012 DTA Überweisung -1.660,89
+        // 01 13.04.2015 / 11.04.2015 SEPA Überweisung online -1,24
+        //
+        // 01 14.11.2012 / 14.11.2012 Einzelüberweisung -5,00
+        // Gebühren für Nachbe-
+        // @formatter:on
+        Block removalBlock = new Block("^[\\d]{2} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} "
+                        + "(Einzel)?(.*berweisung.*"
+                        + "|Festgeld Anlage"
+                        + "|Sparkonto K.ndigung) \\-[\\.,\\d]+$");
+        type.addBlock(removalBlock);
+        removalBlock.setMaxSize(2);
+        removalBlock.set(new Transaction<AccountTransaction>()
 
                         .subject(() -> {
                             AccountTransaction accountTransaction = new AccountTransaction();
@@ -87,20 +90,37 @@ public class AkfBankPDFExtractor extends AbstractPDFExtractor
                             return accountTransaction;
                         })
 
-                        .section("date", "amount", "note", "type") //
+                        .section("date", "note", "amount", "type") //
                         .documentContext("currency") //
-                        .match(regex) //
-                        .match(regexLine2) //
+                        .match("^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} " //
+                                        + "(?<note>(Einzel)?(.*berweisung.*" //
+                                        + "|Festgeld Anlage" //
+                                        + "|Sparkonto K.ndigung)) " //
+                                        + "\\-(?<amount>[\\.,\\d]+)$") //
+                        .match("^(?<type>[^\\s]+).*$")
                         .assign((t, v) -> {
-                            assignmentsProvider(t, v);
+                            // @formatter:off
+                            // Is type is "Gebühren" change from REMOVAL to FEES
+                            // @formatter:on
+                            if (v.get("type") != null && v.get("type").matches("Geb.hren"))
+                                t.setType(Type.FEES);
+
+                            t.setDateTime(asDate(v.get("date")));
+                            t.setAmount(asAmount(v.get("amount")));
+                            t.setCurrencyCode(v.get("currency"));
+                            t.setNote(v.get("note"));
                         })
 
-                        .wrap(TransactionItem::new);
-    }
+                        .wrap(TransactionItem::new));
 
-    private Transaction<AccountTransaction> interestTransaction(DocumentType type, String regex)
-    {
-        return new Transaction<AccountTransaction>() //
+        // @formatter:off
+        // 02 30.11.2012 / 30.11.2012 Kontoabschluß 2,71
+        // Habenzinsen
+        // Gebühren für Nachbe-
+        // @formatter:on
+        Block interestBlock = new Block("^[\\d]{2} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Kontoabschlu. [\\.,\\d]+$");
+        type.addBlock(interestBlock);
+        interestBlock.set(new Transaction<AccountTransaction>()
 
                         .subject(() -> {
                             AccountTransaction accountTransaction = new AccountTransaction();
@@ -110,23 +130,36 @@ public class AkfBankPDFExtractor extends AbstractPDFExtractor
 
                         .section("date", "amount") //
                         .documentContext("currency") //
-                        .match(regex) //
+                        .match("^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Kontoabschlu. (?<amount>[\\.,\\d]+)$") //
                         .assign((t, v) -> {
-                            assignmentsProvider(t, v);
+                            t.setDateTime(asDate(v.get("date")));
+                            t.setAmount(asAmount(v.get("amount")));
+                            t.setCurrencyCode(v.get("currency"));
                         })
 
-                        .wrap(TransactionItem::new);
-    }
+                        // @formatter:off
+                        // 02 30.09.2023 / 30.09.2023 Kontoabschluß -0,25
+                        // Abgeltungssteuer
+                        // aus EUR                1,03
+                        //
+                        // 03 30.09.2023 / 30.09.2023 Kontoabschluß -0,01
+                        // Solidaritätszuschlag
+                        // aus EUR                0,25
+                        //
+                        // 04 30.09.2023 / 30.09.2023 Kontoabschluß -0,02
+                        // Kirchensteuer
+                        // aus EUR                0,25
+                        // @formatter:on
+                        .section("tax", "currency").multipleTimes().optional() //
+                        .match("^[\\d]{2} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Kontoabschlu. \\-(?<tax>[\\.,\\d]+)$") //
+                        .match("^(Abgeltungssteuer|Solidarit.tszuschlag|Kirchensteuer).*$")
+                        .match("^aus (?<currency>[\\w]{3}).*$")
+                        .assign((t, v) -> {
+                            Money taxes = Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("tax")));
 
-    private void assignmentsProvider(AccountTransaction t, ParsedData v)
-    {
-        t.setDateTime(asDate(v.get("date")));
-        t.setAmount(asAmount(v.get("amount")));
-        t.setCurrencyCode(asCurrencyCode(v.get("currency")));
-        t.setNote(v.get("note"));
-        if (v.get("type") != null && v.get("type").matches("Geb.hren"))
-        {
-            t.setType(Type.FEES);
-        }
+                            t.addUnit(new Unit(Unit.Type.TAX, taxes));
+                        })
+
+                        .wrap(TransactionItem::new));
     }
 }
