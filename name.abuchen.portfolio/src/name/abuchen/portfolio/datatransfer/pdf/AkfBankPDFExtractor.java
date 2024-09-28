ddc569f751a949e8220981e2495bd161c91289c8
Alexander Ott <45203494+Nirus2000@users.noreply.github.com>
2024-09-28 08:46:22 +0200

    Modify Akf Bank PDF-Importer to support new transaction (#4251)
    https://forum.portfolio-performance.info/t/pdf-import-von-akf-bank/27325/6

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/AkfBankPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/AkfBankPDFExtractor.java
@@ -6,8 +6,6 @@ import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
 import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.AccountTransaction.Type;
 import name.abuchen.portfolio.model.Client;
-import name.abuchen.portfolio.model.Transaction.Unit;
-import name.abuchen.portfolio.money.Money;
 
 @SuppressWarnings("nls")
 public class AkfBankPDFExtractor extends AbstractPDFExtractor
@@ -116,10 +114,12 @@ public class AkfBankPDFExtractor extends AbstractPDFExtractor
         // @formatter:off
         // 02 30.11.2012 / 30.11.2012 Kontoabschluß 2,71
         // Habenzinsen
-        // Gebühren für Nachbe-
+        // v. 31.10.2012 b. 30.11.2012
+        // Zinsen zu 2,150
         // @formatter:on
         Block interestBlock = new Block("^[\\d]{2} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Kontoabschlu. [\\.,\\d]+$");
         type.addBlock(interestBlock);
+        interestBlock.setMaxSize(4);
         interestBlock.set(new Transaction<AccountTransaction>()
 
                         .subject(() -> {
@@ -128,13 +128,29 @@ public class AkfBankPDFExtractor extends AbstractPDFExtractor
                             return accountTransaction;
                         })
 
-                        .section("date", "amount") //
+                        .section("date", "amount", "note1", "note2", "note3") //
                         .documentContext("currency") //
                         .match("^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Kontoabschlu. (?<amount>[\\.,\\d]+)$") //
+                        .match("v\\. (?<note1>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) b\\. (?<note2>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}).*$")
+                        .match("Zinsen zu[\\s]{1,}(?<note3>[\\.,\\d]+).*$")
                         .assign((t, v) -> {
                             t.setDateTime(asDate(v.get("date")));
                             t.setAmount(asAmount(v.get("amount")));
                             t.setCurrencyCode(v.get("currency"));
+                            t.setNote(v.get("note1") + " - " + v.get("note2") + " (" + v.get("note3") + " %)");
+                        })
+
+                        .wrap(TransactionItem::new));
+
+        Block taxesBlock = new Block("^[\\d]{2} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Kontoabschlu. \\-[\\.,\\d]+$");
+        type.addBlock(taxesBlock);
+        taxesBlock.setMaxSize(3);
+        taxesBlock.set(new Transaction<AccountTransaction>()
+
+                        .subject(() -> {
+                            AccountTransaction accountTransaction = new AccountTransaction();
+                            accountTransaction.setType(AccountTransaction.Type.TAXES);
+                            return accountTransaction;
                         })
 
                         // @formatter:off
@@ -150,14 +166,15 @@ public class AkfBankPDFExtractor extends AbstractPDFExtractor
                         // Kirchensteuer
                         // aus EUR                0,25
                         // @formatter:on
-                        .section("tax", "currency").multipleTimes().optional() //
-                        .match("^[\\d]{2} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Kontoabschlu. \\-(?<tax>[\\.,\\d]+)$") //
-                        .match("^(Abgeltungssteuer|Solidarit.tszuschlag|Kirchensteuer).*$")
-                        .match("^aus (?<currency>[\\w]{3}).*$")
+                        .section("date", "tax", "note", "currency", "taxAmount") //
+                        .match("^[\\d]{2} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) \\/ [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Kontoabschlu. \\-(?<tax>[\\.,\\d]+)$") //
+                        .match("^(?<note>(Abgeltungssteuer|Solidarit.tszuschlag|Kirchensteuer)).*$")
+                        .match("^aus (?<currency>[\\w]{3})[\\s]{1,}(?<taxAmount>[\\.,\\d]+).*$")
                         .assign((t, v) -> {
-                            Money taxes = Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("tax")));
-
-                            t.addUnit(new Unit(Unit.Type.TAX, taxes));
+                            t.setDateTime(asDate(v.get("date")));
+                            t.setAmount(asAmount(v.get("tax")));
+                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setNote(v.get("note") + " (" + v.get("taxAmount") + " " + v.get("currency") + ")");
                         })
 
                         .wrap(TransactionItem::new));
