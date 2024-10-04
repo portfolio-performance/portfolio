953560993eb55d78da63cdc433352799d9c5883e
Alexander Ott <45203494+Nirus2000@users.noreply.github.com>
2024-10-04 19:57:43 +0200

    Modify Barclay PDF-Importer to support new transaction (#4267)
    https://forum.portfolio-performance.info/t/pdf-import-von-barclays-bank/27762/3?u=nirus

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/BarclaysBankIrelandPLCPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/BarclaysBankIrelandPLCPDFExtractor.java
@@ -8,7 +8,8 @@ import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
 import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.Client;
-
+import name.abuchen.portfolio.model.Transaction.Unit;
+import name.abuchen.portfolio.money.Money;
 
 @SuppressWarnings("nls")
 public class BarclaysBankIrelandPLCPDFExtractor extends AbstractPDFExtractor
@@ -30,15 +31,24 @@ public class BarclaysBankIrelandPLCPDFExtractor extends AbstractPDFExtractor
 
     private void addCreditcardStatementTransaction()
     {
-        final DocumentType type = new DocumentType("Kontoauszug vom ", //
+        final DocumentType type = new DocumentType("Kontoauszug", //
                         documentContext -> documentContext //
+                                        // @formatter:off
+                                        // Beleg- Buchungs-/ Beschreibung Karte Betrag (EUR)
+                                        // Gebucht am Wertstellung am Verwendungszweck Betrag (EUR)
+                                        // @formatter:on
                                         .section("currency") //
-                                        .match("^Beleg\\- Buchungs\\-/ Beschreibung Karte Betrag \\((?<currency>[\\w]{3})\\)$") //
+                                        .match("^.* \\((?<currency>[A-Z]{3})\\)$") //
                                         .assign((ctx, v) -> ctx.put("currency", asCurrencyCode(v.get("currency")))));
 
         this.addDocumentTyp(type);
 
-        Block depositRemovalBlock = new Block("^[\\d]{2}\\.[\\d]{2}\\.[\\d]{4} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} .* [\\.,\\d]+[\\-|\\+]$");
+        // @formatter:off
+        // 23.12.2023 25.12.2023 Lidl sagt Danke        Ort           DE Visa B A             60,78-
+        // 23.12.2023 28.12.2023 Lidl sagt Danke        Ort           DE Visa B A             60,78+
+        // @formatter:on
+        Block depositRemovalBlock = new Block("^[\\d]{2}\\.[\\d]{2}\\.[\\d]{4} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} "
+                        + "(?!(Abgeltungsteuer|Solidarit.tszuschlag)).* [\\.,\\d]+[\\-|\\+]$");
         type.addBlock(depositRemovalBlock);
         depositRemovalBlock.set(new Transaction<AccountTransaction>()
 
@@ -57,8 +67,7 @@ public class BarclaysBankIrelandPLCPDFExtractor extends AbstractPDFExtractor
                                         + "(?<amount>[\\.,\\d]+)" //
                                         + "(?<type>[\\-|\\+])$") //
                         .assign((t, v) -> {
-
-                            // Is sign --> "-" change from DEPOSIT to REMOVAL
+                            // Is type --> "-" change from DEPOSIT to REMOVAL
                             if ("-".equals(v.get("type")))
                                 t.setType(AccountTransaction.Type.REMOVAL);
 
@@ -66,6 +75,60 @@ public class BarclaysBankIrelandPLCPDFExtractor extends AbstractPDFExtractor
                             t.setAmount(asAmount(v.get("amount")));
                             t.setCurrencyCode(v.get("currency"));
                             t.setNote(trim(replaceMultipleBlanks(v.get("note"))));
+                        })
+
+                        .wrap(TransactionItem::new));
+
+        Block interestBlock = new Block("^[\\d]{2}\\.[\\d]{2}\\.[\\d]{4} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Habenzinsen [\\.,\\d]+$");
+        type.addBlock(interestBlock);
+        interestBlock.setMaxSize(4);
+        interestBlock.set(new Transaction<AccountTransaction>()
+
+                        .subject(() -> {
+                            AccountTransaction accountTransaction = new AccountTransaction();
+                            accountTransaction.setType(AccountTransaction.Type.INTEREST);
+                            return accountTransaction;
+                        })
+
+                        // @formatter:off
+                        // 31.12.2023 31.12.2023 Habenzinsen 4,80
+                        // @formatter:on
+                        .section("date", "amount") //
+                        .documentContext("currency") //
+                        .match("^[\\d]{2}\\.[\\d]{2}\\.[\\d]{4} " //
+                                        + "(?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) " //
+                                        + "Habenzinsen " //
+                                        + "(?<amount>[\\.,\\d]+)$") //
+                        .assign((t, v) -> {
+                            t.setDateTime(asDate(v.get("date")));
+                            t.setAmount(asAmount(v.get("amount")));
+                            t.setCurrencyCode(v.get("currency"));
+                        })
+
+                        // @formatter:off
+                        // 31.12.2023 31.12.2023 Abgeltungsteuer -1,20
+                        // @formatter:on
+                        .section("tax").optional() //
+                        .documentContext("currency") //
+                        .match("^[\\d]{2}\\.[\\d]{2}\\.[\\d]{4} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Abgeltungsteuer \\-(?<tax>[\\.,\\d]+)$") //
+                        .assign((t, v) -> {
+                            Money tax = Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("tax")));
+
+                            t.addUnit(new Unit(Unit.Type.TAX, tax));
+                            t.setMonetaryAmount(t.getMonetaryAmount().subtract(tax));
+                        })
+
+                        // @formatter:off
+                        // 31.12.2023 31.12.2023 Solidaritätszuschlag -0,06
+                        // @formatter:on
+                        .section("tax").optional() //
+                        .documentContext("currency") //
+                        .match("^[\\d]{2}\\.[\\d]{2}\\.[\\d]{4} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} Solidarit.tszuschlag \\-(?<tax>[\\.,\\d]+)$") //
+                        .assign((t, v) -> {
+                            Money tax = Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("tax")));
+
+                            t.addUnit(new Unit(Unit.Type.TAX, tax));
+                            t.setMonetaryAmount(t.getMonetaryAmount().subtract(tax));
                         })
 
                         .wrap(TransactionItem::new));
