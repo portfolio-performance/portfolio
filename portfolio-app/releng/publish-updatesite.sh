8189e52bbd82c52f15380c8571d1d4d8d50fd9f3
Andreas Buchen <andreas.buchen@gmail.com>
2024-12-30 18:18:24 +0100

    Next development version 0.73.1-SNAPSHOT

--- portfolio-app/releng/publish-updatesite.sh
+++ portfolio-app/releng/publish-updatesite.sh
@@ -7,12 +7,18 @@ if [ -z $PCK_VERSION ]; then
 fi
 
 BASE=$(pwd)/../../portfolio-product/target/repository
+E430BASE=$(pwd)/../../../portfolio-eclipse430/portfolio-app/releng/updatesite/portfolio
 
-rm -rf updatesite
 mkdir updatesite
+rm updatesite/CNAME
+rm updatesite/index.html
+rm updatesite/index.html-e
+rm -rf updatesite/portfolio
 mkdir updatesite/portfolio
-mkdir updatesite/portfolio-x86
+rm -rf updatesite/portfolio-e430
+mkdir updatesite/portfolio-e430
 cp -R ${BASE}/* updatesite/portfolio
+cp -R ${E430BASE}/* updatesite/portfolio-e430
 cp ${BASE}/index.html updatesite
 sed -i -e 's/css\/styles.css/portfolio\/css\/styles.css/g' updatesite/index.html
 sed -i -e 's/images\/pp_16.gif/portfolio\/images\/pp_16.gif/g' updatesite/index.html
@@ -24,9 +30,9 @@ cat >CNAME <<EOF
 updates.portfolio-performance.info
 EOF
 
-git init
+# git init
 git add -A
 git commit -m "Version ${PCK_VERSION}"
-git branch -m main gh-pages
-git remote add origin https://github.com/portfolio-performance/portfolio-updatesite.git
-git config http.postBuffer 524288000
+# git branch -m main gh-pages
+# git remote add origin https://github.com/portfolio-performance/portfolio-updatesite.git
+# git config http.postBuffer 524288000
