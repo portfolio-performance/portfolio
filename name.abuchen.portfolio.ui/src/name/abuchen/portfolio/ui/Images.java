4c3b6eb8c8472879979d5c6361df1f5a40372c92
Andreas Buchen <andreas.buchen@gmail.com>
2025-01-17 11:21:36 +0100

    Updated icons for grouped accounts

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/Images.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/Images.java
@@ -39,6 +39,7 @@ public enum Images
     ACCOUNT("account.png"), //$NON-NLS-1$
     PORTFOLIO("portfolio.png"), //$NON-NLS-1$
     GROUPEDACCOUNTS("groupedaccounts.png"), //$NON-NLS-1$
+    GROUPEDACCOUNTS_ON("groupedaccounts_on.png"), //$NON-NLS-1$
     WATCHLIST("watchlist.png"), //$NON-NLS-1$
     INVESTMENTPLAN("investmentplan.png"), //$NON-NLS-1$
     NOTE("note.png"), //$NON-NLS-1$
