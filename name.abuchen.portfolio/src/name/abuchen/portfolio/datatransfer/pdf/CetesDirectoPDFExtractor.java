bb802747c86506ac55f9d3f8a5dc775079bf4aa6
Jonathan <38994980+jonathlan@users.noreply.github.com>
2025-01-19 11:32:35 +0100

    Separate buy and sell transactions because the rules for both are different. Buy transactions should be working fine now. Sales need work.

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/CetesDirectoPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/CetesDirectoPDFExtractor.java
@@ -37,7 +37,7 @@ public class CetesDirectoPDFExtractor extends AbstractPDFExtractor
 
         Block firstRelevantLine = new Block(
                         "^[\\d]{2}\\/[\\d]{2}\\/[\\d]{2} [\\d]{2}\\/[\\d]{2}\\/[\\d]{2} [A-Z0-9]+.*$");
-        //Block firstRelevantLine = new Block("^Saldo inicial+.*$", "^Saldo final+.*$");
+        //Block firstRelevantLine = new Block("^Saldo inicial+.*$", "^Saldo final+.*$"); // This does not work
         type.addBlock(firstRelevantLine);
         // firstRelevantLine.setMaxSize(1);
         firstRelevantLine.set(new Transaction<BuySellEntry>()
@@ -48,47 +48,32 @@ public class CetesDirectoPDFExtractor extends AbstractPDFExtractor
                             return portfolioTransaction;
                         })
                         
-                        .section("type").optional() //
-                        .match("^.*(?<type>AMORTIZACION) .*$") //
-                        .assign((t, v) -> {
-                            if ("AMORTIZACION".equals(v.get("type")))
-                                t.setType(PortfolioTransaction.Type.SELL);
-                        })
-                        
-                        // @formatter:off
-                        // 04/01/22 06/01/22 SVD147529623COMPRA CETES 220203 6,080 9.95730000 2 5.51 60,540.38 0.00 -60,540.10
-                        // 06/01/22 06/01/22 SVD147779466AMORTIZACION CETES 220106 6,055 0 0.00 60,550.00 9.90
-                        // 06/01/22 06/01/22 SVD147779466ISR CETES 220106 0 3.70 0.00 6.20
-                        // 06/01/22 06/01/22 SVD148097667COMPSI BONDDIA PF2 3 1.57377100 0 0.00 4.72 0.00 1.48
-                        // @formatter:on
-                        .section("date", "name", "shares", "amount") //
-                        .match("^[\\d]{2}\\/[\\d]{2}\\/[\\d]{2} (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{2}) [A-Z0-9]+(?<type>COMPRA|AMORTIZACION|ISR|COMPSI) (?<name>[A-Z]+) [\\dA-Z]+ (?<shares>[\\d,\\.]+) (?<price>[\\d,\\.]+) .* (?<amount>[\\d,\\.\\-]+)$")
+                        .oneOf(
+                                        section -> section
+                                        .attributes("date", "id", "name", "series", "shares", "term", "rate", "amount") //
+                                        .match("^[\\d]{2}\\/[\\d]{2}\\/[\\d]{2} (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{2}) (?<id>[A-Z0-9]+)(?<type>COMPRA|COMPSI) (?<name>[A-Z]+) (?<series>[\\dA-Z]+) (?<shares>[\\d,\\.]+) (?<price>[\\d,\\.]+) (?<term>[\\d]+) (?<rate>[\\d\\.]+) (?<amount>[\\d\\,\\.]+) .*$") //
+                                        .assign((t, v) -> {
+                                            v.put("currency", CurrencyUnit.MXN);
+                                            t.setSecurity(getOrCreateSecurity(v));
+                                            t.setDate(asDate(v.get("date")));
+                                            t.setShares(asShares(v.get("shares")));
+                                            t.setCurrencyCode(CurrencyUnit.MXN);
+                                            t.setAmount(asAmount(v.get("amount")));
+                                            t.setNote("ID:" + v.get("id") + " Series:" + v.get("series") + " Term:" + v.get("term") + " Rate:" + v.get("rate"));
+                                                        }),
 
-                        // .section("date", "name", "shares", "debit", "credit")
-                        // //
-                        // .match("^[\\d]{2}\\/[\\d]{2}\\/[\\d]{2}
-                        // (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{2})
-                        // [A-Z0-9]+(?<type>COMPRA|AMORTIZACION|ISR|COMPSI)
-                        // (?<name>[A-Z]+) [\\dA-Z]+ (?<shares>[\\d,\\.]+)
-                        // (?<price>[\\d,\\.]+)( |.*) (?<debit>[\\d,\\.\\-]+)
-                        // (?<credit>[\\d,\\.\\-]+) (?<amount>[\\d,\\.\\-]+)$")
-                        // //
-                         .assign((t, v) -> {
-                            // v.put("currency", CurrencyUnit.MXN);
-                            t.setSecurity(getOrCreateSecurity(v));
-                            t.setDate(asDate(v.get("date")));
-                            t.setShares(asShares(v.get("shares")));
-                            t.setCurrencyCode(CurrencyUnit.MXN);
-                            t.setAmount(asAmount(v.get("amount")));
-                            // if ("AMORTIZACION".equals(v.get("type")))
-                            // {
-                            // t.setAmount(asAmount(v.get("credit"), "es",
-                            // "MX"));
-                            // }
-                            // else
-                            // t.setAmount(asAmount(v.get("debit"), "es",
-                            // "MX"));
-                         })
+                                        section -> section.attributes("date", "type", "name", "shares", "amount") //
+                                        .match("^[\\d]{2}\\/[\\d]{2}\\/[\\d]{2} (?<date>[\\d]{2}\\/[\\d]{2}\\/[\\d]{2}) [A-Z0-9]+(?<type>AMORTIZACION|ISR) (?<name>[A-Z]+) (?<series>[\\dA-Z]+) (?<shares>[\\d,\\.]+) (?<price>[\\d,\\.]+) (?<term>[\\d]+)( |)(?<rate>[\\d\\.]+)( |)(?<amount>[\\d\\,\\.]+).*(?<balance>[\\d,\\.\\-]+)$") //
+                                        .assign((t, v) -> {
+                                            v.put("currency", CurrencyUnit.MXN);
+                                            t.setSecurity(getOrCreateSecurity(v));
+                                            t.setDate(asDate(v.get("date")));
+                                            t.setShares(asShares(v.get("shares")));
+                                            t.setCurrencyCode(CurrencyUnit.MXN);
+                                            t.setAmount(asAmount(v.get("amount")));
+                                            if ("AMORTIZACION".equals(v.get("type")))
+                                                t.setType(PortfolioTransaction.Type.SELL);
+                                                        }))
 
                         .wrap(BuySellEntryItem::new));
 
