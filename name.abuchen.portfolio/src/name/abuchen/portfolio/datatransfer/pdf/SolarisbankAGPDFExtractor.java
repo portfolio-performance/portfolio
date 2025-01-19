b3a39ff07af5f913556a5730b06ada2da572a6cc
Alexander Ott <45203494+Nirus2000@users.noreply.github.com>
2025-01-19 10:45:45 +0100

    Modify Solarisbank AG PDF-Importer to support new transaction (#4485)
    https://forum.portfolio-performance.info/t/pdf-import-von-solarisbank-ag/22410/2

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/SolarisbankAGPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/SolarisbankAGPDFExtractor.java
@@ -2,11 +2,13 @@ package name.abuchen.portfolio.datatransfer.pdf;
 
 import static name.abuchen.portfolio.util.TextUtil.trim;
 
+import name.abuchen.portfolio.datatransfer.ExtractorUtils;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
 import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.Client;
+import name.abuchen.portfolio.money.Values;
 
 @SuppressWarnings("nls")
 public class SolarisbankAGPDFExtractor extends AbstractPDFExtractor
@@ -19,6 +21,7 @@ public class SolarisbankAGPDFExtractor extends AbstractPDFExtractor
         addBankIdentifier("Solaris SE");
 
         addAccountStatementTransaction();
+        addDepotStatementTransaction();
     }
 
     @Override
@@ -83,5 +86,62 @@ public class SolarisbankAGPDFExtractor extends AbstractPDFExtractor
                         })
 
                         .wrap(TransactionItem::new));
+    }
+
+    private void addDepotStatementTransaction()
+    {
+        final DocumentType type = new DocumentType("KONTOAUSZUG");
+        this.addDocumentTyp(type);
+
+        // @formatter:off
+        // YLbKRus qXwsATAvg 08.11.2024 08.11.2024 3000€
+        // EUWAX Aktiengesellschaft 14.11.2024 14.11.2024 1105.19€
+        // EUWAX Aktiengesellschaft 10.11.2024 10.11.2024 -1000€
+        // @formatter:on
+        Block depositRemovalBlock = new Block("^.* [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} ([\\-])?[\\.,\\d]+\\p{Sc}$");
+        type.addBlock(depositRemovalBlock);
+        depositRemovalBlock.set(new Transaction<AccountTransaction>()
+
+                        .subject(() -> {
+                            AccountTransaction accountTransaction = new AccountTransaction();
+                            accountTransaction.setType(AccountTransaction.Type.DEPOSIT);
+                            return accountTransaction;
+                        })
+
+                        .section("note", "date", "type", "amount", "currency") //
+                        .match("^(?<note>.*) [\\d]{2}\\.[\\d]{2}\\.[\\d]{4} (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4})(?<type>[\\-\\s]+)(?<amount>[\\.,\\d]+)(?<currency>\\p{Sc})$") //
+                        .assign((t, v) -> {
+                            // @formatter:off
+                            // Is type is "-" change from DEPOSIT to REMOVAL
+                            // @formatter:on
+                            if ("-".equals(trim(v.get("type"))))
+                                t.setType(AccountTransaction.Type.REMOVAL);
+
+                            t.setDateTime(asDate(v.get("date")));
+                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setAmount(asAmount(v.get("amount")));
+                            t.setNote(trim(v.get("note")));
+                        })
+
+                        .wrap(TransactionItem::new));
+    }
+
+    @Override
+    protected long asAmount(String value)
+    {
+        String language = "de";
+        String country = "DE";
+
+        int lastDot = value.lastIndexOf(".");
+        int lastComma = value.lastIndexOf(",");
+
+        // returns the greater of two int values
+        if (Math.max(lastDot, lastComma) == lastDot)
+        {
+            language = "en";
+            country = "US";
+        }
+
+        return ExtractorUtils.convertToNumberLong(value, Values.Amount, language, country);
     }
 }
