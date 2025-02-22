1dfdb90ef7d12b76be2cd394300011b1631f5ae7
mierin12 <mattmailspoubelle+GIT@gmail.com>
2025-02-22 22:33:34 +0100

    add Moving Average Capital Gains options in
    the Widget Performance Calculation

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/PerformanceCalculationWidget.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/PerformanceCalculationWidget.java
@@ -62,6 +62,39 @@ public class PerformanceCalculationWidget extends WidgetDelegate<ClientPerforman
         }
     }
 
+    enum CostMethod
+    {
+        FIFO("FIFO", true), MOVING_AVERAGE("Moving Average", false);
+
+        private String label;
+        private boolean isFIFO;
+
+        private CostMethod(String label, boolean isFIFO)
+        {
+            this.label = label;
+            this.isFIFO = isFIFO;
+        }
+
+        @Override
+        public String toString()
+        {
+            return label;
+        }
+
+        public boolean isFIFO()
+        {
+            return isFIFO;
+        }
+    }
+
+    static class CostMethodConfig extends EnumBasedConfig<CostMethod>
+    {
+        public CostMethodConfig(WidgetDelegate<?> delegate)
+        {
+            super(delegate, "Cost Method", CostMethod.class, Dashboard.Config.COST_METHOD, Policy.EXACTLY_ONE);
+        }
+    }
+
     static class PositionToolTip extends ToolTip
     {
         private static final int MAX_NO_OF_ROWS = 10;
@@ -178,6 +211,7 @@ public class PerformanceCalculationWidget extends WidgetDelegate<ClientPerforman
         addConfig(new ReportingPeriodConfig(this));
         addConfig(new DataSeriesConfig(this, false));
         addConfig(new LayoutConfig(this));
+        addConfig(new CostMethodConfig(this));
     }
 
     @Override
@@ -275,7 +309,8 @@ public class PerformanceCalculationWidget extends WidgetDelegate<ClientPerforman
         return () -> {
             PerformanceIndex index = getDashboardData().calculate(get(DataSeriesConfig.class).getDataSeries(),
                             get(ReportingPeriodConfig.class).getReportingPeriod().toInterval(LocalDate.now()));
-            return index.getClientPerformanceSnapshot().orElseThrow(IllegalArgumentException::new);
+            boolean isFIFO = get(CostMethodConfig.class).getValue().isFIFO();
+            return index.getClientPerformanceSnapshot(isFIFO).orElseThrow(IllegalArgumentException::new);
         };
     }
 
