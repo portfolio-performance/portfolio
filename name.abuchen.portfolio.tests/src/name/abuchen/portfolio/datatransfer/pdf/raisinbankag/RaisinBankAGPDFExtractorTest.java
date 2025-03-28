de06605f5f6167453b92f5b59362a8c2c06b2ab8
Alexander Ott <webmaster@nirus-online.de>
2025-03-28 06:21:25 +0100

    Modify Raisin Bank PDF-Importer to support new transaction
    Closes #4538

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/raisinbankag/RaisinBankAGPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/raisinbankag/RaisinBankAGPDFExtractorTest.java
@@ -19,6 +19,7 @@ import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasWkn;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.purchase;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.sale;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.security;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.taxes;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countAccountTransactions;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countBuySell;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countSecurities;
@@ -239,5 +240,36 @@ public class RaisinBankAGPDFExtractorTest
                             Status s = c.process((AccountTransaction) tx, account);
                             assertThat(s, is(Status.OK_STATUS));
                         }))));
+    }
+
+    @Test
+    public void testVorabpauschale01()
+    {
+        RaisinBankAGPDFExtractor extractor = new RaisinBankAGPDFExtractor(new Client());
+
+        List<Exception> errors = new ArrayList<>();
+
+        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "Vorabpauschale01.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(countSecurities(results), is(1L));
+        assertThat(countBuySell(results), is(0L));
+        assertThat(countAccountTransactions(results), is(1L));
+        assertThat(results.size(), is(2));
+        new AssertImportActions().check(results, CurrencyUnit.EUR);
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin("IE00BKX55R35"), hasWkn(null), hasTicker(null), //
+                        hasName("Vanguard FTSE N.America U.ETF Registered Shares USD Dis.oN"), //
+                        hasCurrencyCode("EUR"))));
+
+        // check taxes transaction
+        assertThat(results, hasItem(taxes( //
+                        hasDate("2025-01-02T00:00"), hasShares(257.84), //
+                        hasSource("Vorabpauschale01.txt"), //
+                        hasNote("Dokument ID: 2B872G39-X017-1587-197O-25Ekj9aZ1321"), //
+                        hasAmount("EUR", 13.47), hasGrossValue("EUR", 13.47), //
+                        hasTaxes("EUR", 0.00), hasFees("EUR", 0.00))));
     }
 }
