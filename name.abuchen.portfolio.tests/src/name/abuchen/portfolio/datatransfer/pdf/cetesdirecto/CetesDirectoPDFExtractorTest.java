e88f048f9faafce67595cd5ffe3813c2367d2599
Alexander Ott <webmaster@nirus-online.de>
2025-03-30 19:18:49 +0200

    Improve Cetesdirecto PDF-Importer
    Rework complete Importer
    Fix currency symbol translation

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/cetesdirecto/CetesDirectoPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/cetesdirecto/CetesDirectoPDFExtractorTest.java
@@ -1,84 +1,225 @@
 package name.abuchen.portfolio.datatransfer.pdf.cetesdirecto;
 
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.dividend;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasAmount;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasCurrencyCode;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasDate;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasFees;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasGrossValue;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasIsin;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasName;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasNote;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasShares;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasSource;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasTaxes;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasTicker;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasWkn;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.purchase;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.sale;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.security;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countAccountTransactions;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countBuySell;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countSecurities;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
-import static org.junit.Assert.assertNull;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
-import name.abuchen.portfolio.datatransfer.Extractor.BuySellEntryItem;
-import name.abuchen.portfolio.datatransfer.Extractor.Item;
-import name.abuchen.portfolio.datatransfer.Extractor.SecurityItem;
 import name.abuchen.portfolio.datatransfer.actions.AssertImportActions;
 import name.abuchen.portfolio.datatransfer.pdf.CetesDirectoPDFExtractor;
 import name.abuchen.portfolio.datatransfer.pdf.PDFInputFile;
-import name.abuchen.portfolio.model.AccountTransaction;
-import name.abuchen.portfolio.model.BuySellEntry;
 import name.abuchen.portfolio.model.Client;
-import name.abuchen.portfolio.model.PortfolioTransaction;
-import name.abuchen.portfolio.model.Security;
-import name.abuchen.portfolio.model.Transaction.Unit;
-import name.abuchen.portfolio.money.CurrencyUnit;
-import name.abuchen.portfolio.money.Money;
-import name.abuchen.portfolio.money.Values;
 
 @SuppressWarnings("nls")
 public class CetesDirectoPDFExtractorTest
 {
-
     @Test
-    public void testEdoCta01()
+    public void testWertpapierKauf27()
     {
-        CetesDirectoPDFExtractor extractor = new CetesDirectoPDFExtractor(new Client());
+        var extractor = new CetesDirectoPDFExtractor(new Client());
 
         List<Exception> errors = new ArrayList<>();
 
-        List<Item> results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "EdoCta01.txt"), errors);
+        var results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "EdoCta01.txt"), errors);
 
         assertThat(errors, empty());
-        assertThat(countSecurities(results), is(2L));
-        assertThat(countBuySell(results), is(12L));
-        assertThat(countAccountTransactions(results), is(0L));
-        assertThat(results.size(), is(14));
-        new AssertImportActions().check(results, CurrencyUnit.MXN);
+        assertThat(countSecurities(results), is(12L));
+        assertThat(countBuySell(results), is(8L));
+        assertThat(countAccountTransactions(results), is(4L));
+        assertThat(results.size(), is(24));
+        new AssertImportActions().check(results, "MXN");
 
         // check security
-        Security security = results.stream().filter(SecurityItem.class::isInstance).findFirst()
-                        .orElseThrow(IllegalArgumentException::new).getSecurity();
-        assertNull(security.getIsin());
-        assertNull(security.getWkn());
-        assertNull(security.getTickerSymbol());
-        assertThat(security.getName(), is("CETES"));
-        assertThat(security.getCurrencyCode(), is(CurrencyUnit.MXN));
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD147529623"), hasTicker(null), //
+                        hasName("CETES (220203)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD147779466"), hasTicker(null), //
+                        hasName("CETES (220106)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD148097667"), hasTicker(null), //
+                        hasName("BONDDIA (PF2)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD148801631"), hasTicker(null), //
+                        hasName("CETES (220210)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD148972304"), hasTicker(null), //
+                        hasName("CETES (220113)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD149165188"), hasTicker(null), //
+                        hasName("BONDDIA (PF2)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD149724600"), hasTicker(null), //
+                        hasName("CETES (220217)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD149962624"), hasTicker(null), //
+                        hasName("CETES (220120)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD150259785"), hasTicker(null), //
+                        hasName("BONDDIA (PF2)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD150921807"), hasTicker(null), //
+                        hasName("CETES (220224)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD151085643"), hasTicker(null), //
+                        hasName("CETES (220127)"), //
+                        hasCurrencyCode("MXN"))));
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin(null), hasWkn("SVD151273788"), hasTicker(null), //
+                        hasName("BONDDIA (PF2)"), //
+                        hasCurrencyCode("MXN"))));
 
         // check buy sell transaction
-        BuySellEntry entry = (BuySellEntry) results.stream().filter(BuySellEntryItem.class::isInstance).findFirst()
-                        .orElseThrow(IllegalArgumentException::new).getSubject();
+        assertThat(results, hasItem(purchase( //
+                        hasDate("2022-01-04T00:00"), hasShares(6.080), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote("Term: 2 | Rate: 5.51"), //
+                        hasAmount("MXN", 60540.38), hasGrossValue("MXN", 60540.38), //
+                        hasTaxes("MXN", 0.00), hasFees("MXN", 0.00))));
 
-        assertThat(entry.getPortfolioTransaction().getType(), is(PortfolioTransaction.Type.BUY));
-        assertThat(entry.getAccountTransaction().getType(), is(AccountTransaction.Type.BUY));
+        // check buy sell transaction
+        assertThat(results, hasItem(sale( //
+                        hasDate("2022-01-06T00:00"), hasShares(6.055), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote(null), //
+                        hasAmount("MXN", 60543.80), hasGrossValue("MXN", 60550.00), //
+                        hasTaxes("MXN", 6.20), hasFees("MXN", 0.00))));
 
-        assertThat(entry.getPortfolioTransaction().getDateTime(), is(LocalDateTime.parse("2022-01-06T00:00:00")));
-        assertThat(entry.getPortfolioTransaction().getShares(), is(Values.Share.factorize(6080)));
-        assertThat(entry.getSource(), is("EdoCta01.txt"));
-        assertThat(entry.getNote(), is("ID:SVD147529623 Series:220203 Term:2 Rate:5.51"));
+        // check dividend transaction
+        assertThat(results, hasItem(dividend( //
+                        hasDate("2022-01-06T00:00"), hasShares(3.00), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote(null), //
+                        hasAmount("MXN", 4.72), hasGrossValue("MXN", 4.72), //
+                        hasTaxes("MXN", 0.00), hasFees("MXN", 0.00))));
 
-        assertThat(entry.getPortfolioTransaction().getMonetaryAmount(),
-                        is(Money.of(CurrencyUnit.MXN, Values.Amount.factorize(60540.38))));
-        assertThat(entry.getPortfolioTransaction().getGrossValue(),
-                        is(Money.of(CurrencyUnit.MXN, Values.Amount.factorize(60540.38))));
-        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.TAX),
-                        is(Money.of(CurrencyUnit.MXN, Values.Amount.factorize(0.00))));
-        assertThat(entry.getPortfolioTransaction().getUnitSum(Unit.Type.FEE),
-                        is(Money.of(CurrencyUnit.MXN, Values.Amount.factorize(0.00))));
+        // check buy sell transaction
+        assertThat(results, hasItem(purchase( //
+                        hasDate("2022-01-11T00:00"), hasShares(21.00), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote("Term: 2 | Rate: 5.52"), //
+                        hasAmount("MXN", 209.10), hasGrossValue("MXN", 209.10), //
+                        hasTaxes("MXN", 0.00), hasFees("MXN", 0.00))));
+
+        // check buy sell transaction
+        assertThat(results, hasItem(sale( //
+                        hasDate("2022-01-13T00:00"), hasShares(21.00), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote(null), //
+                        hasAmount("MXN", 207.63), hasGrossValue("MXN", 210.00), //
+                        hasTaxes("MXN", 2.37), hasFees("MXN", 0.00))));
+
+        // check dividend transaction
+        assertThat(results, hasItem(dividend( //
+                        hasDate("2022-01-13T00:00"), hasShares(1.00), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote(null), //
+                        hasAmount("MXN", 1.58), hasGrossValue("MXN", 1.58), //
+                        hasTaxes("MXN", 0.00), hasFees("MXN", 0.00))));
+
+        // check buy sell transaction
+        assertThat(results, hasItem(purchase( //
+                        hasDate("2022-01-18T00:00"), hasShares(2.872), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote("Term: 2 | Rate: 5.57"), //
+                        hasAmount("MXN", 28596.03), hasGrossValue("MXN", 28596.03), //
+                        hasTaxes("MXN", 0.00), hasFees("MXN", 0.00))));
+
+        // check buy sell transaction
+        assertThat(results, hasItem(sale( //
+                        hasDate("2022-01-20T00:00"), hasShares(2.860), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote(null), //
+                        hasAmount("MXN", 28596.99), hasGrossValue("MXN", 28600.00), //
+                        hasTaxes("MXN", 3.01), hasFees("MXN", 0.00))));
+
+        // check dividend transaction
+        assertThat(results, hasItem(dividend( //
+                        hasDate("2022-01-20T00:00"), hasShares(1.00), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote(null), //
+                        hasAmount("MXN", 1.58), hasGrossValue("MXN", 1.58), //
+                        hasTaxes("MXN", 0.00), hasFees("MXN", 0.00))));
+
+        // check buy sell transaction
+        assertThat(results, hasItem(purchase( //
+                        hasDate("2022-01-25T00:00"), hasShares(3.505), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote("Term: 2 | Rate: 5.50"), //
+                        hasAmount("MXN", 34900.61), hasGrossValue("MXN", 34900.61), //
+                        hasTaxes("MXN", 0.00), hasFees("MXN", 0.00))));
+
+        // check buy sell transaction
+        assertThat(results, hasItem(sale( //
+                        hasDate("2022-01-27T00:00"), hasShares(3.491), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote(null), //
+                        hasAmount("MXN", 34901.31), hasGrossValue("MXN", 34910.00), //
+                        hasTaxes("MXN", 8.69), hasFees("MXN", 0.00))));
+
+        // check dividend transaction
+        assertThat(results, hasItem(dividend( //
+                        hasDate("2022-01-27T00:00"), hasShares(5.00), //
+                        hasSource("EdoCta01.txt"), //
+                        hasNote(null), //
+                        hasAmount("MXN", 7.89), hasGrossValue("MXN", 7.89), //
+                        hasTaxes("MXN", 0.00), hasFees("MXN", 0.00))));
     }
-
 }
