ee4781fdf1f5cf542931aac6a301397fb60bd332
mierin12 <mattmailspoubelle+GIT@gmail.com>
2025-07-03 20:49:56 +0200

    add moving average option to TradeProfitLoss Widget
    in addition to the existing FIFO method

---
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/CostMethodConfig.java
@@ -0,0 +1,14 @@
+package name.abuchen.portfolio.ui.views.dashboard;
+
+import name.abuchen.portfolio.model.Dashboard;
+import name.abuchen.portfolio.ui.Messages;
+
+public class CostMethodConfig extends EnumBasedConfig<CostMethod>
+{
+    public CostMethodConfig(WidgetDelegate<?> delegate)
+    {
+        super(delegate, Messages.LabelCapitalGainsMethod, CostMethod.class, Dashboard.Config.COST_METHOD,
+                        Policy.EXACTLY_ONE);
+    }
+}
+
