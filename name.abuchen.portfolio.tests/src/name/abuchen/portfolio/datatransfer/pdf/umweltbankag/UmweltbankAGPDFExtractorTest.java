5217a557fcb9b3696c0b1c527f26fe5127e2737c
Alexander Ott <45203494+Nirus2000@users.noreply.github.com>
2025-09-15 10:30:42 +0200

    Add new UmweltBank AG PDF-Importer (#4984)
    Closes #4945

---
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/umweltbankag/UmweltbankAGPDFExtractorTest.java
@@ -0,0 +1,71 @@
+package name.abuchen.portfolio.datatransfer.pdf.umweltbankag;
+
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasAmount;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasCurrencyCode;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasDate;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasFees;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasGrossValue;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasIsin;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasName;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasNote;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasShares;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasSource;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasTaxes;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasTicker;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasWkn;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.purchase;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.security;
+import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countAccountTransactions;
+import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countAccountTransfers;
+import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countBuySell;
+import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countSecurities;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import name.abuchen.portfolio.datatransfer.actions.AssertImportActions;
+import name.abuchen.portfolio.datatransfer.pdf.PDFInputFile;
+import name.abuchen.portfolio.datatransfer.pdf.UmweltbankAGPDFExtractor;
+import name.abuchen.portfolio.model.Client;
+
+@SuppressWarnings("nls")
+public class UmweltbankAGPDFExtractorTest
+{
+    @Test
+    public void testWertpapierkauf01()
+    {
+        var extractor = new UmweltbankAGPDFExtractor(new Client());
+
+        List<Exception> errors = new ArrayList<>();
+
+        var results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "Kauf01.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(countSecurities(results), is(1L));
+        assertThat(countBuySell(results), is(1L));
+        assertThat(countAccountTransactions(results), is(0L));
+        assertThat(countAccountTransfers(results), is(0L));
+        assertThat(results.size(), is(2));
+        new AssertImportActions().check(results, "EUR");
+
+        // check security
+        assertThat(results, hasItem(security( //
+                        hasIsin("LU2679277744"), hasWkn("A3EV2A"), hasTicker(null), //
+                        hasName("UMWELTBANK ETF-GL SDG FOCUS ACT.PORT. P EUR ACC. ON"), //
+                        hasCurrencyCode("EUR"))));
+
+        // check buy sell transaction
+        assertThat(results, hasItem(purchase( //
+                        hasDate("2025-06-12T09:04:17"), hasShares(500.00), //
+                        hasSource("Kauf01.txt"), //
+                        hasNote("Auftragsnummer 715355/24.00"), //
+                        hasAmount("EUR", 5268.32), hasGrossValue("EUR", 5211.00), //
+                        hasTaxes("EUR", 0.00), hasFees("EUR", 57.32))));
+    }
+}
