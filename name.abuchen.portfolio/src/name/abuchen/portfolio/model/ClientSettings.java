b7ab2b9d0964dfe158326f116f2391ca3d5d3a48
Andreas Buchen <andreas.buchen@gmail.com>
2025-09-20 09:25:55 +0200

    Fixed name of attribute logo as a client migration
    This reverts the previous fix that was always checking for the
    translated labels. However, this requires to always check for
    translations and can suddenly break if the application is opened
    using another locale.
    
    Issue: https://forum.portfolio-performance.info/t/logo-not-replacing-default-icon/37304

--- name.abuchen.portfolio/src/name/abuchen/portfolio/model/ClientSettings.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/model/ClientSettings.java
@@ -92,7 +92,10 @@ public class ClientSettings
     {
         Function<Class<? extends Attributable>, AttributeType> factory = target -> {
             AttributeType logoType = new AttributeType("logo"); //$NON-NLS-1$
-            logoType.setName(Messages.AttributesLogoName);
+
+            // Since we look up the logo by name, the name must not be
+            // translated. We cannot use the id because the user cannot edit it.
+            logoType.setName("Logo"); //$NON-NLS-1$
             logoType.setColumnLabel(Messages.AttributesLogoColumn);
             logoType.setTarget(target);
             logoType.setType(String.class);
@@ -247,11 +250,8 @@ public class ClientSettings
     @SuppressWarnings("unchecked")
     public Optional<AttributeType> getOptionalLogoAttributeType(Class<? extends Object> type)
     {
-        // issue: unfortunately, the name got translated. Therefore checking
-        // only for 'logo' is not good enough
-
         return getAttributeTypes().filter(t -> t.getConverter() instanceof AttributeType.ImageConverter
-                        && (t.getName().equalsIgnoreCase("logo") || Messages.AttributesLogoName.equals(t.getName())) //$NON-NLS-1$
+                        && t.getName().equalsIgnoreCase("logo") //$NON-NLS-1$
                         && t.supports((Class<? extends Attributable>) type)).findFirst();
     }
 }
