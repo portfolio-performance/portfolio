ed36e4028d3308b2937321cd14bcf8181aeacf13
MK-42 <5477999+MK-42@users.noreply.github.com>
2026-01-06 20:55:29 +0100

    Adapt AT-Format for Renault-Bank-Direkt PDF Importer
    Correctly handle multiple whitespace characters in interest transactions

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/renaultbankdirekt/RenaultBankDirektPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/renaultbankdirekt/RenaultBankDirektPDFExtractorTest.java
@@ -10,8 +10,8 @@ import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.removal;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.taxes;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countAccountTransactions;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countAccountTransfers;
-import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countItemsWithFailureMessage;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countBuySell;
+import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countItemsWithFailureMessage;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countSecurities;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -354,4 +354,27 @@ public class RenaultBankDirektPDFExtractorTest
                         hasSource("Kontoauszug10.txt"))));
 
     }
+
+    @Test
+    public void testKontoauszug11()
+    {
+        var extractor = new RenaultBankDirektPDFExtractor(new Client());
+        List<Exception> errors = new ArrayList<>();
+
+        var results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "Kontoauszug11.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(countSecurities(results), is(0L));
+        assertThat(countBuySell(results), is(0L));
+        assertThat(countAccountTransactions(results), is(1L));
+        assertThat(countAccountTransfers(results), is(0L));
+        assertThat(countItemsWithFailureMessage(results), is(0L));
+        assertThat(results.size(), is(1));
+        new AssertImportActions().check(results, "EUR");
+
+        assertThat(results, hasItem(interest(hasDate("2025-11-28"), hasAmount("EUR", 0.01), //
+                        hasSource("Kontoauszug11.txt"))));
+
+    }
+
 }
