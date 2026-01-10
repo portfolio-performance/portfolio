f0191e37b67f35b8dd30044f84c803d7121e9b7a
Johannes Ganzenmüller <johannes.ganzenmueller@gmail.com>
2026-01-10 20:48:47 +0100

    Add new PDF extractor for apoBank
    Closes #5251

---
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/ApoBankPDFExtractor.java
@@ -0,0 +1,92 @@
+package name.abuchen.portfolio.datatransfer.pdf;
+
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
+import name.abuchen.portfolio.model.AccountTransaction;
+import name.abuchen.portfolio.model.Client;
+
+@SuppressWarnings("nls")
+public class ApoBankPDFExtractor extends AbstractPDFExtractor
+{
+    public ApoBankPDFExtractor(Client client)
+    {
+        super(client);
+
+        addBankIdentifier("apoBank");
+
+        addDividendTransaction();
+    }
+
+    @Override
+    public String getLabel()
+    {
+        return "apoBank - Deutsche Apotheker- und Ärztebank eG";
+    }
+
+
+    private void addDividendTransaction()
+    {
+        final var type = new DocumentType("Bardividende");
+        this.addDocumentTyp(type);
+
+        var pdfTransaction = new Transaction<AccountTransaction>();
+
+        var firstRelevantLine = new Block("^Wertpapierbezeichnung.*$");
+        type.addBlock(firstRelevantLine);
+        firstRelevantLine.set(pdfTransaction);
+
+        pdfTransaction //
+
+                        .subject(() -> {
+                            var accountTransaction = new AccountTransaction();
+                            accountTransaction.setType(AccountTransaction.Type.DIVIDENDS);
+                            return accountTransaction;
+                        })
+
+                        // @formatter:off
+                        // Wertpapierbezeichnung iShs VII-$ Trsy Bd 3-7yr U.ETF Registered Shs EUR DIS.Hgd
+                        // o.N
+                        // ISIN IE00BGPP6473
+                        // WKN A2PDTT
+                        // Verwahrart Wertpapierrechnung
+                        // Lagerland Vereinigtes Königreich Grossbritannien und
+                        // Nordirland
+                        // Nominal/Stück 0,707 ST
+                        // Währung EUR
+                        // @formatter:on
+                        .section("name", "isin", "wkn", "currency") //
+                        .match("^Wertpapierbezeichnung (?<name>.*)$") //
+                        .match("^ISIN (?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9])$") //
+                        .match("^WKN (?<wkn>[A-Z0-9]{6})$") //
+                        .match("^Währung (?<currency>[A-Z]{3})$") //
+                        .assign((t, v) -> t.setSecurity(getOrCreateSecurity(v)))
+                        
+                        // @formatter:off
+                        // Nominal/Stück 0,707 ST
+                        // @formatter:on
+                        .section("shares") //
+                        .match("^Nominal/Stück (?<shares>[\\.,\\d]+).*$") //
+                        .assign((t, v) -> t.setShares(asShares(v.get("shares"))))
+                        
+                        // @formatter:off
+                        // Zahlungsdatum 27.08.2025
+                        // @formatter:on
+                        .section("date") //
+                        .match("^Zahlungsdatum (?<date>[\\d]{2}\\.[\\w]{2}\\.[\\d]{4})$")
+                        .assign((t, v) -> t.setDateTime(asDate(v.get("date"))))
+
+                        // @formatter:off
+                        // Bruttobetrag EUR 0,06
+                        // @formatter:on
+                        .section("amount", "currency") //
+                        .match("^Bruttobetrag (?<currency>[A-Z]{3}) (?<amount>[\\.,\\d]+)$") //
+                        .assign((t, v) -> {
+                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setAmount(asAmount(v.get("amount")));
+                        })
+
+                        .wrap(TransactionItem::new);
+
+    }
+}
