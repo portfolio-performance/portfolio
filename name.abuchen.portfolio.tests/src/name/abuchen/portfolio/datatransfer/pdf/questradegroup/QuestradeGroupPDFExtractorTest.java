90bddd19e324d5b0eeb731797e17b557b6e49f5f
Alexander Ott <webmaster@nirus-online.de>
2026-01-25 15:50:43 +0100

    Improve Questrade Group PDF-Importer
    - Improve pull request #5298
    - Rename Questrade to Questrade Group
    - The ticker symbol is added as standard, but the trading exchange is not set by default. This is to be decided by the user. We follow the standard practice of all importers here: if no trading place is specified but a ticker symbol is available, the latter is used.
    - Fix currency regEx [A-Z]{3}
    - Improve TickerSymbol regEx
    - Improve other regEx
    - Remove obsolet .trim() in attribut name
    - Remove obsolet gross attribut
    - Remove obsolet price attribut
    - Add addFeesSectionsTransaction function
    - Fix security name regEx
    - Add countSkippedItems in test cases
    - Fixes variable names
    - Improve addAccountStatementTransaction
    - Rename files

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/questradegroup/QuestradeGroupPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/questradegroup/QuestradeGroupPDFExtractorTest.java
@@ -1,4 +1,4 @@
-package name.abuchen.portfolio.datatransfer.pdf.questrade;
+package name.abuchen.portfolio.datatransfer.pdf.questradegroup;
 
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.deposit;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.dividend;
@@ -22,7 +22,7 @@ import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countAc
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countBuySell;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countItemsWithFailureMessage;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countSecurities;
-
+import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countSkippedItems;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -34,44 +34,17 @@ import java.util.List;
 import org.junit.Test;
 
 import name.abuchen.portfolio.datatransfer.actions.AssertImportActions;
-import name.abuchen.portfolio.datatransfer.pdf.QuestradePDFExtractor;
 import name.abuchen.portfolio.datatransfer.pdf.PDFInputFile;
+import name.abuchen.portfolio.datatransfer.pdf.QuestradeGroupPDFExtractor;
 import name.abuchen.portfolio.model.Client;
 
 @SuppressWarnings("nls")
-public class QuestradePDFExtractorTest
+public class QuestradeGroupPDFExtractorTest
 {
-
     @Test
-    public void testDeposit01()
+    public void testSecurityBuy01()
     {
-        var extractor = new QuestradePDFExtractor(new Client());
-
-        List<Exception> errors = new ArrayList<>();
-
-        var results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "Deposit01.txt"), errors);
-
-        assertThat(errors, empty());
-        assertThat(countSecurities(results), is(0L));
-        assertThat(countBuySell(results), is(0L));
-        assertThat(countAccountTransactions(results), is(1L));
-        assertThat(countAccountTransfers(results), is(0L));
-        assertThat(countItemsWithFailureMessage(results), is(0L));
-        assertThat(results.size(), is(1));
-        new AssertImportActions().check(results, "CAD");
-
-        // assert transaction
-        assertThat(results, hasItem(deposit( //
-                        hasDate("2025-04-09"), //
-                        hasAmount("CAD", 10000.00), //
-                        hasSource("Deposit01.txt"), //
-                        hasNote("Contribution"))));
-    }
-
-    @Test
-    public void testBuy01()
-    {
-        var extractor = new QuestradePDFExtractor(new Client());
+        var extractor = new QuestradeGroupPDFExtractor(new Client());
 
         List<Exception> errors = new ArrayList<>();
 
@@ -83,18 +56,19 @@ public class QuestradePDFExtractorTest
         assertThat(countAccountTransactions(results), is(0L));
         assertThat(countAccountTransfers(results), is(0L));
         assertThat(countItemsWithFailureMessage(results), is(0L));
+        assertThat(countSkippedItems(results), is(0L));
         assertThat(results.size(), is(2));
         new AssertImportActions().check(results, "CAD");
 
         // check security
         assertThat(results, hasItem(security( //
-                        hasIsin(null), hasWkn(null), hasTicker("VEQT.TO"), //
-                        hasName("VANGUARD ALL-EQUITY ETF  PORTFOLIO"), //
+                        hasIsin(null), hasWkn(null), hasTicker("VEQT"), //
+                        hasName("VANGUARD ALL-EQUITY ETF"), //
                         hasCurrencyCode("CAD"))));
 
         // check buy sell transaction
         assertThat(results, hasItem(purchase( //
-                        hasDate("2025-04-10"), hasShares(50.0), //
+                        hasDate("2025-04-10T00:00"), hasShares(50.00), //
                         hasSource("Buy01.txt"), //
                         hasNote(null), //
                         hasAmount("CAD", 2046.50), hasGrossValue("CAD", 2046.50), //
@@ -102,9 +76,9 @@ public class QuestradePDFExtractorTest
     }
 
     @Test
-    public void testBuy02()
+    public void testSecurityBuy02()
     {
-        var extractor = new QuestradePDFExtractor(new Client());
+        var extractor = new QuestradeGroupPDFExtractor(new Client());
 
         List<Exception> errors = new ArrayList<>();
 
@@ -116,18 +90,19 @@ public class QuestradePDFExtractorTest
         assertThat(countAccountTransactions(results), is(0L));
         assertThat(countAccountTransfers(results), is(0L));
         assertThat(countItemsWithFailureMessage(results), is(0L));
+        assertThat(countSkippedItems(results), is(0L));
         assertThat(results.size(), is(2));
         new AssertImportActions().check(results, "CAD");
 
         // check security
         assertThat(results, hasItem(security( //
-                        hasIsin(null), hasWkn(null), hasTicker("VEQT.TO"), //
-                        hasName("VANGUARD ALL-EQUITY ETF|PORTFOLIO ETF"), //
+                        hasIsin(null), hasWkn(null), hasTicker("VEQT"), //
+                        hasName("VANGUARD ALL-EQUITY ETF"), //
                         hasCurrencyCode("CAD"))));
 
         // check buy sell transaction
         assertThat(results, hasItem(purchase( //
-                        hasDate("2023-01-16"), hasShares(29.0), //
+                        hasDate("2023-01-16T00:00"), hasShares(29.00), //
                         hasSource("Buy02.txt"), //
                         hasNote(null), //
                         hasAmount("CAD", 974.50), hasGrossValue("CAD", 974.40), //
@@ -137,7 +112,7 @@ public class QuestradePDFExtractorTest
     @Test
     public void testDividend01()
     {
-        var extractor = new QuestradePDFExtractor(new Client());
+        var extractor = new QuestradeGroupPDFExtractor(new Client());
 
         List<Exception> errors = new ArrayList<>();
 
@@ -149,22 +124,46 @@ public class QuestradePDFExtractorTest
         assertThat(countAccountTransactions(results), is(1L));
         assertThat(countAccountTransfers(results), is(0L));
         assertThat(countItemsWithFailureMessage(results), is(0L));
+        assertThat(countSkippedItems(results), is(0L));
         assertThat(results.size(), is(2));
         new AssertImportActions().check(results, "CAD");
 
         // check security
         assertThat(results, hasItem(security( //
-                        hasIsin(null), hasWkn(null), hasTicker("VEQT.TO"), //
+                        hasIsin(null), hasWkn(null), hasTicker("VEQT"), //
                         hasName(null), //
                         hasCurrencyCode("CAD"))));
 
         // check dividend transaction
         assertThat(results, hasItem(dividend( //
-                        hasDate("2025-01-07"), hasShares(29.0), //
+                        hasDate("2025-01-07T00:00"), hasShares(29.00), //
                         hasSource("Dividend01.txt"), //
                         hasNote("REC 12/30/24"), //
                         hasAmount("CAD", 20.69), hasGrossValue("CAD", 20.69), //
                         hasTaxes("CAD", 0.00), hasFees("CAD", 0.00))));
     }
 
+    @Test
+    public void testAccountTransaction01()
+    {
+        var extractor = new QuestradeGroupPDFExtractor(new Client());
+
+        List<Exception> errors = new ArrayList<>();
+
+        var results = extractor.extract(PDFInputFile.loadTestCase(getClass(), "AccountStatement01.txt"), errors);
+
+        assertThat(errors, empty());
+        assertThat(countSecurities(results), is(0L));
+        assertThat(countBuySell(results), is(0L));
+        assertThat(countAccountTransactions(results), is(1L));
+        assertThat(countAccountTransfers(results), is(0L));
+        assertThat(countItemsWithFailureMessage(results), is(0L));
+        assertThat(countSkippedItems(results), is(0L));
+        assertThat(results.size(), is(1));
+        new AssertImportActions().check(results, "CAD");
+
+        // assert transaction
+        assertThat(results, hasItem(deposit(hasDate("2025-04-09"), hasAmount("CAD", 10000.00),
+                        hasSource("AccountStatement01.txt"), hasNote(null))));
+    }
 }
