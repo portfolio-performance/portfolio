7c07bbce2da4129212ae5ef4aeff3f87fb6885c5
Alexander Ott <webmaster@nirus-online.de>
2026-02-03 23:32:15 +0100

    Improvments

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/austriananadibank/AustrianAnadiBankPDFExtractorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/datatransfer/pdf/austriananadibank/AustrianAnadiBankPDFExtractorTest.java
@@ -3,8 +3,10 @@ package name.abuchen.portfolio.datatransfer.pdf.austriananadibank;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.deposit;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasAmount;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasDate;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasFees;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasGrossValue;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasNote;
+import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasShares;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasSource;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.hasTaxes;
 import static name.abuchen.portfolio.datatransfer.ExtractorMatchers.interest;
@@ -14,6 +16,7 @@ import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countAc
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countBuySell;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countItemsWithFailureMessage;
 import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countSecurities;
+import static name.abuchen.portfolio.datatransfer.ExtractorTestUtilities.countSkippedItems;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -47,17 +50,21 @@ public class AustrianAnadiBankPDFExtractorTest
         assertThat(countAccountTransactions(results), is(3L));
         assertThat(countAccountTransfers(results), is(0L));
         assertThat(countItemsWithFailureMessage(results), is(0L));
+        assertThat(countSkippedItems(results), is(0L));
         assertThat(results.size(), is(3));
         new AssertImportActions().check(results, "EUR");
 
         // assert transaction
         assertThat(results, hasItem(deposit(hasDate("2025-05-14"), hasAmount("EUR", 2500.00), //
-                        hasSource("Kontoauszug01.txt"))));
+                        hasSource("Kontoauszug01.txt"), hasNote("IBAN: TT40 1705 1256 3324 8502"))));
+
+        // assert transaction
         assertThat(results, hasItem(removal(hasDate("2025-05-15"), hasAmount("EUR", 2500.00), //
-                        hasSource("Kontoauszug01.txt"), //
-                        hasNote("ONLINE-FESTGELD / KONTO 12345555555"))));
+                        hasSource("Kontoauszug01.txt"), hasNote("ONLINE-FESTGELD / KONTO 12345555555"))));
+
+        // assert transaction
         assertThat(results, hasItem(deposit(hasDate("2025-05-19"), hasAmount("EUR", 8320.00), //
-                        hasSource("Kontoauszug01.txt"))));
+                        hasSource("Kontoauszug01.txt"), hasNote("IBAN: ur98 0202 5368 2910 8017"))));
     }
 
     @Test
@@ -75,20 +82,29 @@ public class AustrianAnadiBankPDFExtractorTest
         assertThat(countAccountTransactions(results), is(4L));
         assertThat(countAccountTransfers(results), is(0L));
         assertThat(countItemsWithFailureMessage(results), is(0L));
+        assertThat(countSkippedItems(results), is(0L));
         assertThat(results.size(), is(4));
         new AssertImportActions().check(results, "EUR");
 
         // assert transaction
         assertThat(results, hasItem(deposit(hasDate("2025-12-09"), hasAmount("EUR", 3500.00), //
-                        hasSource("Kontoauszug02.txt"))));
+                        hasSource("Kontoauszug02.txt"), hasNote("IBAN: AT12 1234 1234 1234 1234"))));
+
+        // assert transaction
         assertThat(results, hasItem(deposit(hasDate("2025-12-11"), hasAmount("EUR", 500.00), //
-                        hasSource("Kontoauszug02.txt"))));
+                        hasSource("Kontoauszug02.txt"), hasNote("IBAN: AT12 1234 1234 1234 1234"))));
+
+        // assert transaction
         assertThat(results, hasItem(removal(hasDate("2025-12-16"), hasAmount("EUR", 2000.00), //
-                        hasSource("Kontoauszug02.txt"))));
-        assertThat(results, hasItem(interest(hasDate("2025-12-31"), hasAmount("EUR", 123.91), //
-                        hasGrossValue("EUR", 165.21), hasTaxes("EUR", 41.30), //
-                        hasNote("Habenzinsen"), //
-                        hasSource("Kontoauszug02.txt"))));
+                        hasSource("Kontoauszug02.txt"), hasNote("IBAN: AT12 1234 1234 1234 1234"))));
+
+        // assert transaction
+        assertThat(results, hasItem(interest( //
+                        hasDate("2025-12-31"), hasShares(0), //
+                        hasSource("Kontoauszug02.txt"), //
+                        hasNote("Abschluss 31.12.2025"), //
+                        hasAmount("EUR", 123.91), hasGrossValue("EUR", 165.21), //
+                        hasTaxes("EUR", 41.30), hasFees("EUR", 0.00))));
     }
 
     @Test
@@ -106,24 +122,36 @@ public class AustrianAnadiBankPDFExtractorTest
         assertThat(countAccountTransactions(results), is(7L));
         assertThat(countAccountTransfers(results), is(0L));
         assertThat(countItemsWithFailureMessage(results), is(0L));
+        assertThat(countSkippedItems(results), is(0L));
         assertThat(results.size(), is(7));
         new AssertImportActions().check(results, "EUR");
 
         // assert transaction
         assertThat(results, hasItem(removal(hasDate("2025-11-04"), hasAmount("EUR", 500.00), //
-                        hasSource("Kontoauszug03.txt"))));
+                        hasSource("Kontoauszug03.txt"), hasNote("IBAN: AT11 1234 1234 1234 1234"))));
+
+        // assert transaction
         assertThat(results, hasItem(deposit(hasDate("2025-11-05"), hasAmount("EUR", 500.00), //
-                        hasSource("Kontoauszug03.txt"))));
+                        hasSource("Kontoauszug03.txt"), hasNote("IBAN: AT11 1234 1234 1234 1234"))));
+
+        // assert transaction
         assertThat(results, hasItem(deposit(hasDate("2025-11-11"), hasAmount("EUR", 500.00), //
-                        hasSource("Kontoauszug03.txt"))));
+                        hasSource("Kontoauszug03.txt"), hasNote("IBAN: AT11 1234 1234 1234 1234"))));
+
+        // assert transaction
         assertThat(results, hasItem(deposit(hasDate("2025-11-17"), hasAmount("EUR", 2500.00), //
-                        hasNote("ABRECHNUNG ZU KONTO 00123456678"), //
-                        hasSource("Kontoauszug03.txt"))));
-        assertThat(results, hasItem(interest(hasDate("2025-11-17"), hasAmount("EUR", 22.75), //
-                        hasGrossValue("EUR", 30.33), hasTaxes("EUR", 7.58), //
-                        hasNote("ABRECHNUNG ZU KONTO 00123456678"), //
-                        hasSource("Kontoauszug03.txt"))));
+                        hasSource("Kontoauszug03.txt"), hasNote("Abrechnung zu Konto: 00123456678"))));
+
+        // assert transaction
+        assertThat(results, hasItem(interest( //
+                        hasDate("2025-11-17"), hasShares(0), //
+                        hasSource("Kontoauszug03.txt"), //
+                        hasNote("Abrechnung zu Konto: 00123456678"), //
+                        hasAmount("EUR", 22.75), hasGrossValue("EUR", 30.33), //
+                        hasTaxes("EUR", 7.58), hasFees("EUR", 0.00))));
+
+        // assert transaction
         assertThat(results, hasItem(removal(hasDate("2025-11-24"), hasAmount("EUR", 2784.00), //
-                        hasSource("Kontoauszug03.txt"))));
+                        hasSource("Kontoauszug03.txt"), hasNote("IBAN: AT11 1234 1234 1234 1234"))));
     }
 }
--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/AustrianAnadiBankPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/AustrianAnadiBankPDFExtractor.java
@@ -1,11 +1,14 @@
 package name.abuchen.portfolio.datatransfer.pdf;
 
+import static name.abuchen.portfolio.util.TextUtil.trim;
+
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
 import name.abuchen.portfolio.model.AccountTransaction;
 import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.Transaction.Unit;
+import name.abuchen.portfolio.money.Money;
 
 @SuppressWarnings("nls")
 public class AustrianAnadiBankPDFExtractor extends AbstractPDFExtractor
@@ -14,6 +17,7 @@ public class AustrianAnadiBankPDFExtractor extends AbstractPDFExtractor
     {
         super(client);
 
+        addBankIdentifier("anadibank.at");
         addBankIdentifier("HAABAT2K");
 
         addAccountStatementTransaction();
@@ -29,24 +33,20 @@ public class AustrianAnadiBankPDFExtractor extends AbstractPDFExtractor
     {
         final var type = new DocumentType("KONTOAUSZUG", //
                         documentContext -> documentContext //
-                        // @formatter:off
-                        // Neuer Saldo zu Ihren Gunsten
-                        // Max Mustermann EUR 4.348,95
-                        // @formatter:on
+                                        // @formatter:off
+                                        // Neuer Saldo zu Ihren Gunsten
+                                        // Max Mustermann EUR 4.348,95
+                                        // @formatter:on
                                         .section("currency") //
                                         .find("^Neuer Saldo zu Ihren Gunsten.*$") //
-                                        .match("^.*(?<currency>[\\w]{3}).*$") //
-                                        .assign((ctx, v) -> {
-                                            ctx.put("currency", asCurrencyCode(v.get("currency")));
-                                        })
+                                        .match("^.* (?<currency>[A-Z]{3}) [\\.,\\d]+(\\-)?$") //
+                                        .assign((ctx, v) -> ctx.put("currency", asCurrencyCode(v.get("currency"))))
                                         // @formatter:off
-                        // Inglitschstraße 5A, 050202-0 vom 30.05.2025
-                        // @formatter:on
+                                        // Inglitschstraße 5A, 050202-0 vom 30.05.2025
+                                        // @formatter:on
                                         .section("year") //
-                                        .match("^.*[\\d]{6}-[\\d] vom [\\d]{2}.[\\d]{2}.(?<year>[\\d]{4})$") //
-                                        .assign((ctx, v) -> {
-                                            ctx.put("year", v.get("year"));
-                                        }));
+                                        .match("^.*[\\d]{6}\\-[\\d] vom [\\d]{2}.[\\d]{2}.(?<year>[\\d]{4})$") //
+                                        .assign((ctx, v) -> ctx.put("year", v.get("year"))));
 
         this.addDocumentTyp(type);
 
@@ -56,66 +56,93 @@ public class AustrianAnadiBankPDFExtractor extends AbstractPDFExtractor
         // Zahlungsreferenz: Festgeld
         // REF: PSSAAT140525000665510002688
         // @formatter:on
-        // Make sure to not match the Interest- and SubAccountClosure
-        // Blocks as well (negative lookahead)
-        var depositBlock = new Block(
-                        "^[\\d]{1,2}\\.[\\d]{1,2} (?!Abschluss)(?!ABRECHNUNG ZU KONTO).* [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$");
-        type.addBlock(depositBlock);
-        depositBlock.set(new Transaction<AccountTransaction>() //
+        var depositBlock_Format01 = new Block("^[\\d]{1,2}\\.[\\d]{1,2} (?!Abschluss)(?!ABRECHNUNG ZU KONTO).* [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$");
+        type.addBlock(depositBlock_Format01);
+        depositBlock_Format01.set(new Transaction<AccountTransaction>()
 
                         .subject(() -> {
                             var accountTransaction = new AccountTransaction();
                             accountTransaction.setType(AccountTransaction.Type.DEPOSIT);
                             return accountTransaction;
-                        }).section("date", "amount") //
+                        })
+
+                        .section("date", "amount") //
                         .documentContext("currency", "year") //
-                        .match("^(?<date>[\\d]{1,2}\\.[\\d]{1,2}) .* [\\d]{1,2}\\.[\\d]{1,2} (?<amount>[\\.,\\d]+)$") //
+                        .match("^(?<date>[\\d]{1,2}\\.[\\d]{1,2}) .* [\\d]{1,2}\\.[\\d]{1,2} (?<amount>[\\.,\\d]+)$") //)
                         .assign((t, v) -> {
                             t.setDateTime(asDate(v.get("date") + '.' + v.get("year")));
-                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setCurrencyCode(v.get("currency"));
                             t.setAmount(asAmount(v.get("amount")));
-                        }) //
+                        })
 
                         .section("note").optional() //
-                        .match("^(?<note>IBAN .*)$") //
-                        .assign((t, v) -> {
-                            t.setNote(v.get("note"));
-                        }) //
+                        .match("^(?<note>IBAN: .*)$") //
+                        .assign((t, v) -> t.setNote(trim(v.get("note"))))
+
+                        .wrap(TransactionItem::new));
+
+        // @formatter:off
+        // 17.11 ABRECHNUNG ZU KONTO 00123456678 17.11 2.522,75
+        // VERANLAGUNGSBETRAG             2.500,00
+        // LAUFZEIT        2025-05-15 - 2025-11-17
+        // Habenzinsen
+        // AB 2025-05-15          2,4000%              30,33
+        // KESt                                         7,58-
+        // @formatter:on
+        var depositBlock_Format02 = new Block("^[\\d]{1,2}\\.[\\d]{1,2} ABRECHNUNG ZU KONTO .* [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$");
+        type.addBlock(depositBlock_Format02);
+        depositBlock_Format02.set(new Transaction<AccountTransaction>()
+
+                        .subject(() -> {
+                            var accountTransaction = new AccountTransaction();
+                            accountTransaction.setType(AccountTransaction.Type.DEPOSIT);
+                            return accountTransaction;
+                        })
+
+                        .section("date", "note", "amount") //
+                        .documentContext("currency", "year") //
+                        .match("^(?<date>[\\d]{1,2}\\.[\\d]{1,2}) ABRECHNUNG ZU KONTO (?<note>[\\d]+) [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$") //
+                        .match("^VERANLAGUNGSBETRAG[\\s]{1,}(?<amount>[\\.,\\d]+)$") //
+                        .assign((t, v) -> { //
+                            t.setDateTime(asDate(v.get("date") + "." + v.get("year")));
+                            t.setCurrencyCode(v.get("currency"));
+                            t.setAmount(asAmount(v.get("amount")));
+                            t.setNote("Abrechnung zu Konto: " + v.get("note"));
+                        })
 
                         .wrap(TransactionItem::new));
 
         // @formatter:off
         // 16.12 IBAN: AT12 1234 1234 1234 1234 16.12 2.000,00-
         // Max Mustermann
-        // REF: 52000251215EBP00000243646135     
+        // REF: 52000251215EBP00000243646135
+        //
+        // 15.05 ONLINE-FESTGELD / KONTO 12345555555 15.05 2.500,00-
+        //
+        // 16.12 IBAN: AT12 1234 1234 1234 1234 16.12 2.000,00-
         // @formatter:on
-        // Make sure to not match the Interest- and SubAccountClosure
-        // Blocks as well (negative lookahead)
-        var removalBlock = new Block(
-                        "^[\\d]{1,2}\\.[\\d]{1,2} (?!Abschluss)(?!ABRECHNUNG ZU KONTO).* [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+-$");
+        var removalBlock = new Block("^[\\d]{1,2}\\.[\\d]{1,2} (?!Abschluss)(?!ABRECHNUNG ZU KONTO).* [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+\\-$");
         type.addBlock(removalBlock);
-        removalBlock.set(new Transaction<AccountTransaction>() //
+        removalBlock.set(new Transaction<AccountTransaction>()
 
                         .subject(() -> {
                             var accountTransaction = new AccountTransaction();
                             accountTransaction.setType(AccountTransaction.Type.REMOVAL);
                             return accountTransaction;
-                        }) //
+                        })
 
                         .section("date", "amount") //
                         .documentContext("currency", "year") //
-                        .match("^(?<date>[\\d]{1,2}\\.[\\d]{1,2}) .* [\\d]{1,2}\\.[\\d]{1,2} (?<amount>[\\.,\\d]+)-$") //
+                        .match("^(?<date>[\\d]{1,2}\\.[\\d]{1,2}) .* [\\d]{1,2}\\.[\\d]{1,2} (?<amount>[\\.,\\d]+)\\-$") //
                         .assign((t, v) -> {
                             t.setDateTime(asDate(v.get("date") + '.' + v.get("year")));
-                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setCurrencyCode(v.get("currency"));
                             t.setAmount(asAmount(v.get("amount")));
                         })
 
                         .section("note").optional() //
-                        .match("^[\\d]{1,2}\\.[\\d]{1,2} (?<note>ONLINE-FESTGELD.*) [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+-$") //
-                        .assign((t, v) -> {
-                            t.setNote(v.get("note"));
-                        })
+                        .match("^[\\d]{1,2}\\.[\\d]{1,2} (?<note>(ONLINE-FESTGELD|IBAN:).*) [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+-$") //
+                        .assign((t, v) -> t.setNote(trim(v.get("note"))))
 
                         .wrap(TransactionItem::new));
 
@@ -124,61 +151,35 @@ public class AustrianAnadiBankPDFExtractor extends AbstractPDFExtractor
         // Habenzinsen                                165,21
         // KESt                                        41,30-
         // @formatter:on
-        var interestBlock = new Block("^[\\d]{1,2}\\.[\\d]{1,2} Abschluss [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$");
-        type.addBlock(interestBlock);
-        interestBlock.set(new Transaction<AccountTransaction>() //
+        var interestBlock_Format01 = new Block("^[\\d]{1,2}\\.[\\d]{1,2} Abschluss [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$");
+        type.addBlock(interestBlock_Format01);
+        interestBlock_Format01.set(new Transaction<AccountTransaction>()
 
                         .subject(() -> {
                             var accountTransaction = new AccountTransaction();
                             accountTransaction.setType(AccountTransaction.Type.INTEREST);
                             return accountTransaction;
-                        }) //
+                        })
 
-                        .section("date", "amount", "note", "tax") //
+                        .section("date", "note", "amount") //
                         .documentContext("currency", "year") //
-                        .match("^(?<date>[\\d]{1,2}\\.[\\d]{1,2}) Abschluss [\\d]{1,2}\\.[\\d]{1,2} (?<amount>[\\.,\\d]+)$") //
-                        .match("^(?i)(?<note>Habenzinsen)[^\\w]*[\\.,\\d]+$") //
-                        .match("^(?i)KESt[^\\w]*(?<tax>[\\.,\\d]+)-$") //
+                        .match("^(?<date>[\\d]{1,2}\\.[\\d]{1,2}) (?<note>Abschluss [\\d]{1,2}\\.[\\d]{1,2}) (?<amount>[\\.,\\d]+)$") //
+                        .match("^(?i)Habenzinsen[\\s]{1,}[\\.,\\d]+$") //
                         .assign((t, v) -> {
                             t.setDateTime(asDate(v.get("date") + '.' + v.get("year")));
-                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
-                            t.setAmount(asAmount(v.get("amount")));
-                            t.setNote(v.get("note"));
-                            processTaxEntries(t, v, type);
-                        }) //
-
-                        .wrap(TransactionItem::new));
-        
-        // @formatter:off
-        // 17.11 ABRECHNUNG ZU KONTO 00123456678 17.11 2.522,75
-        // VERANLAGUNGSBETRAG             2.500,00
-        // LAUFZEIT        2025-05-15 - 2025-11-17
-        // Habenzinsen
-        // AB 2025-05-15          2,4000%              30,33
-        // KESt                                         7,58-
-        // @formatter:on
-        var subAccountClosureDepositBlock = new Block(
-                        "^[\\d]{1,2}\\.[\\d]{1,2} ABRECHNUNG ZU KONTO .* [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$");
-        type.addBlock(subAccountClosureDepositBlock);
-        subAccountClosureDepositBlock.set(new Transaction<AccountTransaction>() //
-
-                        .subject(() -> {
-                            var accountTransaction = new AccountTransaction();
-                            accountTransaction.setType(AccountTransaction.Type.DEPOSIT);
-                            return accountTransaction;
-                        }) //
-
-                        .section("date", "amount", "account") //
-                        .documentContext("currency", "year") //
-                        .match("^(?<date>[\\d]{1,2}\\.[\\d]{1,2}) ABRECHNUNG ZU KONTO (?<account>[\\d]+) [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$") //
-                        .match("^VERANLAGUNGSBETRAG[^\\w]*(?<amount>[\\.,\\d]+)$") //
-
-                        .assign((t, v) -> { //
-                            t.setDateTime(asDate(v.get("date") + "." + v.get("year")));
                             t.setCurrencyCode(v.get("currency"));
                             t.setAmount(asAmount(v.get("amount")));
-                            t.setNote("ABRECHNUNG ZU KONTO " + v.get("account"));
-                        }) //
+                            t.setNote(v.get("note") + '.' + v.get("year"));
+                        })
+
+                        .section("tax").optional() //
+                        .documentContext("currency") //
+                        .match("^(?i)KESt[\\s]{1,}(?<tax>[\\.,\\d]+)\\-$") //
+                        .assign((t, v) -> {
+                            var tax = Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("tax")));
+
+                            t.addUnit(new Unit(Unit.Type.TAX, tax));
+                        })
 
                         .wrap(TransactionItem::new));
 
@@ -190,39 +191,39 @@ public class AustrianAnadiBankPDFExtractor extends AbstractPDFExtractor
         // AB 2025-05-15          2,4000%              30,33
         // KESt                                         7,58-
         // @formatter:on
-        var subAccountClosureInterestBlock = new Block(
-                        "^[\\d]{1,2}\\.[\\d]{1,2} ABRECHNUNG ZU KONTO .* [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$");
-        type.addBlock(subAccountClosureInterestBlock);
-        subAccountClosureInterestBlock.set(new Transaction<AccountTransaction>() //
+        var interestBlock_Format02 = new Block("^[\\d]{1,2}\\.[\\d]{1,2} ABRECHNUNG ZU KONTO .* [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$");
+        type.addBlock(interestBlock_Format02);
+        interestBlock_Format02.set(new Transaction<AccountTransaction>()
 
                         .subject(() -> {
                             var accountTransaction = new AccountTransaction();
                             accountTransaction.setType(AccountTransaction.Type.INTEREST);
                             return accountTransaction;
-                        }) //
+                        })
 
-                        .section("date", "account", "interest", "tax") //
+                        .section("date", "note", "amount") //
                         .documentContext("currency", "year") //
-                        .match("^(?<date>[\\d]{1,2}\\.[\\d]{1,2}) ABRECHNUNG ZU KONTO (?<account>[\\d]+) [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$") //
-                        .match("^VERANLAGUNGSBETRAG[^\\w]*[\\.,\\d]+$") //
+                        .match("^(?<date>[\\d]{1,2}\\.[\\d]{1,2}) ABRECHNUNG ZU KONTO (?<note>[\\d]+) [\\d]{1,2}\\.[\\d]{1,2} [\\.,\\d]+$") //
                         .find("^Habenzinsen$") //
-                        .match("^AB [\\d]{4}-[\\d]{2}-[\\d]{2} .* (?<interest>[\\.,\\d]+)$") //
-                        .match("^(?i)KESt[^\\w]*(?<tax>[\\.,\\d]+)-$")
-
+                        .match("^AB [\\d]{4}\\-[\\d]{2}\\-[\\d]{2} .* (?<amount>[\\.,\\d]+)$") //
                         .assign((t, v) -> { //
                             t.setDateTime(asDate(v.get("date") + "." + v.get("year")));
                             t.setCurrencyCode(v.get("currency"));
-                            t.setAmount(asAmount(v.get("interest")));
-                            processTaxEntries(t, v, type);
+                            t.setAmount(asAmount(v.get("amount")));
+                            t.setNote("Abrechnung zu Konto: " + v.get("note"));
+                        })
 
-                            // recalculate net-amount after computing tax
-                            t.setMonetaryAmount(t.getMonetaryAmount().subtract(t.getUnitSum(Unit.Type.TAX)));
-                            t.setNote("ABRECHNUNG ZU KONTO " + v.get("account"));
-                        }) //
+                        .section("tax").optional() //
+                        .documentContext("currency") //
+                        .match("^(?i)KESt[\\s]{1,}(?<tax>[\\.,\\d]+)\\-$") //
+                        .assign((t, v) -> {
+                            var tax = Money.of(asCurrencyCode(v.get("currency")), asAmount(v.get("tax")));
+
+                            t.addUnit(new Unit(Unit.Type.TAX, tax));
+                            t.setMonetaryAmount(t.getMonetaryAmount().subtract(tax));
+                        })
 
                         .wrap(TransactionItem::new));
 
     }
-
-
 }
