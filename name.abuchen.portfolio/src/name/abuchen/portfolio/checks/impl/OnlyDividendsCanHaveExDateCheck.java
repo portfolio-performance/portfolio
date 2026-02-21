1efdc4a9711dc7fb59702989a47309e659c2fb02
Andreas Buchen <andreas.buchen@gmail.com>
2026-02-21 08:59:50 +0100

    Added check to ensure that only dividend transactions have an ex-date
    Issue: #5439

---
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/checks/impl/OnlyDividendsCanHaveExDateCheck.java
@@ -0,0 +1,31 @@
+package name.abuchen.portfolio.checks.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import name.abuchen.portfolio.checks.Check;
+import name.abuchen.portfolio.checks.Issue;
+import name.abuchen.portfolio.model.AccountTransaction;
+import name.abuchen.portfolio.model.Client;
+
+public class OnlyDividendsCanHaveExDateCheck implements Check
+{
+    @Override
+    public List<Issue> execute(Client client)
+    {
+        var answer = new ArrayList<Issue>();
+
+        for (var account : client.getAccounts())
+        {
+            for (var tx : account.getTransactions())
+            {
+                if (tx.getExDate() != null && tx.getType() != AccountTransaction.Type.DIVIDENDS)
+                {
+                    tx.setExDate(null);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
