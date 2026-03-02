f135cb74aad79871cb016e94937401a8bce7fcb5
JGK <jkcoder@skymail.app>
2026-03-01 17:09:31 -0800

    More sensible default

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/FIREWidget.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/views/dashboard/FIREWidget.java
@@ -46,7 +46,7 @@ public class FIREWidget extends WidgetDelegate<FIREWidget.FIREData>
     private static final double MAX_TIME_TO_FIRE_YEARS = 50;
     private static final double EPSILON_MONTHS = 0.01;
     private static final String DEFAULT_FIRE_NUMBER_INPUT = "1500000"; //$NON-NLS-1$
-    private static final String DEFAULT_MONTHLY_SAVINGS_INPUT = "500000"; //$NON-NLS-1$
+    private static final String DEFAULT_MONTHLY_SAVINGS_INPUT = "5000"; //$NON-NLS-1$
 
     public static class FIREData
     {
