835ab355aac1bb7d9de50d1bc114beb835ca61a9
Christoph Linimayer <24368986+iAppDeveloper88@users.noreply.github.com>
2026-03-09 12:39:53 +0100

    Added import of delivery inbound and outbound documents for Oberbank

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/OberbankPDFExtractor.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/OberbankPDFExtractor.java
@@ -3,9 +3,15 @@ package name.abuchen.portfolio.datatransfer.pdf;
 import static name.abuchen.portfolio.util.TextUtil.concatenate;
 import static name.abuchen.portfolio.util.TextUtil.trim;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.regex.Pattern;
+
 import name.abuchen.portfolio.datatransfer.ExtractorUtils;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Block;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.DocumentType;
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.LineSpan;
+import name.abuchen.portfolio.datatransfer.pdf.PDFParser.SplittingStrategy;
 import name.abuchen.portfolio.datatransfer.pdf.PDFParser.Transaction;
 import name.abuchen.portfolio.model.BuySellEntry;
 import name.abuchen.portfolio.model.Client;
@@ -23,6 +29,7 @@ public class OberbankPDFExtractor extends AbstractPDFExtractor
         addBankIdentifier("Oberbank AG");
 
         addBuySellTransaction();
+        addDeliveryInOutBoundTransaction();
     }
 
     @Override
@@ -33,7 +40,7 @@ public class OberbankPDFExtractor extends AbstractPDFExtractor
 
     private void addBuySellTransaction()
     {
-        final var type = new DocumentType("(Wertpapier-Abrechnu\\s*n\\s*g\\s*" //
+        final var type = new DocumentType("(Wertpapier-Abrechnu\\s*n\\s*g\\s+" //
                         + "(Kauf" //
                         + "|Verkauf))");
         this.addDocumentTyp(type);
@@ -124,15 +131,12 @@ public class OberbankPDFExtractor extends AbstractPDFExtractor
                                                                             .factorize(shares.doubleValue() / 100));
                                                         }))
 
-                        .oneOf( //
                         // @formatter:off
-                                        // Handelszeitpunkt: 28.01.2021 09:16:17
-                                        // @formatter:on
-                                        section -> section //
-                                                        .attributes("date", "time") //
-                                                        .match("^Handelszeitpunkt: (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2})$") //
-                                                        .assign((t, v) -> t
-                                                                        .setDate(asDate(v.get("date"), v.get("time")))))
+                        // Handelszeitpunkt: 28.01.2021 09:16:17
+                        // @formatter:on
+                        .section("date", "time")
+                        .match("^Handelszeitpunkt: (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}) (?<time>[\\d]{2}:[\\d]{2}:[\\d]{2})$") //
+                        .assign((t, v) -> t.setDate(asDate(v.get("date"), v.get("time"))))
 
                         .oneOf( //
                         // @formatter:off
@@ -185,6 +189,125 @@ public class OberbankPDFExtractor extends AbstractPDFExtractor
 
         addTaxesSectionsTransaction(pdfTransaction, type);
         addFeesSectionsTransaction(pdfTransaction, type);
+    }
+
+    private void addDeliveryInOutBoundTransaction()
+    {
+        final var type = new DocumentType("(Durchf.hrungsanzeig\\s*e\\s+" //
+                        + "(Freier Erhalt" //
+                        + "|Freie Lieferung))");
+        this.addDocumentTyp(type);
+
+        var pdfTransaction = new Transaction<PortfolioTransaction>();
+
+        // Delivery inbound and outbound documents have multiple pages. The
+        // first two start with the same line,
+        // e.g.: Durchführungsanzeig e Freier Erhalt
+        //
+        // Repeated occurrences must be ignored to prevent the creation of
+        // duplicate blocks.
+        var startsWith = Pattern.compile("^Durchf.hrungsanzeig\\s*e\\s+(Freie Lieferung|Freier Erhalt)$");
+        var splittingStrategy = (SplittingStrategy) lines -> {
+            var blockIdentifiers = new HashSet<String>();
+
+            // first: find the start of the blocks
+            var blockStarts = new ArrayList<Integer>();
+
+            for (var ii = 0; ii < lines.length; ii++)
+            {
+                var matcher = startsWith.matcher(lines[ii]);
+                if (matcher.matches() && blockIdentifiers.add(lines[ii]))
+                    blockStarts.add(ii);
+            }
+
+            // second: convert to line spans
+            var spans = new ArrayList<LineSpan>();
+            for (var ii = 0; ii < blockStarts.size(); ii++)
+            {
+                int startLine = blockStarts.get(ii);
+                var endLine = ii + 1 < blockStarts.size() ? blockStarts.get(ii + 1) - 1 : lines.length - 1;
+                spans.add(new LineSpan(startLine, endLine));
+            }
+            return spans;
+        };
+
+        var firstRelevantLine = new Block(splittingStrategy);
+        type.addBlock(firstRelevantLine);
+        firstRelevantLine.set(pdfTransaction);
+
+        pdfTransaction //
+
+                        .subject(() -> {
+                            var portfolioTransaction = new PortfolioTransaction();
+                            portfolioTransaction.setType(PortfolioTransaction.Type.DELIVERY_INBOUND);
+                            return portfolioTransaction;
+                        })
+
+                        // @formatter:off
+                        // Is type --> "Einbuchung" change from DELIVERY_INBOUND to DELIVERY_OUTBOUND
+                        // @formatter:on
+                        .section("type").optional() //
+                        .match("^Durchf.hrungsanzeig\\s*e\\s+" //
+                                        + "(?<type>(Freier Erhalt" //
+                                        + "|Freie Lieferung))$") //
+                        .assign((t, v) -> {
+                            if ("Freie Lieferung".equals(v.get("type")))
+                                t.setType(PortfolioTransaction.Type.DELIVERY_OUTBOUND);
+                        })
+
+                        // @formatter:off
+                        // Wertpapiernummer Bezeichnung Nominale/Stück
+                        // IE00BYZK4552 iShsIV-Automation&Robot.U.ETF Zugang Stk.              123,00
+                        // Registered Shares o.N.
+                        // Wertpapiernummer Bezeichnung Nominale/Stück
+                        // IE00BYZK4552 iShsIV-Automation&Robot.U.ETF Abgang Stk.              123,00
+                        // Registered Shares o.N.
+                        // @formatter:on
+                        .section("isin", "name", "nameContinued") //
+                        .find("^Wertpapiernummer Bezeichnung Nominale/St.ck$") //
+                        .match("^(?<isin>[A-Z]{2}[A-Z0-9]{9}[0-9]) (?<name>.*) (Zugang|Abgang) Stk\\s*\\.\\s+[\\.,\\d]+$") //
+                        .match("^(?<nameContinued>.*)$") //
+                        .assign((t, v) -> {
+                            t.setSecurity(getOrCreateSecurity(v));
+                        })
+
+                        // @formatter:off
+                        // Wertpapiernummer Bezeichnung Nominale/Stück
+                        // IE00BYZK4552 iShsIV-Automation&Robot.U.ETF Zugang Stk.              123,00
+                        // Wertpapiernummer Bezeichnung Nominale/Stück
+                        // IE00BYZK4552 iShsIV-Automation&Robot.U.ETF Abgang Stk.              123,00
+                        // @formatter:on
+                        .section("local", "shares") //
+                        .find("^Wertpapiernummer Bezeichnung Nominale/St.ck$") //
+                        .match("^[A-Z]{2}[A-Z0-9]{9}[0-9] .* (Zugang|Abgang) (?<local>Stk)\\s*\\.\\s+(?<shares>[\\.,\\d]+)$") //
+                        .assign((t, v) -> {
+                            t.setShares(asShares(v.get("shares")));
+                        })
+
+                        // @formatter:off
+                        // Schlusstag 05.02.2021
+                        // @formatter:on
+                        .section("date").match("^Schlusstag (?<date>[\\d]{2}\\.[\\d]{2}\\.[\\d]{4})$") //
+                        .assign((t, v) -> t.setDateTime(asDate(v.get("date"))))
+
+                        // @formatter:off
+                        //   steuerlicher Anschaffungswert:                                                 1.199,76 EUR
+                        // @formatter:on
+                        .section("currency", "amount")
+                        .match("^\\s*Anschaffungskurswert:\\s+(?<amount>[\\.,\\d]+) (?<currency>[A-Z]{3})$") //
+                        .assign((t, v) -> {
+                            t.setCurrencyCode(asCurrencyCode(v.get("currency")));
+                            t.setAmount(asAmount(v.get("amount")));
+                        })
+
+                        // @formatter:off
+                        // Auftrags-Nr. 999999-28.01.2021
+                        // @formatter:on
+                        .section("note").optional() //
+                        .match("^(?<note>Auftrags-Nr\\. \\d+)-[\\d]{2}\\.[\\d]{2}\\.[\\d]{4}$") //
+                        .assign((t, v) -> t.setNote(trim(v.get("note"))))
+
+                        .wrap(TransactionItem::new);
     }
 
     private <T extends Transaction<?>> void addFeesSectionsTransaction(T transaction, DocumentType type)
