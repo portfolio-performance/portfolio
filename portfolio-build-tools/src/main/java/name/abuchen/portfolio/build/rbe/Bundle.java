137ca07be630cc8aed3d3246689bf9370efb4d4a
Andreas Buchen <andreas.buchen@gmail.com>
2026-04-18 10:56:32 +0200

    Add check for the sort order of messages.properties files

---
+++ portfolio-build-tools/src/main/java/name/abuchen/portfolio/build/rbe/Bundle.java
@@ -0,0 +1,51 @@
+package name.abuchen.portfolio.build.rbe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public final class Bundle
+{
+    private String comment = ""; //$NON-NLS-1$
+    private final Map<String, BundleEntry> entries = new LinkedHashMap<>();
+    private final SortedSet<String> keys = new TreeSet<>();
+    private final SortedSet<String> readOnlyKeys = Collections.unmodifiableSortedSet(keys);
+
+    public String getComment()
+    {
+        return comment;
+    }
+
+    public void setComment(String comment)
+    {
+        this.comment = comment == null ? "" : comment; //$NON-NLS-1$
+    }
+
+    public BundleEntry getEntry(String key)
+    {
+        return entries.get(key);
+    }
+
+    public void addEntry(BundleEntry entry)
+    {
+        if (entry == null || entry.getKey().trim().isEmpty())
+            return;
+
+        entries.put(entry.getKey(), entry);
+        keys.add(entry.getKey());
+    }
+
+    public SortedSet<String> getKeys()
+    {
+        return readOnlyKeys;
+    }
+
+    public List<String> getInsertionOrderKeys()
+    {
+        return new ArrayList<>(entries.keySet());
+    }
+}
