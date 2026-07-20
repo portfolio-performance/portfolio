a0d39816f7c5663fd57c957c8e6bb101403b51aa
aragar <radorachev@gmail.com>
2026-07-20 17:34:33 +0300

    Add Trading 212 PDF-Importer
    Closes #5379
    Issue: #5895

--- name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
+++ name.abuchen.portfolio/src/name/abuchen/portfolio/datatransfer/pdf/PDFImportAssistant.java
@@ -147,6 +147,7 @@ public class PDFImportAssistant
         extractors.add(new TigerBrokersPteLtdPDFExtractor(client));
         extractors.add(new TradegateAGPDFExtractor(client));
         extractors.add(new TradeRepublicPDFExtractor(client));
+        extractors.add(new Trading212PDFExtractor(client));
         extractors.add(new UBSAGBankingAGPDFExtractor(client));
         extractors.add(new UmweltbankAGPDFExtractor(client));
         extractors.add(new UnicreditPDFExtractor(client));
